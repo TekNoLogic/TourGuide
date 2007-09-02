2a3af33751bef435f2b808629e1400a915a976c2
Thiana <kt.thiana@gmail.com>
2007-09-02 16:41:26 +0000

    TourGuide: Changed Barrens (12-15) to (12-20), added Blasted Lands (50-51)
    git-svn-id: https://tekkub-wow.googlecode.com/svn/trunk/TourGuide@512 86fe6d9a-1522-0410-a387-bf9db416f0a0

--- Horde/12_15_Barrens.lua
+++ Horde/12_15_Barrens.lua
@@ -1,4 +1,4 @@
-TourGuide:RegisterGuide("Joana's - The Barrens (12-15)", "Joana's - Stonetalon Mountains (15-16)", "Horde", function()
+TourGuide:RegisterGuide("Joana's - The Barrens (12-20)", "Joana's - Stonetalon Mountains (15-16)", "Horde", function()
     return [[
 T Conscript of the Horde |N|Just over the bridge from Durotar to The Barrens|
 A Crossroads Conscription
@@ -52,12 +52,121 @@ T Plainstrider Menace
 A The Zhevra
 T Raptor Thieves
 
-A Kolkar Leaders |N|Run west of the Crossroads (45,28)|
+A Kolkar Leaders |N|Run west of the Crossroads (45.4,28.4)|
 A Centaur Bracers
 C Kolkar Leaders |N|Barak is at (43,24). Do "The Zhevra", "Raptor Thieves", "The Forgotten Pools" and "Centaur Bracers" as seen.|
 C Fungal Spores
 C The Forgotten Pools
 C Harpy Raiders |N|Northwest corner of the Barrens (38,17)|
-G Stonetalon Mountains]]
+R Honor's Stand |N|West of Crossroads|
+A Goblin Invaders
+A Avenge My Village
+R Stonetalon Mountains
+C Avenge My Village
+T Avenge My Village
+A Kill Grundig Darkcloud
+R Stonetalon Mountains
+C Kill Grundig Darkcloud |N|Grimtotem camp north of the little troll village (73,86)|
+T Kill Grundig Darkcloud
+
+G Grind to the Kolkar quest guy |N|West of XR (45.4,28.4)|
+T Kolkar Leaders
+A Verog the Dervish
+C Centaur Bracers
+C Raptor Thieves
+C The Zhevra
+R The Crossroads
+T Fungal Spores
+T The Zhevra
+A Prowlers of the Barrens
+T The Forgotten Pools
+A The Stagnant Oasis
+N Turn in quests, grab new ones...
+C Apothecary Zamah |N|Run to Thunder Bluff|
+C Lost in Battle |N|South of Crossroads after the bridge (49.3,50.4)|
+R Camp Taurajo
+A Tribes at War
+f Camp Taurajo
+N Get "Melor Sends Word" if needed.
+R Mulgore
+R Thunder Bluff |N|Thunder Bluff (37.0,31.8)|
+t Weapon Master |N|(40,62)|
+t Class Training
+T Apothecary Zamah |N|Cave below Spirit Rise (22.9,20.9)|
+t First Aid |N|First Aid Trainer (29.6,21.3)|
+f Thunder Bluff
+A The Sacred Flame (Part 1) |N|(55.1,53.1)|
+H The Crossroads
+T Lost in Battle
+C Prowlers of the Barrens |N|(37,20)|
+C Harpy Lieutenants |N|Kill Witchwing Slayer's - Northwest Barrens (38,14)|
+T Samophlange (Part 1)
+A Samophlange (Part 2)
+T Samophlange (Part 2)
+A Samophlange (Part 3)
+C Samophlange (Part 3) |N|Tinkerer is in the hut on the hill|
+T Samophlange (Part 3)
+A Samophlange (Part 4)
+C Ignition
+C The Escape
+C Miner's Fortune
+C The Stagnant Oasis
+C Verog the Dervish
+R Ratchet
+T Miner's Fortune
+T Samophlange (Part 4)
+F The Crossroads
+T The Stagnant Oasis
+A Altered Beings
+T Prowlers of the Barrens
+A Echeyakee
+A Stolen Silver
+A Report to Kadrak
+A Serena Bloodfeather
+T Harpy Lieutenants
+N Accept New Quests
+T Centaur Bracers
+T Verog the Dervish
+C Serena Bloodfeather |N|Serena Bloodfeather - Northwest Barrens (38,11)|
+C Echeyakee
+H The Crossroads
+T Echeyakee
+A The Angry Scytheclaws
+T Serena Bloodfeather
+A Letter to Jin'Zil
+A Consumed by Hatred
+C Altered Beings |N|(55,42)|
+C The Angry Scytheclaws |N|(51,46)|
+C Raptor Horns
+C Consumed by Hatred |N|(51,54)|
+G Camp Taurajo
+A Weapons of Choice
+T Tribes at War
+A Blood Shards of Agamaggan
+T Blood Shards of Agamaggan
+A Betrayal from Within (Part 1)
+T Spirit of the Wind |N|10 Blood Shards, repeatable|
+N Grind to one bar from Level 20 (or group for Wailing Caverns)
+T The Angry Scytheclaws
+A Jorn Skyseer
+T Consumed by Hatred
+T Altered Beings
+A Egg Hunt
+F Orgrimmar
+t Level 20
+A The Ashenvale Hunt
+H The Crossroads
+F Ratchet
+T Raptor Horns
+A Deepmoss Spider Eggs
+A Ziz Fizziks
+C The Guns of Northwatch
+C Stolen Silver
+C Free From the Hold |N|Escort Quest|
+R Ratchet
+T The Guns of Northwatch
+T Free From the Hold
+H The Crossroads
+T Stolen Silver]]
 end)
 
---
+++ Horde/50_51_Blasted_Lands.lua
@@ -0,0 +1,38 @@
+TourGuide:RegisterGuide("Joana's - Blasted Lands (50-51)", "Joana's - UnGoro Crater (51-52)", "Horde", function()
+    return [[
+R Blasted Lands
+A A Boar's Vitality |N|Bloodmage Drazial (50,14)|
+A Snickerfang Jowls
+A The Decisive Striker
+A The Basilisk's Bite
+A Vulture's Vigor
+A A Boar's Vitality
+A Snickerfang Jowls
+A The Decisive Striker
+A The Basilisk's Bite
+A Vulture's Vigor
+A Everything Counts In Large Amounts |U|10593| |N|Near the basilisks/felhounds (51,35)|
+A To Serve Kum'isha |U|8244| |N|Near the basilisks/felhounds (51,35)|
+N Collect [Vulture Gizzards]x14 |L|8396x14|
+N Collect [Basilisk Brain]x11 |L|8394x11|
+N Collect [Blasted Boar Lungs]x6 |L|8392x6|
+N Collect [Scorpok Pincers]x6 |L|8393x6|
+N Collect [Snickerfang Jowls]x5 |L|8391x5|
+T A Boar's Vitality
+T Snickerfang Jowls
+T The Decisive Striker
+T The Basilisk's Bite
+T Vulture's Vigor
+N Abandon "To Serve Kum'isha" and "Everything Counts in Large Amounts" if you have them
+
+H The Crossroads |N|Or whatever method is fastest to get to Gadgetzan|
+F Gadgetzan
+T Sprinkle's Secret Ingredient
+A Delivery for Marin
+T March of the Silithid
+A Bungle in the Jungle
+T Delivery for Marin
+A Noggenfogger Elixir
+T Noggenfogger Elixir
+T The Stone Circle |N|(52,46)|]]
+end)
