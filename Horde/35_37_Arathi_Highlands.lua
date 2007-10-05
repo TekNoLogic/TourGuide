9ddd42f7dbbf26e647431f84ed27ea6a6a36956c
Industrial <tom.wieland@gmail.com>
2007-10-05 21:45:40 +0000

    TourGuide:  - 35-37 Arathi Highlands: added all points, no coords yet.
    git-svn-id: https://tekkub-wow.googlecode.com/svn/trunk/TourGuide@570 86fe6d9a-1522-0410-a387-bf9db416f0a0

--- Horde/35_37_Arathi_Highlands.lua
+++ Horde/35_37_Arathi_Highlands.lua
@@ -1,16 +1,31 @@
 TourGuide:RegisterGuide('Joana\'s - Arathi Highlands (35-37)', 'Joana\'s - Alterac Mountains (37-37)', 'Horde', function()
 	return [[
 h Hammerfall
-A Call to Arms
+A Call to Arms (Part 1)
 A Foul Magics
-A Guile of the Raptor
-C Call to Arms |N|(68,61)|
+A Guile of the Raptor (Part 2)
+C Call to Arms (Part 1) |N|(68,61)|
 A The Princess Trapped |N|(62.50,33.80)|
 C The Princess Trapped |N|(77.7,35.3)|
 T The Princess Trapped |N|(84.32,30.92)|
 A Stones of Binding
-T Call to Arms |N|(74.24,33.92)|
-A Call to Arms
+T Call to Arms (Part 1) |N|(74.24,33.92)|
+A Call to Arms (Part 2)
+A Triage |N|(73.4,36.8)
+C Triage
+T Triage
+R Stones of Binding
+C To Steam From Thieves
+R Stones of Binding
+C Guile of the Raptor (Part 2)
+C Call to Arms (Part 2)
+C Foul Magics
+C Stones of Binding
+H Hammerfall
+T Call to Arms (Part 2)
+T Foul Magics
+T Guile of the Raptor
+F Tarren Mill
 ]]
 end)
 
