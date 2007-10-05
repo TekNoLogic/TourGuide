ab850b5f7a5698c3a3cb5f36f1533089b5c043df
Industrial <tom.wieland@gmail.com>
2007-10-05 21:25:52 +0000

    TourGuide:  - Adding the Arathi Highlands (35-37) bit. I will finish this as I complete it.
    git-svn-id: https://tekkub-wow.googlecode.com/svn/trunk/TourGuide@569 86fe6d9a-1522-0410-a387-bf9db416f0a0

---
+++ Horde/35_37_Arathi_Highlands.lua
@@ -0,0 +1,16 @@
+TourGuide:RegisterGuide('Joana\'s - Arathi Highlands (35-37)', 'Joana\'s - Alterac Mountains (37-37)', 'Horde', function()
+	return [[
+h Hammerfall
+A Call to Arms
+A Foul Magics
+A Guile of the Raptor
+C Call to Arms |N|(68,61)|
+A The Princess Trapped |N|(62.50,33.80)|
+C The Princess Trapped |N|(77.7,35.3)|
+T The Princess Trapped |N|(84.32,30.92)|
+A Stones of Binding
+T Call to Arms |N|(74.24,33.92)|
+A Call to Arms
+]]
+end)
+
