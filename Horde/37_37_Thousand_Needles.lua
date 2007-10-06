ca28dfab50ddd4f392b17ff9276900c7ebd713e3
Industrial <tom.wieland@gmail.com>
2007-10-06 23:30:32 +0000

    TourGuide:  - Completed Thousand Needles (37-37)
    git-svn-id: https://tekkub-wow.googlecode.com/svn/trunk/TourGuide@591 86fe6d9a-1522-0410-a387-bf9db416f0a0

--- Horde/37_37_Thousand_Needles.lua
+++ Horde/37_37_Thousand_Needles.lua
@@ -1,5 +1,20 @@
 TourGuide:RegisterGuide('Thousand Needles (37-37)', 'Dustwallow March (37-38)', 'Horde', function()
 	return [[
+R The Shimmering Flats |N|(69.5,57.0)|
+T The Swarm Grows (Part 1) |N|(67.58,63.94)|
+A The Swarm Grows (Part 2)
+A Parts for Kravel |N|(77.79,77.26)|
+T Parts for Kravel
+T Delivery to the Gnomes
+T Goblin Sponsorship (Part 3)
+A The Rumormonger
+C The Swarm Grows (Part 1) |N|(70.77,82.86)|
+C Parts of the Swarm
+T The Swarm Grows (Part 1)
+H Crossroads
+T Parts of the Swarm (Part 1)
+A Parts of the Swarm (Part 2)
+F Dustwallow Marsh
 ]]
 end)
 
