49a1f436b2f2d5c61bc1c2585cd19ebf3b590f14
Mikma <mikko.majuri@gmail.com>
2007-10-06 20:06:13 +0000

    levels 14-16 confirmed working
    git-svn-id: https://tekkub-wow.googlecode.com/svn/trunk/TourGuide@590 86fe6d9a-1522-0410-a387-bf9db416f0a0

--- Alliance/12_17_Darkshore.lua
+++ Alliance/12_17_Darkshore.lua
@@ -8,7 +8,7 @@ h Auberdine
 F Rut'theran Village
 T Return to Nessa
 F Auberdine
-A For Love Eternal |N|On the docks, west of Inn (35.74,43.70)|
+A For Love Eternal |N|On the docks, west of Inn|
 A Buzzbox 827 |N|Inn, upstairs|
 A Cave Mushrooms
 A The Red Crystal
@@ -26,10 +26,10 @@ A Washed Ashore (Part 2)
 C Washed Ashore (Part 2) |N|Southwest of dock, Northeast of the small island (31.85,46.29)|
 C Buzzbox 411 |N|Threshers are in the sea|
 T Washed Ashore (Part 2)
-T Buzzbox 411 |N|Follow the road north out of Auberdine. When you reach Bashal'Aran, head Northwest above the cove. Buzzbox is at the beach north of the cove (41.96,28.64)|
+T Buzzbox 411 |N|Follow the road north out of Auberdine. When you reach Bashal'Aran, head Northwest above the cove. Buzzbox is at the beach north of the cove|
 A Buzzbox 323
-A Beached Sea Creature |N|South of the Buzzbox 411 at the cove is a Sea Creature Corpse (42.10,31.90)|
-R Bashal'Aran |N|Grind your way to Bashal'Aran (44.18,36.30)|
+N Beached Sea Creature |N|South of the Buzzbox 411 at the cove is a Sea Creature Corpse|
+R Bashal'Aran |N|Grind your way to Bashal'Aran|
 T Bashal'Aran (Part 1)
 A Bashal'Aran (Part 2)
 C Bashal'Aran (Part 2)
@@ -38,14 +38,14 @@ A Bashal'Aran (Part 3)
 C Bashal'Aran (Part 3)
 T Bashal'Aran (Part 3)
 A Bashal'Aran (Part 4)
-C The Red Crystal |N|Grind your way to the mountains east of Auberdine (47.32,48.67)|
-C How Big a Threat? (Part 1) |N|Grind your way to Northwest of Ameth'Aran to the Blackwood camp (40.31,53.56)|
+C The Red Crystal |N|Grind your way to the mountains east of Auberdine to the red crystal|
+C How Big a Threat? (Part 1) |N|Grind your way to Northwest of Ameth'Aran to the Blackwood camp|
 R Twilight Vale |N|Follow the road south|
-A The Fall of Ameth'Aran |N|Continue going south until you see the NPC next to a tree (40.30,59.74)|
+A The Fall of Ameth'Aran |N|Continue going south until you see the NPC next to a tree|
 C Tools of the Highborne
 C For Love Eternal
 C The Fall of Ameth'Aran |N|Look for the big white tablets on the ground (43.31,58.70) and (42.65,63.13)|
-C Bashal'Aran (Part 4) |N|(42,61)|
+C Bashal'Aran (Part 4)
 C Plagued Lands |N|West from Ameth'Aran|
 T The Fall of Ameth'Aran
 H Auberdine
@@ -62,7 +62,7 @@ T The Red Crystal
 A As Water Cascades
 N Fill the [Empty Water Tube] in the Moonwell |L|14339|
 T For Love Eternal |N|West of Inn, at the dock|
-T Beached Sea Creature
+N Turn in: Beached Sea Creature
 
 N You should be level 14 now. If not, grind until you are
 F Rut'theran Village |N|Time has come to Train in Darnassus|
@@ -71,43 +71,49 @@ t Train (Level 14)
 R Rut'theran Village
 F Auberdine
 
-T Bashal'Aran (Part 4) |N|(44,36)|
-C The Cliffspring River |N|Grind along the way to (50,25)|
-A Beached Sea Turtle |N|(44,20)|
-T Buzzbox 323 |N|(51,24)|
+R Bashal'Aran |N|Follow the road north out of Auberdine|
+T Bashal'Aran (Part 4)
+C Cleansing of the Infected |N|You should head North while doing this quest|
+C Buzzbox 323 |N|You should head North while doing this quest|
+N Beached Sea Turtle |N|Go north until you are at the beach, there start running west until you see the big turtle. Take the quest from the bones of frontleg|
+R Cliffspring River |N|Waterfall in Northeast|
+C The Cliffspring River |N|Go under the bridge into the water to get the sample|
+T Buzzbox 323 |N|Buzzbox is on the other side of the bridge, left side|
 A Buzzbox 525
-C Cave Mushrooms |N|Around (54,32), in the caves|
-T As Water Cascades |N|(47,48)|
+R Cliffspring Falls |N|Follow the river to Southeast|
+C Cave Mushrooms |N|In the cave above waterfall|
+T As Water Cascades |N|Follow the mountains to Southwest until you are at the red Crystal|
 A The Fragments Within
-C How Big a Threat? (Part 2) |N|Grind your way to (40,53)|
-A Beached Sea Turtle |N|(37,62)|
-A Beached Sea Creature |N|(36,70)|
-T Grove of the Ancients |N|(43,76)|
-C Buzzbox 525
-T Buzzbox 525 |N|(41,80)|
-A Beached Sea Creature |N|Grind your way to (32,80)|
+C How Big a Threat? (Part 2) |N|Grind your way west across the road to Blackwood camp|
+N Beached Sea Turtle |N|Go southwest until you are at the beach, then start running south|
+N Beached Sea Creature |N|Continue running south along the beach|
+R Grove of the Ancients
+T Grove of the Ancients
+C Buzzbox 525 |N|Head southwest to the road, bears are at the other side of the road|
+T Buzzbox 525 |N|Go back to the road, Buzzbox is next to two wooden fences and a fallen tree that has small path under it leading to the digsite|
+N Beached Sea Creature |N|Grind your way directly to west from Buzzbox|
 H Auberdine
-T Beached Sea Turtle
-T Beached Sea Turtle
-T Beached Sea Creature
-T Beached Sea Creature
+N Turn in: Beached Sea Turtle
+N Turn in: Beached Sea Turtle
+N Turn in: Beached Sea Creature
+N Turn in: Beached Sea Creature
 A Fruit of the Sea |N|Under the flightpath|
 T Cave Mushrooms
 A Onu
 T The Fragments Within
-A The Absend Minded Prospector |N|(37,41)|
+A The Absent Minded Prospector
 T The Cliffspring River
-N You should be level 15 now. If not, you haven't grinded like was told. Go grind until you are
-A The Blackwood Corrupted |N|Wanted poster infront of Inn|
-A WANTED: Murkdeep! |N|The same Wanted poster|
-T Cleansing the Infected
+N You should be level 15 now. If not, grind
+A The Blackwood Corrupted
+A WANTED: Murkdeep! |N|In The Wanted poster infront of Inn|
+T Cleansing of the Infected
 A Tharnariun's Hope
 T How Big a Threat? (Part 2)
 A A Lost Master
 N Fill the [Empty Cleansing Bowl] in the Moonwell |L|12347|
 
 N You should be level 16 now. If not, grind until you are
-F Rut'theran Village
+F Rut'theran Village |N|Time has come to Train in Darnassus|
 R Darnassus
 t Train (Level 16)
 R Rut'theran Village
@@ -120,10 +126,10 @@ A The Tower of Althalaxx (Part 2)
 C The Tower of Althalaxx (Part 2) |N|mobs around the tower, not inside|
 T The Tower of Althalaxx (Part 2)
 A The Tower of Althalaxx (Part 3)
-A Beached Sea Creature |N|(53,18)|
+N Beached Sea Creature |N|(53,18)|
 C Fruit of the Sea
 H Auberdine
-T Beached Sea Creature
+N Turn in: Beached Sea Creature
 C WANTED: Murkdeep!
 T Fruit of the Sea
 T The Blackwood Corrupted
