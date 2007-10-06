ad61b27e73daeea41e2fa7c95301a7a05a304838
Industrial <tom.wieland@gmail.com>
2007-10-06 16:42:17 +0000

    TourGuide:  - Adding AlteracMountains (37-37)
    git-svn-id: https://tekkub-wow.googlecode.com/svn/trunk/TourGuide@581 86fe6d9a-1522-0410-a387-bf9db416f0a0

---
+++ Horde/37_37_Alterac_Mountains.lua
@@ -0,0 +1,25 @@
+TourGuide:RegisterGuide('Alterac Mountains (37-37)', 'Thousand Needles (37-37)', 'Horde', function()
+	return [[
+A Prison Break In
+A Stone Tokens
+C Prison Break In |N|()|
+C Stone Tokens
+T Prison Break In
+T Stone Tokens
+A Dalaran Patrols
+A Bracers of Binding
+C Dalaran Patrols |N|()|
+C Bracers of Binding
+T Dalaran Patrols
+T Bracers of Binding
+F Undercity
+T To Steal From Thieves
+R Felicia Doan |N|(64.3,50.2)|
+B Soothing Spices |I|3713 3|
+b Orgrimmar
+F Crossroads
+h Crossroads
+F Freewind Post
+]]
+end)
+
