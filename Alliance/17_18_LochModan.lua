3e9e2cdcfcd0bcc6e8d59585ffc4d0e5dfb183c6
Mikma <mikko.majuri@gmail.com>
2007-10-07 20:42:05 +0000

    TourGuide: 17_18_LochModan.lua added. TG doesn't load it yet
    git-svn-id: https://tekkub-wow.googlecode.com/svn/trunk/TourGuide@601 86fe6d9a-1522-0410-a387-bf9db416f0a0

---
+++ Alliance/17_18_LochModan.lua
@@ -0,0 +1,51 @@
+TourGuide:RegisterGuide("Loch Modan (17-18)", nil, "Alliance", function()
+return [[
+A Filthy Paws
+A Stormpike's Order
+C Filthy Paws |N|In the Cave west of the tower|
+R Thelsamar
+f Grab flight point
+A Ironband's Excavation |N|Last house on the right in the village|
+R Ironband's Excavation Site
+T Ironband's Excavation
+A Gathering Idols
+A Excavation Progress Report
+C Gathering Idols
+R Farstrider Lodge |N|East of Excavation site|
+t Train Level 18, visit Pet Trainer too |C|Hunter|
+A Crocolisk Hunting
+A A Hunter's Boast
+C A Hunter's Boast
+T A Hunter's Boast
+A A Hunter's Challenge
+C A Hunter's Challenge
+T A Hunter's Challenge
+A Vyrin's Revenge (Part 1)
+A Bingles' Missing Supplies |N|The NPC can be found at the easternmost shore of The Loch|
+C Crocolishk Hunting
+C Bingles' Missing Supplies |N|The supplies are scattered around northernmost islands. Blastencapper (54.21,26.61), Hammer (51.78,24.09), Screwdriver (48.37,20.50), Wrench (48.73,30.10)|
+R Stonewrought Dam
+A A Dark Threat Looms (Part 1) |N|Middle of the dam|
+T A Dark Threat Looms (Part 1) |N|Barrel at the east of damn|
+A A Dark Threat Looms (Part 2)
+T A Dark Threat Looms (Part 2)
+R Thelsamar
+T Excavation Progress Report
+A Report to Ironforge
+C Vyrin's Revenge (Part 1) |N|Cave top of the mountain south of Thelsamar. Use Immolation Trap and Fear|
+R Farstrider Lodge
+T Crocolisk Hunting
+T Vyrin's Revenge (Part 1)
+A Vyrin's Revenge (Part 2)
+T Vyrin's Revenge (Part 2)
+T Bingles' Missing Supplies |N|Remember Bingles at the beach?|
+R Ironforge
+T Report to Ironforge
+f Grab flight point
+R Stormwind |N|Use the Deeprun Tram|
+T Stormpike's Order
+f Grab flight point
+R Redridge Mountains
+
+]]
+end)
