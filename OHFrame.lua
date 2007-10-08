572a334a5d351f34bce439a0ffd4b47f79c58eed
Tekkub Stoutwrithe <tekkub@gmail.com>
2007-10-08 03:15:29 +0000

    TourGuide - Added scrollbar to OH obj frame - Added hover tooltips to details in OH obj frame if the detail is truncated
    git-svn-id: https://tekkub-wow.googlecode.com/svn/trunk/TourGuide@605 86fe6d9a-1522-0410-a387-bf9db416f0a0

--- OHFrame.lua
+++ OHFrame.lua
@@ -11,23 +11,61 @@ local NUMROWS = math.floor(305/(ROWHEIGHT+4))
 
 local offset = 0
 local rows = {}
-local frame
+local frame, scrollbar
 
 
 local function OnShow(self)
-	offset = TourGuide.current - NUMROWS/2 - 1
-	if offset < 0 then offset = 0
-	elseif (offset + NUMROWS) > #TourGuide.actions then offset = #TourGuide.actions - NUMROWS end
-	TourGuide:UpdateOHPanel()
+	scrollbar:SetMinMaxValues(1, math.max(#TourGuide.actions - NUMROWS, 1))
+	scrollbar:SetValue(TourGuide.current - NUMROWS/2 - 1)
 
 	self:SetAlpha(0)
 	self:SetScript("OnUpdate", ww.FadeIn)
 end
 
 
+local function HideTooltip() GameTooltip:Hide() end
+
+local function ShowTooltip(f)
+	if f.text:GetStringWidth() <= f:GetWidth() then return end
+
+	GameTooltip:SetOwner(f, "ANCHOR_RIGHT")
+	GameTooltip:SetText(f.text:GetText(), nil, nil, nil, nil, true)
+end
+
+
 function TourGuide:CreateObjectivePanel()
 	frame = CreateFrame("Frame", nil, UIParent)
+	frame:SetFrameStrata("DIALOG")
 
+	scrollbar = CreateFrame("Slider", "TourGuideOHScroll", frame, "UIPanelScrollBarTemplate")
+	scrollbar:SetPoint("TOPRIGHT", frame, "TOPRIGHT", 0, -16)
+	scrollbar:SetPoint("BOTTOMRIGHT", frame, "BOTTOMRIGHT", 0, 16)
+	scrollbar:SetScript("OnValueChanged", function(f, val) self:UpdateOHPanel(val) end)
+	TourGuideOHScrollScrollUpButton:SetScript("OnClick", function(f)
+		scrollbar:SetValue(offset - NUMROWS)
+		PlaySound("UChatScrollButton")
+	end)
+
+	TourGuideOHScrollScrollDownButton:SetScript("OnClick", function(f)
+		scrollbar:SetValue(offset + NUMROWS)
+		PlaySound("UChatScrollButton")
+	end)
+
+	local uptext = scrollbar:CreateTexture(nil, "BACKGROUND")
+	uptext:SetWidth(31)
+	uptext:SetHeight(256)
+	uptext:SetPoint("TOPLEFT", TourGuideOHScrollScrollUpButton, "TOPLEFT", -7, 5)
+	uptext:SetTexture("Interface\\PaperDollInfoFrame\\UI-Character-ScrollBar")
+	uptext:SetTexCoord(0, 0.484375, 0, 1.0)
+
+	local downtex = scrollbar:CreateTexture(nil, "BACKGROUND")
+	downtex:SetWidth(31)
+	downtex:SetHeight(106)
+	downtex:SetPoint("BOTTOMLEFT", TourGuideOHScrollScrollDownButton, "BOTTOMLEFT", -7, -3)
+	downtex:SetTexture("Interface\\PaperDollInfoFrame\\UI-Character-ScrollBar")
+	downtex:SetTexCoord(0.515625, 1.0, 0, 0.4140625)
+
+	local function LevelCorrection(f) f:SetFrameLevel(frame:GetFrameLevel()+1); f:SetScript("OnShow", nil) end
 	for i=1,NUMROWS do
 		local row = CreateFrame("Button", nil, frame)
 		row:SetPoint("TOPLEFT", i == 1 and frame or rows[i-1], i == 1 and "TOPLEFT" or "BOTTOMLEFT", 0, -ROWOFFSET)
@@ -37,10 +75,20 @@ function TourGuide:CreateObjectivePanel()
 		local check = ww.SummonCheckBox(ROWHEIGHT, row, "TOPLEFT", ROWOFFSET, 0)
 		local icon = ww.SummonTexture(row, nil, ROWHEIGHT, ROWHEIGHT, nil, "TOPLEFT", check, "TOPRIGHT", ROWOFFSET, 0)
 		local text = ww.SummonFontString(row, nil, "GameFontNormal", nil, "LEFT", icon, "RIGHT", ROWOFFSET, 0)
-		local detail = ww.SummonFontString(row, nil, "GameFontNormal", nil, "RIGHT", -ROWOFFSET, 0)
-		detail:SetPoint("LEFT", text, "RIGHT", ROWOFFSET*3, 0)
+
+		local detailhover = CreateFrame("Button", nil, frame)
+		detailhover:SetHeight(ROWHEIGHT)
+		detailhover:SetPoint("LEFT", text, "RIGHT", ROWOFFSET*3, 0)
+		detailhover:SetPoint("RIGHT", scrollbar, "LEFT", -ROWOFFSET-7, 0)
+		detailhover:SetScript("OnEnter", ShowTooltip)
+		detailhover:SetScript("OnLeave", HideTooltip)
+		detailhover:SetScript("OnShow", LevelCorrection)
+
+		local detail = ww.SummonFontString(detailhover, nil, "GameFontNormal", nil)
+		detail:SetAllPoints(detailhover)
 		detail:SetJustifyH("RIGHT")
 		detail:SetTextColor(240/255, 121/255, 2/255)
+		detailhover.text = detail
 
 		check:SetScript("OnClick", function(f) self:SetTurnedIn(row.i, f:GetChecked()) end)
 
@@ -53,10 +101,7 @@ function TourGuide:CreateObjectivePanel()
 
 	frame:EnableMouseWheel()
 	frame:SetScript("OnMouseWheel", function(f, val)
-		offset = offset - val
-		if (offset + NUMROWS) > #self.actions then offset = #self.actions - NUMROWS end
-		if offset < 0 then offset = 0 end
-		self:UpdateOHPanel()
+		scrollbar:SetValue(offset - val)
 	end)
 
 	frame:SetScript("OnShow", OnShow)
@@ -67,8 +112,12 @@ end
 
 
 local accepted = {}
-function TourGuide:UpdateOHPanel()
+function TourGuide:UpdateOHPanel(value)
 	if not frame or not frame:IsVisible() then return end
+
+	if value then offset = math.floor(value) end
+	if (offset + NUMROWS) > #self.actions then offset = #self.actions - NUMROWS end
+	if offset < 0 then offset = 0 end
 
 	for i in pairs(accepted) do accepted[i] = nil end
 
