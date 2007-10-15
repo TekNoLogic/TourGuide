dc99512dd13d0bc6a7d38efd66472eda84a8f72b
Tekkub Stoutwrithe <tekkub@gmail.com>
2007-10-15 09:37:16 +0000

    TourGuide - Ally Un'goro cleanup
    git-svn-id: https://tekkub-wow.googlecode.com/svn/trunk/TourGuide@641 86fe6d9a-1522-0410-a387-bf9db416f0a0

--- Alliance/51_52_UnGoro_Crater.lua
+++ Alliance/51_52_UnGoro_Crater.lua
@@ -19,7 +19,6 @@ T The Scent of Lar'korwi
 A The Bait for Lar'korwi
 G Grind on raptors |N|Until you find [A Mangled Journal]| |L|11116|
 A Williden's Journal |U|11116|
-C The Bait for Lar'korwi |N|Use the meat and mixture at the flat rock behind the east pylon (79.9, 49.9)| |U|11568|
 
 N Find 7 Red power crystals |L|11186 7|
 N Find 7 Yellow power crystals |L|11188 7|
@@ -44,15 +43,16 @@ A The Northern Pylon
 A The Eastern Pylon
 A The Western Pylon
 
-N Kill stuff... |N|Kill any bloodpetals, diametradons,  pterrordaxes and frenzied pterrordaxes you come across|
+N Kill stuff... |N|Kill any bloodpetals, diametradons, and pterrordaxes you come across|
 
-C The Northern Pylon |N|Along the north edge of the zone to the east (56,12).  Kill pterrordaxes here.|
+C The Northern Pylon |N|Along the north edge of the zone to the east (56.46, 12.48).  Kill pterrordaxes here.|
 T Chasing A-Me 01 |N|East in the gorilla cave (63,17).  Skip the follow-up.|
 C The Apes of Un'Goro
 C Super Sticky |N|At the tarpits in the north half of the zone|
 N Find [Crate of Foodstuffs] |N|At the abandoned camp in the east side of the zone (68.52, 36.59)| |L|11113|
-C Shizzle's Flyer |N|Kill them dinos!|
+C Muigin and Larion |N|Kill lasher around the camp area.  Also kill any diemetradons or pterrordaxes you come across|
 C The Eastern Pylon |N|To the east, of course (76,51)|
+C The Bait for Lar'korwi |N|Use the meat and mixture at the flat rock behind the east pylon (79.9, 49.9)| |U|11568|
 T The Bait for Lar'korwi
 T The Apes of Un'Goro |N|Skip the follow-up|
 
@@ -62,16 +62,13 @@ C Expedition Salvation |N|At the other camp (38.5, 66.0)|
 C The Western Pylon |N|(23,59)|
 A Finding the Source |N|From a goblin near the hot springs (30.9, 50.4)|
 
-C Beware of Pterrordax |N|You can find the Frenzied Pterrordaxes all over the west half of the zone|
-C Roll the Bones
-C Muigin and Larion
+C Roll the Bones |N|Scattered all over the west half of the zone and drop from diemetradons.  Kill pterrordaxes over here too.|
+C Beware of Pterrordax |N|You can find the Frenzied Pterrordaxes all over the west half of the zone, more towards the northwest part.  Normal Pterrordaxes are in the south half of the zone, and a few near the North pylon.|
+C Shizzle's Flyer |N|Kill diemetradons and pterrordaxes, all over the zone.|
 
-C Finding the Source |N|At the volcano in the center of the zone.  Right click on "Hot Spots" until you complete the quest.|
-C Volcanic Activity |N|Kill elementals|
-T Finding the Source
-A The New Springs
-
-T Lost! |N|In a cave (51,49)|
+C Finding the Source |N|At the volcano in the center of the zone.  Use the thermometer at "Hot Spots" until you complete the quest.| |U|12472|
+C Volcanic Activity |N|Kill elementals, mainly around the cave on the south side of the volcano.|
+T Lost! |N|In the cave on the south side of the volcano (51,49)|
 A A Little Help From My Friends |NODEBUG|
 C A Little Help From My Friends |NODEBUG| |N|Use the canteen on him if he faints. Run back to Marshal's Refuge.| |U|11804|
 T A Little Help From My Friends |NODEBUG|
@@ -86,6 +83,9 @@ T The Eastern Pylon
 T The Western Pylon
 A Making Sense of It
 T Making Sense of It
+
+T Finding the Source |N|Back at the hot springs (30.9, 50.4)|
+A The New Springs
 
 R Silithus |N|Take the path up out of the crater in the northwest corner of the zone (29,22)|
 R Cenarion Hold |N|Follow the road|
