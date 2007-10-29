fe9b5aff38f76026d880567a362f7db451a25f2d
Mikma <mikko.majuri@gmail.com>
2007-10-29 00:03:25 +0000

    28_29_Duskwood.lua added for testing. not in .toc
    git-svn-id: https://tekkub-wow.googlecode.com/svn/trunk/TourGuide@655 86fe6d9a-1522-0410-a387-bf9db416f0a0

---
+++ Alliance/28_29_Duskwood.lua
@@ -0,0 +1,85 @@
+TourGuide:RegisterGuide("Duskwood (28-29)", nil, "Alliance", function()
+return [[
+f Grab flight point |N|East of Darkshire, above the hill|
+A Look to the Stars (Part 1) |N|Southeast from flight point in the house|
+B 1x Bronze Tube |N|The Engineer vendor sells this sometimes|
+T Look to the Stars (Part 1)
+A Look to the Stars (Part 2)
+A Worgen in the Woods (Part 1)
+A Raven Hill
+A The Hermit
+A Deliveries to Sven
+A The Legend of Stalvan (Part 1)
+A The Totem of Infliction
+h Darkshire
+A The Night Watch (Part 1)
+T The Legend of Stalvan (Part 1)
+C Worgen in the Woods (Part 1) |N|West of Darkshire, around (64,46)|
+R Darkshire
+T Worgen in the Woods (Part 1)
+A Worgen in the Woods (Part 2)
+C Worgen in the Woods (Part 2) |N|West of Darkshire, around (64,46)|
+T Worgen in the Woods (Part 2)
+A Worgen in the Woods (Part 3)
+T Look to the Stars (Part 2) |N|(81,59)|
+A Look to the Stars (Part 3)
+C The Night Watch (Part 1)
+G 10x Skeleton Finger |L|2378 10|
+C Look to the Stars (Part 3) |N|Insane Ghoul inside the chapel|
+C Worgen in the Woods (Part 3) |N|(73,73)|
+H Darkshire
+T The Night Watch (Part 1)
+A The Night Watch (Part 2)
+T Worgen in the Woods (Part 3)
+A Worgen in the Woods (Part 4)
+T Look to the Stars (Part 3)
+A Look to the Stars (Part 4)
+R Someplacethatneedsdefination
+C Look to the Stars (Part 4)
+R Raven Hill
+T Raven Hill
+C The Night Watch (Part 2)
+G 6x Vial of Spider Venom |L|1130 5|
+C The Totem of Infliction
+T The Hermit |N|Shack north of the cemetary|
+A Supplies From Darkshire
+A The Weathered Grave |N|Grave at (17,29)|
+R someplace where Sven is
+T Delivers for Sven
+A Sven's Revenge
+H Darkshire
+T The Night Watch (Part 2)
+A The Night Watch (Part 3)
+T The Weathered Grave
+A Morgan Ladimore
+T Morgan Ladimore
+T The Totem of Infliction
+T Supplies to Darkshire
+A Ghost Hair Thread
+T Look to the Stars (Part 4)
+T Ghost Hair Thread |N|(81,59)|
+A Return the Comb
+T Return the Comb 
+A Deliver the Thread
+T Sven's Revenge |N|(49.77)|
+A Sven's Camp
+T Deliver the Thread |N|Shack north of cemetary|
+A Zombie Juice
+C The Night Watch (Part 3)
+R Sven's Revenge
+A The Shadowy Figure  
+H Darkshire
+T Zombie Juice
+T The Night Watch (Part 3)
+T The Shadowy Figure
+A The Shadowy Search Continues
+T The Shadowy Search Continues
+A Inquire at the Inn
+T Inquire at the Inn
+N If you found [An Old History Book], start the quest "An Old History Book" from it, return it to Stormwind City (74,4) and accept quest "Southshore"
+F Menethil Harbor
+B Auberdine
+F Astranaar
+
+]]
+end)
