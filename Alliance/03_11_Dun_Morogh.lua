664607c88be64f4844b6e183bd6f854fde8ae756
Tekkub Stoutwrithe <tekkub@gmail.com>
2007-11-12 10:42:45 +0000

    TourGuide - Cleanup of submitted Dun Morough guide
    git-svn-id: https://tekkub-wow.googlecode.com/svn/trunk/TourGuide@697 86fe6d9a-1522-0410-a387-bf9db416f0a0

--- Alliance/03_11_Dun_Morogh.lua
+++ Alliance/03_11_Dun_Morogh.lua
@@ -11,10 +11,9 @@ T The Boar Hunter
 T Coldridge Valley Mail Delivery (Part 2)
 A The Troll Cave
 A Scalding Mornbrew Delivery
-T Scalding Mornbrew Delivery |N|Run back to Anvilmar! (28.8,66.4)|
+T Scalding Mornbrew Delivery |N|Run back to Anvilmar (28.8,66.4)|
 A Bring Back the Mug
-
-t Train, level 4!
+t Train (Level 4)
 
 A In Favor of the Light |C|Priest|
 A Beginnings |C|Warlock|
@@ -26,16 +25,16 @@ C A Refugee's Quandary |N|Bucket (26.3,79.3), Chest (22.8,78), Box(20.9,76.1)|
 C The Troll Cave
 T The Troll Cave
 A The Stolen Journal
-C The Stolen Journal |N|Back of the cave, lower level. (31,80)|
+C The Stolen Journal |N|Back of the cave, lower level (31,80)|
 T The Stolen Journal |N|Fight your way out of the cave.|
 A Senir's Observations (Part 1)
 
 T A Refugee's Quandary |N|Back in Anvilmar.|
-T Senir's Observations (Part 1) |N|East of Anvilmar. (33.5,71.8)|
+T Senir's Observations (Part 1) |N|East of Anvilmar (33.5,71.8)|
 A Senir's Observations (Part 2)
 A Supplies to Tannok
 
-R Kharanos |N|Kill mobs in the tunnel on your way!|
+R Kharanos |N|You should be able to just run thru the tunnel.|
 T Senir's Observations (Part 2)
 A Beer Basted Boar Ribs
 T Supplies to Tannok
@@ -44,45 +43,39 @@ A Tools for Steelgrill
 
 T In Favor of the Light |C|Priest|
 A Garments of the Light |C|Priest|
-C Garments of the Light |N|(45.8,54.6)| |C|Priest|
+C Garments of the Light |N|South of town (45.8,54.6)| |C|Priest|
 T Garments of the Light |C|Priest|
 
-T Tools for Steelgrill |N|Northeast of Kharanos. (50.4,49.1)|
+T Tools for Steelgrill |N|Northeast of Kharanos (50.4,49.1)|
 A Ammo for Rumbleshot
 A The Grizzled Den
 A Stocking Jetsteam
+t Train (Level 6)
 
-t Train in Kharanos, level 6!
+C Ammo for Rumbleshot |N|Open the crate (44.1,56.9)|
+C The Grizzled Den |N|South of Kharanos (42.5, 54.4)|
+T Ammo for Rumbleshot |N|South of Kharanos (40.7,65.1)|
 
-C Ammo for Rumbleshot |N|Open the crate. (44.1,56.9)|
-C The Grizzled Den |N|South of Kharanos. (42.5, 54.4)|
-T Ammo for Rumbleshot |N|South of Kharanos. (40.7,65.1)|
-
-R Brewnall Village |N|Go northwest. (30.3,46.3)|
+R Brewnall Village |N|Go northwest (30.3,46.3)|
 A The Perfect Stout
 A Bitter Rivals
-C The Perfect Stout |N|Northeast across the lake. (38,36)|
+C The Perfect Stout |N|Northeast across the lake (38,36)|
 
-K Boars for Chunk of Boar Meat x4. |N|Go southeast towards Kharanos to find more mobs.| |L|769 4|
-K Boars for Crag Boar Ribs x6. |L|2886 6|
-K Bears for Thick Bear Fur x2. |L|6952 2|
+C Stocking Jetsteam |N|Go southeast towards Kharanos to find more mobs.| |L|769 4|
+K Boars |N|For [Crag Boar Rib] x6.| |L|2886 6|
 
-R Steelgrill's Depot |N|Southeast, north of Kharanos.|
-T The Grizzled Den
+T The Grizzled Den |N|At Steelgrill's Depot, east of Kharanos.|
 T Stocking Jetsteam
 A Evershine
 
-R Kharanos
-B Rhapsody Malt x1 |N|From innkeeper or general supplier.| |L|2894 1|
-B Thunder Ale x1 |L|2686 1|
-N Give the Thunder Ale to Jarven. |N|Downstairs. (47.6,52.6)|
-T Bitter Rivals |N|Click on the barrel.|
+B [Rhapsody Malt] |N|From innkeeper or general supplier in Kharanos's inn.| |L|2894|
+B [Thunder Ale] |L|2686|
+T Bitter Rivals |N|Give the Thunder Ale to Jarven downstairs, then talk to the barrel.|
 A Return to Marleth
 T Beer Basted Boar Ribs
 A Frostmane Hold |N|He's a little south of the inn.|
-A Operation Recombobulation |N|In a house a bit northwest of the inn. (45.8,49.4)|
-
-t Train in Kharanos, level 8!
+A Operation Recombobulation |N|In a house a bit northwest of the inn (45.8,49.4)|
+t Train (Level 8)
 
 R Brewnall Village
 T Evershine
@@ -91,9 +84,9 @@ T The Perfect Stout
 A Shimmer Stout
 T Return to Marleth
 
-C Frostmane Hold |N|Go southwest, into the back of the cave to complete. (21,51)|
+C Frostmane Hold |N|Go southwest, into the back of the cave to complete (21,51)|
 C Operation Recombobulation |N|The green leper gnomes near the moving cog objects.|
-C A Favor for Evershine |N|A bit west of Brewnall Village. (28,48.5)|
+C A Favor for Evershine |N|A bit west of Brewnall Village (28,48.5)|
 T A Favor for Evershine
 A Return to Bellowfiz
 
@@ -102,15 +95,11 @@ C Tundra MacGrann's Stolen Stash |N|Wait until Old Icebeard leaves his cave and 
 T Tundra MacGrann's Stolen Stash
 
 H Kharanos
-
 T Frostmane Hold
 A The Reports
 T Operation Recombobulation
 T Return to Bellowfiz |N|Northeast of Kharanos.|
-
-G Grind until level 10. |N|Should only be a few kills.| |LV|10|
-
-t Train in Kharanos, level 10!
+t Train (Level 10)
 
 A Taming the Beast (Part 1) |C|Hunter|
 C Taming the Beast (Part 1) |C|Hunter|
@@ -157,22 +146,20 @@ R Dun Morogh |C|Warrior|
 C Ironband's Compound |N|(77.9,62.2)| |C|Warrior|
 T Ironband's Compound |C|Warrior|
 
-R Gol'Bolar Quarry |N|East of Kharanos. (69.1,56.3)|
-A Those Blasted Troggs!
+A Those Blasted Troggs! |N|At Gol'Bolar Quarry, East of Kharanos (69.1,56.3)|
 A The Public Servant
 C Those Blasted Troggs! |N|Kill towards the cave.|
 C The Public Servant |N|Inside the cave.|
 T Those Blasted Troggs!
 T The Public Servant
 
-A The Lost Pilot |N|Northeast through a tunnel. (83.8,39.2)|
-T The Lost Pilot |N|A bit northwest, click on the corpse. (79.7,36.2)|
+A The Lost Pilot |N|Northeast through a tunnel (83.8,39.2)|
+T The Lost Pilot |N|A bit northwest, talk to the corpse (79.7,36.2)|
 A A Pilot's Revenge
 C A Pilot's Revenge |N|(78.5,37.6)|
 T A Pilot's Revenge
 
-T Shimmer Stout |N|Southwest through a tunnel. (86.3,48.9)|
+T Shimmer Stout |N|Southwest through a tunnel (86.3,48.9)|
 A Stout to Kadrell
-R Loch Modan
 ]]
 end)
