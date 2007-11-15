f88bf172abf17bfe3e4c3a09f495ac6ef577dc1d
Mikma <mikko.majuri@gmail.com>
2007-11-15 15:53:05 +0000

    TourGuide: err, I actually forgot what I changed. something in Darkshore + maybe more
    git-svn-id: https://tekkub-wow.googlecode.com/svn/trunk/TourGuide@706 86fe6d9a-1522-0410-a387-bf9db416f0a0

--- Alliance/20_21_Darkshore.lua
+++ Alliance/20_21_Darkshore.lua
@@ -1,7 +1,5 @@
 TourGuide:RegisterGuide("Darkshore (20-21)", "Ashenvale (21-22)", "Alliance", function()
 return [[
-t Train in Darnassus (Level 20)
-t Train pet skills (Level 20) |C|Hunter|
 A Onward to Ashenvale |N|Northernmost building in Auberdine|
 R Grove of the Ancients |N|Get back to Auberdine and run south|
 T Onu
@@ -18,14 +16,11 @@ R Remtravel's Excavation |N|West from The Twilight Camp|
 T The Absent Minded Prospector (Part 1)
 A The Absent Minded Prospector (Part 2) |N|Escort around the excavation|
 C The Absent Minded Prospector (Part 2)
-A Beached Sea Turtle (Part 4) |N|Go west until you are at the beach|
-A Beached Sea Turtle (Part 5) |N|South, almost next to the previous one|
-A Beached Sea Creature (Part 4) |N|South, almost next to the previous one|
 C WANTED: Murkdeep! |N|Clear the camp, after that there's 3 waves of murlocs. Murkdeep is in the last wave|
 R Grove of the Ancients
 T Return to Onu
 A Mathystra Relics
-A The Sleeper Has Awaken |N|Escort. REMEMBER TO TAKE THE HORN FROM THE BOX NEXT TO HIM!|
-C The Sleeper Has Awaken |N|Use the [Horn of Awakening] when he falls asleep. That happens quite alot...|
+A The Sleeper Has Awakened |N|Escort. REMEMBER TO TAKE THE HORN FROM THE BOX NEXT OF HIM!|
+C The Sleeper Has Awakened |N|Use the [Horn of Awakening] when he falls asleep. That happens quite alot...| |U|13536|
 ]]
 end)
