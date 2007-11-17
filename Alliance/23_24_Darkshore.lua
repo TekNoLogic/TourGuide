62cc52dc9fcc3096c5d2d8d5416f285d4584657d
Mikma <mikko.majuri@gmail.com>
2007-11-17 00:26:23 +0000

    TourGuide: tweaks to 23-24
    git-svn-id: https://tekkub-wow.googlecode.com/svn/trunk/TourGuide@710 86fe6d9a-1522-0410-a387-bf9db416f0a0

--- Alliance/23_24_Darkshore.lua
+++ Alliance/23_24_Darkshore.lua
@@ -1,8 +1,5 @@
 TourGuide:RegisterGuide("Darkshore (23-24)", "Ashenvale (24-25)", "Alliance", function()
 return [[
-T Beached Sea Turtle (Part 4)
-T Beached Sea Turtle (Part 5)
-T Beached Sea Creature (Part 4)
 h Auberdine
 T WANTED: Murkdeep!
 T The Absent Minded Prospector (Part 2)
@@ -12,7 +9,7 @@ R Darnassus
 T The Absent Minded Prospector (Part 3) |N|On the west side of Temple of the Moon, outside|
 A The Absent Minded Prospector (Part 4)
 R Ruins of Mathystra
-C Mathystra Relics |N|(58,21)|
+C Mathystra Relics
 A Gyromast's Retrieval |N|Standing on a stone at the beach north of ruins|
 C Gyromast's Retrieval |N|Reef Crawlers drop the bottom, Forestriders drop the top and Murlocs drop the middle|
 C A Lost Master (Part 1) |N|Northeast corner of the zone|
@@ -24,16 +21,12 @@ R Auberdine
 T A Lost Master (Part 1)
 A A Lost Master (Part 2)
 R Wildbend River
-N Do a /wave on the Grimclaw, he's down the road south (41.73,81,53)
 T A Lost Master (Part 2)
 A Escape Through Force
 C Escape Through Force
-N Grind until you are missing 1740 xp from level 24
 H Auberdine
 T Escape Through Force
 A Trek to Ashenvale
-t Train in Darnassus (Level 24)
-t Train pet skills (Level 24) |C|Hunter|
 F Astranaar
 
 ]]
