7ae789600d8efce8468cafb6a9d07eeb1d4bbb95
Mikma <mikko.majuri@gmail.com>
2007-11-19 18:38:04 +0000

    TG: Added 28-29_Duskwood in .toc, also fixed stuff from the same part
    git-svn-id: https://tekkub-wow.googlecode.com/svn/trunk/TourGuide@720 86fe6d9a-1522-0410-a387-bf9db416f0a0

--- Alliance/28_29_Duskwood.lua
+++ Alliance/28_29_Duskwood.lua
@@ -3,10 +3,10 @@ return [[
 R Darkshire
 f Grab flight point |N|East of Darkshire, above the hill|
 
-A Look to the Stars (Part 1) |N|Southeast from flight point in the house|
-B 1x Bronze Tube |N|The Engineer vendor sells this sometimes|
-T Look to the Stars (Part 1)
-A Look to the Stars (Part 2)
+A Look To The Stars (Part 1) |N|Southeast from flight point in the house| |NODEBUG|
+B 1x [Bronze Tube] |N|The Engineer vendor sells this sometimes| |L|4371 1|
+T Look To The Stars (Part 1) |NODEBUG|
+A Look To The Stars (Part 2) |NODEBUG|
 A Worgen in the Woods (Part 1)
 A Raven Hill
 A The Hermit
@@ -24,11 +24,11 @@ A Worgen in the Woods (Part 2)
 C Worgen in the Woods (Part 2) |N|West of Darkshire (64,46)|
 T Worgen in the Woods (Part 2)
 A Worgen in the Woods (Part 3)
-T Look to the Stars (Part 2) |N|(81,59)|
-A Look to the Stars (Part 3)
+T Look To The Stars (Part 2) |N|(81,59)| |NODEBUG|
+A Look To The Stars (Part 3) |NODEBUG|
 C The Night Watch (Part 1)
 K Skeletons for 10x Skeleton Fingers |L|2378 10|
-C Look to the Stars (Part 3) |N|Insane Ghoul inside the chapel|
+C Look To The Stars (Part 3) |N|Insane Ghoul inside the chapel| |NODEBUG|
 A An Old History Book |O| |U|2794|
 C Worgen in the Woods (Part 3) |N|(73,73)|
 
@@ -38,16 +38,16 @@ A The Night Watch (Part 2)
 T Worgen in the Woods (Part 3)
 A Worgen in the Woods (Part 4)
 T Worgen in the Woods (Part 4)
-T Look to the Stars (Part 3)
-A Look to the Stars (Part 4)
+T Look To The Stars (Part 3) |N|Southeast of Darkshire, in a shack| |NODEBUG|
+A Look To The Stars (Part 4) |NODEBUG|
 
-C Look to the Stars (Part 4) |N|At Vol'Gol Ogre Mound, in a cave (34,77)|
+C Look To The Stars (Part 4) |N|At Vol'Gol Ogre Mound, in a cave (34,77)| |NODEBUG|
 T Raven Hill |N|In Raven Hill Cemetery|
 C The Night Watch (Part 2)
 T The Hermit |N|Shack north of the cemetery|
-A Supplies From Darkshire
+A Supplies from Darkshire
 A The Weathered Grave |N|Grave at (17,29)|
-T Deliveries for Sven |N|At The Hushed Bank (8,34)|
+T Deliveries to Sven |N|At The Hushed Bank (8,34)|
 A Sven's Revenge
 H Darkshire
 
@@ -56,10 +56,10 @@ A The Night Watch (Part 3)
 T The Weathered Grave
 A Morgan Ladimore
 T Morgan Ladimore
-T Supplies to Darkshire
+T Supplies from Darkshire
 A Ghost Hair Thread
-T Look to the Stars (Part 4)
-T Ghost Hair Thread |N|(81,59)|
+T Look To The Stars (Part 4) |NODEBUG|
+T Ghost Hair Thread |N|Southeast of Darkshire, in a shack|
 A Return the Comb
 T Return the Comb
 A Deliver the Thread
@@ -71,14 +71,14 @@ T Deliver the Thread |N|Shack north of cemetary.|
 A Zombie Juice
 C The Night Watch (Part 3)
 
-K Undead for 10x Goul Fangs |L|1129 10|
-K Spiders for 6x Vials of Spider Venom |L|1130 5|
+K Undeads for 10x [Ghoul Fang] |L|1129 10|
+K Spiders for 5x [Vial of Spider Venom] |L|1130 5|
 
 C The Totem of Infliction
 T Sven's Camp
 A The Shadowy Figure
 
-H Darkshire
+H Scarlet Raven Tavern
 T Zombie Juice
 T The Night Watch (Part 3)
 T The Totem of Infliction
