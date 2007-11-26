98d9710e112ebcee5ffa28fb1fbd7e2c2bc8364a
Tekkub Stoutwrithe <tekkub@gmail.com>
2007-11-26 09:09:18 +0000

    TourGuide - Adding user-submitted Horde 44-47 guides
    git-svn-id: https://tekkub-wow.googlecode.com/svn/trunk/TourGuide@729 86fe6d9a-1522-0410-a387-bf9db416f0a0

---
+++ Horde/44_46_Feralas.lua
@@ -0,0 +1,120 @@
+TourGuide:RegisterGuide("Feralas (44-46)", "Azshara (46)", "Horde", function()
+return [[
+R Camp Mojache
+
+A A New Cloak's Sheen
+A The Ogres of Feralas (Part 1)
+A Gordunni Cobalt
+A War on the Woodpaw
+A The Mark of Quality
+A A Strange Request
+
+f Camp Mojache
+h Camp Mojache
+
+C War on the Woodpaw |N|North of Camp Mojache around (71,37)|
+C Gordunni Cobalt |N|North of Camp Mojache, up the path by the waterfall (75,31)|
+C The Ogres of Feralas (Part 1)
+A The Gordunni Scroll |N|From the scrolls lying around on the floor|
+C A New Cloak's Sheen |N|West of Camp Mojache, on the North side of the path (70,47)|
+
+R Camp Mojache
+
+T War on the Woodpaw
+T Gordunni Cobalt
+T The Ogres of Feralas (Part 1)
+T The Gordunni Scroll
+T A New Cloak's Sheen
+
+A Alpha Strike
+A A Grim Discovery (Part 1)
+A Dark Ceremony
+A The Ogres of Feralas (Part 2)
+
+C Alpha Strike |N|Southwest of Camp Mojache (72,56)|
+T Alpha Strike
+A Woodpaw Investigation
+C Woodpaw Investigation |N|Southwest of Camp Mojache (71.6,55.9)|
+T Woodpaw Investigation
+A The Battle Plans
+
+C A Grim Discovery (Part 1) |N|West of Camp Mojache (66,46)|
+H Camp Mojache
+T The Battle Plans
+T A Grim Discovery (Part 1)
+
+A A Grim Discovery (Part 2)
+A Stinglasher
+A Zukk'ash Infestation
+
+C Stinglasher |N|Both in the hive area south of Camp Mojache (74,62)|
+C Zukk'ash Infestation
+C Screecher Spirits |N|South of Dire Maul (55,56)|
+C The Ogres of Feralas (Part 2) |N|South of Dire Maul (59,68)|
+C Dark Ceremony |N|In the ruins to the south (59,68)|
+C The Mark of Quality |N|Southwest of Dire Maul (55.4,56.4)|
+
+R Camp Mojache
+
+T Stinglasher
+T Zukk'ash Infestation
+T The Ogres of Feralas (Part 2)
+T Dark Ceremony
+T The Mark of Quality
+
+A Zukk'ash Report
+A The Gordunni Orb
+
+F Orgrimmar
+
+N Get 120 Silk cloth from bank or mailbox |L|4338 120|
+N Get 120 Mageweave from bank or mailbox |L|4306 120|
+
+T Zukk'ash Report |N|In The Drag (56,46)| |Z|Orgrimmar|
+T A Donation of Silk |N|Magar's Cloth Goods, in The Drag (63,51)| |Z|Orgrimmar|
+T A Donation of Mageweave
+
+A Ripple Recovery (Part 1) |N|From Dran Droffers in The Drag (59.4,36.9)| |Z|Orgrimmar|
+T Ripple Recovery (Part 1) |N|The other guy in the same building|
+A Ripple Recovery (Part 2)
+
+T Parts of the Swarm |N|At Belgrom Rockmaul in the Valley of Honor (75,34)| |Z|Orgrimmar|
+T A Grim Discovery (Part 2)
+A Betrayed
+
+T A Strange Request |N|In the Cleft of Shadow (49.6,50.6)| |Z|Orgrimmar|
+A Return to Witch Doctor Uzer'i
+
+T Report to Zor |N|In the Valley of Wisdom (38.9,38.4)| |Z|Orgrimmar|
+A Service to the Horde
+T Service to the Horde
+
+T The Gordunni Orb |N|In the Valley of Spirits (39,86)| |Z|Orgrimmar|
+A A Donation of Silk
+T A Donation of Silk
+A A Donation of Mageweave
+T A Donation of Mageweave
+
+N This is a good time for Trolls and Orcs to buy a mount if you haven't done so already
+
+H Camp Mojache
+T Return to Witch Doctor Uzer'i
+A Natural Materials
+A Testing the Vessel
+
+K Sprite Darters |Q|Natural Materials| |QO|Encrusted Minerals: 6/6| |N|Sprite Darters west of Camp Mojache (68,48)|
+C Natural Materials |N|Hippogryphs SW of Dire Maul (54,72)|
+A Find OOX-22/FE! |N|If the beacon dropped|
+T Find OOX-22/FE! |N|In the yeti cave (53.4,55.7), skip the escort quest|
+G Grind on Gordunni Ogres until lvl 46 |LV|46|
+
+H Camp Mojache
+T Natural Materials
+A The Sunken Temple
+
+F Thunder Bluff
+
+T Deadmire |N|Hunter Rise (61,80)| |Z|Thunder Bluff|
+t Train (Level 46)
+]]
+end)
---
+++ Horde/46_47_Hinterlands.lua
@@ -0,0 +1,74 @@
+TourGuide:RegisterGuide("The Hinterlands (46-47)", "Stranglethorn Vale (47)", "Horde", function()
+return [[
+
+F Tarren Mill
+h Tarren Mill
+R The Hinterlands
+
+T Ripple Recovery |N|In the hills above Quel'Danil Lodge (26.7,48.5)|
+A A Sticky Situation
+
+R Revantusk Village |N|Head to the East coast, Revantusk is at the South end (76.9,77.5)|
+A Vilebranch Hooligans
+A Cannibalistic Cousins
+A Message to the Wildhammer
+A Stalking the Stalkers
+A Hunt the Savages
+A Avenging the Fallen
+
+C Vilebranch Hooligans
+C Cannibalistic Cousins
+C A Sticky Situation |N| In the cave at Skulk Rock, (57,41)|
+C Stalking the Stalkers
+C Hunt the Savages
+C Testing the Vessel
+C Avenging the Fallen
+C Lines of Communication
+C Message to the Wildhammer
+C Rin'ji is Trapped! |N|He's in a cage in Quel'Danil Lodge (30.7,47.0)|
+A Venom Bottles |N|You will find bright green bottles lying around at the Witherbark Troll villages, loot one to start the quest (31.6,57.8)|
+C Grim Message
+
+T A Sticky Situation |N|In the hills above Quel'Danil Lodge (26.7,48.5)|
+A Ripple Delivery
+
+A Find OOX-09/HL! |O| |L|8704|
+T Find OOX-09/HL! |N|On a small island in the lake west of Skulk Rock (49.4,37.7) Skip the escort|
+
+T Rin'ji is Trapped! |N|On the small island off the coast, Southeast of the point where the river meets the sea (86.3,59.1)|
+A Rin'ji's Secret
+
+R Revantusk Village
+
+T Vilebranch Hooligans
+T Cannibalistic Cousins
+T Message to the Wildhammer
+T Stalking the Stalkers
+T Hunt the Savages
+T Avenging the Fallen
+
+f Revantusk Village
+F Hammerfall |N|This is to level first aid, if you've already got Runecloth Bandages or don't have enough Mageweave, skip this step|
+
+N Level first aid, train new bandages if possible (Runecloth Bandage requires 260)|
+
+F Tarren Mill
+T Venom Bottles
+A Undamaged Venom Sac
+R The Hinterlands |N|The next quests are done in the Western part of the zone, so don't be tempted to fly to Revantusk Village|
+C Undamaged Venom Sac
+C The Atal'ai Exile |N|Climb the South temple and look down off the stairs to your left. You should be able to see the exile on the platform (33.7,75.2)|
+A Return to Fel'Zerul
+
+H Tarren Mill
+T Undamaged Venom Sac
+F Undercity
+
+T A Donation of Mageweave |O| |L|4338 60| |N|In the Magic Quarter|
+T Lines of Communication
+T Rin'ji's Secret
+A Oran's Gratitude
+C Oran's Gratitude
+
+]]
+end)
