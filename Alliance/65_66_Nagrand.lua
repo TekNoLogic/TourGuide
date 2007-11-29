7f8e74f0afcaee0494760e04c2e4685257ced0c3
Tekkub Stoutwrithe <tekkub@gmail.com>
2007-11-28 22:54:54 -0700

    TourGuide - User submitted Ally 64-66 guides

---
+++ Alliance/65_66_Nagrand.lua
@@ -0,0 +1,183 @@
+
+TourGuide:RegisterGuide("Nagrand (65-66)", nil, "Alliance", function()
+return [[
+R Nesingwary's Camp |N|In the east of the zone (71,40).|
+A Windroc Mastery (Part 1)
+A Clefthoof Mastery (Part 1)
+A Talbuk Mastery (Part 1)
+
+N Powder, Tusks and Wind... |N|If you're not already friendly with Consortium from Mana Tombs, you can collect Elekk tusks for rep and xp. Keep any Oshu'gun Crystal Powder you find for rewards later. Also, kill any air elementals you come across until a Howling Wind drops.|
+
+C Windroc Mastery (Part 1) |N|There's a camp of these southwest of Nesingwary, as well as northwest.|
+C Talbuk Mastery (Part 1) |N|Just north of Nesingwary.|
+C Clefthoof Mastery (Part 1) |N|Mostly south of Nesingwary.|
+
+T Windroc Mastery (Part 1)
+T Clefthoof Mastery (Part 1)
+T Talbuk Mastery (Part 1)
+
+A Windroc Mastery (Part 2)
+A Clefthoof Mastery (Part 2)
+A Talbuk Mastery (Part 2)
+
+T The Howling Wind
+A Murkblood Corruptors
+A The Underneath
+A A Rare Bean
+A Muck Diving
+T The Underneath
+A The Tortured Earth
+
+C Talbuk Mastery (Part 2) |N|West of the Throne of Elements. Keep an eye out for dung for A Rare Bean too - it really blends in, but it's in this area.|
+C A Rare Bean
+C Clefthoof Mastery (Part 2) |N|West again, south of the Laughing Skull Ruins.|
+C Muck Diving |N|Southwest, the water around Halaa.|
+
+N Oshu'gun Crystal Powder |N|If Halaa is under Alliance control, you can turn in your powder for tokens. Easiest item to get is an 18-slot bag, needs 160 powder.|
+
+C Murkblood Corruptors |N|At Sunspring Post (31,43).|
+
+A Matters of Security |N|At the Consortium Outpost, straight south of Sunspring (31,57).|
+A Stealing from Thieves
+N Ivory |N|If you're not already friendly with the Consortium, you can turn in your ivory now.|
+
+C Matters of Security
+C Stealing from Thieves
+
+T Matters of Security
+T Stealing from Thieves
+
+A Gava'xi
+C Gava'xi
+T Gava'xi
+
+R Telaar |N|The main Alliance base in Nagrand is in the south center of the zone (50,70).|
+A The Ravaged Caravan |N|Huntress Kima walks around Telaar.|
+A Wanted: Giselda the Crone |N|Off the signpost beside Warden Iolol (54,70).|
+A Wanted: Zorbo the Advisor
+A Fierce Enemies |N|From Warden Iolol.|
+T A Message to Telaar
+A Do My Eyes Deceive Me |N|From Huntress Bintook.|
+A The Throne of the Elements
+A Stopping the Spread
+A Solving the Problem
+h Telaar
+f Telaar
+
+A I Must Have Them! |N|Kill any air elementals you happen across, but don't go hunting them just yet. Wazat is just east of Telaar (61,67).|
+
+C The Tortured Earth |N|At the hill just east of Telaar (64,70).|
+C Do My Eyes Decieve Me
+
+C Wanted: Giselda the Crone |N|Kil'Sorrow Fortress is in the far south-east of Nagrand (69,81).|
+
+H Telaar
+T Wanted: Giselda the Crone
+T Do My Eyes Decieve Me
+A Not On My Watch!
+
+C Windroc Mastery (Part 2) |N|NW of Telaar (47,63).|
+
+T Gava'xi
+
+C Solving the Problem |N|Back to Sunspring Post (31,43). Also torch any corpses you come across.|
+C Stopping the Spread
+
+C The Ravaged Caravan |N|Warmaul Hill is in the northwest of the zone (29,31).|
+
+N The Ring of Blood |N|If you can find a good group, the Ring of Blood is easy money.|
+
+C Wanted: Zorbo the Advisor |N|At the Laughing Skull Ruins (43,21)|
+
+T Murkblood Corruptors
+T The Throne of the Elements
+T A Rare Bean
+A Agitated Spirits of Skysong
+T Muck Diving
+T The Tortured Earth
+A Eating Damnation
+
+
+C Eating Damnation |N|To the west of the throne, on the edge of the mountains.|
+C Agitated Spirits of Skysong |N|Skysong Lake. Duh?|
+
+T Eating Damnation
+A Shattering The Veil
+T Agitated Spirits of Skysong
+A Blessing of Incineratus
+
+T Windroc Mastery (Part 2)
+T Clefthoof Mastery (Part 2)
+T Talbuk Mastery (Part 2)
+
+N Big Game Hunting... |N|If you have a group, feel free to accept and do the follow-up quests. They are not soloable.|
+
+C Blessing of Incineratus |N|Windyreed Village is south of the Throne of Elements (71,51). To fire the big hut you need to be close to it's center pole.|
+
+T Blessing of Incineratus
+A The Spirit Polluted
+C The Spirit Polluted |N|Lake Sunspring is south of Sunspring Post.|
+C Shattering the Veil |N|These earth elementals are south of Oshu'gun.|
+
+H Telaar
+T Solving the Problem
+T Stopping the Spread
+T Wanted: Zorbo the Advisor
+T Fierce Enemies
+T The Ravaged Caravan
+
+T I Must Have Them!
+N Egging yourself on ...|N|If you're a) brave b) stupid(ly skillful, of course) c) over-level or d) have a group, you can take the followup. You have to fight a 66 elite though.|
+C Not On My Watch! |N|Lump is just over the hill from Wazat (62,71).|
+
+T Not On My Watch!
+A Mo'mor the Breaker
+T Mo'mor the Breaker
+A The Ruins of Burning Blade
+
+A HELP! |N|Corki is in a cage in the Burning Blade Ruins (72,70).|
+C The Ruins of Burning Blade
+C HELP! |N|The key should have dropped off an ogre by now. If not, grind until you get it.|
+
+T HELP!
+A Corki's Gone Missing Again!
+T The Ruins of Burning Blade
+A The Twin Clefts of Nagrand
+
+C Corki's Gone Missing Again! |N|Corki is in a cage at the back of Northwind Cleft (39,27).|
+C The Twin Clefts of Nagrand
+
+T Corki's Gone Missing Again!
+A Corki's Ransom
+T The Twin Clefts of Nagrand
+A Diplomatic Measures
+
+T Diplomatic Measures
+A Armaments for Deception
+A Ruthless Cunning
+
+C Ruthless Cunning
+C Armaments for Deception
+
+T Ruthless Cunning
+T Armaments for Deception
+A Returning the Favor
+A Body of Evidence
+
+T The Spirit Polluted
+T Shattering the Veil
+
+C Returning the Favor
+C Body of Evidence
+
+H Telaar
+
+T Returning the Favor
+T Body of Evidence
+A Message to Telaar
+
+T Message to Telaar
+
+]]
+
+end)
