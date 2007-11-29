8750590d3fce7137a16a7ea9b82dfa7df1df44af
Tekkub Stoutwrithe <tekkub@gmail.com>
2007-11-29 07:03:19 -0700

    Cleanup of user submitted alliance guides

--- Alliance/64_65_Terokkar.lua
+++ Alliance/64_65_Terokkar.lua
@@ -1,30 +1,29 @@
 
-TourGuide:RegisterGuide("Terokkar Forest (64-65)", nil, "Alliance", function()
+TourGuide:RegisterGuide("Terokkar Forest (64-65 Aldor)", nil, "Alliance", function()
 return [[
-A A'dal |Z|Shattrath City| |N|You're looking for a Haggard War Veteran, he should be fairly easy to find.|
-f Get the Shatt FP |N|The Flightpath is on the northwest side of the main ring.| |Z|Shattrath City|
+A A'dal |N|You're looking for a Haggard War Veteran, he should be fairly easy to find.|
+f Get the flightpoint |N|On the northwest side of the main ring.|
 
-T A'dal |Z|Shattrath City|
-A City of Light |Z|Shattrath City|
-A Rather Be Fishin' |N|You can accept this and the next while doing the walk-around for City of Light. Seth is at the ruined infirmary (63.95,15.50). Don't get the other quest unless you can solo (or find a group for) a 63 elite that's immune to fear and periodically stuns you.| |Z|Shattrath City|
-A The Eyes of Skettis |Z|Shattrath City| |N|Rilak is in Lower City, just before the guardian turns to take you back up to the main ring (52.56,20.98)|
-T City of Light |Z|Shattrath City|
-N Aldor |N|I'm basing this guide off the presumption that most people are going to pick Aldor. If you go Scryer you're on your own.|
-A Allegiance to the Aldor |Z|Shattrath City|
-T Allegiance to the Aldor |Z|Shattrath City|
-A Ishanah |Z|Shattrath City|
-A Marks of Kil'jaeden |N|Up on Aldor Rise. Don't fall off the elevator.| |Z|Shattrath City|
-T Ishanah |Z|Shattrath City|
-A Restoring the Light |Z|Shattrath City|
-h Hearth to Aldor Rise |N|Looking from the Aldor temple toward the centre of town, the inn is on your right. Repair guy on your left.| |Z|Shattrath City|
+T A'dal
+A City of Light
+A Rather Be Fishin' |N|You can accept this and the next while doing the walk-around for City of Light. Seth is at the ruined infirmary (63.95, 15.50). Don't get the other quest unless you can solo (or find a group for) a 63 elite that's immune to fear and periodically stuns you.| |Z|Shattrath City|
+A The Eyes of Skettis |Z|Shattrath City| |N|Rilak is in Lower City, just before the guardian turns to take you back up to the main ring (52.56, 20.98)|
+T City of Light
+A Allegiance to the Aldor
+T Allegiance to the Aldor
+A Ishanah
+A Marks of Kil'jaeden |N|Up on Aldor Rise.|
+T Ishanah
+A Restoring the Light
+h Shattrath City |N|As you leave the temple the inn is to your right.|
 
-C Rather Be Fishin' |N|Silmyr Lake is northeast of Shattrath.|
+C Rather Be Fishin' |N|Silmyr Lake, northeast of Shattrath.|
 T What's Wrong at Cenarion Thicket
 A Strange Energy
 A Clues in the Thicket
 A It's Watching You!
 C Clues in the Thicket |N|The clue you're after is in the inn.|
-C It's Watching You! |N|Up the tower.|
+C It's Watching You! |N|Up the tower|
 T It's Watching You!
 C Strange Energy
 T Strange Energy
@@ -34,24 +33,24 @@ C By Any Means Necessary |N|Empoor travels the road between Tuurem and Shattrath
 T By Any Means Necessary
 A Wind Trader Lathrai
 
-N Lower City ... |N|Save any Arakkoa Feathers you get, you'll need them for a quest later for Lower City rep.|
+N Rep tokens... |N|Save any Arakkoa Feathers you get, you'll need them for a quest later for Lower City rep.|
 
-N Western Altar |N|Use the Blessed Vial at the Western Altar (48.11,14.52).| |Q|Restoring the Light| |QO|Western Altar: 1/1|
-N Northern Altar |N|Use the Blessed Vial at the Northern Altar (50.66,16.55).| |Q|Restoring the Light| |QO|Northern Altar: 1/1|
-N Eye of Veil Reskk |N|Get the Eye of Veil Reskk from the tree hut near the north altar (49.90,16.52).| |Q|The Eyes of Skettis| |QO|Eye of Veil Reskk: 1/1|
-N Eastern Altar |N|Use the Blessed Vial at the Eastern Altar (49.25,20.32).| |Q|Restoring the Light| |QO|Eastern Altar: 1/1|
-N Eye of Veil Shienor |N|Get the Eye of Veil Shienor from the tree hut in Veil Shienor (57.90,23.12).| |Q|The Eyes of Skettis| |QO|Eye of Veil Shienor: 1/1|
+N Western Altar |N|Use the Blessed Vial at the Western Altar (48.11, 14.52).| |Q|Restoring the Light| |QO|Western Altar: 1/1| |U|TODO|
+N Northern Altar |N|Use the Blessed Vial at the Northern Altar (50.66, 16.55).| |Q|Restoring the Light| |QO|Northern Altar: 1/1| |U|TODO|
+N Eye of Veil Reskk |N|Get the Eye of Veil Reskk from the tree hut near the north altar (49.90, 16.52).| |Q|The Eyes of Skettis| |QO|Eye of Veil Reskk: 1/1|
+N Eastern Altar |N|Use the Blessed Vial at the Eastern Altar (49.25, 20.32).| |Q|Restoring the Light| |QO|Eastern Altar: 1/1| |U|TODO|
+N Eye of Veil Shienor |N|Get the Eye of Veil Shienor from the tree hut in Veil Shienor (57.90, 23.12).| |Q|The Eyes of Skettis| |QO|Eye of Veil Shienor: 1/1|
 
 H Aldor Rise
 T Restoring the Light
 T The Eyes of Skettis |N|Back to Rilak in Lower City|
 A Seek Out Kirrik
-T Rather Be Fishin' |N|Don't get the other quest unless you can solo (or find a group for) a 63 elite that's immune to fear and periodically stuns you.|
+T Rather Be Fishin' |N|Skip the follow-up.|
 T Wind Trader Lathrai
 A A Personal Favor
 
 C Marks of Kil'jaeden |N|Marks drop off the Cabal mobs around the Shadow Tomb in the Bone Wastes (30,52).|
-A Cabal Orders |N|The item that starts this quest drops from Cabal Skirmishers and Spellweavers. Keep killing them until you get it.|
+A Cabal Orders |N|The item that starts this quest drops from Cabal Skirmishers and Spellweavers. Keep killing them until you get it.| |U|TODO|
 T Cabal Orders |N|The Refugee Caravan is somewhat east of the Shadow Tomb (38,50).|
 A The Shadow Tomb
 A Before Darkness Falls
@@ -59,9 +58,9 @@ T Seek Out Kirrik
 A Veil Skith: Darkstone of Terokk
 A Missing Friends
 A The Infested Protectors
-C The Infested Protectors |N|Kill the infested/rotting treants in the Bone Wastes, then kill the mites that come from them.|
+C The Infested Protectors |N|Kill the infested/rotting treants in the Bone Wastes, then kill the mites that spawn from them.|
 
-C Veil Skith: Darkstone of Terokk |N|Veil Skith is just south of Shattrath (30,41).|
+C Veil Skith: Darkstone of Terokk |N|Veil Skith, south of Shattrath (30,41).|
 C A Personal Favor
 C Missing Friends |N|Kill Arakkoa, collect keys, free kids.|
 
@@ -81,7 +80,7 @@ T Veil Rhaze: Unliving Evil
 A Veil Lithic: Preemptive Strike
 
 C Veil Lithic: Preemptive Strike |N|Southwest of the caravan (23,71).|
-A The Dread Relic |N|The Sha'tari Base Camp is southeast of Veil Lithic (31,76).|
+A The Dread Relic |N|Southeast of Veil Lithic at the Sha'tari Base Camp (31,76).|
 A An Improper Burial
 A Taken in the Night
 A Digging Through Bones |N|Listed as a group quest, but it's not difficult, and the character dialog is worth it.|
@@ -90,20 +89,17 @@ T Digging Through Bones
 A Fumping |N|Again group, but this one isn't elite.|
 
 C An Improper Burial |N|Just east of the base camp.|
-C The Dread Relic |N|The Derelict Caravan is east of the base camp (43.94,76.50).|
+C The Dread Relic |N|The Derelict Caravan is east of the base camp (43.94, 76.50).|
 C Fumping |N|Recommended you do the Fumping a bit east of the caravan, since the mobs around here are neutral (46,78).|
 
-A For the Fallen |N|Vindicator Haylen is northwest of the Netherweb Ridge (49.71,76.18).|
+A For the Fallen |N|Vindicator Haylen is northwest of the Netherweb Ridge (49.71, 76.18).|
 C Taken in the Night
 C For the Fallen
 T For the Fallen
-N Terokkantula |N|If you have a group, or think you can solo a 65 elite that hits like a truck and can't be controlled at all, take the quest from Haylen, kill it then get the followup back to the base camp. Otherwise just head back to the base camp.|
 
-T Taken in the Night
+T Taken in the Night |N|Back at the base camp.|
 T Fumping
-N The Big Bone Worm |N|Similar deal to Terokkantula, you need a group or mad solo skills for this.|
 T The Dread Relic
-N Teribus |N|Another group quest. Sigh. (If you do do it, be aware that the doom skulls aren't tradable, and also when you summon him it takes like 30s for him to actually show.|
 T An Improper Burial
 A A Hero is Needed
 C A Hero is Needed
@@ -115,15 +111,14 @@ T The Fallen Exarch
 T Veil Lithic: Preemptive Strike
 A Veil Shalas: Signal Fires
 A The Tomb of Lights
-N Vengeful Harbinger |N|Another group quest that's probably soloable if you're over-level. In the same place as the tomb of lights.|
 C The Tomb of Lights
 A Skywing |N|Just outside Veil Shalas (54,72). If you don't see him, just move on.|
 C Veil Shalas: Signal Fires |N|Veil Shalas is on the southeast side of the bone wastes (55,66). The signal fires are all around the treehuts. If you did Skywing you should've cleared three of them already, the last one is in the north set of huts.|
 T Veil Shalas: Signal Fires
 A Return to Shattrath
 
-R Allerian Stronghold |N|Well west of the caravan (55,54).|
-h Allerian |N|The inn is on the left as you come in.|
+R Allerian Stronghold |N|Well east of the caravan (55,54).|
+h Allerian Stronghold |N|The inn is on the left as you come in.|
 A Wanted: Bonelashers Dead! |N|Off the wanted poster outside the town hall.|
 A Olemba Seeds
 A Speak with Private Weeks
@@ -134,7 +129,6 @@ A Timber Worg Tails
 
 T The Fate of Tuurem
 A Magical Disturbances
-f Allerian |N|Over the river from the main part of town.|
 
 F Shattrath City
 T Return to Shattrath
@@ -144,16 +138,13 @@ T A Personal Favor
 A Investigate Tuurem
 T Marks of Kil'jaeden |N|Up on Aldor Rise.|
 
-N Training |N|Now would be a good time to grab a port back to the old world and train/AH stuff etc if you need to.|
-
-H Allerian Stronghold |N|Once you're done, hearth back to Allerian.|
-
+H Allerian Stronghold
 N Collection Quests... |N|While you're running around, keep an eye out for Olemba Cones (bright blue things on the ground). Also kill Warp Stalkers and Timber Worgs as you run across them.|
 
-C Unruly Neighbors |N|The Bonechewer Ruins are east of Allerian Stronghold (66,53).|
+C Unruly Neighbors |N|East of Allerian Stronghold at the Bonechewer Ruins (66,53).|
 C Magical Disturbances
 C Timber Worg Tails
-T Unruly Neighbors |N|Allerian Post is north of the ruins (69,44).|
+T Unruly Neighbors |N|North of the ruins at Allerian Post (69,44).|
 A The Firewing Liaison
 A Thinning the Ranks
 C The Firewing Liaison |N|Lisaile Fireweaver is inside or around the only building in the ruins that has a roof (67,54).|
@@ -162,21 +153,21 @@ C Thinning the Ranks
 T The Firewing Liaison
 T Thinning the Ranks
 
-N Ayit |N|Kill Ayit at Veil Shienor for Stymying the Arrakoa - he's in a purple hut on the ground level (59,23).| |Q|Stymying the Arakkoa| |QO|Ayit slain: 1/1 (Complete)|
-C Investigate Tuurem |N|The Sealed Box is in one of the huts in Tuurem (54.00,30.00).|
-N Ashkaz |N|Kill Ashkaz at Veil Reskk for Stymying the Arrakoa - he's in the trees (49,16).| |Q|Stymying the Arakkoa| |QO|Ashkaz slain: 1/1 (Complete)|
+K Ayit |N|Kill Ayit at Veil Shienor for Stymying the Arrakoa - he's in a purple hut on the ground level (59,23).| |Q|Stymying the Arakkoa| |QO|Ayit slain: 1/1 (Complete)|
+C Investigate Tuurem |N|The Sealed Box is in one of the huts in Tuurem (54.00, 30.00).|
+K Ashkaz |N|Kill Ashkaz at Veil Reskk for Stymying the Arrakoa - he's in the trees (49,16).| |Q|Stymying the Arakkoa| |QO|Ashkaz slain: 1/1 (Complete)|
 C Thin the Flock
 T Investigate Tuurem
 A What Are These Things?
 
-T Speak with Private Weeks |N|Private Weeks is just north of Grangol'var Village (40.46,36.16).|
+T Speak with Private Weeks |N|Private Weeks is just north of Grangol'var Village (40.46, 36.16).|
 A Who Are They?
-C Who Are They? |N|Shadowy Hunters are bad news. Stay away from them. Initiate (39,39.68), Laborer (38.09,41.09), Advisor (40,39) inside the building.|
+C Who Are They? |N|Shadowy Hunters are bad news. Stay away from them. Initiate (39, 39.68), Laborer (38.09, 41.09), Advisor (40,39) inside the building.|
 T Who Are They?
 A Kill the Shadow Council!
 C Kill the Shadow Council!
 
-N Urdak |N|Kill Urdak at Veil Skith for Stymying the Arrakoa - he's up in the trees (29,42).| |Q|Stymying the Arakkoa| |QO|Urdak slain: 1/1 (Complete)|
+K Urdak |N|Kill Urdak at Veil Skith for Stymying the Arrakoa - he's up in the trees (29,42).| |Q|Stymying the Arakkoa| |QO|Urdak slain: 1/1 (Complete)|
 C Wanted: Bonelashers Dead! |N|In the Bone Wastes, mostly south and east of the caravan.|
 
 H Allerian Stronghold
@@ -191,7 +182,6 @@ A Vessels of Power
 T Kill the Shadow Council!
 T Thin the Flock
 T Wanted: Bonelashers Dead!
-N Torgos |N|He's probably not soloable at-level, but you could duo him easy enough. Take it if you can find someone to do it.|
 T Magical Disturbances
 
 T Report to the Allerian Post
