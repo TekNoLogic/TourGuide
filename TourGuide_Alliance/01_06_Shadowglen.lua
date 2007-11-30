3f14856cc462b5f2b62e4ad6d16d3a271f6c16cb
Tekkub Stoutwrithe <tekkub@gmail.com>
2007-11-30 04:05:07 -0700

    Shadowglen tweaks

--- TourGuide_Alliance/01_06_Shadowglen.lua
+++ TourGuide_Alliance/01_06_Shadowglen.lua
@@ -14,11 +14,11 @@ A Verdant Sigil |C|Druid|
 
 A The Woodland Protector (Part 1)
 A A Good Friend
-T The Woodland Protector (Part 1) |N|Dryad to the southewest (57.77,45.20)|
+T The Woodland Protector (Part 1) |N|Dryad to the southwest (57.77, 45.20)| |Z|Teldrassil|
 A The Woodland Protector (Part 2)
 C The Woodland Protector (Part 2)
 T The Woodland Protector (Part 2)
-A Webwood Venom |N|At the base of the ramp (57.79,41.70)|
+A Webwood Venom |N|At the base of the ramp (57.79, 41.70)| |Z|Teldrassil|
 
 T Encrypted Sigil |C|Rogue|
 T Etched Sigil |C|Hunter| |N|Up the ramp!|
@@ -28,8 +28,8 @@ T Verdant Sigil |C|Druid| |N|Up the ramp!|
 
 t Train (Level 2) |N|Notice! There are no reminders to train levels after this. Remember to do so after even levels|
 
-C The Balance of Nature (Part 2) |N|Mobs are to the northeast (60,35)|
-C Webwood Venom |N|To the northwest at Shadowthread Cave (57,33)|
+C The Balance of Nature (Part 2) |N|Mobs are to the northeast (60,35)| |Z|Teldrassil|
+C Webwood Venom |N|To the northwest at Shadowthread Cave (57,33)| |Z|Teldrassil|
 T A Good Friend |N|Just west of the cave entrance|
 A A Friend in Need
 T Webwood Venom
@@ -40,20 +40,20 @@ T A Friend in Need
 A Iverron's Antidote (Part 1)
 
 C Iverron's Antidote (Part 1) |N|Lilies around the pond, mushrooms at the bases of trees, ichor from the spiders|
-C Webwood Egg |N|(56,26)|
+C Webwood Egg |N|Inside the cave (56,26)| |Z|Teldrassil|
 T Webwood Egg
 A Tenaron's Summons
 T Tenaron's Summons |N|Up the ramp|
 A Crown of the Earth (Part 1)
 T Iverron's Antidote (Part 1)
 A Iverron's Antidote (Part 2)
-C Crown of the Earth (Part 1) |N|Moonwell north of Aldrassil (59.93,33.07)|
+C Crown of the Earth (Part 1) |N|Moonwell north of Aldrassil (59.93, 33.07)| |Z|Teldrassil| |U|5185|
 T Iverron's Antidote (Part 2)
 T Crown of the Earth (Part 1)
 A Crown of the Earth (Part 2)
 
-A Dolanaar Delivery |N|South out of Shadowglen (61,47)|
-A Zenn's Bidding |N|Along the road to Dolannar (60,56)|
+A Dolanaar Delivery |N|South out of Shadowglen (61,47)| |Z|Teldrassil|
+A Zenn's Bidding |N|Along the road to Dolannar (60,56)| |Z|Teldrassil|
 ]]
 end)
 
