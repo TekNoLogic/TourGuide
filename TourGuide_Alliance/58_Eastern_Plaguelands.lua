8ab6a93944418d26dbabe4e8385de9779cef3db4
Tekkub Stoutwrithe <tekkub@gmail.com>
2007-12-01 01:30:30 -0700

    Ally EPL guide updates

--- TourGuide_Alliance/58_Eastern_Plaguelands.lua
+++ TourGuide_Alliance/58_Eastern_Plaguelands.lua
@@ -16,17 +16,17 @@ A Hameya's Plea |N|From the scroll near Zaeldarr|
 T Of Forgotten Memories |N|Back on the west edge of the zone|
 A Of Lost Honor
 
-T Brother Carlin |N|Back at Light’s Hope Chapel|
+T Brother Carlin |N|Back at Light's Hope Chapel|
 A Villains of Darrowshire
 T Zaeldarr the Outcast |N|Down by the corpse pit|
 
-C Villains of Darrowshire |N|Sword is north of Corin’s Crossing (53,65).  Skull is in Blackwood Lake (51,49)|
-C Of Lost Honor |N|Find the flag on the island at TODO Lake (71,33)|
+C Villains of Darrowshire |N|Sword is north of Corin's Crossing down in the Infectis Scar (53.84, 65.78).  Skull is in Blackwood Lake (51,49)|
+C Of Lost Honor |N|Find the flag in teh lake at Northdale (71.28, 33.97)|
 
-T Troubled Spirits of Kel'Theril |N|At the north tower (56,24).  Skip the follow-up.|
-C Defenders of Darrowshire |N|Kill cannibal ghouls, gibbering ghouls and diseased flayers in the northwest part of the zone, talk to the ghosts that spawn.|
+T Troubled Spirits of Kel'Theril |N|Northwest of Northpass Tower (53.43, 22.01).  Skip the follow-up.|
+C Defenders of Darrowshire |N|Kill cannibal ghouls, gibbering ghouls and diseased flayers in Plaguewood, talk to the ghosts that spawn.|
 
-T Defenders of Darrowshire |N|Back at Light’s Hope Chapel|
+T Defenders of Darrowshire |N|Back at Light's Hope Chapel|
 T Villains of Darrowshire
 ]]
 end)
