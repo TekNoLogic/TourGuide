c1a9837bacfadc713258b1d6f499821ae190027e
Tekkub Stoutwrithe <tekkub@gmail.com>
2007-12-02 21:55:32 -0700

    More Teldrassil tweaks

--- TourGuide_Alliance/06_12_Teldrassil.lua
+++ TourGuide_Alliance/06_12_Teldrassil.lua
@@ -42,26 +42,28 @@ T Seek Redemption! |N|East at Zenn Foulhoof|
 C Ferocitas the Dream Eater |N|North of Starbreeze.  Kill Ferocitas (69.37, 53,41) and open the necklace he drops for the jewel.| |U|8049|
 
 R Dolanaar |N|Or die and use spirit ressurrection|
-C Twisted Hatred |N|North of town (54,52).  You can also wait until you're 10 to do this.|
+C Twisted Hatred |N|North of town (52.83, 50.19).  You can also wait until you're 10 to do this.|
 T Ferocitas the Dream Eater
 T Twisted Hatred
 
 A The Road to Darnassus |N|From a mounted patrol on the road west of town|
 C The Road to Darnassus |N|Near the cave south of the road where the NPC partrols (46,52)|
-R Ban'ethil Barrow Den |N|West of Dolanaar (44,57)|
-N Keep an eye out for relics |N|Four items needed for "The Relics of Wakening" are in this cave|
-A The Sleeping Druid |N|In the cave at (44,57), questgiver at (45,61)|
+
+N Rune of Nesting |N|Head down into the Ban'ethil Barrow Den (44,57).  At the first set of bridges take the left bridge (44.40, 60.67).| |Q|The Relics of Wakening| |QO|Rune of Nesting: 1/1|
+N Black Feather Quill |N|Across the other bridge (43.70, 61.22).| |Q|The Relics of Wakening| |QO|Black Feather Quill: 1/1|
+A The Sleeping Druid |N|Jump down and take the passage to the west, run across the bridge to Oben Rageclaw (44.94, 61,60).|
+N Sapphire of Sky |N|Jump down and go south (44.70, 62.44).| |Q|The Relics of Wakening| |QO|Sapphire of Sky: 1/1|
 C The Sleeping Druid |N|Kill shamans|
 T The Sleeping Druid
 A Druid of the Claw
-C Druid of the Claw
+C Druid of the Claw |N|Jump down and run north.  Rageclaw's body patrols around this room in a circle.  Target his body and use the charm.| |U|8149|
+N Raven Claw Talisman |N|On the ledge overlooking this room.  Run up onto the center pilar, take the bridge to the right and follow the path around to the left (45.65, 57.44).| |Q|The Relics of Wakening| |QO|Raven Claw Talisman: 1/1|
 C The Relics of Wakening
 T Druid of the Claw
 
-C Crown of the Earth (Part 4) |N|South around (42,67)|
+C Crown of the Earth (Part 4) |N|Moonwell at the Pools of Arlithrien (42.42, 67.08)| |U|5621|
 
-R Dolanaar |N|Or die and spirit rez|
-T Crown of the Earth (Part 4)
+T Crown of the Earth (Part 4) |N|Back at Dolanaar, you can die and spirit rez.  Down by the moonwell.|
 A Crown of the Earth (Part 5)
 T The Relics of Wakening
 A Ursal the Mauler
@@ -82,15 +84,14 @@ N Tame your pet |C|Hunter| |N|Elder Nightsaber (42,42) is nice|
 A Heeding the Call |C|Druid|
 
 R Darnassus
-
-T Rellian Greenspyre
+T Rellian Greenspyre |N|At Cenarion Enclave (38.17, 21.65)|
 A Tumors
 
 T Training the Beast |C|Hunter| |N|(40,8)|
 
-T Heeding the Call |C|Druid| |N|Nortwest in Darnassus, Cenarion Enclave (33,8)|
+T Heeding the Call |C|Druid| |N|Northwest in Cenarion Enclave (33,8)|
 A Moonglade |C|Druid|
-T Moonglade |N| Teleport there, go into the house past the light armor salesmen| |C|Druid|
+T Moonglade |N|Teleport there, go into the house past the light armor salesmen| |C|Druid|
 A Great Bear Spirit |C|Druid|
 T Great Bear Spirit |C|Druid| |N|Northwest corner of Moonglade (39,28)|
 A Back to Darnassus |C|Druid|
@@ -102,13 +103,13 @@ C Body and Heart |N|The cave west of Auberdine. You might want to ask someone to
 F Darnassus |C|Druid|
 T Body and Heart |C|Druid|
 
-A Nessa Shadowsong |N|On your way out of Darnassus|
+A Nessa Shadowsong |N|From Mydrannul (70.67, 45.37) on your way out of Darnassus.| |R|Night Elf|
 
 A The Glowing Fruit |N|Out of town, to the south (42,76)|
 C Ursal the Mauler |N|West of the glowing tree|
 
 C Tumors |N|Elementals at the river east of The Oracle Glade (42,42)|
-C Crown of the Earth (Part 5) |N|Moonwell at The Oracle Glade|
+C Crown of the Earth (Part 5) |N|Moonwell at The Oracle Glade| |U|18152|
 A The Enchanted Glade
 C The Enchanted Glade |N|Harpys southwest of The Oracle Glade|
 T The Enchanted Glade
