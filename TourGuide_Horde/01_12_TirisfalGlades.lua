c8907557299b00cda05894087ddc82367595461a
Tekkub Stoutwrithe <tekkub@gmail.com>
2007-12-12 18:15:21 -0700

    Updates to user-submitted undead start zone guide

--- TourGuide_Horde/01_12_TirisfalGlades.lua
+++ TourGuide_Horde/01_12_TirisfalGlades.lua
@@ -1,59 +1,69 @@
-TourGuide:RegisterGuide("Tirisfal Glades (1-12)", "The Barrens (12-20)", "Horde", function()
+TourGuide:RegisterGuide("Tirisfal Glades (1-12)", "The Barrens (12-15)", "Horde", function()
 return [[
-A Rude Awakening |N| The guy right outside of the starting crypt|
-T Rude Awakening |N| Right down the hill|
+A Rude Awakening |N|The guy right outside of the starting crypt|
+T Rude Awakening |N|Right down the hill|
 A The Mindless Ones
 A Piercing the Veil |C|Warlock|
 C The Mindless Ones
 C Piercing the Veil |C|Warlock|
 T The Mindless Ones
 T Piercing the Veil |C|Warlock|
+
 A Rattling the Rattlecages
 A The Damned
 A Encrypted Scroll |C|Rogue|
 A Tainted Scroll |C|Warlock|
 T Encrypted Scroll |C|Rogue|
 T Tainted Scroll |C|Warlock|
-t Get your first class skills
+t Train (Level 2)
+
 C The Damned
 C Rattling the Rattlecages
+
 T The Damned
 T Rattling the Rattlecages
 A Marla's Last Wish
 A Night Web's Hollow
 A Scavenging Deathknell
-C Night Web's Hollow |N| Cave to the north (27.5,58)|
+
+C Night Web's Hollow |N|Cave to the north (27.5, 58.0)|
 K Samuel Fipps |L|16333|
-C Scavenging Deathknell |L|11127 6|
+C Scavenging Deathknell
 T Scavenging Deathknell
 C Marla's Last Wish |N|Bury Samuel's remains|
+
 T Marla's Last Wish
 T Night Web's Hollow
 A The Scarlet Crusade
-C The Scarlet Crusade |N|They are at a camp at (36,67.5)|
+C The Scarlet Crusade |N|They are at a camp at (36.0, 67.5)|
 T The Scarlet Crusade
 A The Red Messenger
 C The Red Messenger |N|Kill Meven Korgal to get the documents|
 T The Red Messenger
 A Vital Intelligence
 A A Rogue's Deal (Part 1) |N|North end of Deathknell|
-A Fields of Grief (Part 1) |N|Dont do it now|
-R Brill |N|Grab Gordo's Task on the way.|
+A Fields of Grief (Part 1)
+A Gordo's Task |N|Along the road to Brill|
+
+R Brill
 A A Putrid Task
 T Vital Intelligence
 A At War With The Scarlet Crusade (Part 1)
 T A Rogue's Deal (Part 1)
 h Gallows' End Tavern
-t Dont forget your skills!
+t Train (Level 4)
 A A New Plague (Part 1)|N|Near the herbalism trainer|
+
 C A New Plague (Part 1)
 C A Putrid Task
 C Gordo's Task
+
 A A Rogue's Deal (Part 2) |N|Back in Deathknell|
+C A Rogue's Deal (Part 2)
 T A Rogue's Deal (Part 2)
-C Fields of Grief (Part 1) |N|Farm at (35,50) above Deathknell|
+C Fields of Grief (Part 1) |N|At the farm above Deathknell (35,50)|
 C At War With The Scarlet Crusade (Part 1)
-G Hit lvl 7 |LV|7|
+
 H Gallows' End Tavern
 A Deaths in the Family
 A The Haunted Mills
@@ -72,88 +82,84 @@ A Wanted: Maggot Eye
 T Fields of Grief (Part 2)
 T Gordo's Task
 A Doom Weed
+
 C Graverobbers
 C Doom Weed
-C Wanted: Maggot Eye |L|3635| |N|(58,30) In the smaller building at the north end of the farm|
+C Wanted: Maggot Eye |N|In the smaller building at the north end of the farm (58,30)|
 C A New Plague (Part 2)
-C The Chill Of Death |L|2876 5|
+N The Chill Of Death |L|2876 5|
+
 T Doom Weed
 T A New Plague (Part 2)
 A A New Plague (Part 3)
 T Graverobbers
 A Forsaken Duties
 A The Prodigal Lich
-t Dont forget your skills!
-R Agamand Mills
-C The Mills Overrun
+t Train (Level 8)
+
+C The Mills Overrun |N|At Agamand Mills|
 C The Haunted Mills
 C Deaths in the Family
-N Kill till you get 'A Letter to Yvette' |L|2839|
-A A Letter Undelivered |N|From item 'A Letter  to Yvette'|
+
 H Gallows' End Tavern
 T Deaths in the Family
 T The Haunted Mills
 A Speak with Sevren
-T A Letter Undelivered
+A A Letter Undelivered |U|2839| |O|
+T A Letter Undelivered |O|
 T Speak with Sevren
 A The Family Crypt
 T The Mills Overrun
-T Forsaken Duties |N|(65.5,60) Southeast of Brill at the campfire|
+
+T Forsaken Duties |N|Southeast of Brill at the campfire (65.5, 60.0)|
 A Return to the Magistrate
 A Rear Guard Patrol
-C Rear Guard Patrol |N|(75,60) At the farm further down the road east.|
+C Rear Guard Patrol |N|At the farm further east down the road (75,60).|
 T Rear Guard Patrol
 T Return To The Magistrate
 T The Chill of Death |N|Buy the coarse thread off the vendor|
-t dont forget your skills!
-R Undercity
+t Train (Level 10)
 
+R Undercity
 T The Prodigal Lich |N|In the magic quarter|
 A The Lich's Identity
 A The Deathstalkers |C|Rogue|
-N Leave Undercity through the sewers.
 
-C At War With The Scarlet Crusade
+C At War With The Scarlet Crusade |N|Leave Undercity through the sewers and you'll end up near the mobs for this quest.|
 C Proof of Demise
 C The Deathstalkers |C|Rogue|
-R Brill
-T At War With The Scarlet Crusade (Part 2)
+
+T At War With The Scarlet Crusade (Part 2) |N|Back in Brill|
 A At War With The Scarlet Crusade (Part 3)
 T Proof of Demise
+
 C The Lich's Identity |N|Book on the island (67,42)|
 C At War With The Scarlet Crusade (Part 3)
 C A New Plague (Part 3)
+
 H Gallows' End Tavern
 T At War With The Scarlet Crusade (Part 3)
 A At War With The Scarlet Crusade (Part 4)
 T A New Plague (Part 3)
 A A New Plague (Part 4)
 C A New Plague (Part 4)
-N Do not accept "Letter to Silverpine"
-R Undercity
-T The Lich's Identity
+
+T The Lich's Identity |N|Back in Undercity|
 A Return the Book
-C Return the Book |N|(68, 42) On the island. Talk to the guy.|
+C Return the Book |N|Back on the island (68,42). Talk to the guy.|
 A Proving Allegiance
-N Dont forget to grab a candle from the crate |L|3080|
+N Grab a candle from the crate |L|3080|
 C Proving Allegiance
 T Proving Allegiance
 A The Prodigal Lich Returns
 C At War With The Scarlet Crusade (Part 4)
-R Brill
-T At War With The Scarlet Crusade (Part 4)
+
+T At War With The Scarlet Crusade (Part 4) |N|Back in Brill|
 C The Family Crypt |N|At Agamand Mills|
 T The Family Crypt
 T The Prodigal Lich Returns |N|Back in Undercity|
-t Dont forget your skills!
-N Go to the zeplin and fly to orgrimar
-R Razor Hill |N| Dont forget to get orgrimars FP first|
-A Conscript of the Horde
-R The Barrens
-T Conscript of the Horde
-A Crossroads Conscription
-R Crossroads
-T Crossroads Conscription
-
+t Train (Level 12)
+F Orgrimmar
+f Grab flight point
 ]]
 end)
