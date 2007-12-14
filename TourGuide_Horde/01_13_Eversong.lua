eacf59327a6a12747cbc28affa6ab4ae8d2bb330
Tekkub Stoutwrithe <tekkub@gmail.com>
2007-12-13 20:10:20 -0700

    Merge sunstrider guide into eversong, they're all the same zone

--- TourGuide_Horde/01_13_Eversong.lua
+++ TourGuide_Horde/01_13_Eversong.lua
@@ -1,5 +1,52 @@
-TourGuide:RegisterGuide("Eversong Woods (5-13)", "Ghostlands (13-20)", "Horde", function()
+TourGuide:RegisterGuide("Eversong Woods (1-13)", "Ghostlands (13-20)", "Horde", function()
 return [[
+A Reclaiming Sunstrider Isle
+C Reclaiming Sunstrider Isle
+T Reclaiming Sunstrider Isle
+A Unfortunate Measures
+
+A Warlock Training |C|Warlock|
+T Warlock Training |C|Warlock|
+A Windows to the Source |C|Warlock|
+
+t Train (Level 2)
+
+A Well Watcher Solanian
+T Well Watcher Solanian
+A The Shrine of Dath'Remar
+A Solanian's Belongings |N|Don't forget to equip the bag he gives you|
+A A Fistful of Slivers
+A Thirst Unending
+
+C Unfortunate Measures
+T Unfortunate Measures
+A Report to Lanthan Perilon
+T Report to Lanthan Perilon
+A Aggression
+
+C Aggression
+C The Shrine of Dath'Remar
+C Thirst Unending
+C A Fistful of Slivers
+C Solanian's Belongings |N|South near a crystal, Southwest near a pond, West near treants|
+
+T Thirst Unending
+T A Fistful of Slivers
+t Train (Level 4)
+T The Shrine of Dath'Remar
+T Solanian's Belongings
+T Aggression
+
+C Windows to the Source |C|Warlock|
+A Felendren the Banished
+C Felendren the Banished
+T Windows to the Source |C|Warlock|
+T Felendren the Banished
+
+A Aiding the Outrunners
+A Tainted Arcane Sliver |L|20483| |O|
+T Tainted Arcane Sliver |O|
+
 R Dawning Lane
 T Aiding the Outrunners
 A Slain by the Wretched
