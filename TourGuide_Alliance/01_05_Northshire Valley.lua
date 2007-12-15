b43e4af61e01266a8edf50a92a23fc97d4b24ec0
Tekkub Stoutwrithe <tekkub@gmail.com>
2007-12-14 21:22:17 -0700

    Cleanup of user-submitted human starting guide

--- TourGuide_Alliance/01_05_Northshire Valley.lua
+++ TourGuide_Alliance/01_05_Northshire Valley.lua
@@ -1,65 +1,60 @@
-TourGuide:RegisterGuide("Northshire Valley (1-5)", "Elwynn Forest (5-?)", "Alliance", function()
+TourGuide:RegisterGuide("Northshire Valley (1-5)", nil, "Alliance", function()
 return [[
+A A Threat Within
+T A Threat Within
+A Kobold Camp Cleanup
+A Wolves Across the Border
 
-A A Threat Within |N|From Deputy Willem|
-T A Threat Within |N|To Marshal McBride|
-A Kobold Camp Cleanup |N|From Marshal McBride|
-A Wolves Across the Border |N|From Eagan Peltskinner|
-N Refreshing Spring Water |N|Save 1 Refreshing Spring Water for later|
-C Kobold Camp Cleanup |N|Head to the Kobold Camp to the North, Kill Wolves also|
-C Wolves Across the Border |N|Kill more Wolves the get you Tough Wolf Meat|
+C Kobold Camp Cleanup |N|Kobold Camp to the North|
+C Wolves Across the Border
 
-T Wolves Across the Border |N|To Eagan Peltskinner|
-T Kobold Camp Cleanup |N|To Marshal McBride|
+T Wolves Across the Border
+T Kobold Camp Cleanup
 
-A Consecrated Letter |N|From Marshal McBride| |C|Paladin|
-A Glyphic Letter |N|From Marshal McBride| |C|Mage|
-A Encrypted Letter |N|From Marshal McBride| |C|Rogue|
-A Simple Letter |N|From Marshal McBride| |C|Warrior|
-A Hallowed Letter |N|From Marshal McBride| |C|Priest|
-A Tainted Letter |N|From Marshal McBride| |C|Warlock|
-A Investigate Echo Ridge |N|From Marshal McBride|
-A Brotherhood of Thieves |N|From Deputy Willem|
+A Consecrated Letter |C|Paladin|
+A Glyphic Letter |C|Mage|
+A Encrypted Letter |C|Rogue|
+A Simple Letter |C|Warrior|
+A Hallowed Letter |C|Priest|
+A Tainted Letter |C|Warlock|
 
-T Consecrated Letter |N|Read the Letter then go to Brother Sammuel| |C|Paladin|
-T Glyphic Letter |N|Read the Letter then go to Khelden |  |C|Mage|
-T Encrypted Letter |N|Read the Letter then go to Jorik Kerridan| |C|Rogue|
-T Simple Letter |N|Read the Letter then go to Llane Beshere| |C|Warrior|
-T Questname |N|Read the Letter then go to Priestess Anetta| |C|Priest|
-T Tainted Letter |N|Read the Letter then go to Drusilla La Salle| |C|Warlock|
-A The Stolen Tome |N|From Drusilla La Salle| |C|Warlock|
+A Investigate Echo Ridge
+A Brotherhood of Thieves
 
-t Get some Training done here!
-C Investigate Echo Ridge |N|A good location is just to to North East of the Abbey (51.25,37.25)|
-N Find the Tome [Powers of the Void] |N|The book is on the ground next to the tent (56.7,44.0). Kill the thieves in the area| |C|Warlock| |L|6785|
-C Brotherhood of Thieves |N|To the East of Abbey accross bridge|
-G Grind here till 4th Level |LV|4|
+T Consecrated Letter |C|Paladin|
+T Glyphic Letter |C|Mage|
+T Encrypted Letter |C|Rogue|
+T Simple Letter |C|Warrior|
+T Questname |C|Priest|
+T Tainted Letter |C|Warlock|
+A The Stolen Tome |C|Warlock|
 
-T The Stolen Tome |N|To Drusilla La Salle| |C|Warlock|
-T Brotherhood of Thieves |N|To Deputy Willem|
-A Milly Osworth |N|From Deputy Willem|
-A Bounty on Garrick Padfoot |N|From Deputy Willem|
+C Investigate Echo Ridge |N|Northeast of the Abbey (51.25, 37.25)|
+C The Stolen Tome |N|The book is on the ground next to the tent (56.7, 44.0)| |C|Warlock|
+C Brotherhood of Thieves |N|East of Abbey across bridge|
 
-T Investigate Echo Ridge 
-A Skirmish at Echo Ridge |N|From Marshal McBride|
-
-T Milly Osworth |N|To Milly Osworth|
-A Milly's Harvest |N|From Milly Osworth|
+T The Stolen Tome |C|Warlock|
+T Brotherhood of Thieves
+A Milly Osworth
+A Bounty on Garrick Padfoot
+T Investigate Echo Ridge
+A Skirmish at Echo Ridge
+T Milly Osworth
+A Milly's Harvest
 
 C Skirmish at Echo Ridge |N|To the north in Echo Ridge Mine|
-G Grind here till 5th Level |LV|5|
-T Skirmish at Echo Ridge |N|To Marshal McBride|
-A Report to Goldshire |N|From Marshal McBride|
 
-A In Favor of the Light |N|From Priestess Anetta| |C|Priest|
-C Milly's Harvest |N|In the Farm to the South East|
-C Bounty on Garrick Padfoot |N|At the small wooden Shed Behind the Farm (57.50,48.25)|
+T Skirmish at Echo Ridge
+A Report to Goldshire
+A In Favor of the Light |C|Priest|
 
-T Milly's Harvest |N|To Milly Osworth|
-A Grape Manifest |N|From Milly Osworth|
-T Bounty on Garrick Padfoot |N|To Deputy Willem|
-T Grape Manifest |N|To Brother Neals up stairs in the Abbey|
+C Milly's Harvest |N|In the Farm to the southeast|
+C Bounty on Garrick Padfoot |N|At the small wooden shed behind the farm (57.50, 48.25)|
 
-A Rest and Relaxation|N|From Falkhaan Isenstrider Fallow the road south|
+T Milly's Harvest
+A Grape Manifest
+T Bounty on Garrick Padfoot
+T Grape Manifest |N|To Brother Neals upstairs in the Abbey|
+A Rest and Relaxation |N|Along the road south out of the abbey|
 ]]
 end)
