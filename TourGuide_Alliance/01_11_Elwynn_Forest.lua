fd7d3489973756d571698306e4c7ca160e51b6d0
Tekkub Stoutwrithe <tekkub@gmail.com>
2007-12-16 01:58:51 -0700

    Merge Northshire Valley and Elwynn guides together (same zone)

--- TourGuide_Alliance/01_11_Elwynn_Forest.lua
+++ TourGuide_Alliance/01_11_Elwynn_Forest.lua
@@ -1,23 +1,81 @@
-TourGuide:RegisterGuide("Elwynn Forest (5-11)", "Westfall (11-15)", "Alliance", function()
+
+TourGuide:RegisterGuide("Elwynn Forest (1-11)", nil, "Alliance", function()
 return [[
+A A Threat Within
+T A Threat Within
+A Kobold Camp Cleanup |N|Save your Refreshing Spring Water for later| |C|Warrior| |C|Paladin| |C|Rogue| |C|Warlock|
+A Kobold Camp Cleanup
+A Wolves Across the Border
+
+C Kobold Camp Cleanup |N|Kobold Camp to the North|
+C Wolves Across the Border
+
+T Wolves Across the Border
+T Kobold Camp Cleanup
+
+A Consecrated Letter |C|Paladin|
+A Glyphic Letter |C|Mage|
+A Encrypted Letter |C|Rogue|
+A Simple Letter |C|Warrior|
+A Hallowed Letter |C|Priest|
+A Tainted Letter |C|Warlock|
+
+A Investigate Echo Ridge
+A Brotherhood of Thieves
+
+T Consecrated Letter |C|Paladin|
+T Glyphic Letter |C|Mage|
+T Encrypted Letter |C|Rogue|
+T Simple Letter |C|Warrior|
+T Questname |C|Priest|
+T Tainted Letter |C|Warlock|
+A The Stolen Tome |C|Warlock|
+
+C Investigate Echo Ridge |N|Northeast of the Abbey (51.25, 37.25)|
+C The Stolen Tome |N|The book is on the ground next to the tent (56.7, 44.0)| |C|Warlock|
+C Brotherhood of Thieves |N|East of Abbey accross bridge|
+
+T The Stolen Tome |C|Warlock|
+T Brotherhood of Thieves
+A Milly Osworth
+A Bounty on Garrick Padfoot
+T Investigate Echo Ridge
+A Skirmish at Echo Ridge
+T Milly Osworth
+A Milly's Harvest
+
+C Skirmish at Echo Ridge |N|To the north in Echo Ridge Mine|
+
+T Skirmish at Echo Ridge
+A Report to Goldshire
+A In Favor of the Light |C|Priest|
+
+C Milly's Harvest |N|In the Farm to the Southeast|
+C Bounty on Garrick Padfoot |N|At the small wooden shed behind the farm (57.50, 48.25)|
+
+T Milly's Harvest
+A Grape Manifest
+T Bounty on Garrick Padfoot
+T Grape Manifest
+A Rest and Relaxation |N|Along the road south out of Valley|
 
 T Report to Goldshire |N|Fallow the road south to Goldshire|
-A The Fargodeep Mine 
+A The Fargodeep Mine
 A Gold Dust Exchange
 
 T Rest and Relaxation
 h Goldshire
 
-A Kobold Candles 
+A Kobold Candles
 N Learn First Aid |N|Up Stair From Michelle Belle <Physician> You'll need it!|
 T Favor of the Light |C|Priest|
 A Garments of the Light |C|Priest|
 C Garments of the Light |N|East south shore of Crystal Lake (48.2, 68.0)| |C|Priest|
 T Garments of the Light |C|Priest|
 
-R Stormwind City |N|Go north Left at Y (32.50, 50.35)| 
-A Harlan Needs A Resupply 
-T Harlan Needs A Resupply 
+R Stormwind City |N|Go north Left at Y (32.50, 50.35)|
+A Harlan Needs A Resupply
+T Harlan Needs A Resupply
 A Package for Thurman
 T Wine Shop Advert
 T Package for Thurman
@@ -25,7 +83,7 @@ T Package for Thurman
 H Lion's Pride Inn |N|Or Run to Goldshire if for some reason you can't Hearth|
 B [Refreshing Spring Water] |N|If you don't save Any! You can to trade them in for Apples later| |C|Warrior| |C|Paladin| |C|Rogue| |C|Warlock|
 
-A Lost Necklace |N|South of Goldshire West of Fargodeep Mine (35.5, 84,3)| 
+A Lost Necklace |N|South of Goldshire West of Fargodeep Mine (35.5, 84,3)|
 A Princess Must Die!
 
 T Give Gerard a Drink |N|Maclure Vineyards south of Goldshire Trade your Water for Apples! (43.1,85.6)| |C|Warrior|  |C|Rogue|
@@ -33,7 +91,7 @@ T Give Gerard a Drink |N|Maclure Vineyards south of Goldshire if you want Apples
 
 T Lost Necklace
 A Pie for Billy
-A Young Lovers 
+A Young Lovers
 C Pie for Billy
 T Pie for Billy
 A Back to Billy
@@ -59,7 +117,7 @@ C Goldtooth
 T Goldtooth
 
 T Gold Dust Exchange |N|Back to Goldshire|
-A A Fishy Peril 
+A A Fishy Peril
 T The Fargodeep Mine
 A The Jasperlode Mine
 T A Fishy Peril
@@ -71,36 +129,36 @@ C The Jasperlode Mine |N|North half way between Goldshire and Logging Camp (60.5
 N Grind here till 9th Level |LV|9|
 T Further Concerns |N|Along Road East of Goldshire (73.9, 72.2)|
 A Find the Lost Guards
-A Protect the Frontier 
+A Protect the Frontier
 
 A Red Linen Goods |N|East you'll see a road to the north (79.5, 68.7)|
 A A Bundle of Trouble |N|Logging Camp (81.4, 66.1)|
 
 T Find the Lost Guards |N|Half-eaten body west side of the Rivers mouth to the north. (72.7, 60.3)|
-A Discover Rolf's Fate 
+A Discover Rolf's Fate
 C A Bundle of Trouble |N|North of Lumber Camp|
 
 T Discover Rolf's Fate |N|North of Logging Camp Middle of Murloc Village. You may need help! (79.8, 55.6)|
-A Report to Thomas 
+A Report to Thomas
 C Protect the Frontier
 
-T A Bundle of Trouble 
+T A Bundle of Trouble
 T Report to Thomas
 A Deliver Thomas' Report
 T Protect the Frontier
 
 H Goldshire |N|Or Run to Goldshire if you can't Hearth|
 T Deliver Thomas' Report
-A Cloth and Leather Armor 
+A Cloth and Leather Armor
 T The Jasperlode Mine
 A Westbrook Garrison Needs Help!
 T Westbrook Garrison Needs Help!|N|Along road west of Goldshire (24.3, 74.6)|
 A Riverpaw Gnoll Bounty
-A Wanted:  "Hogger" |N|Poster in front of Garrison (24.50, 74.70)| 
+A Wanted:  "Hogger" |N|Poster in front of Garrison (24.50, 74.70)|
 
 A The Collector |N|If you found Gold Pickup Schedule off the Gnolls| |U|1307| |O| |L|1307|
 C Riverpaw Gnoll Bounty |N|Gruff Swiffbitr patrolling a Gnoll camp (26, 94)|
-T Riverpaw Gnoll Bounty 
+T Riverpaw Gnoll Bounty
 T The Collector |O| |L|2223|
 A Manhunt
 A A Warrior's Training |C|Warrior|
