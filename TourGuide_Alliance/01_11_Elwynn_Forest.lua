b9fdad15e83ffb0abb592c3c07adddf9c8037590
Tekkub Stoutwrithe <tekkub@gmail.com>
2007-12-16 02:03:43 -0700

    Fixes to user-submitted hoomon guide

--- TourGuide_Alliance/01_11_Elwynn_Forest.lua
+++ TourGuide_Alliance/01_11_Elwynn_Forest.lua
@@ -1,9 +1,7 @@
-
 TourGuide:RegisterGuide("Elwynn Forest (1-11)", nil, "Alliance", function()
 return [[
 A A Threat Within
 T A Threat Within
-A Kobold Camp Cleanup |N|Save your Refreshing Spring Water for later| |C|Warrior| |C|Paladin| |C|Rogue| |C|Warlock|
 A Kobold Camp Cleanup
 A Wolves Across the Border
 
@@ -33,7 +31,7 @@ A The Stolen Tome |C|Warlock|
 
 C Investigate Echo Ridge |N|Northeast of the Abbey (51.25, 37.25)|
 C The Stolen Tome |N|The book is on the ground next to the tent (56.7, 44.0)| |C|Warlock|
-C Brotherhood of Thieves |N|East of Abbey accross bridge|
+C Brotherhood of Thieves |N|East of Abbey across bridge|
 
 T The Stolen Tome |C|Warlock|
 T Brotherhood of Thieves
@@ -50,45 +48,36 @@ T Skirmish at Echo Ridge
 A Report to Goldshire
 A In Favor of the Light |C|Priest|
 
-C Milly's Harvest |N|In the Farm to the Southeast|
+C Milly's Harvest |N|In the farm to the southeast|
 C Bounty on Garrick Padfoot |N|At the small wooden shed behind the farm (57.50, 48.25)|
 
 T Milly's Harvest
 A Grape Manifest
 T Bounty on Garrick Padfoot
-T Grape Manifest
-A Rest and Relaxation |N|Along the road south out of Valley|
+T Grape Manifest |N|To Brother Neals upstairs in the Abbey|
+A Rest and Relaxation |N|Along the road south out of the valley|
 
-T Report to Goldshire |N|Fallow the road south to Goldshire|
+T Report to Goldshire |N|Follow the road south to Goldshire|
 A The Fargodeep Mine
 A Gold Dust Exchange
-
 T Rest and Relaxation
 h Goldshire
-
 A Kobold Candles
-N Learn First Aid |N|Up Stair From Michelle Belle <Physician> You'll need it!|
 T Favor of the Light |C|Priest|
 A Garments of the Light |C|Priest|
-C Garments of the Light |N|East south shore of Crystal Lake (48.2, 68.0)| |C|Priest|
+C Garments of the Light |N|Southeast shore of Crystal Lake (48.2, 68.0)| |C|Priest|
 T Garments of the Light |C|Priest|
 
-R Stormwind City |N|Go north Left at Y (32.50, 50.35)|
+R Stormwind City |N|Follow the road northwest|
 A Harlan Needs A Resupply
 T Harlan Needs A Resupply
 A Package for Thurman
 T Wine Shop Advert
 T Package for Thurman
 
-H Lion's Pride Inn |N|Or Run to Goldshire if for some reason you can't Hearth|
-B [Refreshing Spring Water] |N|If you don't save Any! You can to trade them in for Apples later| |C|Warrior| |C|Paladin| |C|Rogue| |C|Warlock|
-
+H Lion's Pride Inn
 A Lost Necklace |N|South of Goldshire West of Fargodeep Mine (35.5, 84,3)|
 A Princess Must Die!
-
-T Give Gerard a Drink |N|Maclure Vineyards south of Goldshire Trade your Water for Apples! (43.1,85.6)| |C|Warrior|  |C|Rogue|
-T Give Gerard a Drink |N|Maclure Vineyards south of Goldshire if you want Apples for your Water. If not skip this one (43.1,85.6)| |C|Paladin| |C|Warlock| |C|Mage| |C|Priest|
-
 T Lost Necklace
 A Pie for Billy
 A Young Lovers
@@ -103,15 +92,15 @@ A Note to William
 T Note to William |N|Goldshire Inn|
 A Collecting Kelp
 C Collecting Kelp |N|East of Goldshire|
-T Collecting Kelp |N|Wait for fallowup Quest!|
+T Collecting Kelp
 A The Escape
 
 T Back to Billy
 A Goldtooth
-T The Escape |N|Maclure Vineyards (43.1, 89.6)|
+T The Escape |N|At the Maclure Vineyards (43.1, 89.6)|
 
-C The Fargodeep Mine |N|Fargodeep Mine (39.60, 80.30)|
-C Gold Dust Exchange |N|Kobold carry Gold Dust|
+C The Fargodeep Mine |N|At the Fargodeep Mine (39.60, 80.30)|
+C Gold Dust Exchange
 C Kobold Candles
 C Goldtooth
 T Goldtooth
@@ -126,19 +115,18 @@ T Kobold Candles
 A Shipment to Stormwind
 
 C The Jasperlode Mine |N|North half way between Goldshire and Logging Camp (60.5, 50.1)|
-N Grind here till 9th Level |LV|9|
 T Further Concerns |N|Along Road East of Goldshire (73.9, 72.2)|
 A Find the Lost Guards
 A Protect the Frontier
 
 A Red Linen Goods |N|East you'll see a road to the north (79.5, 68.7)|
-A A Bundle of Trouble |N|Logging Camp (81.4, 66.1)|
+A A Bundle of Trouble |N|At the Logging Camp (81.4, 66.1)|
 
 T Find the Lost Guards |N|Half-eaten body west side of the Rivers mouth to the north. (72.7, 60.3)|
 A Discover Rolf's Fate
 C A Bundle of Trouble |N|North of Lumber Camp|
 
-T Discover Rolf's Fate |N|North of Logging Camp Middle of Murloc Village. You may need help! (79.8, 55.6)|
+T Discover Rolf's Fate |N|North of Logging Camp in the middle of a murloc village (79.8, 55.6)|
 A Report to Thomas
 C Protect the Frontier
 
@@ -147,24 +135,23 @@ T Report to Thomas
 A Deliver Thomas' Report
 T Protect the Frontier
 
-H Goldshire |N|Or Run to Goldshire if you can't Hearth|
+H Goldshire
 T Deliver Thomas' Report
 A Cloth and Leather Armor
 T The Jasperlode Mine
 A Westbrook Garrison Needs Help!
 T Westbrook Garrison Needs Help!|N|Along road west of Goldshire (24.3, 74.6)|
 A Riverpaw Gnoll Bounty
-A Wanted:  "Hogger" |N|Poster in front of Garrison (24.50, 74.70)|
+A Wanted: "Hogger" |N|Poster in front of Garrison (24.50, 74.70)|
 
-A The Collector |N|If you found Gold Pickup Schedule off the Gnolls| |U|1307| |O| |L|1307|
-C Riverpaw Gnoll Bounty |N|Gruff Swiffbitr patrolling a Gnoll camp (26, 94)|
+C Riverpaw Gnoll Bounty |N|Gruff Swiffbitr patrolling a Gnoll camp (26,94)|
 T Riverpaw Gnoll Bounty
-T The Collector |O| |L|2223|
+A The Collector |O| |U|1307|
+T The Collector |O|
 A Manhunt
 A A Warrior's Training |C|Warrior|
 
-R Stormwind City |C|Warrior|
-T A Warrior's Training |N|Old Town| |C|Warrior|
+T A Warrior's Training |N|In Old Town, Stormwind| |C|Warrior|
 A Bartleby the Drunk |C|Warrior|
 T Bartleby the Drunk |C|Warrior|
 A Beat Bartleby |C|Warrior|
@@ -173,14 +160,14 @@ A Bartleby's Mug |C|Warrior|
 T Bartleby's Mug |C|Warrior|
 A Marshal Haggard |C|Warrior|
 
-H Lion's Pride Inn |N|Or Run to Goldshire if you can't Hearth| |C|Warrior|
-T Marshal Haggard |N|Logging Camp (84.6,69.4)| |C|Warrior|
+H Lion's Pride Inn |C|Warrior|
+T Marshal Haggard |N|Logging Camp (84.6, 69.4)| |C|Warrior|
 A Dead-tooth Jack |C|Warrior|
 
-A Furlbrow's Deed |N|If you found The Westfall Deed off the Defias| |U|1972| |O| |L|1972|
-C Princess Must Die!|N|Pumpkin Patch farm (69, 79)|
+A Furlbrow's Deed |U|1972| |O|
+C Princess Must Die!|N|Pumpkin Patch farm (69,79)|
 C Manhunt |N|In Pumpkin Patch farm house|
-C Red Linen Goods |N|Brackwell Pumpkin Patch (69, 79)|
+C Red Linen Goods |N|Brackwell Pumpkin Patch (69,79)|
 
 A Bounty on Murlocs |N|Guard Thomas (73.9, 72.2)|
 C Bounty on Murlocs |N|North (76.75, 58.75)|
@@ -190,14 +177,14 @@ T Red Linen Goods
 T Bounty on Murlocs
 
 C Dead-tooth Jack |N|Get his Key! Camp (89.2,78.7)| |C|Warrior|
-T Dead-tooth Jack | |C|Warrior|
+T Dead-tooth Jack |C|Warrior|
 
-H Lion's Pride Inn |N|Or Run to Goldshire if you can't Hearth|
+H Lion's Pride Inn
 T Princess Must Die! |N|Farm far West (34.6, 84.4)|
-C Wanted:  "Hogger" |N|South of Westbrook Garrison Yon May Need Help (27, 88)|
+C Wanted: "Hogger" |N|South of Westbrook Garrison (27,88)|
 
 T Manhunt
-T Wanted:  "Hogger"
+T Wanted: "Hogger"
 A Report to Gryan Stoutmantle
 A Elmore's Task
 ]]
