d73808be034662924f688e7dd2b1d76b4457d71c
Tekkub Stoutwrithe <tekkub@gmail.com>
2007-12-20 12:53:54 -0700

    Human crap, actually leveling a stupid human, bleh...
    Stupid Hogger quest has two stupid spaces in it aparently.  GG Blue

--- TourGuide_Alliance/01_11_Elwynn_Forest.lua
+++ TourGuide_Alliance/01_11_Elwynn_Forest.lua
@@ -3,9 +3,11 @@ return [[
 A A Threat Within
 T A Threat Within
 A Kobold Camp Cleanup
+A Eagan Peltskinner
+T Eagan Peltskinner
 A Wolves Across the Border
 
-C Kobold Camp Cleanup |N|Kobold Camp to the North|
+C Kobold Camp Cleanup |N|Kobold camps to the North|
 C Wolves Across the Border
 
 T Wolves Across the Border
@@ -25,36 +27,33 @@ T Consecrated Letter |C|Paladin|
 T Glyphic Letter |C|Mage|
 T Encrypted Letter |C|Rogue|
 T Simple Letter |C|Warrior|
-T Questname |C|Priest|
+T Hallowed Letter |C|Priest|
 T Tainted Letter |C|Warlock|
 A The Stolen Tome |C|Warlock|
 
-C Investigate Echo Ridge |N|Northeast of the Abbey (51.25, 37.25)|
+C Investigate Echo Ridge |N|Back at the kobold camps|
 C The Stolen Tome |N|The book is on the ground next to the tent (56.7, 44.0)| |C|Warlock|
-C Brotherhood of Thieves |N|East of Abbey across bridge|
+C Brotherhood of Thieves |N|East of the abbey across bridge|
 
 T The Stolen Tome |C|Warlock|
 T Brotherhood of Thieves
 A Milly Osworth
-A Bounty on Garrick Padfoot
 T Investigate Echo Ridge
 A Skirmish at Echo Ridge
 T Milly Osworth
 A Milly's Harvest
 
-C Skirmish at Echo Ridge |N|To the north in Echo Ridge Mine|
-
-T Skirmish at Echo Ridge
-A Report to Goldshire
-A In Favor of the Light |C|Priest|
-
+C Skirmish at Echo Ridge |N|To the north in Echo Ridge Mine (47.65, 32.00)|
 C Milly's Harvest |N|In the farm to the southeast|
 C Bounty on Garrick Padfoot |N|At the small wooden shed behind the farm (57.50, 48.25)|
 
 T Milly's Harvest
 A Grape Manifest
+T Skirmish at Echo Ridge
+A Report to Goldshire
 T Bounty on Garrick Padfoot
-T Grape Manifest |N|To Brother Neals upstairs in the Abbey|
+A In Favor of the Light |C|Priest|
+T Grape Manifest |N|Upstairs in the abbey|
 A Rest and Relaxation |N|Along the road south out of the valley|
 
 T Report to Goldshire |N|Follow the road south to Goldshire|
@@ -63,35 +62,28 @@ A Gold Dust Exchange
 T Rest and Relaxation
 h Goldshire
 A Kobold Candles
-T Favor of the Light |C|Priest|
+T In Favor of the Light |C|Priest|
 A Garments of the Light |C|Priest|
 C Garments of the Light |N|Southeast shore of Crystal Lake (48.2, 68.0)| |C|Priest|
 T Garments of the Light |C|Priest|
 
-R Stormwind City |N|Follow the road northwest|
-A Harlan Needs A Resupply
-T Harlan Needs A Resupply
-A Package for Thurman
-T Wine Shop Advert
-T Package for Thurman
-
-H Lion's Pride Inn
-A Lost Necklace |N|South of Goldshire West of Fargodeep Mine (35.5, 84,3)|
-A Princess Must Die!
-T Lost Necklace
+A Lost Necklace |N|Take the road west out of town, then south to the Stonefield Farm (34.5, 84.3)|
+T Lost Necklace |N|East at the Maclure Vineyard (43.13, 85.73)|
 A Pie for Billy
-A Young Lovers
+
+A Young Lovers |N|In the house at the Maclure Vineyard|
 C Pie for Billy
-T Pie for Billy
+T Pie for Billy |N|Back at the Stonefield Farm|
 A Back to Billy
+A Princess Must Die!
 T Young Lovers |N|West of farm by the water (29.8, 86.0)|
-A Speak with Gramma
+A Speak with Gramma |N|Back to Farm House|
 T Speak with Gramma
 A Note to William
 
-T Note to William |N|Goldshire Inn|
+T Note to William |N|Back at the Goldshire inn|
 A Collecting Kelp
-C Collecting Kelp |N|East of Goldshire|
+C Collecting Kelp |N|To the east around the Crystal Lake|
 T Collecting Kelp
 A The Escape
 
@@ -99,12 +91,12 @@ T Back to Billy
 A Goldtooth
 T The Escape |N|At the Maclure Vineyards (43.1, 89.6)|
 
-C The Fargodeep Mine |N|At the Fargodeep Mine (39.60, 80.30)|
+C Goldtooth |N|Deep inside the Fargodeep Mine (41.60, 77.90).  Take the upper entrance to the mine for faster access.|
 C Gold Dust Exchange
 C Kobold Candles
-C Goldtooth
-T Goldtooth
+C The Fargodeep Mine
 
+T Goldtooth
 T Gold Dust Exchange |N|Back to Goldshire|
 A A Fishy Peril
 T The Fargodeep Mine
@@ -139,17 +131,42 @@ H Goldshire
 T Deliver Thomas' Report
 A Cloth and Leather Armor
 T The Jasperlode Mine
-A Westbrook Garrison Needs Help!
-T Westbrook Garrison Needs Help!|N|Along road west of Goldshire (24.3, 74.6)|
-A Riverpaw Gnoll Bounty
-A Wanted: "Hogger" |N|Poster in front of Garrison (24.50, 74.70)|
 
-C Riverpaw Gnoll Bounty |N|Gruff Swiffbitr patrolling a Gnoll camp (26,94)|
-T Riverpaw Gnoll Bounty
-A The Collector |O| |U|1307|
-T The Collector |O|
-A Manhunt
+N YOU BE 10???
+
+R Stormwind City |N|Follow the road northwest|
+A Wine Shop Advert
+A Harlan Needs a Resupply
+T Harlan Needs a Resupply
+A Package for Thurman
+T Wine Shop Advert
+T Package for Thurman
+
+H Goldshire
+N 53 min on hearth |N|22:19|
+
+
+A Speak with Jennea |N|In the Goldshire inn| |C|Mage|
+A Seek out SI: 7 |N|In the Goldshire inn| |C|Rogue|
+A Gakin's Summons |N|In the Goldshire inn| |C|Warlock|
 A A Warrior's Training |C|Warrior|
+
+T Shipment to Stormwind |N|In the Trade District, Stormwind| |C|Mage|
+T Shipment to Stormwind |N|In the Trade District, Stormwind| |C|Rogue|
+T Shipment to Stormwind |N|In the Trade District, Stormwind| |C|Warlock|
+T Shipment to Stormwind |N|In the Trade District, Stormwind| |C|Warrior|
+
+T Speak with Jennea |N|In Mage Quarter, Stormwind| |C|Mage|
+A Mirror Lake |C|Mage|
+R Elwynn Forest |C|Mage|
+C Mirror Lake |N|Fill Flask at Mirror Lake (28.6, 61.4)| |U|7207| |C|Mage|
+T Mirror Lake |N|In Mage Quarter, Stormwind| |C|Mage|
+
+T Seek out SI: 7 |N|In Old Town, Stormwind| |C|Rogue|
+A Snatch and Grab |C|Rogue|
+
+T Gakin's Summons |N|In Mage Quarter, Stormwind| |C|Warlock|
+A Surena Caledon |C|Warlock|
 
 T A Warrior's Training |N|In Old Town, Stormwind| |C|Warrior|
 A Bartleby the Drunk |C|Warrior|
@@ -160,13 +177,19 @@ A Bartleby's Mug |C|Warrior|
 T Bartleby's Mug |C|Warrior|
 A Marshal Haggard |C|Warrior|
 
-H Lion's Pride Inn |C|Warrior|
-T Marshal Haggard |N|Logging Camp (84.6, 69.4)| |C|Warrior|
+H Goldshire |C|Mage|
+H Goldshire |C|Rogue|
+H Goldshire |C|Warlock|
+H Goldshire |C|Warrior|
+
+C Snatch and Grab |N|Sap & Pickpocket the Dockmaster at Jerod's Landing (48,87)| |C|Rogue|
+
+T Marshal Haggard |N|In Logging Camp (84.6, 69.4)| |C|Warrior|
 A Dead-tooth Jack |C|Warrior|
 
 A Furlbrow's Deed |U|1972| |O|
 C Princess Must Die!|N|Pumpkin Patch farm (69,79)|
-C Manhunt |N|In Pumpkin Patch farm house|
+C Surena Caledon |N|In House at Pumpkin Patch (71,80)| |C|Warlock|
 C Red Linen Goods |N|Brackwell Pumpkin Patch (69,79)|
 
 A Bounty on Murlocs |N|Guard Thomas (73.9, 72.2)|
@@ -176,15 +199,29 @@ T Cloth and Leather Armor
 T Red Linen Goods
 T Bounty on Murlocs
 
-C Dead-tooth Jack |N|Get his Key! Camp (89.2,78.7)| |C|Warrior|
-T Dead-tooth Jack |C|Warrior|
+T Snatch and Grab |N|SW Old Town| |C|Rogue|
+T Surena Caledon |N|SW Mage Quarter| |C|Warlock|
+A The Binding |C|Warlock|
+C The Binding |N|Summoning circle downstairs| |U|6928| |C|Warlock|
+T The Binding |C|Warlock|
+C Dead-tooth Jack |N|Get his Key! Camp (89.2, 78.7)| |C|Warrior|
+T Dead-tooth Jack |N|Back to Logging Camp| |C|Warrior|
 
-H Lion's Pride Inn
-T Princess Must Die! |N|Farm far West (34.6, 84.4)|
-C Wanted: "Hogger" |N|South of Westbrook Garrison (27,88)|
+H Goldshire
+T Princess Must Die! |N|Farm far west (34.6, 84.4)|
 
-T Manhunt
-T Wanted: "Hogger"
+A Westbrook Garrison Needs Help!
+T Westbrook Garrison Needs Help! |N|Along the road west of Goldshire (24.3, 74.6)|
+A Riverpaw Gnoll Bounty
+A Wanted:  "Hogger" |N|Poster in front of Garrison (24.50, 74.70)|
+
+C Wanted:  "Hogger" |N|South of Westbrook Garrison (27,88)|
+C Riverpaw Gnoll Bounty
+
+A The Collector |U|1307| |O|
+T Riverpaw Gnoll Bounty
+T The Collector |O|
+T Wanted:  "Hogger"
 A Report to Gryan Stoutmantle
 A Elmore's Task
 ]]
