b88e220ae3fe06686fd590c02d19a8c8751f938b
Tekkub Stoutwrithe <tekkub@gmail.com>
2007-12-28 22:40:59 -0700

    User tweakings to Ally Loch

--- TourGuide_Alliance/17_18_Loch_Modan.lua
+++ TourGuide_Alliance/17_18_Loch_Modan.lua
@@ -1,22 +1,27 @@
 TourGuide:RegisterGuide("Loch Modan (17-18)", "Redridge Mountains (18-20)", "Alliance", function()
 return [[
-R Loch Modan |N|Boat to Menethil Harbor, grab the flight point there, then run thru Wetlands to Loch.|
-R Algaz Station |N|In Loch Modan. Watch out the big bad highlevel mobs on the way. Good luck!|
+F Loch Modan |N|Boat to Menethil Harbor, then fly there!| |R|Human|
+F Loch Modan |N|Boat to Menethil Harbor, then fly there!| |R|Dwarf|
+R Loch Modan |N|Boat to Menethil Harbor, grab the flight point there, then run thru Wetlands to Loch.| |R|Night Elf|
+F Loch Modan |N|Boat to Menethil Harbor, then fly there!| |R|Gnome|
+R Loch Modan |N|Boat to Menethil Harbor, grab the flight point there, then run thru Wetlands to Loch.| |R|Draenei|
+R Algaz Station |N|In the north of Loch Modan. Watch out the big bad highlevel mobs on the way. Good luck!|
 
 A Filthy Paws |N|Upstairs, inside the tower|
 A Stormpike's Order
 C Filthy Paws |N|In the mine east of the tower|
 T Filthy Paws
 R Thelsamar
-f Grab flight point
+f Grab flight point |R|Night Elf|
+f Grab flight point |R|Draenei|
 A Ironband's Excavation |N|Downstairs, Last house|
-R Ironband's Excavation Site
+R Ironband's Excavation Site |N|(64.9, 66.6)|
 T Ironband's Excavation
 A Gathering Idols
 A Excavation Progress Report
 C Gathering Idols
 T Gathering Idols
-R Farstrider Lodge |N|East of Excavation site|
+R The Farstrider Lodge |N|East of Excavation site (83.8, 62.4)|
 A Crocolisk Hunting
 A A Hunter's Boast
 C A Hunter's Boast
@@ -25,7 +30,7 @@ A A Hunter's Challenge
 C A Hunter's Challenge
 T A Hunter's Challenge
 A Vyrin's Revenge (Part 1)
-A Bingles' Missing Supplies |N|The NPC can be found at the easternmost shore of The Loch|
+A Bingles' Missing Supplies |N|The NPC can be found at the easternmost shore of The Loch (63.5, 47.9)|
 C Crocolisk Hunting
 C Bingles' Missing Supplies |N|The supplies are scattered around northernmost islands. Blastencapper (54.21,26.61), Hammer (51.78,24.09), Screwdriver (48.37,20.50), Wrench (48.73,30.10)|
 R Stonewrought Dam
@@ -36,19 +41,27 @@ T A Dark Threat Looms (Part 2)
 R Thelsamar
 T Excavation Progress Report
 A Report to Ironforge
-C Vyrin's Revenge (Part 1) |N|Cave top of the mountain south of Thelsamar|
+C Vyrin's Revenge (Part 1) |N|Cave top of the mountain south of Thelsamar (39.7, 66.6)|
 R Farstrider Lodge
 T Crocolisk Hunting
 T Vyrin's Revenge (Part 1)
 A Vyrin's Revenge (Part 2)
 T Vyrin's Revenge (Part 2)
 T Bingles' Missing Supplies |N|Remember Bingles at the shore?|
-R City of Ironforge
-f Grab flight point
+F City of Ironforge |R|Human|
+F City of Ironforge |R|Dwarf|
+R City of Ironforge |R|Night Elf|
+f Grab flight point |R|Night Elf|
+F City of Ironforge |R|Gnome|
+R City of Ironforge |R|Draenei|
+f Grab flight point |R|Draenei|
 T Report to Ironforge |N|Hall of Explorers. Skip the next quest he tries to give you|
 R Stormwind City |N|Use the Deeprun Tram|
 T Stormpike's Order
-f Grab flight point
+f Grab flight point |R|Dwarf|
+f Grab flight point |R|Night Elf|
+f Grab flight point |R|Gnome|
+f Grab flight point |R|Draenei|
 R Redridge Mountains
 ]]
 end)
