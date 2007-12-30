e4d0edc71698a0bbf43433d0f7b1d047b5105d5b
Tekkub Stoutwrithe <tekkub@gmail.com>
2007-12-30 04:55:51 -0700

    More Shadowmoon crap

--- TourGuide_Alliance/70_Shadowmoon_Valley_Aldor.lua
+++ TourGuide_Alliance/70_Shadowmoon_Valley_Aldor.lua
@@ -15,7 +15,7 @@ A The Hand of Gul'dan
 A Put On Yer Kneepads...
 A Spleendid!
 
-N Kill crap... |N|Kill any Felboars, Vilewing Chimaeras and Scorchshell Pincers you come across for "Put On Yer Kneepads..."  Also loot any [Ever-burning Ash] you see, and kill Diemetradons fo "Spleendid!"|
+N Kill crap... |N|Kill any Felboars, Vilewing Chimaeras and Scorchshell Pincers you come across for "Put On Yer Kneepads..."  Also loot any [Ever-burning Ash] you see, and kill Diemetradons for "Spleendid!"|
 T The Hand of Gul'dan |N|Northeast at the Altar of Damnation (42,45)|
 A Enraged Spirits of Fire and Earth
 C Enraged Spirits of Fire and Earth |N|Use the totem, then kill the elementals near it| |U|30094|
@@ -24,14 +24,12 @@ A Enraged Spirits of Water
 C Enraged Spirits of Water |N|Kill water elementals to the northeast at Coilskar Point (48,24).  Use the totem, same as before.| |U|30094|
 
 C The Sketh'lon Wreckage |N|Northeast at the Sketh'lon Wreckage (37,30)|
-C Put On Yer Kneepads... |N|Southwest at the Magma Fields (32,39)|
-C Spleendid!
+C Spleendid! |N|Near any of the green lava cracks on the west side of Hand of Gul'dan|
 C A Ghost in the Machine
 
 C To Legion Hold |N|Northwest at Legion Hold (23,36).  Find the Legion Communication Device up in the building and use your "disguise".| |U|30638|
 
 H Wildhammer Stronghold
-T Put On Yer Kneepads...
 T A Ghost in the Machine
 A Harbingers of Shadowmoon
 T Spleendid!
@@ -58,14 +56,17 @@ C Asghar's Totem |N|Kill Asghar (40,31)|
 T Asghar's Totem
 A The Rod of Lianthe
 
-C Setting Up the Bomb |N|Back at Legion Hold.  Armor Plating is near the fel reaver (22,35), Power Core is underwater (26,41).|
+K Overseer Ripsaw |N|Back at Legion Hold.  Near the fel reaver (22,35)| |L|30756|
+C Setting Up the Bomb |N|Armor Plating is near the fel reaver (22,35), Power Core is underwater (26,41).|
 
 T Setting Up the Bomb |N|Back at Wildhammer Stronghold|
 A Blast the Infernals!
 T The Second Course...
 A The Main Course!
+A Illidari-Bane Shard |U|30756|
+T Illidari-Bane Shard
 
-C Blast the Infernals! |N|Again at Legion Hold.  Find the green circle (22,39), use the bomb.| |U|30614|
+C Blast the Infernals! |N|Again at Legion Hold.  Find the green circle near the infernal ranks (22,39), use the bomb.| |U|30614|
 
 H Wildhammer Stronghold
 T Blast the Infernals!
@@ -74,9 +75,14 @@ A The Deathforge
 C The Rod of Lianthe |N|Kill Blood Elves at Eclipse Point to the southeast for the key.  Strongbox is east of the elite (47,71).|
 C Enraged Spirits of Air |N|Take the road east to the Netherwing Fields, drop totem, kill air elementals.| |U|30094|
 
-T Teron Gorefiend - Lore and Legend |N|South (58,70).  Skip the follow-ups.|
+T Teron Gorefiend - Lore and Legend |N|South (58,70)|
+A Divination: Gorefiend's Cloak
+A Divination: Gorefiend's Truncheon
 
 C The Main Course! |N|Kill Felspine the Greater north of the Warden's Cage (56,43)|
+U Put on your Spectrecles |U|30719|
+N Keep an eye out... |N|Watch the main road around the Hand of Gul'dan for Ghostrider of Karabor.  If you see them kill one for "Divination: Gorefiend's Truncheon".  You need your Spectrecles to see them!|
+C Divination: Gorefiend's Cloak |N|Kill Clerics of Karabor in front of the Black Temple|
 
 A Tablets of Baa'ri |N|North at the Altar of Sha'tar (61,30)|
 A The Ashtongue Tribe
@@ -84,21 +90,134 @@ A Karabor Training Grounds
 
 C The Ashtongue Tribe |N|South at the Ruins of Baa'ri|
 C Tablets of Baa'ri
+C Karabor Training Ground |N|Inside the Black Temple, take the first right (68,50).  Kill Demon Hunter Supplicants and Initiates, try to catch them after a duel when they are low on HP.|
 
-C Karabor Training Ground |N|Inside the Black Temple, take teh first right (68,50).  Kill Demon Hunter Supplicants and Initiates, try to catch them after a duel when they are low on HP.|
-
-T Tablets of Baa'ri |N|Back at the Altar of Sha'tar, skip the follow-up.|
-T The Ashtongue Tribe |N|Skip the follow-up|
-
-N Add more quests here for Altar of Sha'tar
+T Tablets of Baa'ri |N|Back at the Altar of Sha'tar|
+A Oronu the Elder
+T The Ashtongue Tribe
+T Karabor Training Grounds
 
 F Wildhammer Stronghold
 T The Main Course!
 T Enraged Spirits of Air |N|Back at the Altar of Damnation|
-T The Deathforge |N|North at Deathforge Tower (40,41).  Skip the follow-up.|
-T The Rod of Lianthe
+A Oronok Torn-heart
 
-N Guide end here?  What the hell?
+F Altar of Sha'tar
+T Oronok Torn-heart |N|To the west, above the mine|
+A I Was A Lot Of Things...
+C I Was A Lot Of Things... |N|(52,15)|
+T I Was A Lot Of Things...
+A A Lesson Learned
+C A Lesson Learned |N|(57,20)|
+T A Lesson Learned
+A The Cipher of Damnation - Truth and History
+C The Cipher of Damnation - Truth and History
+T The Cipher of Damnation - Truth and History
+A Grom'tor, Son of Oronok
+A Ar'tor, Son of Oronok
+A Borak, Son of Oronok
+
+T The Deathforge |N|North at Deathforge Tower (40,41)|
+A Minions of the Shadow Council
+C Minions of the Shadow Council |N|(38,35)|
+T Minions of the Shadow Council
+A The Summoning Chamber
+A The Fate of Flanis
+C The Summoning Chamber |N|Kill Summoner Skartax in a cave located at (40,39), then kill Infernal Oversoul.|
+C The Fate of Flanis |N|Enter a cave located at (40,38) then head to (34,39) (dead dwarf)|
+T The Summoning Chamber
+T The Fate of Flanis
+A Bring Down the Warbringer!
+C Bring Down the Warbringer! |N|(39,40) (39,45)|
+T Bring Down the Warbringer!
+A Gaining Access
+
+T The Rod of Lianthe |N|West along the road|
+A Sketh'lon Feathers
+C Gaining Access |N|(22,35)|
+
+T Ar'tor, Son of Oronok
+A Demonic Crystal Prisons
+C Demonic Crystal Prisons
+T Demonic Crystal Prisons
+A Lohn'goron, Bow of the Torn-heart
+C Lohn'goron, Bow of the Torn-heart
+T Lohn'goron, Bow of the Torn-heart
+A The Cipher of Damnation - Ar'tor's Charge
+C The Cipher of Damnation - Ar'tor's Charge
+T The Cipher of Damnation - Ar'tor's Charge
+A The Cipher of Damnation - The Second Fragment Recovered
+
+T Gaining Access
+A Invasion Point: Cataclysm
+T Invasion Point: Cataclysm
+A The Art of Fel Reaver Maintenance
+
+T Grom'tor, Son of Oronok
+A The Cipher of Damnation - Grom'tor's Charge
+C The Cipher of Damnation - Grom'tor's Charge
+T The Cipher of Damnation - Grom'tor's Charge
+A The Cipher of Damnation - The First Fragment Recovered
+
+C The Art of Fel Reaver Maintenance |N|(39,19). Use key on the Strongbox located nearby.|
+T The Art of Fel Reaver Maintenance
+A The Fel and the Furious
+C The Fel and the Furious
+T The Fel and the Furious
+A News of Victory
+T News of Victory |N|Back at Wildhammer Stronghold|
+
+T Borak, Son of Oronok
+A Of Thistleheads and Eggs...
+C Of Thistleheads and Eggs...
+
+F Shattrath City
+T Of Thistleheads and Eggs... |N|In Lower City|
+A The Bundle of Bloodthistle
+
+H Wildhammer Stronghold
+T The Bundle of Bloodthistle
+A To Catch A Thistlehead
+C To Catch A Thistlehead
+T To Catch A Thistlehead
+A The Shadowmoon Shuffle
+C The Shadowmoon Shuffle
+T The Shadowmoon Shuffle
+A What Illidan Wants, Illidan Gets...
+C What Illidan Wants, Illidan Gets...
+T What Illidan Wants, Illidan Gets...
+
+C Sketh'lon Feathers |N|(43,57)|
+T Sketh'lon Feathers
+A Imbuing the Headpiece
+C Imbuing the Headpiece |N|Use quest item at (43,44). Equip it before getting there.|
+T Imbuing the Headpiece
+A Thwart the Dark Conclave
+C Thwart the Dark Conclave |N|Use quest item at (41,60) after clearing out all the enemies.|
+T Thwart the Dark Conclave
+
+T The Cipher of Damnation - The First Fragment Recovered
+T The Cipher of Damnation - The Second Fragment Recovered
+
+C Oronu the Elder
+T Oronu the Elder
+A The Ashtongue Corruptors
+C The Ashtongue Corruptors
+T The Ashtongue Corruptors
+A The Warden's Cage
+T The Warden's Cage
+A Proof of Allegiance
+C Proof of Allegiance
+T Proof of Allegiance
+A Akama
+T Akama
+
+C Put On Yer Kneepads...
+C Divination: Gorefiend's Truncheon
+
+T Put On Yer Kneepads...
+T Divination: Gorefiend's Truncheon
+T Divination: Gorefiend's Cloak
 ]]
 end)
 
