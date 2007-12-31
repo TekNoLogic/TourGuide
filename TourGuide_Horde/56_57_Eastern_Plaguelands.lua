53dfc5b79beafd03577255f162482b84185e566a
Tekkub Stoutwrithe <tekkub@gmail.com>
2007-12-31 00:54:09 -0700

    Horde Plaguelands tweaks, including a lot of notes from the Ally guides

--- TourGuide_Horde/56_57_Eastern_Plaguelands.lua
+++ TourGuide_Horde/56_57_Eastern_Plaguelands.lua
@@ -1,52 +1,63 @@
+
 TourGuide:RegisterGuide("Eastern Plaguelands (56-57)", "Western Plaguelands (57-59)", "Horde", function()
 return [[
 R Eastern Plaguelands
-A Demon Dogs |N|(7,43)|
+A Demon Dogs |N|Follow the river north (7,43)|
 A Blood Tinged Skies
 A Carrion Grubbage
-A To Kill With Purpose |N|Grind your way there (26,74)|
+
+N Kill crap... |N|Kill any plaguehounds, carrion worms, and plaguebats you encounter in EPL for each respective quest.|
+A To Kill With Purpose |N|At the Marris Stead (26,74)|
 A Un-Life's Little Annoyances
 A Hameya's Plea |N|From the scroll on the ground at (27,85)|
-N Grind down to Darrowshire while working on Blood Tinged Skies, Demon Dogs, Carrion Grubbage |N|(36,90)|
-T Sister Pamela |N|At Darrowshire (36,90)|
+
+T Sister Pamela |N|In a house in Darrowshire (36,90)|
 A Pamela's Doll
-C Pamela's Doll |N|Three parts in closest building|
+C Pamela's Doll |N|Parts are found in the houses nearby.  Ghosts spawn when you get near them.|
 T Pamela's Doll
 A Auntie Marlene
 A Uncle Carlin
-C Blood Tinged Skies |N|Finish it this time|
-N Dogs... |N|Make sure you kill 20 Plaguehound Runts for Demon Dogs|
 
-R Light's Hope Chapel |N|At (81,60)|
-A Zaeldarr the Outcast
+K Plaguehounds |Q|Demon Dogs| |QO|Plaguehound Runt slain: 20/20| |N|All over the southwestern section of the zone|
+C Blood Tinged Skies
+
+T Uncle Carlin |N|East at Light's Hope Chapel (81,58)|
+A Defenders of Darrowshire
 A The Restless Souls
 T Duke Nicholas Zverenhoff
-T Uncle Carlin
-A Defenders of Darrowshire
+A Zaeldarr the Outcast |N|Southwest near the corpse pits (79,63)|
+
+N Free Spirits... |N|Kill any cannibal ghouls, gibbering ghouls and diseased flayers you come across.  Speak to any ghosts that spawn for "Defenders of Darrowshire".|
+C Demon Dogs |N|Plaguehounds near Light's Hope to the east, Frenzied Plaguehound in the north half of the zone.|
+C A Plague Upon Thee (Part 1) |N|Find termite mounds all around the northern part of the zone.|
 
 C To Kill With Purpose
 C Defenders of Darrowshire
-C Hameya's Plea |N|(72,14)|
-C Demon Dogs
 C Carrion Grubbage
 C Un-Life's Little Annoyances
-C A Plague Upon Thee (Part 1) |N|Done at Plaguewood up North|
 
 A Augustus' Receipt Book |N|From hut at (13,34)|
 C Augustus' Receipt Book |N|Book is upstairs in the inn at (15,33)|
 T The Restless Souls |N|Egan is in the hut at (13,34)|
 T Augustus' Receipt Book
 
-N Grind/stealth through the cave, not easy!! |N|TODO: Get rid of this, there's an elite in there|
-T Demon Dogs
+T Demon Dogs |N|On the west edge of the zone|
 T Blood Tinged Skies
 T Carrion Grubbage
 A Redemption
-T Redemption |N|Talk to the guy to complete it|
+C Redemption |N|Blah blah blah... he talks to much.|
+T Redemption
+A Of Forgotten Memories
+
 T To Kill With Purpose |N|at (26,74)|
 T Un-Life's Little Annoyances
-C Zaeldarr the Outcast |N|At (27,85)|
-T Hameya's Plea |N|Dirt mound behind the crypt|
+
+C Of Forgotten Memories |N|South at the Undercroft (28,86).  Talk to the grave and Mercutio and his adds will walk in (not spawn).  Try to pull him away from the adds, kill and loot him, and get out.  You might need help.|
+C Zaeldarr the Outcast |N|(27,85)|
+A Hameya's Plea |N|From the scroll near Zaeldarr|
+
+T Of Forgotten Memories |N|Back on the west edge of the zone|
+A Of Lost Honor
 
 H Undercity
 T Better Late Than Never
--- TourGuide_Horde/56_Western_Plaguelands.lua
+++ TourGuide_Horde/56_Western_Plaguelands.lua
@@ -19,6 +19,7 @@ A A Plague Upon Thee (Part 1)
 A Better Late Than Never (Part 1)|N|Lady in the house at Felstone Field (38,55)|
 T Better Late Than Never (Part 1)|N|Barn next door|
 A Better Late Than Never (Part 2) |N|Click chest again|
+
 C Scarlet Diversions |N|Burn the tent, plant the banner (40,51)|
 
 T Scarlet Diversions |N|Back at the Bulwark|
@@ -26,33 +27,55 @@ A All Along the Watchtowers
 A The Scourge Cauldrons
 T The Scourge Cauldrons
 A Target: Felstone Field
-C Target: Felstone Field |N|(37,56)|
-T Target: Felstone Field
+
+C Target: Felstone Field |N|To the east at Felstone Field (37,56).  Kill Cauldron Lord Bilemaw for the key, then talk to the cauldren.|
+T Target: Felstone Field |N|Talk to the cauldren|
 A Return to the Bulwark (Part 1)
+
+A Better Late Than Never (Part 1) |N|In the house on the northeast side of the field (38,54), upstairs|
+T Better Late Than Never (Part 1) |N|Next door in the barn, talk to the box (38.8, 55.2)|
+A Better Late Than Never (Part 2)
+
 T Return to the Bulwark (Part 1)
 A Target: Dalson's Tears
-A Mrs. Dalson's Diary |N|Diary in the barn at (47,50)|
-T Mrs. Dalson's Diary
-K Wandering Skeleton |N|Get a key|
-A Locked Away (Part 1) |N|Open the Outhouse behind the farm with the key|
-T Locked Away (Part 1)
-K Farmer Dalson |N|Spawns from opening the outhouse|
-A Locked Away (Part 2) |N|Locker upstairs in the house|
-T Locked Away (Part 2)
 
-C Target: Dalson's Tears |N|(46,52)|
-T Target: Dalson's Tears |N|Bulwark at (29.5,57.5)|
+C Target: Dalson's Tears |N|To the east at Dalson's Tears (46,52).  Kill Cauldron Lord Malvinious for the key, then talk to the cauldren.|
+T Target: Dalson's Tears |N|Talk to the cauldren|
 A Return to the Bulwark (Part 2)
-T Return to the Bulwark (Part 2) |N|Bulwark at (29.5,57.5)|
+
+N Read Mrs. Dalson's Diary |N|On the floor in the barn (47.8, 50.7).  Nothing to accept, just read the book.|
+K Wandering Skeleton |N|Patrols around the house and barn, you are looking for the outhouse key.  Scarlets might kill this mob.| |L|12738|
+K Farmer Dalson |N|Open the outhouse, get his key| |L|12739|
+N Open cabinet |N|Locked cabinet upstairs in the house.|
+
+T Return to the Bulwark (Part 2)
 A Target: Writhing Haunt
-C Target: Writhing Haunt |N|(53,65)|
-T Target: Writhing Haunt |N|Bulwark at (29.5,57.5)|
+
+C Target: Writhing Haunt |N|To the east at Writhing Haunt (53,65).  Kill Cauldron Lord Razarch for the key, then talk to the cauldren.|
+T Target: Writhing Haunt |N|Talk to the cauldren|
 A Return to the Bulwark (Part 3)
-A The Wildlife Suffers Too (Part 1) |N|From the almost dead tauren in hut (53,64)|
-T Return to the Bulwark (Part 3) |N|Bulwark at (29.5,57.5)|
+
+A The Wildlife Suffers Too (Part 1) |N|In the house by the field (54,65)|
+C The Wildlife Suffers Too (Part 1) |N|To the north, between the Felstone and Dalson's Fields, and north of Dalson's Field.|
+
+T Return to the Bulwark (Part 3)
 A Target: Gahrron's Withering
-C Target: Gahrron's Withering |N|(62,59)Once you turn in the Key, don't return to Bulwark|
-T Target: Gahrron's Withering
+
+C All Along the Watchtowers |U|12815| |N|Mark each tower in Andorhal, you can get close enough to mark without aggroing mobs inside if you are careful. (47,71) (40,71) (42,66) (44,63)|
+
+T The Wildlife Suffers Too (Part 1)
+A The Wildlife Suffers Too (Part 2)
+C The Wildlife Suffers Too (Part 2) |N|East, between Writhing Haunt and Gahrron's Withering (52,56)|
+
+C Target: Gahrron's Withering |N|To the east at Writhing Haunt (62,58).  Kill Cauldron Lord Soulwrath for the key, then talk to the cauldren.|
+T Target: Gahrron's Withering |N|Talk to the cauldren|
 A Return to the Bulwark (Part 4)
+
+T The Wildlife Suffers Too (Part 2)
+
+T All Along the Watchtowers |N|Back at the Bulwark|
+A Scholomance
+T Scholomance
+A Skeletal Fragments
 ]]
 end)
