dfb0fff53d4e714cbc45fca503e848c1aa5b488a
Tekkub Stoutwrithe <tekkub@gmail.com>
2008-01-01 00:20:15 -0700

    Terokkar tweakkars

--- TourGuide_Alliance/64_65_Terokkar_Aldor.lua
+++ TourGuide_Alliance/64_65_Terokkar_Aldor.lua
@@ -1,7 +1,8 @@
 
 TourGuide:RegisterGuide("Terokkar Forest (64-65 Aldor)", "Nagrand (65-66)", "Alliance", function()
 return [[
-A A'dal |N|You're looking for a Haggard War Veteran, he should be fairly easy to find.|
+N Aldorks or Scrybabies? |N|You need to pick which faction you will gain rep with.  This is a decision YOU MUST MAKE FOR YOURSELF!  Pop open your web browser, go to wowwiki.com, click on "Aldor v Scryer" on the left, and look at the rewards for each faction.  Make sure you check the quest rewards and crafting recipes as well.  If you choose Scryers then you will have to substitute their quests for the aldor quests in this guide (Scryer guides are on the to do list).|
+A A'dal |N|You're looking for a Haggard War Veteran, he should be fairly easy to find near the center of the city|
 T A'dal
 A City of Light
 A Rather Be Fishin' |N|You can accept this and the next while doing the walk-around for City of Light. Seth is at the ruined infirmary (63.95, 15.50).| |Z|Shattrath City|
@@ -12,21 +13,21 @@ T Allegiance to the Aldor
 A Ishanah
 T Ishanah
 A Restoring the Light
-h Shattrath City |N|As you leave the temple the inn is to your right.|
+h Shattrath City |N|As you leave the temple the inn is to your right|
 
 C Rather Be Fishin' |N|Silmyr Lake, northeast of Shattrath.|
 T What's Wrong at Cenarion Thicket?
 A Strange Energy
 A Clues in the Thicket
 A It's Watching You!
-C Clues in the Thicket |N|The clue you're after is in the inn.|
+C Clues in the Thicket |N|The clue you're after is in the inn|
 C It's Watching You! |N|Up the tower|
 T It's Watching You!
 C Strange Energy |N|Kill the moths in the thicket, and the ones south of the road|
 T Strange Energy
 T Clues in the Thicket
 A By Any Means Necessary
-C By Any Means Necessary |N|Empoor travels the road between Tuurem and Shattrath.|
+C By Any Means Necessary |N|Empoor travels the road between Tuurem and Shattrath|
 T By Any Means Necessary
 A Wind Trader Lathrai
 
@@ -39,13 +40,13 @@ H Aldor Rise
 T Restoring the Light
 T The Eyes of Skettis |N|Back to Rilak in Lower City|
 A Seek Out Kirrik
-T Rather Be Fishin' |N|Skip the follow-up.|
+T Rather Be Fishin' |N|Skip the follow-up|
 T Wind Trader Lathrai
 A A Personal Favor
 
-A Before Darkness Falls |N|Follow the road out of Shattrath south to the Refugee Caravan (38,50).|
+A Before Darkness Falls |N|Follow the road out of Shattrath south to the Refugee Caravan (38,50)|
 C Before Darkness Falls |N|South around the Shadow Stair (39.61, 56.25)|
-A Cabal Orders |N|The item that starts this quest drops from Cabal Skirmishers, Spell-weavers and Initiates. Keep killing them until you get it.| |U|31707|
+A Cabal Orders |N|The item that starts this quest drops from Cabal Skirmishers, Spell-weavers and Initiates.  Keep killing them until you get it.| |U|31707|
 T Cabal Orders |N|Back at the caravan|
 T Before Darkness Falls
 A The Shadow Tomb
@@ -58,7 +59,7 @@ C The Infested Protectors |N|Kill the infested/rotting treants around the edges 
 C Veil Skith: Darkstone of Terokk |N|Veil Skith, south of Shattrath (30.77, 42.03).| |U|31610|
 C A Personal Favor
 C Missing Friends |N|Kill Arakkoa, collect keys, free kids.|
-C The Shadow Tomb |N|The three items are in the three "wings" of the Shadow Tomb (30.95, 52.67).|
+C The Shadow Tomb |N|The three items are in the three "wings" of the Shadow Tomb (30.95, 52.67)|
 
 A Escaping the Tomb
 C Escaping the Tomb
@@ -70,13 +71,13 @@ A Veil Rhaze: Unliving Evil
 T Missing Friends
 T The Shadow Tomb
 
-C Veil Rhaze: Unliving Evil |N|West of the caravan (26,53).|
+C Veil Rhaze: Unliving Evil |N|West of the caravan (26,53)|
 
 T Veil Rhaze: Unliving Evil
 A Veil Lithic: Preemptive Strike
 
-C Veil Lithic: Preemptive Strike |N|Southwest of the caravan (23,71).|
-A The Dread Relic |N|Southeast of Veil Lithic at the Sha'tari Base Camp (31,76).|
+C Veil Lithic: Preemptive Strike |N|Southwest of the caravan (23,71)|
+A The Dread Relic |N|Southeast of Veil Lithic at the Sha'tari Base Camp (31,76)|
 A An Improper Burial
 A Taken in the Night
 A Digging Through Bones |N|Listed as a group quest, but it's not difficult, and the character dialog is worth it.|
@@ -85,15 +86,15 @@ T Digging Through Bones
 A Fumping |N|Again group, but this one isn't elite.|
 
 C An Improper Burial |N|Just east of the base camp.| |U|31769|
-C The Dread Relic |N|The Derelict Caravan is east of the base camp (43.94, 76.50).|
+C The Dread Relic |N|The Derelict Caravan is east of the base camp (43.94, 76.50)|
 C Fumping |N|Recommended you do the Fumping a bit east of the caravan, since the mobs around here are neutral (46,78).| |U|31810|
 
-A For the Fallen |N|Vindicator Haylen is northwest of the Netherweb Ridge (49.71, 76.18).|
+A For the Fallen |N|Vindicator Haylen is northwest of the Netherweb Ridge (49.71, 76.18)|
 C Taken in the Night
 C For the Fallen
 T For the Fallen
 
-T Taken in the Night |N|Back at the base camp.|
+T Taken in the Night |N|Back at the base camp|
 T Fumping
 T The Dread Relic
 T An Improper Burial
@@ -101,7 +102,7 @@ A A Hero Is Needed |NODEBUG|
 C A Hero Is Needed |NODEBUG|
 T A Hero Is Needed |NODEBUG|
 A The Fallen Exarch
-C The Fallen Exarch |N|The west side of Auchindoun (35,66).|
+C The Fallen Exarch |N|The west side of Auchindoun (35,66)|
 T The Fallen Exarch
 
 T Veil Lithic: Preemptive Strike
@@ -115,9 +116,9 @@ T Veil Shalas: Signal Fires
 A Return to Shattrath
 T The Tomb of Lights
 
-R Allerian Stronghold |N|Well east of the caravan (55,54).|
-h Allerian Stronghold |N|The inn is on the left as you come in.|
-A Wanted: Bonelashers Dead! |N|Off the wanted poster outside the town hall.|
+R Allerian Stronghold |N|Well east of the caravan (55,54)|
+h Allerian Stronghold |N|The inn is on the left as you come in|
+A Wanted: Bonelashers Dead! |N|Off the wanted poster outside the town hall|
 A Olemba Seeds
 A Speak with Private Weeks
 A Thin the Flock
@@ -131,42 +132,42 @@ A Magical Disturbances
 F Shattrath City
 T Return to Shattrath
 T Skywing
-A The Skettis Offensive |N|This quest is tricky. Bring a friend if you can.|
+A The Skettis Offensive |N|This quest is tricky, bring a friend if you can.|
 C The Skettis Offensive
 T The Skettis Offensive
 T A Personal Favor
 A Investigate Tuurem
 
 H Allerian Stronghold
-N Collection Quests... |N|While you're running around, keep an eye out for Olemba Cones (bright blue things on the ground). Also kill Warp Stalkers and Timber Worgs as you run across them.|
+N Collection Quests... |N|While you're running around, keep an eye out for Olemba Cones (bright blue things on the ground).  Also kill Warp Stalkers and Timber Worgs as you run across them.|
 
-C Unruly Neighbors |N|East of Allerian Stronghold at the Bonechewer Ruins (66,53).|
+C Unruly Neighbors |N|East of Allerian Stronghold at the Bonechewer Ruins (66,53)|
 C Magical Disturbances
 C Timber Worg Tails
 T Unruly Neighbors |N|North of the ruins at Allerian Post (69,44).|
 A The Firewing Liaison
 A Thinning the Ranks
-C The Firewing Liaison |N|Lisaile Fireweaver is inside or around the only building in the ruins that has a roof (67,54).|
+C The Firewing Liaison |N|Lisaile Fireweaver is inside the only building in the ruins that has a roof (67.89, 53.33)|
 A Fel Orc Plans |N|On the ground inside the same building|
 C Thinning the Ranks
-T The Firewing Liaison
+T The Firewing Liaison |N|Back up at Allerian Post (69.73, 44.27)|
 T Thinning the Ranks
 
-K Ayit |N|Kill Ayit at Veil Shienor for Stymying the Arrakoa - he's in a purple hut on the ground level (59,23).| |Q|Stymying the Arakkoa| |QO|Ayit slain: 1/1 (Complete)|
-C Investigate Tuurem |N|The Sealed Box is in one of the huts in Tuurem (54.00, 30.00).|
-K Ashkaz |N|Kill Ashkaz at Veil Reskk for Stymying the Arrakoa - he's in the trees (49,16).| |Q|Stymying the Arakkoa| |QO|Ashkaz slain: 1/1 (Complete)|
+K Ayit |N|Kill Ayit at Veil Shienor for "Stymying the Arrakoa" - he's in a purple hut on the ground level (59.40, 23.53).| |Q|Stymying the Arakkoa| |QO|Ayit slain: 1/1|
+C Investigate Tuurem |N|The Sealed Box is in one of the huts in Tuurem (54.00, 30.00)|
+K Ashkaz |N|Kill Ashkaz at Veil Reskk for "Stymying the Arrakoa" - he's in the trees (49,16)| |Q|Stymying the Arakkoa| |QO|Ashkaz slain: 1/1|
 C Thin the Flock
 T Investigate Tuurem
 A What Are These Things?
 
-T Speak with Private Weeks |N|Private Weeks is just north of Grangol'var Village (40.46, 36.16).|
+T Speak with Private Weeks |N|Just north of Grangol'var Village (40.46, 36.16)|
 A Who Are They?
-C Who Are They? |N|Shadowy Hunters are bad news. Stay away from them. Initiate (39, 39.68), Laborer (38.09, 41.09), Advisor (40,39) inside the building.|
+C Who Are They? |N|Shadowy Hunters are bad news, stay away from them.  Initiate (39, 39.68), Laborer (38.09, 41.09), Advisor (40,39) inside the building.|
 T Who Are They?
 A Kill the Shadow Council!
 C Kill the Shadow Council!
 
-K Urdak |N|Kill Urdak at Veil Skith for Stymying the Arrakoa - he's up in the trees (29,42).| |Q|Stymying the Arakkoa| |QO|Urdak slain: 1/1 (Complete)|
+C Stymying the Arakkoa |N|Kill Urdak at Veil Skith - he's up in the trees (29,42)|
 C Wanted: Bonelashers Dead! |N|In the Bone Wastes, mostly south and east of the caravan.|
 
 H Allerian Stronghold
@@ -185,26 +186,26 @@ T Magical Disturbances
 
 T Report to the Allerian Post
 A Attack on Firewing Point
-C The Elusive Ironjaw |N|Should be just north of the post (67,35).|
+C The Elusive Ironjaw |N|Should be just north of the post (67,35)|
 C Attack on Firewing Point
-A Escape from Firewing Point! |N|Isla Starmane is inside the main building (73,36).|
+A Escape from Firewing Point! |N|Isla Starmane is inside the main building (73,36)|
 C Escape from Firewing Point!
 
-C Vessels of Power |N|Inside Tuurem. Some of them are fake.|
+C Vessels of Power |N|Inside Tuurem, some of them are fake.|
 
 H Allerian Stronghold
 T Vessels of Power
 T Escape from Firewing Point!
 T The Elusive Ironjaw
 
-T Attack on Firewing Point |N|Back to the Post.|
+T Attack on Firewing Point |N|Back to the Post|
 A The Final Code
 C The Final Code |N|Inside the same building you found the escort in, just keep going up.|
 
-T The Final Code |N|Back to the Stronghold.|
+T The Final Code |N|Back to the Stronghold|
 A Letting Earthbinder Tavgren Know
 
-T Letting Earthbinder Tavgren Know |N|Fastest way to the Thicket is to fly to Shatt then ride east.|
+T Letting Earthbinder Tavgren Know |N|Fastest way to the Thicket is to fly to Shatt then ride east|
 ]]
 
 end)
