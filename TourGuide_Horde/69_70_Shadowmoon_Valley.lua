820015bda9df6f6f7e62cb6330a8fda304a95d65
Tekkub Stoutwrithe <tekkub@gmail.com>
2008-01-02 09:23:49 -0700

    Syncing Ally and Horde Shadownmoon guides

--- TourGuide_Horde/69_70_Shadowmoon_Valley.lua
+++ TourGuide_Horde/69_70_Shadowmoon_Valley.lua
@@ -1,166 +1,209 @@
+
 TourGuide:RegisterGuide("Shadowmoon Valley (69-70)", nil, "Horde", function()
 return [[
-R Shadowmoon Village |N|(30,32)|
+F Stonebreaker Hold |N|Terokkar Forest|
+R Shadowmoon Village |N|East across the bridge, follow the road southeast in Shadowmoon Valley(30,32)|
+
 A Besieged!
+C Besieged!
+T Besieged!
+A To Legion Hold
 A The Sketh'lon Wreckage
 A What Strange Creatures...
 A A Haunted History
 A The Hand of Gul'dan
 A A Grunt's Work...
-A The Path of Conquest
-A Wanted: Uvuros, Scourge of Shadowmoon
-f Get Flight Path
-h Shadowmoon Village |N|Make it your home|
-C Besieged!
-R Shadowmoon Village |N|(30,32)|
-T Besieged!
-C To Legion Hold |N|(23,36) Use Box of Tricks around there|
-R Shadowmoon Village |N|(30,32)|
-T To Legion Hold
-A Setting Up the Bomb
-C Setting Up the Bomb |N|Core: (26,41) Plate: (22,35)|
-A Illidari-Bane Shard |N|From Overseer Ripsaw item drop|
-R Shadowmoon Village |N|(30,32)|
-T Setting Up the Bomb
-A Blast the Infernals!
-T Illidari-Bane Shard
-A Capture the Weapons
-C Capture the Weapons |N|Kill 2 elite mobs in Shadowmoon.. maybe soloable|
-R Shadowmoon Village |N|(30,32)|
-T Capture the Weapons
-C Blast the Infernals! |N|(22,39) Green Glyph is here|
-R Shadowmoon Village |N|(30,32)|
-T Blast the Infernals!
-A The Deathforge
+h Shadowmoon Village
 
-C The Sketh'lon Wreckage |N|(36,29) item drops|
-C A Grunt's Work... |N|+2 Quests, West side of shadowmoon|
-C What Strange Creatures...
-C A Haunted History 
-R Shadowmoon Village |N|(30,32)|
-T The Sketh'lon Wreckage
-T A Grunt's Work...
-T What Strange Creatures...
-T A Haunted History
-A Find the Deserter
-A Frankly, It Makes No Sense...
-A Spectrecles
-C Spectrecles |N|Use spectrecles and kill ghosts around the village|
-T Spectrecles
-A Teron Gorefiend - Lore and Legend
-T Find the Deserter |N|(35,37)Turn in at wandering dude| 
-A Asghar's Totem
-C Asghar's Totem |N|(39,30) Asghar there|
-T Asghar's Totem
-A The Rod of Lianthe
-R The Deathforge |N|(38,38)|
-T The Deathforge
-A Minions of the Shadow Council
-C Minions of the Shadow Council |N|(40,39)In/around deathforge|
-R The Deathforge |N|(38,38)|
-T Minions of the Shadow Council
-A The Fate of Kagrosh
-A The Summoning Chamber
-C The Fate of Kagrosh |N|Inside deathforge|
-C The Summoning Chamber
-R The Deathforge |N|(38,38)|
-T The Fate of Kagrosh
-T The Summoning Chamber
-A Bring Down the Warbringer!
-C Bring Down the Warbringer! |N|Razuun is behind the locked gates in Deathforge|
-R Altar of Damnation |N|(42,45)|
-T The Hand of Gul'dan
+N Kill crap... |N|Kill any Felboars, Vilewing Chimaeras and Scorchshell Pincers you come across for "A Grunt's Work..."  Also loot any [Ever-burning Ash] you see, and kill Diemetradons for "What Strange Creatures..."|
+T The Hand of Gul'dan |N|Northeast at the Altar of Damnation (42,45)|
 A Enraged Spirits of Fire and Earth
-C Enraged Spirits of Fire and Earth |N|(46,46)|
-R Altar of Damnation |N|(42,45)|
+C Enraged Spirits of Fire and Earth |N|Use the totem, then kill the elementals near it| |U|30094|
 T Enraged Spirits of Fire and Earth
 A Enraged Spirits of Water
-C The Rod of Lianthe |N|(47,71) Strongbox|
+C Enraged Spirits of Water |N|Kill water elementals to the northeast at Coilskar Point (48,24).  Use the totem, same as before.| |U|30094|
 
-R Sanctum of the Stars |N|(56,58) Scryer-only, aldor at 62,30|
-f Get FP
+C The Sketh'lon Wreckage |N|Northeast at the Sketh'lon Wreckage (37,30)|
+C What Strange Creatures... |N|Near any of the green lava cracks on the west side of the Hand of Gul'dan|
+C A Haunted History
+
+C To Legion Hold |N|Northwest at Legion Hold (23,36).  Find the Legion Communication Device up in the building and use your "disguise".| |U|30638| TODO ID?
+
+T Haunted History |N|Back at Shadowmoon Village|
+A Spectrecles
+T What Strange Creatures...
+A Frankly, It Makes No Sense...
+
+U Put on your Spectrecles |U|30719|
+C Spectrecles |N|You don't have to leave the village!|
+T Spectrecles
+A Teron Gorefiend - Lore and Legend
+T The Sketh'lon Wreckage
+A Find the Deserter
+T To Legion Hold
+A Setting Up the Bomb
+
+T Enraged Spirits of Water |N|Back at the Altar of Damnation|
+A Enraged Spirits of Air
+
+C Frankly, It Makes No Sense... |N|East under the bridge where you killed water elementals|
+
+T Find the Deserter |N|Patrols along the road to the west (35,39)|
+A Asghar's Totem
+C Asghar's Totem |N|Kill Asghar (40,31)|
+
+T Asghar's Totem
+A The Rod of Lianthe
+
+K Overseer Ripsaw |N|Back at Legion Hold.  Near the fel reaver (22,35)| |L|30756|
+C Setting Up the Bomb |N|Armor Plating is near the fel reaver (22.21, 35.59), Power Core is underwater at the Fetid Pool (26.29, 41.24).|
+
+T Setting Up the Bomb |N|Back at Shadowmoon Village|
+A Blast the Infernals!
+T Frankly, It Makes No Sense...
+A Felspine the Greater
+A Illidari-Bane Shard |U|30756|
+T Illidari-Bane Shard
+
+C Blast the Infernals! |N|Again at Legion Hold.  Find the green circle near the infernal ranks (22.63, 39.93), use the bomb.| |U|30614|
+
+T Blast the Infernals! |N|Back at Shadowmoon Village|
+A The Deathforge
+
+C The Rod of Lianthe |N|Kill Blood Elves at Eclipse Point to the southeast for the key.  Strongbox is east of the elite (47,71).|
+C Enraged Spirits of Air |N|Take the road east to the Netherwing Fields, drop totem, kill air elementals.| |U|30094|
+
+A The Ashtongue Tribe |N|ALDOR ONLY!  North at the Altar of Sha'tar (62,30)|
+A The Ashtongue Broken |N|SCRYER ONLY  West at the Sanctum of the Stars (54,58)|
 A Tablets of Baa'ri
 A Karabor Training Grounds
-A The Ashtongue Broken |N|(54,58) on the hill|
-A Kindness |N|(60,59) Mordenai|
-C Kindness
-T Kindness
-A Seek Out Neltharaku
-C Seek Out Neltharaku |N|Need flying mount to do this quest chain|
 
-R Altar of Shadows |N|(58,70)|
-T Teron Gorefiend - Lore and Legend
+T Teron Gorefiend - Lore and Legend |N|South at the Altar of Shadows (58,70)|
 A Divination: Gorefiend's Cloak
 A Divination: Gorefiend's Truncheon
-A Divination: Gorefiend's Armor
-C Divination: Gorefiend's Truncheon |N|Ghostriders of Karabor circling shadowmoon|
-C Divination: Gorefiend's Cloak |N|(64,48)From Cleric of Karabor|
-C Karabor Training Grounds |N|(68,50)|
-C Frankly, It Makes No Sense... |N|(62,40)|
-R Ruins of Baa'ri |N|(59,38)|
-C Tablets of Baa'ri |N|+1 Quest|
-C The Ashtongue Broken
-C Enraged Spirits of Water |N|(50,24)|
 
-H Shadowmoon Village
-T Frankly, It Makes No Sense... 
+C Felspine the Greater |N|Kill Felspine the Greater north of the Warden's Cage (56.26, 42.10)|
+U Put on your Spectrecles |U|30719|
+N Keep an eye out... |N|Watch the main road around the Hand of Gul'dan for Ghostrider of Karabor.  If you see them kill one for "Divination: Gorefiend's Truncheon".  You need your Spectrecles to see them!|
+C Divination: Gorefiend's Cloak |N|Kill Clerics of Karabor in front of the Black Temple|
+
+C The Ashtongue Tribe |O| |N|At the Ruins of Baa'ri|
+C The Ashtongue Broken |O| |N|At the Ruins of Baa'ri|
+C Tablets of Baa'ri
+C Karabor Training Ground |N|Inside the Black Temple, take the first right (68,50).  Kill Demon Hunter Supplicants and Initiates, try to catch them after a duel when they are low on HP.|
+
+T The Ashtongue Tribe |O| |N|Back at the Altar of Sha'tar|
+T The Ashtongue Broken |O| |N|Back at the Sanctum of the Stars|
+T Oronu the Elder
+T Tablets of Baa'ri
+T Karabor Training Grounds
+A A Necessary Distraction
+
+T Felspine the Greater |N|Back at Shadowmoon Village|
+T Enraged Spirits of Air |N|Back at the Altar of Damnation|
+A Oronok Torn-heart
+
+T Oronok Torn-heart |N|To the west of the Altar of Sha'tar, above the mine (53.92, 23.49)|
+A I Was A Lot Of Things...
+C I Was A Lot Of Things... |N|(52,15)|
+T I Was A Lot Of Things...
+A A Lesson Learned
+C A Lesson Learned |N|(57,20)|
+T A Lesson Learned
+A The Cipher of Damnation - Truth and History
+C The Cipher of Damnation - Truth and History
+T The Cipher of Damnation - Truth and History
+A Grom'tor, Son of Oronok
+A Ar'tor, Son of Oronok
+A Borak, Son of Oronok
+
+T The Deathforge |N|(38,38)|
+A Minions of the Shadow Council
+C Minions of the Shadow Council |N|(38,35)|
+T Minions of the Shadow Council
+A The Summoning Chamber
+A The Fate of Kagrosh
+C The Summoning Chamber |N|Kill Summoner Skartax in a cave located at (40,39), then kill Infernal Oversoul.|
+C The Fate of Kagrosh
+T The Summoning Chamber
+T The Fate of Kagrosh
+A Bring Down the Warbringer!
+C Bring Down the Warbringer! |N|(39,40) (39,45)|
 T Bring Down the Warbringer!
-A Felspine the Greater
 A Gaining Access
-C Gaining Access
+
+T The Rod of Lianthe |N|West along the road|
+A Sketh'lon Feathers
+C Gaining Access |N|(22,35)|
+
+T Ar'tor, Son of Oronok
+A Demonic Crystal Prisons
+C Demonic Crystal Prisons
+T Demonic Crystal Prisons
+A Lohn'goron, Bow of the Torn-heart
+C Lohn'goron, Bow of the Torn-heart
+T Lohn'goron, Bow of the Torn-heart
+A The Cipher of Damnation - Ar'tor's Charge
+C The Cipher of Damnation - Ar'tor's Charge
+T The Cipher of Damnation - Ar'tor's Charge
+A The Cipher of Damnation - The Second Fragment Recovered
+
 T Gaining Access
 A Invasion Point: Cataclysm
-C Invasion Point: Cataclysm |N|(25,36)Teleporter|
-T Invasion Point: Cataclysm
+T Invasion Point: Cataclysm |N|Take the teleporter (25.5, 36.3)|
 A The Art of Fel Reaver Maintenance
-C The Art of Fel Reaver Maintenance
+
+T Grom'tor, Son of Oronok
+A The Cipher of Damnation - Grom'tor's Charge
+C The Cipher of Damnation - Grom'tor's Charge
+T The Cipher of Damnation - Grom'tor's Charge
+A The Cipher of Damnation - The First Fragment Recovered
+
+C The Art of Fel Reaver Maintenance |N|(39,19). Use key on the Strongbox located nearby.|
 T The Art of Fel Reaver Maintenance
 A The Fel and the Furious
 C The Fel and the Furious
 T The Fel and the Furious
 A News of Victory
 T News of Victory |N|(28,26) Through teleporter to Overlord Or'barokh|
-T The Rod of Lianthe |N|(35,37) Parshah goes N/S on this|
-A Sketh'lon Feathers
-C Sketh'lon Feathers |N|(43,54)On ground|
-T Sketh'lon Feathers |N|(35,37)|
+
+T Borak, Son of Oronok
+A Of Thistleheads and Eggs...
+C Of Thistleheads and Eggs...
+
+F Shattrath City
+T Of Thistleheads and Eggs... |N|In Lower City|
+A The Bundle of Bloodthistle
+
+H Shadowmoon Village
+T The Bundle of Bloodthistle
+A To Catch A Thistlehead
+C To Catch A Thistlehead
+T To Catch A Thistlehead
+A The Shadowmoon Shuffle
+C The Shadowmoon Shuffle
+T The Shadowmoon Shuffle
+A What Illidan Wants, Illidan Gets...
+C What Illidan Wants, Illidan Gets...
+T What Illidan Wants, Illidan Gets...
+
+C Sketh'lon Feathers |N|(43,57)|
+T Sketh'lon Feathers
 A Imbuing the Headpiece
-T Enraged Spirits of Water |N|(42,45)|
-A Enraged Spirits of Air
-C Imbuing the Headpiece |N|(42,44)|
+C Imbuing the Headpiece |N|Use quest item at (43,44). Equip it before getting there.|
 T Imbuing the Headpiece
 A Thwart the Dark Conclave
-C Thwart the Dark Conclave |N|(41,62)|
-T Thwart the Dark Conclave |N|(35,37) Parshah|
+C Thwart the Dark Conclave |N|Use quest item at (41,60) after clearing out all the enemies.|
+T Thwart the Dark Conclave
 
-R Sanctum of the Stars |N|(56,58)|
-T Karabor Training Grounds
-T Tablets of Baa'ri
-T The Ashtongue Broken
-A A Necessary Distraction
-A Oronu the Elder
-h Sanctum of the Stars |N|Make it your home|
-C A Necessary Distraction |N|Warlock drops item to free Azaloth|
-C Felspine the Greater |N|(55,43)|
+C A Necessary Distraction |N|Same area as "Karabor Training Ground".  Kill Summoners and Warlocks until the scroll drops, then use it on Azaloth.  Best not kill the mobs near him, or he'll come after you!| |U|30811|
+
 C Oronu the Elder |N|(57,32)|
-H Sanctum of the Stars
-T A Necessary Distraction
 T Oronu the Elder
 A The Ashtongue Corruptors
-C Enraged Spirits of Air |N|(59,62)|
-
-R Altar of Shadows |N|(58,70)|
-T Divination: Gorefiend's Truncheon
-T Divination: Gorefiend's Cloak
-N The Ashtongue Corruptors |N|Haalum: (57,73) Eykenen: (51,52)|
-T Enraged Spirits of Air |N|(42,45)|
-A Oronok Torn-heart
-N The Ashtongue Corruptors |N|Uylaru: (48,39) Lakaan: (49,23)|
-T Oronok Torn-heart |N|(53,23)|
-H Sanctum of the Stars
+C The Ashtongue Corruptors |N|Haalum: (57,73) Eykenen: (51,52) Uylaru: (48,39) Lakaan: (49,23)|
 T The Ashtongue Corruptors
 A The Warden's Cage
-C Wanted: Uvuros, Scourge of Shadowmoon |N|(55,48) NEED GROUP|
 T The Warden's Cage |N|(57,49) In the hole|
 A Proof of Allegiance
 C Proof of Allegiance |N|Pull Zandras down and kill him|
@@ -168,10 +211,14 @@ T Proof of Allegiance
 A Akama
 T Akama |N|Drop in the water|
 
-R Sanctum of the Stars |N|(56,58)|
-F Shadowmoon Village
-T Felspine the Greater
-T Wanted: Uvuros, Scourge of Shadowmoon
+C A Grunt's Work...
+C Divination: Gorefiend's Truncheon |N|Ghostriders of Karabor circling The Hand of Gul'dan|
 
+T A Grunt's Work...
+T The Cipher of Damnation - The First Fragment Recovered
+T The Cipher of Damnation - The Second Fragment Recovered
+T A Necessary Distraction
+T Divination: Gorefiend's Truncheon
+T Divination: Gorefiend's Cloak
 ]]
 end)
