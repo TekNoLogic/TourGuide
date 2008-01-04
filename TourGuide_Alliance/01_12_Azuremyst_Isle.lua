4b7cfe56aaa52127a3f6d8c206ed19bd05f57eff
Tekkub Stoutwrithe <tekkub@gmail.com>
2008-01-04 16:58:05 -0700

    Cleanup of Azuremyst/Bloodmyst

--- TourGuide_Alliance/01_12_Azuremyst_Isle.lua
+++ TourGuide_Alliance/01_12_Azuremyst_Isle.lua
@@ -4,8 +4,10 @@ A You Survived!
 T You Survived!
 A Replenishing the Healing Crystals
 A Volatile Mutations
+
 C Volatile Mutations
 C Replenishing the Healing Crystals
+
 T Volatile Mutations
 A What Must Be Done...
 A Botanical Legwork
@@ -13,20 +15,26 @@ T Replenishing the Healing Crystals
 A Urgent Delivery!
 T Urgent Delivery!
 A Rescue the Survivors!
+
 A Shaman Training |C|Shaman|
 T Shaman Training |C|Shaman|
+
 A Spare Parts
 A Inoculation
+
 C Rescue the Survivors! |N|Find a Draenei Survivor, use your spell Gift of the Naaru on him|
 C Botanical Legwork |N|South of Ammen Fields|
 C What Must Be Done...
+
 T Botanical Legwork
 T What Must Be Done...
 A Healing the Lake
 T Rescue the Survivors!
-C Healing the Lake |N|Southwest of Crash Site|
-C Inoculation |N|Southeast of Crash Site|
+
+C Healing the Lake |N|Southwest of Crash Site at the crystal in the lake (77,58)|
+C Inoculation |N|Southeast of Crash Site| |U|22962|
 C Spare Parts
+
 H Ammen Vale
 T Healing the Lake
 A Call of Earth (Part 1) |C|Shaman|
@@ -39,6 +47,7 @@ C Call of Earth (Part 2) |C|Shaman|
 T Call of Earth (Part 2) |C|Shaman|
 A Call of Earth (Part 3) |C|Shaman|
 T Call of Earth (Part 3) |N|At the Crash Site| |C|Shaman|
+
 T The Missing Scout |N|Southwest of Crash Site (72,60)|
 A The Blood Elves
 C The Blood Elves
@@ -46,8 +55,8 @@ T The Blood Elves
 A Blood Elf Spy
 C Blood Elf Spy
 A Blood Elf Plans |N|Use the item that drops from the Surveyor Candress|
-R Crash Site
-T Blood Elf Spy
+
+T Blood Elf Spy |N|Back at the Crash Site|
 T Blood Elf Plans
 A The Emitter
 T The Emitter
@@ -58,7 +67,8 @@ A Red Snapper - Very Tasty!
 C Red Snapper - Very Tasty!
 T Red Snapper - Very Tasty!
 A Find Acteon!
-R Azure Watch
+
+R Azure Watch |N|Follow the road west|
 T Find Acteon!
 A The Great Moongraze Hunt (Part 1)
 A Medicinal Purpose
@@ -66,147 +76,136 @@ T Travel to Azure Watch
 T Word from Azure Watch
 h Azure Watch
 
-R Odesyus' Landing |N|The beach south of Azure Watch|
+R Odesyus' Landing |N|Take the road south out of Azure Watch to the shore|
 A Cookie's Jumbo Gumbo
 A A Small Start
 C Cookie's Jumbo Gumbo
 C A Small Start |N|Go east along the shore to the Venture Camp (59,67)|
-R Odesyus' Landing
-T A Small Start
+
+T A Small Start |N|Back at Odesyus' Landing|
 A I've Got a Plant
 T Cookie's Jumbo Gumbo
 
-N Grind to level 7 if you aren't yet, you need it to accept next quests |LV|7|
 A Reclaiming the Ruins
 A Precious and Fragile Things Need Special Handling
 C The Great Moongraze Hunt (Part 1)
 C Medicinal Purpose
-C I've Got a Plant |N|Hollowed out tree north of Odesyus' Landing (48,62), leaves around the ground|
-R Azure Watch
-T Medicinal Purpose
+C I've Got a Plant |N|Hollowed out tree is north of Odesyus' Landing (48,62), leaves around the ground|
+
+T Medicinal Purpose |N|Back at Azure Watch|
 A An Alternative Alternative
 T The Great Moongraze Hunt (Part 1)
 A The Great Moongraze Hunt (Part 2)
 C An Alternative Alternative
-N Kill all Infected Nightstalker Runts for a Quest item |L|23678 1|
-A Strange Findings |N|Item in your inventory|
-R Wrathscale Point
-C Reclaiming the Ruins
+
+K Infected Nightstalker Runts |N|Looking for [Faintly Glowing Crystal]| |L|23678|
+A Strange Findings |U|23678|
+
+C Reclaiming the Ruins |N|At Wrathscale Point, the ruins west of Odesyus' Landing|
 C Precious and Fragile Things Need Special Handling
-N Kill nagas until they drop the quest item |L|23759 1|
-A Rune Covered Tablet |N|Item in your inventory|
-R Odesyus' Landing
-T I've Got a Plant
+A Rune Covered Tablet |U|23759| |N|Kill more naga if you don't have this item yet|
+
+T I've Got a Plant |N|Back at Odesyus' Landing|
 A Tree's Company
 T Rune Covered Tablet
 A Warlord Sriss'tiz |N|After the NPC's stop talking|
 T Reclaiming the Ruins
 T Precious and Fragile Things Need Special Handling
-R Azure Watch
-A The Missing Fisherman
+
+A The Missing Fisherman |N|Back at Azure Watch|
 A Learning the Language
 C Learning the Language |N|Read the book in your inventory|
 T Learning the Language
 A Totem of Coo |N|No need to follow the NPC, point will be shown later|
 T Strange Findings
 A Nightstalker Clean Up, Isle 2...
+
 C The Great Moongraze Hunt (Part 2) |N|North of Azure Watch|
+
 T Totem of Coo |N|On top the the cliifs north of Azure Watch (55,41)|
 A Totem of Tikti
-T Totem of Tikti |N|Jump to northeast with your wings, run over water to (64,39)|
+T Totem of Tikti |N|Jump to northeast with your wings, run over the water to the next totem (64,39)|
 A Totem of Yor
 T Totem of Yor |N|Swim down the river to the south (63,68)|
 A Totem of Vark
-T Totem of Vark |N|Follow the NPC|
+T Totem of Vark |N|Follow the NPC.  If you lose him, the quest ends at Bristlelimb Village (28,63)|
 A The Prophecy of Akida
+
 C The Prophecy of Akida
 C Nightstalker Clean Up, Isle 2...
-R Tides' Hollow
-C Warlord Sriss'tiz |N|In the cave, at lower level|
+
+C Warlord Sriss'tiz |N|In the cave at Tides' Hollow, on the lower level|
+
 R Traitor's Cove |N|Go to the beach with a flag (18,83)|
-N Use [Tree Disguise Kit] |U|23792|
-T The Missing Fisherman |N|At the dock (16,92)|
+U Tree Disguise Kit |U|23792|
+T The Missing Fisherman |N|At the dock to the northwest (16,92)|
 A All That Remains
 C All That Remains
 T All That Remains
+
 H Azure Watch
 T An Alternative Alternative
 T The Prophecy of Akida
 A Stillpine Hold
 T The Great Moongraze Hunt (Part 2)
-R Odesyus' Landing
-T Tree's Company
+T Nightstalker Clean Up, Isle 2...
+
+T Tree's Company |N|Back at Odesyus' Landing|
 A Show Gnomercy
 T Warlord Sriss'tiz
 C Show Gnomercy |N|Talk to the gnome that wanders at the beach|
 T Show Gnomercy
 A Deliver Them From Evil...
-R Azure Watch
-T Nightstalker Clean Up, Isle 2...
-T Deliver Them From Evil...
 
+T Deliver Them From Evil... |N|Back at Azure Watch|
+
+A Coming of Age |N|You must be Lv 10 to accept this|
 A Strength of One |C|Warrior|
-N Grind until you are level 10 |LV|10|
-A Coming of Age
 A Call of Fire (Part 1) |C|Shaman|
-R The Exodar
+
+R The Exodar |N|Follow the road west|
 T Coming of Age
 A Elekks Are Serious Business
-R Stillpine Hold
+
+R Stillpine Hold |N|On the road north of Azure Watch (45,22)|
 A Beasts of the Apocalypse!
 A Murlocs... Why Here? Why Now?
 T Stillpine Hold
+
 C Strength of One |C|Warrior|
 C Beasts of the Apocalypse! |N|Northeast of Stillpine Hold|
 T Call of Fire (Part 1) |N|Go further northeast| |C|Shaman|
 A Call of Fire (Part 2) |C|Shaman|
-R Stillpine Hold
-T Beasts of the Apocalypse!
+
+T Beasts of the Apocalypse! |N|Back at Stillpine Hold|
 A Chieftain Oomooroo
 A Search Stillpine Hold
+
 N Kill mobs until you get the questitem for your Shaman quest |L|23733 1| |C|Shaman|
 C Chieftain Oomooroo
 T Search Stillpine Hold |N|There's a red crystal in the cave you need to touch|
-A Blood Crystals |N|Spawns 2 owlkins|
+A Blood Crystals |N|Spawns 2 owlbeasts|
 T Blood Crystals
 T Chieftain Oomooroo
 A The Kurken is Lurkin'
 C The Kurken is Lurkin' |Kill the 2 headed dog next to the red crystal, in the cave|
+K Crazed Wildkin |C|Shaman| |L|23733|
 T The Kurken is Lurkin'
 A The Kurken's Hide
-C The Kurken's Hide
+T The Kurken's Hide
 T Call of Fire (Part 2) |C|Shaman|
 A Call of Fure (Part 3) |C|Shaman|
-C Murlocs... Why Here? Why Now? |N|West of Stillpine Hold, along the shore (30,29)|
-N Kill the named Murloc called Murgurgula to get a quest |L|23850 1|
-A Gurf's Dignity
-R Stillpine Hold
-T Murlocs... Why Here? Why Now?
-T Gurf's Dignity
-A Warn Your People |N|From the Chief.|
-R Kessel's Crossing |N|The second island north of Azuremyst Isle|
-T Elekks Are Serious Business
-A The Kessel Run |N|Timed quest, 15 minutes. Deliver word to 3 places|
-C The Kessel Run |N|Talk to High Chieftain Stillpine at Stillpine Hold (46,20), Exarch Menelaous at Azure Watch (47,50) and Admiral Odesyus at Odesyus' Landing (46,70) then head back to Kessel. Turn in Warn Your People to the Exarch while you're there.|
-T Warn Your People
-T The Kessel Run
 
-R Silvermyst Isle |N|Run to the small island to the southwest of Azuremyst Isle (11,82)| |C|Shaman|
-C Call of Fire (Part 3) |N|Wickerman Effigy (11,82)| |C|Shaman|
-U Orb of Returning |C|Shaman|
+C Murlocs... Why Here? Why Now? |N|West of Stillpine Hold, along the shore (30,29)|
+
+T Murlocs... Why Here? Why Now? |N|Back at Stillpine Hold|
+A Gurf's Dignity |O| |U|23850|
+T Gurf's Dignity |O|
+A Warn Your People |N|From the Chief|
+
+C Call of Fire (Part 3) |N|Head down to Silvermyst Isle, find the Wickerman Effigy (11,82)| |C|Shaman|
+U Orb of Returning |C|Shaman| |U|24335|
 T Call of Fire (Part 3) |C|Shaman|
 A Call of Fire (Part 4) |C|Shaman|
-H Azure Watch |C|Shaman|
-T Call of Fire (Part 4) |C|Shaman|
-A Call of Fire (Part 5) |C|Shaman|
-R The Exodar |C|Shaman|
-T Call of Fire (Part 5) |C|Shaman|
-
-H Azure Watch |C|Warrior|
-T Strength of One |C|Warrior|
-A Behomat |C|Warrior|
-R The Exodar |C|Warrior|
-T Behomat |C|Warrior|
-
 ]]
 end)
--- TourGuide_Alliance/12_20_Bloodmyst_Isle.lua
+++ TourGuide_Alliance/12_20_Bloodmyst_Isle.lua
@@ -1,11 +1,35 @@
 TourGuide:RegisterGuide("Bloodmyst Isle (12-20)", nil, "Alliance", function()
 return [[
+R Kessel's Crossing |N|On Bloodmyst Isle to the north|
+T Elekks Are Serious Business
+A The Kessel Run |N|Timed quest, 15 minutes. Deliver word to 3 places|
 
-R Kessel's Crossing
+N Warn Stillpine Hold |Q|The Kessel Run| |QO|High Chief Stillpine Warned: 1/1|
+N Warn Azure Watch |Q|The Kessel Run| |QO|Exarch Menelaous Warned: 1/1|
+T Warn Your People
+T Call of Fire (Part 4) |C|Shaman|
+A Call of Fire (Part 5) |C|Shaman|
+T Strength of One |C|Warrior|
+A Behomat |C|Warrior|
+
+N Warn Odesyus' Landing |Q|The Kessel Run| |QO|Admiral Odesyus Warned: 1/1|
+
+T The Kessel Run |N|Back up at Kessel's Crossing|
+A Declaration of Power
+
+R Blood Watch |N|Follow the road north| |C|Shaman|
+F The Exodar |C|Shaman|
+T Call of Fire (Part 5) |C|Shaman| |N|In the Exodar|
+R Kessel's Crossing |N|Fly back to Blood watch, run down| |C|Shaman|
+
+R Blood Watch |N|Follow the road north| |C|Warrior|
+F The Exodar |C|Warrior|
+T Behomat |C|Warrior|
+R Kessel's Crossing |N|Fly back to Blood watch, run down| |C|Warrior|
+
 A A Favorite Treat
 A Alien Predators
 
-A Declaration of Power
 N Sand Pears |N|Keep an eye out for Sand Pears as you move around for these next two quests - little brown things at the base of trees|
 
 C Alien Predators
