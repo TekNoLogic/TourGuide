47f5c10536b69948dc0e0643259fb65f3824f92c
Tekkub Stoutwrithe <tekkub@gmail.com>
2008-01-05 13:07:16 -0700

    Arathi cleanup

--- TourGuide_Alliance/32_33_Arathi_Highlands.lua
+++ TourGuide_Alliance/32_33_Arathi_Highlands.lua
@@ -4,11 +4,11 @@ H Southshore |N|If you can or fly to Theramore Isle and take boat to Menethil Ha
 F Refuge Pointe
 A Worth Its Weight in Gold
 A Northfold Manor
-C Northfold Manor |N|Go Northwest.(34,30)| |Z|Arathi Highlands|
+C Northfold Manor |N|Go Northwest (34,30).| |Z|Arathi Highlands|
 T Northfold Manor
-T Hints of a New Plague? |N|East of Refuge Pointe (60.1,53.8)|
-C Worth Its Weight in Gold |N|Go Southeast.|
-A MacKreel's Moonshine |N|Use Swiftness Potion or class ability to jump from bridge to the platform if you miss swim east and run up path (43.2,92.6)|
+T Hints of a New Plague? |N|East of Refuge Pointe (60.1, 53.8)|
+C Worth Its Weight in Gold |N|Go Southeast|
+A MacKreel's Moonshine |N|Use your [Swiftness Potion] or class ability to jump from bridge to the platform.  If you miss swim east and run up the path (43.2, 92.6).|
 
 H Southshore
 T MacKreel's Moonshine
