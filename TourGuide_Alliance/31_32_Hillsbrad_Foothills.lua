48ee62123cd74814788c804bf16ee0396ada6574
Tekkub Stoutwrithe <tekkub@gmail.com>
2008-01-04 21:34:34 -0700

    User ally Hillsbrad tweaks

--- TourGuide_Alliance/31_32_Hillsbrad_Foothills.lua
+++ TourGuide_Alliance/31_32_Hillsbrad_Foothills.lua
@@ -6,8 +6,8 @@ T Plea To The Alliance |NODEBUG|
 
 R Southshore |N|In Hillsbrad Foothills (50.8,51.2)|
 f Grab flight point!
+T Southshore |N|If you don't have it, skip this!|
 h Southshore
-T Southshore |O|
 A Missing Crystals
 A Down the Coast
 A Soothing Turtle Bisque
@@ -16,7 +16,8 @@ A Bartolo's Yeti Fur Cloak
 A Costly Menace
 A Syndicate Assassins
 
-C Down the Coast
+N Moldy Tome |N|On beach west of Southshore (27.7, 72.8)| |L|6931| |C|Warlock|
+C Down the Coast |N|On beach west of Southshore| 
 T Down the Coast
 A Farren's Proof (Part 1)
 C Farren's Proof (Part 1)
@@ -26,23 +27,23 @@ T Farren's Proof (Part 2)
 A Farren's Proof (Part 3)
 T Farren's Proof (Part 3)
 A Stormwind Ho!
-C Stormwind Ho!
+C Stormwind Ho! |N|On beach east of Southshore| 
 T Stormwind Ho!
 A Reassignment
 
-K Kill turtles |N|All along the creek.  Need [Turtle Meat] x10 for "Soothing Turtle Bisque"| |L|3712 10|
-C Missing Crystals |N|(55.4,35)|
+N Kill turtles |N|All along the creek. Need [Turtle Meat] x10 for "Soothing Turtle Bisque"| |L|3712 10|
+C Missing Crystals |N|Tower north of Southshore (55.4,35)|
 
 R Alterac Mountains |N|Go north (54,9)|
-C Syndicate Assassins |N|(58,67)| |Z|Alterac Mountains|
-A Encrypted Letter |N|Examine documents on the table in the camp.|
+A Encrypted Letter |N|Examine documents on the table in the camp to your east. (58,67)| |Z|Alterac Mountains|
 A Foreboding Plans |N|Examine documents on the table in the camp.|
-C Costly Menace |N|Go west. (46,81)| |Z|Alterac Mountains|
+C Syndicate Assassins 
+C Costly Menace |N|Go west (46,81)| |Z|Alterac Mountains|
 
 R Hillsbrad Foothills |N|Go south down the hills.|
-K Kill yetis |N|In the yeti cave (48,33).  Need [Yeti Fur] x10 for "Bartolo's Yeti Fur Cloak".| |L|3720 10|
+N Kill yetis |N|In the yeti cave (48,33).  Need [Yeti Fur] x10 for "Bartolo's Yeti Fur Cloak".| |L|3720 10|
 
-B Soothing Spices |L|3713| |N|From vendor in Southshore (49,55.1).|
+B Soothing Spices |L|3713| |N|From vendor in Southshore (49.0, 55.1)|
 B Fine Thread |L|2321|
 T Encrypted Letter
 A Letter to Stormpike
@@ -55,20 +56,36 @@ T Costly Menace
 F Menethil Harbor
 T The Eye of Paleth
 A Cleansing the Eye
+T Defeat Nek'rosh |O| |L|3625|
 
 F City of Ironforge
-T Letter to Stormpike |Z|City of Ironforge| |N|Hall of Explorers (72,15)|
+T Letter to Stormpike |Z|City of Ironforge| |N|Hall of Explorers (74.25, 12.70)|
 A Further Mysteries
+T Tome of the Cabal (Part 2) |N|Hall of Explorers| |C|Warlock|
+A Tome of the Cabal (Part 3) |N|Hall of Explorers| |C|Warlock|
 A Reclaimers' Business in Desolace |N|From Roetten Stonehammer who walks around nearby.|
+A In Search of Menara Voidrender |N|In The Forlorn Cavern (50.2, 6.2)| |C|Warlock|
 
 F Stormwind City |N|Take the Deep Run Tram if you don't have the flight point.|
 T Reassignment |Z|Stormwind City| |N|In Stormwind Keep. (73.7,17.6)|
 T Cleansing the Eye |Z|Stormwind City| |N|In Stormwind Cathedral. (39,27)|
+A Brother Anton |N|Down stairs in Stormwind Cathedral|
 B Bolt of Woolen Cloth |L|2997| |N|Buy from AH or a Tailor.|
 B Hillman's Cloak |L|3719| |N|Buy from AH or a Leatherworker.|
 
 H Southshore |N|Or fly back if on cooldown.|
 T Bartolo's Yeti Fur Cloak
 T Further Mysteries |N|Ignore next quest.|
+
+F Menethil Harbor |C|Warlock|
+N 3x [Rod of Channeling] |N|Get them off the Dragonmaw Bonewarder,Shadowwarder (47,47)| |L|6930 3| |C|Warlock| |Z|Wetlands|
+b Theramore Isle |C|Warlock|
+F Ratchet |C|Warlock|
+T In Search of Menara Voidrender |C|Warlock| |N|Ratchet (62.6, 35.5)| |C|Warlock| |Z|The Barrens|
+T Tome of the Cabal (Part 3) |C|Warlock|
+A The Binding |N|Ratchet (62.6, 35.5)| |C|Warlock|
+T In Search of Menara Voidrender |C|Warlock|
+C The Binding |C|Inside| |U|6999| |C|Warlock|
+T The Binding |C|Warlock
 ]]
 end)
