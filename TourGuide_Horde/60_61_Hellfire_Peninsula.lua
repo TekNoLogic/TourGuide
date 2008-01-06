25b07f93d54953af28cb421b80b55ce3e077be6f
Tekkub Stoutwrithe <tekkub@gmail.com>
2008-01-05 19:09:49 -0700

    Cleanup of user-submitted Horde Hellfire guide

--- TourGuide_Horde/60_61_Hellfire_Peninsula.lua
+++ TourGuide_Horde/60_61_Hellfire_Peninsula.lua
@@ -1,6 +1,6 @@
+
 TourGuide:RegisterGuide("Hellfire Peninsula (60-61)", "Zangarmarsh (61-63)", "Horde", function()
 return [[
-
 T Eradicate the Burning Legion |N|(58,41) Just outside Thrallmar|
 A Felspark Ravine
 C Felspark Ravine |N|Kill mobs just east|
@@ -8,7 +8,7 @@ T Felspark Ravine
 A Forward Base: Reaver's Fall
 T Forward Base: Reaver's Fall |N|(54,43)|
 A Disrupt Their Reinforcements
-C Disrupt Their Reinforcements |N|Kill mobs just east to get 4 tokens, click in portal at (70,45), Kill mobs at (74,38) to get 4 more tokens, click on portal|
+C Disrupt Their Reinforcements |N|Kill mobs just east to get 4 runes, click in portal at (70,45), Kill mobs at (74,38) to get 4 more tokens, click on portal|
 T Disrupt Their Reinforcements
 A Mission: Gateways Murketh and Shaadraz
 C Mission: Gateways Murketh and Shaadraz |N|Click on the NPC next to you, get the item and get on wyvern, bomb the 2 bases|
@@ -18,18 +18,15 @@ A Return to Thrallmar
 C Mission: The Abyssal Shelf |N|Click on guy next to you, get on bat, bomb away|
 T Mission: The Abyssal Shelf
 
-R Thrallmar |N|(55,35)|
 T Return to Thrallmar |N|Nazgrel in the big building in Thrallmar|
 A Forge Camp: mageddon
 A The Assassin
 A Bonechewer Blood
 A I Work... For the Horde!
 A Falcon Watch
-A Hellfire Fortifications
 C Forge Camp: Mageddon |N|(64,32) Kill Slaves and Razorsaw|
 A Vile Plans |N| Click on the item that Razorsaw Drops|
 
-R Thrallmar |N|(55,35)|
 T Vile Plans |N|At Thrallmar, Nazgrel building|
 T Forge Camp: Mageddon
 A Cannons of Rage
@@ -38,138 +35,111 @@ T Cannons of Rage
 A Doorway to the Abyss
 C Doorway to the Abyss |N|(53,27)All done up there|
 T Doorway to the Abyss
-A Cruel's Intentions
 A The Agony and the Darkness
 C Bonechewer Blood |N|(58,46)With next quest, Just south of Thrallmar|
 C I Work... For the Horde!
 
-R Thrallmar |N|(55,35)|
-T Bonechewer Blood
+T Bonechewer Blood |N|Back at Thrallmar|
 T I Work... For the Horde!
 A Burn It Up... For the Horde!
 A Apothecary Zelana
 C Burn It Up... For the Horde! |N|East Cannon: (60,52), West Cannon: (54,53)|
 T Apothecary Zelana |N|(66,43)|
 A Spinebreaker Post
-C Spinebreaker Post |N|Talk to the guy next to you to fly to Spinebreaker, turn it in|
 
-F Spinebreaker Post
-f Spinebreaker Post |N|Get FP|
+F Spinebreaker Post |N|Talk to the guy next to you to fly to Spinebreaker|
 T Spinebreaker Post
 A Boiling Blood
 A Preparing the Salve
 A The Warp Rifts
 A Make Them Listen
-A Wanted: Worg Master Kruush |N|Poster|
+A Wanted: Worg Master Kruush |N|On a wanted poster|
 C Make Them Listen |N|Mobs directly west|
-A Decipher the Tome |N|Item drop while doing Make them Listen|
+A Decipher the Tome |N|Item drop while doing Make them Listen| |U|28552|
 
-R Spinebreaker Post |N|(61,81)|
-T Make Them Listen
+T Make Them Listen |N|Back at Spinebreaker Post|
 T Decipher the Tome
 A The Battle Horn
 C The Battle Horn |N|(54,83)|
-R Spinebreaker Post |N|(61,81)|
 T The Battle Horn
 A Bloody Vengeance
 C Bloody Vengeance |N|(63,77)|
-R Spinebreaker Post |N|(61,81)|
 T Bloody Vengeance
 A Honor the Fallen
 T Honor the Fallen |N|Turn in at (56,77)|
-N While doing the following, focus on picking up plants for Preparing the Salve
-C Boiling Blood |N|Cauldron: (65,71), do it along with...|
+
+N Pick plants... |N|While doing the following, focus on picking up plants for "Preparing the Salve"|
+C Boiling Blood |N|Cauldron: (65,71)|
 C Wanted: Worg Master Kruush |N|(68,73) in the worg stables|
 
-R Spinebreaker Post |N|(61,81)|
-T Boiling Blood
+T Boiling Blood |N|Back at Spinebreaker Post|
 T Wanted: Worg Master Kruush
 A Apothecary Antonivich
 A Zeth'Gor Must Burn!
 C Zeth'Gor Must Burn! |N|Forge at (69,69), Barracks at (69,76), Eastern Hovel at (70,70), Western Hovel at (67,76), Stable at (68,73)|
-
-R Spinebreaker Post |N|(61,81)|
 T Zeth'Gor Must Burn!
 A The Eyes of Grillok
 C The Eyes of Grillok |N|(67,74)Use Zezzak's Shar on an Eye of Grillok|
-R Spinebreaker Post |N|(61,81)|
 T The Eyes of Grillok
 A Grillok "Darkeye"
 C Grillok "Darkeye" |N|(66,71) in the Cave|
-R Spinebreaker Post  |N|(61,81)|
 T Grillok "Darkeye"
-C Preparing the Salve |N|Run around near Spinebreaker|
-R Spinebreaker Post  |N|(61,81)|
+C Preparing the Salve
 T Preparing the Salve
 A Investigate the Crash
 
-R Zeppelin Crash |N|(49,74)|
-T Investigate the Crash |N|(49,74)|
+T Investigate the Crash |N|TODO at the Zeppelin Crash (49,74)|
 A In Case of Emergency...
 A Ravager Egg Roundup
-C In Case of Emergency... |N|(58,71)Zeppelin Debris all around|
-R Zeppelin Crash |N|(49,74)|
+C In Case of Emergency... |N|(58,71)|
 T In Case of Emergency...
 A Voidwalkers Gone Wild
-C Voidwalkers Gone Wild |N|(49,81) Along with Warp Rifts|
+C Voidwalkers Gone Wild |N|(49,81)|
 C The Warp Rifts
 C Ravager Egg Roundup |N|(36,89)|
-R Zeppelin Crash |N|(49,74)|
 T Voidwalkers Gone Wild
 T Ravager Egg Roundup
 A Helboar, the Other White Meat
 C Helboar, the Other White Meat |N|Helboars are all around|
-
-R Zeppelin Crash |N|(49,74)|
 T Helboar, the Other White Meat
-A Smooth as Butter
-C Smooth as Butter |N|(62,66)|
 
-R Spinebreaker Post |N|(61,81)|
-T The Warp Rifts
+T The Warp Rifts |N|Back at Spinebreaker Post|
 A Void Ridge
 C Void Ridge |N|Take shortcut at (72,67)|
-R Spinebreaker Post |N|(61,81)|
 T Void Ridge
 A From the Abyss
 C From the Abyss |N|(80,78) Go to purple crystals|
-R Spinebreaker Post |N|(61,81)|
-T From the Abyss
-T Smooth as Butter |N|(49,74)|
+
+T From the Abyss |N|Back at Spinebreaker Post|
+
 H Thrallmar
 T Burn It Up... For the Horde!
 T Apothecary Antonivich |N|(52,36)|
 A A Burden of Souls
 
-R Stonewall Canyon |N|(51,30)|
-A Outland Sucks! |N|(51,30) Just NW of Thrallmar|
-N During next quest, kill Marauding Crust Bursters til they drop Eroded Leather Case
-C Outland Sucks! |N|Boxes just SW of there in ravine|
-A Missing Missive |N|Click on Eroded Leather Case|
-R Stonewall Canyon |N|(51,30)|
+A Outland Sucks! |N|(51,30) Just northwest of Thrallmar|
+N During next quest, kill Marauding Crust Bursters til they drop Eroded Leather Case TODO
+C Outland Sucks! |N|Boxes southwest in the ravine|
+A Missing Missive |U|23338|
 T Outland Sucks!
 A How to Serve Goblins
-C How to Serve Goblins |N|(45,42)Rescue Manni, Moh and Jakk Peons, Along with...|
-C A Burden of Souls |N|Kill 20 Shattered Hand orcs|
-R Stonewall Canyon |N|(51,30)|
+C How to Serve Goblins |N|(45,42)Rescue Manni, Moh and Jakk Peons|
+C A Burden of Souls
 T How to Serve Goblins
 A Shizz Work
-C Shizz Work |N|Take Shredder Keys from Fel Guard's poo|
-R Stonewall Canyon |N|(51,30)|
+C Shizz Work |U|30803| |N|Summon a Felhound, kill some helboars... and have fun|
 T Shizz Work
 A Beneath Thrallmar
-C Beneath Thrallmar |N|Entrance to the mine is right there|
+C Beneath Thrallmar
 T Beneath Thrallmar
-R Thrallmar
-T A Burden of Souls  |N|(52,36)|
+
+T A Burden of Souls |N|Back at Thrallmar (52,36)|
 A The Demoniac Scryer
 C The Demoniac Scryer |N|(44,50)|
-C Hellfire Fortifications |N|Flip towers, can skip/do it later if easier|
 T The Assassin |N|(33,43)Krun Spinebreaker|
 A A Strange Weapon
 
 R Falcon Watch |N|(27,60)|
-f Falcon Watch FP
 A Arelion's Journal
 A Source of the Corruption
 A In Need of Felblood
@@ -177,42 +147,38 @@ A Magic of the Arakkoa
 A Trueflight Arrows
 A Birds of a Feather
 A Helping the Cenarion Post
-A Wanted: Blacktalon the Savage |N|Wanted Poster|
 T Falcon Watch
 A Report to Zurai
 A The Great Fissure
-N Build up First Aid at the trainer
 C Birds of a Feather |N|(27,80)Along with the next 2 quests|
 C Magic of the Arakkoa
 C The Great Fissure
 A The Road to Falcon Watch |N|Escort starts at (24,76), needs level 61|
-R Falcon Watch |N|(27,60)|
-T The Road to Falcon Watch
+
+T The Road to Falcon Watch |N|Back at Falcon Watch|
 T The Great Fissure
 A A Pilgrim's Plight
 A Marking the Path
 C A Pilgrim's Plight |N|(22,68) Pack|
 C Marking the Path |N|West: (29,60), Central: (34,60), Southern: (36,65)|
-C Wanted: Blacktalon the Savage |N|Need a group or a 70|
 
-R Falcon Watch |N|(27,60)|
-T A Pilgrim's Plight
+T A Pilgrim's Plight |N|Back at Falcon Watch|
 T Magic of the Arakkoa
 T Birds of a Feather
 T Marking the Path
-T Wanted: Blacktalon the Savage
-C Source of the Corruption |N|(14,59)Ruins of Sha'naar.. while there..|
-A Naladu |N|(15,58)In the Hut|
-T Naladu |N|(16,65)|
+
+A Naladu ||N|TODO at The Ruins of Sha'naar, from Akoru the Firecaller in a hut on the west side of the ruins (15.58, 58.74).  When fighting the groups focus on the taskmasters, the adds won't attack after you kill the taskmaster.|
+T Naladu |N|Up the stairs, in a hut to the southeast (16.2, 65.0).  Watch out for the elite.|
 A A Traitor Among Us
-C A Traitor Among Us |N|(14,63) Key in Hut|
-T A Traitor Among Us |N|(16,65)|
+C A Traitor Among Us |N|Get the key in the hut back at the top of the stairs (14.3, 63.4).|
+T A Traitor Among Us
 A The Dreghood Elders
-C The Dreghood Elders |N|Morod: (13,61) Akoru: (15,58) Aylaan: (13,58)|
-T The Dreghood Elders |N|(16,65)|
+C The Dreghood Elders |N|Free the elders, Morod (13.12, 61.02) and Aylaan (13.00, 58.43) to the west, Akoru (15.58, 58.74) to the east.|
+T The Dreghood Elders
 A Arzeth's Demise
-C Arzeth's Demise
+C Arzeth's Demise |U|29513| |N|Use the staff on Arzeth and kill him.|
 T Arzeth's Demise
+C Source of the Corruption
 
 R Cenarion Post |N|(15,52)|
 T Missing Missive
@@ -229,26 +195,13 @@ T Testing the Antidote
 C Keep Thornfang Hill Clear! |N|(8,51)|
 R Cenarion Post |N|(15,52)|
 T Keep Thornfang Hill Clear!
-A Crimson Crystal Clue |N|(14,44)Try to kill one Collosus Giant solo if you can, click on Crimson Crystal Shard drop|
-T Crimson Crystal Clue |N|(15,52)|
-A The Earthbinder
-T The Earthbinder
-A Natural Remedies
-C Natural Remedies |N|(14,44)With a group or 70, along with...|
-C Colossal Menace
-
-R Cenarion Post |N|(15,52)|
-T Natural Remedies
-T Colossal Menace
 
 C Trueflight Arrows |N|(26,47)just north of Falcon Crest|
-C The Agony and the Darkness |N|(38,40)Pools of Aggonar +next 2 Qs|
+C The Agony and the Darkness |N|(38,40)Pools of Aggonar|
 C In Need of Felblood
 C Arelion's Journal |N|(39,37)Hanging from a Tree Limb|
-C Cruel's Intentions |N|(42,31)Need a FULL GROUP or 70 and help possibly|
-R Thrallmar |N|(55,35)|
-T Cruel's Intentions
-T The Agony and the Darkness
+
+T The Agony and the Darkness |N|Back at Thrallmar|
 T A Strange Weapon
 A The Warchief's Mandate
 T The Warchief's Mandate
@@ -259,11 +212,9 @@ T Report to Nazgrel
 C A Spirit Guide |N|(33,43)Follow him up to Mag'har post|
 T A Spirit Guide
 A The Mag'har
-H Moonglade |C|Druid|
-t Train |C|Druid|
-H Thrallmar
 T The Mag'har
 A Messenger to Thrall
+
 F Falcon Watch
 T Trueflight Arrows
 T Source of the Corruption
@@ -274,11 +225,9 @@ A Arelion's Secret
 T Arelion's Secret |N|(15,52)Turn it in at Magister Aledis at Cenarion Post|
 A The Mistress Revealed
 C The Cleansing Must Be Stopped
-R Falcon Watch |N|(27,60)|
-T The Mistress Revealed
+
+T The Mistress Revealed |N|Back at Falcon Watch|
 A Arelion's Mistress
 T The Cleansing Must Be Stopped
-
-R Cenarion Refuge |N|Road west out to Zangarmarsh|
 ]]
 end)
