d0de705d7de1f5f6766afdbce388ecfbbf2fe438
rsmozang <rsmozang@gmail.com>
2008-01-05 22:20:58 -0700

    User draenei guides

--- TourGuide_Alliance/12_20_Bloodmyst_Isle.lua
+++ TourGuide_Alliance/12_20_Bloodmyst_Isle.lua
@@ -1,44 +1,307 @@
-TourGuide:RegisterGuide("Bloodmyst Isle (12-20)", nil, "Alliance", function()
+TourGuide:RegisterGuide("Bloodmyst Isle (12-20)","Darkshore (20-21)", "Alliance", function()
 return [[
-R Kessel's Crossing |N|On Bloodmyst Isle to the north|
-T Elekks Are Serious Business
-A The Kessel Run |N|Timed quest, 15 minutes. Deliver word to 3 places|
+C Alien Predators|N|Kill Bloodmyst Hatchings to the SW.  Look for sand pears as you move|
+C A Favorite Treat|N|Collect the sand pears from under the trees|
 
-N Warn Stillpine Hold |Q|The Kessel Run| |QO|High Chief Stillpine Warned: 1/1|
-N Warn Azure Watch |Q|The Kessel Run| |QO|Exarch Menelaous Warned: 1/1|
-T Warn Your People
-T Call of Fire (Part 4) |C|Shaman|
-A Call of Fire (Part 5) |C|Shaman|
-T Strength of One |C|Warrior|
-A Behomat |C|Warrior|
+R Kessel's Crossing
+T A Favorite Treat|N|Aonar|
+T Alien Predators|N|Vorkhan the Elekk Herder|
+A Saving Princess Stillpine|N|Find Princess Stillpine (68.1,81.1) locked in a cage in the Bristlelimb Camp|
+N Retrieve The High Chief's Key| |N|Go to the northermost camp (65, 75) and kill 8 Bristlelimb Furbolgs to summon Highchief Bristlelimb.  Kill him and loot the key| |L|24099 1|
+C Saving Princess Stillpine|N|Release Princess Stillpine from her cage|
+C Declaration of Power|N|Head north to the Wrathscale Lair and kill Lord Xiz (68.5,68.0) and Plant the Draenei Banner|U| 24084|
 
-N Warn Odesyus' Landing |Q|The Kessel Run| |QO|Admiral Odesyus Warned: 1/1|
+R Kessel's Crossing
+T Declaration of Power|N|Kessel|
+A Report to Exarch Admetius|N|Kessel|
+N Grind to level 12|LV|12|
 
-T The Kessel Run |N|Back up at Kessel's Crossing|
-A Declaration of Power
+R Blood Watch|N|Take the road north|
+A Beds, Bandages, and Beyond|N|Caregiver Topher Loaal (55.8,59.7)|
+h Blood Watch
+A Know Thine Enemy|N|Vindicator Aalesia (55.1,58.2)|
+A Catch and Release|N|Morae (53.3,57.7)|
+T Report to Exarch Admetius|N|Exarch Admetius (53.1,54.0)|
+A What Argus Means to Me|N|Exarch Admetius|
+A Learning from the Crystals|N|Harbinger Mikolaas (53.1, 54.0)|
+T What Argus Means to Me|N|Vindicator Boros (54.1,56.8)|
+A Blood Watch|N|Vindicator Boros|
+A Irradiated Crystal Shards|N|Vindicator Boros|
+T Saving Princess Stillpine|N|Stillpine Ambassador Frasaboo (55.3,54.9)|
+A Explorers' League, Is That Something for Gnomes?|N|Prospector Nachlan (56.28, 54.31)|
+A Mac'Aree Mushroom Menagerie|N|Jessera of Mac'Aree (56.40, 56.72)|
+f Blood Watch|N|Laando (57.67, 53.92)|
+T Beds, Bandages, and Beyond|N|Laando|
+A On the Wings of a Hippogryph|N|Laando|
+C Blood Watch|N|Head towards Bladewood (46,45) just to the NW.  Kill 10 Sunhawk Spies|
 
-R Blood Watch |N|Follow the road north| |C|Shaman|
-F The Exodar |C|Shaman|
-T Call of Fire (Part 5) |C|Shaman| |N|In the Exodar|
-R Kessel's Crossing |N|Fly back to Blood watch, run down| |C|Shaman|
+R Blood Watch
+T Blood Watch|N|Vindicator Boros|
+N Kill Tzerak|N|Head to Nazzivian (42.06, 74.37) in the Southwest and look for a summoning circle. If he isn't there look to the north. He'll drop Tzerak's Armor Plate|L|23900 1|
+A Signs of the Legion|U|23900| |N|Use Tzerak's Armor Place|
+C Signs of the Legion|N|Kill Satyrs and Felsworn as you move to the north|
+C Know Thine Enemy|N|Click on the Nazzivus Monument Glyphs (36.52, 71.50) to create a rubbing|
+N Find a Fel Cone Fungus|N|They should be all around the area| |L|24043 1|
+N Find a Blood Mushroom|N|They should be all around the area| |L|24040 1|
+C Catch and Release|N|Head South to the shore and tag 6 Blacksilt Scouts with your Murloc Tagger.  Keep and eye out for Cruelfin| |U|23995|
+N Kill Cruelfin and loot his Red Crystal Pendant|L|23870 1|
+A Cruelfin's Necklace|U|23870|
+C Learning from the Crystals|N|Use the pick near the solid Red Crystal west of Kessel's Crossing (58.09, 83.51) and gather an Impact Site Crystal Sample| |U|23875|
+N Find an Aquatic Stinkhorn|N|Look on the river bed to the north (57.9,72.7) or kill Stinkhorn Strikers| |L|24041 1|
+N Find a Ruinous Polyspore|N|At the Wrathscale Ruins (66.56, 70.11)| |L|24042 1|
+N You need to be level 14 to accept the following quests
 
-R Blood Watch |N|Follow the road north| |C|Warrior|
-F The Exodar |C|Warrior|
-T Behomat |C|Warrior|
-R Kessel's Crossing |N|Fly back to Blood watch, run down| |C|Warrior|
+R Blood Watch
+T Irradiated Crystal Shards|N|Vindicator Boros|
+A Intercepting the Message|N|Vindicator Boros|
+T Mac'Aree Mushroom Menagerie|N|Jessera of Mac'Aree|
+A Constrictor Vines|N|Tracker Lyceon (55.94, 56.92)|
+A The Bear Necessities|N|Tracker Lyceon|
+T Know Thine Enemy|N|Vindicator Aalesia (55.06, 58.05) |
+T Signs of the Legion|N|Vindicator Aalesia|
+A Containing the Threat|N|Vindicator Aalesia|
+A WANTED: Deathclaw|N|From the billboard outside the inn (55.18, 59.06)|
+T Cruelfin's Necklace|N|Morae|
+T Catch and Release|N|Morae|
+A Victims of Corruption|N|Morae|
+T Learning from the Crystals|N|Harbinger Mikolaas|
+A The Missing Survey Team|N|Harbinger Mikolaas|
 
-A A Favorite Treat
-A Alien Predators
+F The Exodar
+T On the Wings of a Hippogryph|N|Nurguni - she is in the central area at the bottom of the ramp|
+A Hippogryph Master Stephanos|N|Nurguni|
+T Hippogryph Master Stephanos|N|Hippogryph Master Stephanos - Outside|
+A Return to Topher Loaal|N|Hippogryph Master Stephanos|
 
-N Sand Pears |N|Keep an eye out for Sand Pears as you move around for these next two quests - little brown things at the base of trees|
+F Blood Watch
+T Return to Topher Loaal|N|Caregiver Topher Loaal|
+C Victims of Corruption|N|Head to Middenvale (51,73) to the SW and kill Treants|
+T The Missing Survey Team|N|Find the Draenei Cartographer (61.15, 48.39) in the Ruins of Loreth'Aran to the NE of Bloodwatch|
+A Salvaging the Data|N|Draenei Cartographer|
+C Salvaging the Data|N|Kill the Wrathscale naga until The Survey Data Crystal drops|
 
-C Alien Predators
-T Alien Predators
-A Saving Princess Stillpine |N|She is on the other side of the road from Kessel's Crossing (68.20,81.00)|
-C Saving Princess Stillpine |N|Princess Stillpine completely fails at directions. High Chief Bristlelimb spawns (after you kill a bunch of his goons) at the camp *northwest* of her current one.|
-T Saving Princess Stillpine
+R Blood Watch
+T Victims of Corruption|N|Morae|
+T Salvaging the Data|N|Harbinger Mikolaas|
+N You need to be level 15 to accept some of the following quests
+A The Second Sample|N|Harninger Mikolaas (52.64, 53.29)|
+A Searching for Galaen|N|Morae (53.28, 57.71)|
+T Urgent Delivery|N|Look for Messenger Hermesius.  He wanders about Blood Watch|
+A Newfound Allies|N|Anchorite Paetheus (54.69, 54.06)|
+A Ysera's Tears|N|Jessera of Mac'Aree|
+N Open your Mailbox|N|Retrieve A Letter from the Admiral|L|24132 1|
+A The Bloodcurse Legacy|U|24132| |N|Read A Letter from the Admiral|
 
-C Declaration of Power |N|Xiz is atop the draenai ruins northeast of the northenmost Bristlelimb camp (69,67). Don't forget to use the banner.|
+R The Cryo-Core|N|To the NW of Bloodwatch.  Kill Elder Brown Bears and Mutated Constrictors you see enroute|
+T Searching for Galaen|N|Look for his corpse inside the Cryo-core.  It is usually safer to enter from the west|
+A Galaen's Fate|N|Galaen's Corpse|
+A Galaen's Journal - The Fate of Vindicator Saruan|N|Look for Galaen's Journal near his corpse|
+C Intercepting the Message|N|Kill Sunhawk elves until you find the Sunhawk Missive|
+C Galaen's Fate|N|Kill Sunhawk Reclaimers until Galaen's Amulet drops|
+R Bladewood|N|(46,45) Again killing every Elder Brown Bears and Mutated Constrictor you see|
+C The Second Sample|N|Use the Crystal Minig Pick to collect the Altered Crystal Sample.  Look for the solid crystals near Bladewood (45.61, 47.82)|U|23876|
+C The Bear Necessities|N|Make you way back towards Blood Watch.  Collect 8 Elder Bear Flanks if you haven't already|
+C Constrictor Vines|N|Make you way back towards Blood Watch.  Collect 6 Thorny Constrictor Vines if you haven't already|
+
+R Blood Watch
+T The Second Sample|N|Harbinger Mikolaas|
+A The Final Sample|N|Harbinger Mikolaas|
+T Galaen's Journal - The Fate of Vindicator Saruan|N|Vindicator Kuros|
+A Matis the Cruel|N|Vindicator Kuros|
+T Intercepting the Message|N|Vindicator Boros|
+A Translations...|N|Vindicator Boros|
+T Translations...|N|Interrogator Elysia (54.51, 54.50)|
+A Audience with the Prophet|N|Interrogator Elysia|
+A The Missing Expedition|N|Achelus (53.31, 57.08)|
+T Galaen's Fate|N|Morae|
+T The Bear Necessities|N|Tracker Lyceon|
+T Constrictor Vines|N|Tracker Lyceon|
+A Culling the Flutterers|N|Tracker Lyceon|
+N Grind to 16|LV|16|
+
+R Axxarien|N|Look for a camp of Satyrs to the Northwest (41,32)|
+C Containing the Threat|N|Head towards Zevrax (41.6, 29.8) killing Satyrs and collecting crystals along the way|
+C The Final Sample|N|Use the pick on the Axxarien Crystal near Zevrax|U|23877|
+C WANTED: Deathclaw|N|Head NW towards the coast and look for Deathclaw (37.5,30)|
+T Explorers' League, Is That Something for Gnomes?|N|Look for a Clopper Wizbang living in a turtle shell to the NE (42.07, 21.19)|
+A Pilfered Equipment|N|Clopper Wizbang|
+A Artifacts of the Blacksilt|N|Clopper Wizbang|
+C Pilfered Equipment|N|Look for a wooden box at one of Blacksilt Murloc camps|
+C Artifacts of the Blacksilt|N|Kill Blacksilt Seers for the Idols and the other Blacksilt for the knives|
+T Pilfered Equipment|N|Clopper Wizbang|
+T Artifacts of the Blacksilt|N|Clopper Wizbang|
+A A Map to Where?|N|Use the Weathered Treasure Map|U|23837|
+
+R Wyrmscar Island|N|The large island off the NE coast of Bloodmyst Isle (65,25)|
+C Ysera's Tears|N|Look for green mushrooms scattered about the island.  Keep an eye out for Prince Toreth|
+A Restoring Sanctity|N|Prince Toreths wander the island, but is usually in front of the lighthouse on the southern tip of the island (60.34, 32.91)|
+
+R Ragefeather Ridge|N|Head W back to the main island (60,36)|
+C Restoring Sanctity|N|Look for the dragon bones around the owlkin encampments|
+
+R Wyrmscar Island|N|Head to the lighthouse and find Prince Toreth|
+T Restoring Sanctity|N|Prince Toreth|
+A Into the Dream|N|Prince Toreth|
+C Into the Dream|N|Kill 5 Whelps and 5 Broodlings.  They are all over the island|
+T Into the Dream|N|Prince Toreth|
+T The Bloodcurse Legacy|N|Captain Edward Hanes (79.1,22.7).  He is a ghost on the eastern shore of the island|
+A The Bloodcursed Naga|N|Captain Edward Hanes|
+C The Bloodcursed Naga|N|Kill 10 naga|
+T The Bloodcursed Naga|N|Captain Edward Hanes|
+A The Hopeless Ones...|N|Captain Edward Hanes|
+C The Hopeless Ones...|N|Kill the Bloodcursed Voyagers down by the shipwrecks|
+T The Hopeless Ones...|N|Captain Edward Hanes|
+A Ending the Bloodcurse|N|Captain Edward Hanes|
+
+R Bloodcurse Isle|N|The island to the SE of the Lighthouse|
+C Ending the Bloodcurse|N|Fight you way to the top of the island, destroy the Statue of Queen Azshara (85.7,54.3), and kill Atoph the Bloodcursed|
+T Ending the Bloodcurse|N|Captain Edward Hanes|
+
+H Blood Watch|U|6948|
+T Containing the Threat|N|Vindicator Aalesia|
+T Ysera's Tears|N|Jessera of Mac'Aree|
+A What We Know...|N|Vindicator Aesom|
+T What We Know...|N|Exarch Admetius|
+A What We Don't Know...|N|Exarch Admetius|
+C What We Don't Know...|N|Talk to the Captured Sunhawk Agent (54.4,54.3)|
+T What We Don't Know...|N|Exarch Admetius|
+A Vindicator's Rest|N|Exarch Admetius|
+T The Final Sample|N|Harbinger Mikolaas|
+T WANTED: Deathclaw|N|Harbinger Mikolaas|
+A Talk to the Hand|N|Harbinger Mikolaas|
+A Fouled Water Spirits|N|Defender Sorli (51.83, 52.10)|
+A Oh, the Tangled Webs They Weave|N|Defender Sorli|
+
+R Ruins of Loreth'Aran|N|Look for some elvish ruins in the NE of Bloodwatch|
+T A Map to Where?|N|Look for a Battered Ancient Book near one of the columns (61.18, 41.81)|
+A Deciphering the Book|N|Battered Ancient Book|
+
+R Blood Watch
+T Deciphering the Book|N|Anchorite Paetheus|
+A Nolkai's Words|N|Anchorite Paetheus|
+
+R Ruins of Loreth'Aran|N|Elvish ruins to the NE|
+T Nolkai's Words|N|Look for a Mound of Dirt (61.22, 49.73)|
+
+R Blood Watch
+F The Exodar
+
+R The Vault of Lights|N|The "middle" vault (39.99, 60.69)|Z|The Exodar|
+T Audience with the Prophet|N|Prophet Velen|
+A Truth or Fiction|N|Prophet Velen|
+
+R Valaar's Berth|N|Head to the docks (23.77,53.60) through the Vault of Lights exit|Z|Azuremyst Isle|
+T Newfound Allies|N|Huntress Kella Nightbow (24.2,54.2)|Z|Azuremyst Isle|
+A The Way to Auberdine|N|Huntress Kella Nightbow|
+
+F Blood Watch
+T Truth or Fiction|N|Vindicator Boros|
+A I Shoot Magic Into the Darkness|N|Vindicator Boros|
+
+R The Warp Piston|N| Follow the road North from Bloodwatch (50.55, 20.32)|
+C I Shoot Magic Into the Darkness|N|Kill 5 Void Anomaly and enter the building|
+C Culling the Flutterers|N|Kill any Royal Blue Flutterers you see as you move along the road south then west|
+
+R Vindicator's Rest|N|Follow the road south then west (30,46)|
+T Vindicator's Rest|N|Vindicator Corin (30.3,45.8)|
+T Talk to the Hand|N|Scout Jorli (29.7,46.9)|
+A Cutting a Path|N|Scout Jorli|
+A Critters of the Void|N|Scout Loryi (30.3,46.0)|
+C Oh, the Tangled Webs They Weave|N|South of Vindicator's Rest.  Kill 8 Mutated Tanglers.  Also, kill any Enraged Ravagers you see|
+C Cutting a Path|N|Kill 10 Enraged Ravagers|
+C Matis the Cruel|N|Head to the area just south of the Vector Coil (24.4,59.4) followng the road. Fire your flare gun when you spot him and assist the NPC that spawns| |U|24278|
+C Critters of the Void|N|Slay 12 Void Critters. They come in waves from the water (20, 62)|
+
+R Amberweb Pass|N|North of the Vector Coil (24.6,35.2)|
+C Fouled Water Spirits|N|(26.47,38.99) Kill 6|
+A They're Alive! Maybe...|N|Researcher Cornelius (24.6,35.2)|
+C They're Alive! Maybe...|N|Destroy the cocoons to free the Expedtion Researchers.  Make your way to the top of the pass|
+C The Missing Expedition|N|Kill Leechers and Spinners on the way up.  Zarakh is at the top of pass|
+T They're Alive! Maybe...|N|Researcher Cornelius|
+
+R Vindicator's Rest
+T Oh, the Tangled Webs They Weave|N|Vindicator Corin|
+T Fouled Water Spirits|N|Vindicator Corin|
+T Cutting a Path|N|Scout Jorli|
+
+H Blood Watch|U|6948|
+T I Shoot Magic Into the Darkness|N|Vindicator Boros|
+T Critters of the Void|N|Vindicator Aesom|
+T Matis the Cruel|N|Vindicator Kuros|
+A The Cryo-Core|N|Vindicator Kuros|
+T Culling the Flutterers|N|Tracker Lyceon|
+T The Missing Expedition|N|Achelus|
+
+N Skip ahead if you are 20th level|N|Just head to Auberdine and start the quests there|
+
+R The Cryo-Core|N|Where Galaen's Body was|
+C The Cryo-Core|N|Collect 12 Medical Supplies|
+
+R Blood Watch
+T The Cryo-Core|N|Vindicator Kuros|
+A Don't Drink the Water|N|Vindicator Aesom| |
+
+R The Bloodwash|N|Look for a waterfall near Deathclaw|
+C Don't Drink the Water|N|Use the vial near the waterfall|U|24318| 
+
+R Blood Watch|N|Hearth if you can| 
+T Don't Drink the Water|N|Vindicator Aesom| 
+A Limits of Physical Exhaustion|N|Vindicator Aesom|
+
+R The Sun Gate 
+C Limits of Physical Exhaustion|N|Kill 10 Sunhawk Pyromancers and 10 Sunhawk Defenders| 
+
+R Blood Watch
+T Limits of Physical Exhaustion|N|Vindicator Aesom|
+A The Sun Gate|N|Vindicator Aesom| 
+
+R The Sun Gate
+C The Sun Gate|N|Destroy the Sunhawk Portal Controllers (they look like small black diamonds).  Then destroy the Sun Gate (The red orb in the middle of the lake)|
+
+R Blood Watch 
+T The Sun Gate|N|Vindicator Aesom|
+N Stop Skipping Here|N|If you were already level 20|
+
+F The Exodar
+R Valaar's Berth|N|Take the boat|
+b Mist's Edge|N|This arrives in Auberdine|
+A Journey to Astranaar|N|Fiora Longears - On the Dock|
+
+h Auberdine|N|The inn is at the end of the dock|
+f Auberdine|N|The Flight Master is on the platform just south of the inn|
+A Washed Ashore (Part 1)|N|Gwennyth Bly'Leggonde (36.6,45.5)| |Z|Darkshore|
+A Fruit of the Sea|N|Gubber Blump (36.1,44.9) - Below the dock| |Z|Darkshore|
+A WANTED: Murkdeep!|N|From the bulletin board infront of the inn|
+A Cave Mushrooms|N|Barithras Moonshade (37.3,43.7) - Just outside the in|
+A The Absent Minded Prospector|N|Archaeologist Hollee (37.5,41.8) - Over by town hall|
+T The Way to Auberdine|N|Thundris Windweaver (37.4,40.2) - Inside town hall|
+A Onward to Ashenvale|N|Thundris Windweaver|
+A The Tower of Althalaxx (Part 1)|N|Sentinel Elissa Starbreeze (39.1,43.5) - Last buildig to the east|
+C Washed Ashore (Part 1)|N|Look for the Beached Sea Creature (36.4,50.9) to the south and loot it|Z|Darkshore|
+T Washed Ashore (Part 1)|N|Gwennyth Bly'Leggonde|
+A Washed Ashore (Part 2)|N|Gwennyth Bly'Leggonde|
+C Washed Ashore (Part 2)|N|Walk out the dock, make you first left, and jump off the end of the pier.  Look for the Skeletal Sea Turtle (31.9,46.3) and loot it|
+T Washed Ashore (Part 2)|N|Gwennyth Bly'Leggonde|
+A Beached Sea Creature (Part 1)|N|Follow the coastline North from Auberdine.  Look for a Beached Sea Creature (42,32) and loot it|Z|Darkshore|
+A Beached Sea Turtle (Part 1)|N|Follow the coastline North.  Look for a Beached Sea Turtle (44,20) and loot it|Z|Darkshore|
+C Fruit of the Sea|N|Collect 6 Fine Crab Chunks from the Reef Crawlers|
+T The Tower of Althalaxx (Part 1)|N|Balthule Shadowstrike (54.9,24.9) - Just off the road|Z|Darkshore|
+A The Tower of Althalaxx (Part 2)|N|Balthule Shadowstrike|
+C The Tower of Althalaxx (Part 2)|N|Kill Dark Strand Fanatics until you collect 4 Worn Parchments|
+T The Tower of Althalaxx (Part 2)|N|Balthule Shadowstrike|
+A The Tower of Althalaxx (Part 3)|N|Balthule Shadowstrike|
+R Cliffspring Falls|N|Head South to the cave at the source of the river (54,32)|Z|Darkshore|
+C Cave Mushrooms|N|
+
+H Auberdine|U|6948|
+T Fruit of the Sea|N|Gubber Blump|
+T Cave Mushrooms|N|Barithras Moonshade|
+A Onu|N|Barithras Moonshade|
 
 ]]
 end)
+
+
+
+
+
