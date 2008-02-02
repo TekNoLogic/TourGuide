920eb75ac48cdf480810625f2738691bb9d65521
Tekkub Stoutwrithe <tekkub@gmail.com>
2008-02-02 16:15:08 -0700

    Fix horde guide transitions

--- TourGuide_Horde/53_54_Burning_Steppes.lua
+++ TourGuide_Horde/53_54_Burning_Steppes.lua
@@ -1,4 +1,4 @@
-TourGuide:RegisterGuide( "Burning Steppes (54)", "Felwood (54-54)", "Horde", function()
+TourGuide:RegisterGuide( "Burning Steppes (53-54)", "Felwood (54)", "Horde", function()
 return [[
 F Burning Steppes
 A Broodling Essence
