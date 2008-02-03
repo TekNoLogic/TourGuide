cb178c2058a2235cca1bef7a0e80e8c09317f119
Tekkub Stoutwrithe <tekkub@gmail.com>
2008-02-03 03:45:27 -0700

    Ashenvale tweaks

--- TourGuide_Alliance/21_22_Ashenvale.lua
+++ TourGuide_Alliance/21_22_Ashenvale.lua
@@ -27,35 +27,35 @@ N Stable your pet |C|Hunter| |T|
 T Orendil's Cure |T|
 A Elune's Tear |T|
 
-A The Ancient Statuette |N|NPC at the small camp south of The Zoram Strand (14.8, 31.2)|
+A The Ancient Statuette |N|NPC at the small camp south of the Zoram Strand (14.8, 31.2)|
 
 N Tame Clattering Crawler |C|Hunter| |N|Make sure it's lvl20 so you'll get Claw (Rank 3).  Teach Growl (Rank 3)|
 
 C The Ancient Statuette |N|Follow the shoreline north until you find the statue near the small rowboat (14.20, 20.64)|
 T The Ancient Statuette
 A Ruuzel
-C Ruuzel |N|At the island in the northwest corner of the zone.  No need to kill Ruuzel if you find the Rare mob called 'Lady Vespia', she drops the same item.|
+C Ruuzel |N|On the island in the northwest corner of the zone.  No need to kill Ruuzel if you find the Rare mob named 'Lady Vespia', she drops the same item.|
 C The Zoram Strand
 T Ruuzel
-T Raene's Cleansing (Part 1) |N|The small puddle southeast of The Zoram Strand|
+T Raene's Cleansing (Part 1) |N|Southeast of Zoram Strand at Lake Falathim, on the island (20.31, 42.35).|
 A Raene's Cleansing (Part 2)
-C Raene's Cleansing (Part 2) |N|Kill the murlocs to get the item|
+C Raene's Cleansing (Part 2) |N|Kill murlocs, huzzah!|
 
 H Astranaar
-T The Zoram Strand |T|
-A Pridewings of Stonetalon |T|
 T Raene's Cleansing (Part 2) |T|
 A Raene's Cleansing (Part 3) |T|
+T The Zoram Strand |T|
+A Pridewings of Stonetalon |T|
 A An Aggressive Defense |T|
 N Unstable your pet |C|Hunter| |T|
 C Elune's Tear |N|Northeast at Iris Lake, on the island (46,46).|
-T Raene's Cleansing (Part 3) |N|East of the puddle|
+T Raene's Cleansing (Part 3) |N|To the east at the Moonwell (53.50, 46.30)|
 A Raene's Cleansing (Part 4)
 
 C An Aggressive Defense |N|Camp northeast of Silverwind Refuge|
 
 T An Aggressive Defense |T| |N|Back at Astranaar|
-T Elune's Tear |N|East of Inn| |T|
+T Elune's Tear |T|
 A The Ruins of Stardust |T|
 C The Ruins of Stardust |N|Due south of Astranaar, on an island yet again.| |T|
 C Call of Water (Part 4) |C|Shaman| |U|23749| |N|Use the bag at the fountain in the center of the island (33.54, 67.45)|
