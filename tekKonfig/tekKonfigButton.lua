c61731c1f84c201e7e7afe23eb1f5bd856bbca98
Tekkub Stoutwrithe <tekkub@gmail.com>
2008-03-05 00:12:45 -0700

    Change tekKonfig to not use submodules, they don't work with git-archive it seems

---
+++ tekKonfig/tekKonfigButton.lua
@@ -0,0 +1,45 @@
+
+local lib, oldminor = LibStub:NewLibrary("tekKonfig-Button", 1)
+if not lib then return end
+
+
+local GameTooltip = GameTooltip
+local function HideTooltip() GameTooltip:Hide() end
+local function ShowTooltip(self)
+	if self.tiptext then
+		GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
+		GameTooltip:SetText(self.tiptext, nil, nil, nil, nil, true)
+	end
+end
+
+
+-- Create a button.
+-- All args optional, parent recommended
+function lib.new(parent, ...)
+	local butt = CreateFrame("Button", nil, parent)
+	if select("#", ...) > 0 then butt:SetPoint(...) end
+	butt:SetWidth(80) butt:SetHeight(22)
+
+	-- Fonts --
+	butt:SetFont(GameFontNormal)
+	butt:SetDisabledFontObject(GameFontDisable)
+	butt:SetHighlightFontObject(GameFontHighlight)
+	butt:SetTextFontObject(GameFontNormal)
+
+	-- Textures --
+	butt:SetNormalTexture("Interface\\Buttons\\UI-Panel-Button-Up")
+	butt:SetPushedTexture("Interface\\Buttons\\UI-Panel-Button-Down")
+	butt:SetHighlightTexture("Interface\\Buttons\\UI-Panel-Button-Highlight")
+	butt:SetDisabledTexture("Interface\\Buttons\\UI-Panel-Button-Disabled")
+	butt:GetNormalTexture():SetTexCoord(0, 0.625, 0, 0.6875)
+	butt:GetPushedTexture():SetTexCoord(0, 0.625, 0, 0.6875)
+	butt:GetHighlightTexture():SetTexCoord(0, 0.625, 0, 0.6875)
+	butt:GetDisabledTexture():SetTexCoord(0, 0.625, 0, 0.6875)
+	butt:GetHighlightTexture():SetBlendMode("ADD")
+
+	-- Tooltip bits
+	butt:SetScript("OnEnter", ShowTooltip)
+	butt:SetScript("OnLeave", HideTooltip)
+
+	return butt
+end
