541767fca6cf229ad81f274a51e72b012418964a
Tekkub Stoutwrithe <tekkub@gmail.com>
2008-03-10 00:15:56 -0600

    Ally STV reworking (god this zone sucks)

--- TourGuide_Alliance/38_40_Stranglethorn.lua
+++ TourGuide_Alliance/38_40_Stranglethorn.lua
@@ -2,57 +2,86 @@
 TourGuide:RegisterGuide("Stranglethorn Vale (38-40)", "Badlands (40-41)", "Alliance", function()
 return [[
 b Booty Bay
-A The Bloodsail Buccaneers (Part 1) |N|(28,76)|
-A Scaring Shaky |N|In the building next to the upside down half boat|
-h Booty Bay
-A Venture Company Mining |N|Near the innkeeper (27,77)|
-T The Rumormonger |N|Upstairs|
-A Dream Dust in the Swamp
-A Skullsplitter Tusks
-T Sunken Treasure (Part 4) |N|Skip the next part|
+A Singing Blue Shards
+A Bloodscalp Ears
 
-C Raptor Mastery (Part 3) |N|Around (33,39)|
-C Water Elementals |N|On the island (20,23)|
-C Some Assembly Required |N|Kill Snapjaw Crocolisks from the stream (38,30) to the lake (41,19)|
-C Venture Company Mining |N|East past the road (41,41)|
-C Skullsplitter Tusks |N|In the camps around (42,37)|
-C Panther Mastery (Part 4) |N|Bhag'thera is anywhere between just north of Mosh'ogg (49,25) to just north of the ZG entrance (48,17)|
+A The Haunted Isle |N|From Krazek in the tavern|
+T The Haunted Isle |N|Baron Revilgaz, upstairs in the tavern|
+A The Stone of the Tides
 
-H Booty Bay |SZ|The Salty Sailor Tavern|
-T Venture Company Mining
-T Skullsplitter Tusks |N|Upstairs|
-T Water Elementals |N|Skip next part|
+F Rebel Camp
+A Special Forces
+A Kurzen's Mystery
+
+C Singing Blue Shards |N|Kill Crystal Spine Basilisks west of the Bal'lal Ruins along the shore (25,18)|
+C The Stone of the Tides |N|On the island to the west (21,22)|
+
+C Kurzen's Mystery |N|First legend is on a tablet named "Moon Over the Vale" in the Bal'lal Ruins (29.5, 19.1).  Second is "Gri"lek the Wanderer", west of Bal'lal Ruins underwater (24.8, 22.8).  Third is "Fall of Gurubashi" in the Ruins of Zul'Kunda (32.25, 12.05).  Fourth is "The Emperor's Tomb" in the Ruins of Zul'Kunda (24.7, 9.0)|
+C Bloodscalp Ears |N|Kill trolls in the Bal'lal Ruins, Ruins of Zul'Kunda and Tkashi Ruins|
+
+C Tiger Mastery (Part 3) |N|South of Nessy's camp, north of Grom'Gol.|
+C Raptor Mastery (Part 2) |N|North of Grom'Gol, from Grow'gol east to the road, and north of the road|
+C Some Assembly Required |N|Snapjaw crocolisks along the river and lake.|
+C Special Forces |N|In the cave at the Kurzen Compound (45,8)|
+
+T Tiger Mastery (Part 3) |N|Back at Nesingwary's|
+A Tiger Mastery (Part 4)
+T Raptor Mastery (Part 2)
+A Raptor Mastery (Part 3)
+
+T Special Forces |N|Back at the Rebel Camp|
+A Colonel Kurzen
+T Kurzen's Mystery
+A Troll Witchery
+
+C Colonel Kurzen |N|Fly to Rebel Camp.  East in the cave at the Kurzen Compound (45,8)|
+C Tiger Mastery (Part 4) |N|Sin'Dall is on a hill southwest of Nessy's camp (32.20, 17.25)|
+
+T Tiger Mastery (Part 4) |N|Back at Nesingwary's|
+A Chapter I
+A Chapter II
+A Chapter III
+A Chapter IV
+
+T Colonel Kurzen |N|Back at the Rebel Camp|
+
+T Singing Blue Shards |N|Fly back to Booty Bay|
+A Venture Company Mining
+T Bloodscalp Ears
+A Skullsplitter Tusks |N|From Kebok in the inn (2nd Floor)|
+T The Stone of the Tides
+A Water Elementals
+
+C Chapter I |N|Buy any pages you don't have off the AH.  If you can't find some just skip this chapter.|
+C Chapter II |N|Buy any pages you don't have off the AH.  If you can't find some just skip this chapter.|
+C Chapter III |N|Buy any pages you don't have off the AH.  If you can't find some just skip this chapter.|
+C Chapter IV |N|Buy any pages you don't have off the AH.  If you can't find some just skip this chapter.|
+
 T Some Assembly Required
 A Excelsior
 
-T The Bloodsail Buccaneers (Part 1) |N|Run out of BB, then West.  Letter on a barrel in the pirate camp (27,69)|
-A The Bloodsail Buccaneers (Part 2)
-C Scaring Shaky |N|Gorillas just northeast of BB (32,65)|
+C Venture Company Mining |N|From the Booty Bay exit take the road north beyond Gurubashi Arena on the east side of the road (39,41)|
+C Skullsplitter Tusks |N|In the area around Ziata'Jai Ruins (42,36). Clear that camp, then hit Balia'mah Ruins (45,33) to the northeast and then Zul'Mamwe (47,38) to the southeast.  Repeat!|
+C Troll Witchery |N|Around the Balia'mah Ruins (43,41)|
+C Raptor Mastery (Part 3) |N|West of the ruins across the road (33,37).|
+C Panther Mastery (Part 3) |N|Northeast of the raptors in a suspiciously "empty" area (36,35).  They stealth.|
+C Excelsior |N|Travel north along the shore killing all the Saltwater Crocolisks you come across until an Elder Saltwater Crocolisks spawns|
+C Water Elementals |N|On the island (21,22)|
 
-R Booty Bay
-T Scaring Shaky
-A Return to MacKinley
-T The Bloodsail Buccaneers (Part 2) |N|Along the lower dock|
-A The Bloodsail Buccaneers (Part 3)
-T Return to MacKinley |N|In the house across from the half ship|
-A Voodoo Dues
-A Up to Snuff |N|Upstairs in the inn|
-T The Bloodsail Buccaneers (Part 3)
-A The Bloodsail Buccaneers (Part 4)
-N Buy pages from the AH |N|Buy any pages you don't have.  You need these pages: 1, 4, 6, 8, 10, 11, 14, 16, 18, 20, 21, 24, 25, 26, 27|
-
-C Excelsior |N|Elite crocolisk near Grom'Gol.  Stear clear of the base if you don't like dying to guards|
-R Nesingwary's Expedition |N|(35,10)|
+T Panther Mastery (Part 3) |N|Back at Nesingwary's|
 T Raptor Mastery (Part 3)
-T Panther Mastery (Part 4)
-A Raptor Mastery (Part 4)
-A The Green Hills of Stranglethorn
+T Chapter I
+T Chapter II
+T Chapter III
+T Chapter IV
+A The Green Hills of Stranglethorn |N|Skip this if you didn't get every page|
 T The Green Hills of Stranglethorn
 
-H Booty Bay |SZ|The Salty Sailor Tavern|
+T Troll Witchery |N|Back at the Rebel Camp|
+
+T Water Elementals |N|Fly back to Booty Bay|
+T Venture Company Mining
+T Skullsplitter Tusks |N|Upstairs|
 T Excelsior
-F City of Ironforge
 ]]
-
 end)
-
--- TourGuide_Alliance/41_42_Stranglethorn.lua
+++ TourGuide_Alliance/41_42_Stranglethorn.lua
@@ -1,32 +1,28 @@
 
-TourGuide:RegisterGuide("Stranglethorn Vale (41-42)", "Feralas (43-45)", "Alliance", function()
+TourGuide:RegisterGuide("Stranglethorn Vale (41-42)", "Tanaris (43)", "Alliance", function()
 return [[
 F Stormwind City
 A In Search of The Temple |N|In the Dwarven District (63,24)| |Z|Stormwind City| |NODEBUG|
 
 F Booty Bay
-h Booty bay
-A Goblin Sponsorship |N|From Baron Revilgaz in the Inn (Top Floor)|
-A Skullsplitter Tusks |N|From Kebok in the inn (2nd Floor).|
-A Tran'rek |N|From Krazek in the Inn (2nd Floor)|
+A Tran'Rek |N|From Krazek in the Inn (2nd Floor)|
 A Zanzil's Secret |N|From Crank Fizzlebub in the Inn.|
+h Booty bay
 A Akiris by the Bundle (Part 1) |N|From Privateer Bloads next to the Bank.|
 A The Bloodsail Buccaneers (Part 1) |N|From First Mate Crazz just past the "Old Port Authority" building (First big building when getting off the Boat).|
 
-C "Pretty Boy" Duncan |N|Outside, head west right after exiting Booty Bay.  He's on the shore (28,69)|
+C "Pretty Boy" Duncan |N|Outside, head west after exiting Booty Bay.  He's on the shore (28,69).|
 T The Bloodsail Buccaneers (Part 1) |N|On top of one of the barrels|
 A The Bloodsail Buccaneers (Part 2)
 C Akiris by the Bundle (Part 1) |N|Northwest across the bridge (24,65)|
 C Scaring Shaky |N|Head back to the Booty Bay entrance, then follow the path north a bit (31,67)|
 
-T Scaring Shaky |N|At "Shaky" Philippe on the peer|
+T Scaring Shaky |N|Back in Booty Bay, at "Shaky" Philippe on the pier.|
 A Return to MacKinley
 T The Bloodsail Buccaneers (Part 2) |N|At First Mate Crazz a bit further towards the Inn.|
 A The Bloodsail Buccaneers (Part 3)
 T Akiris by the Bundle (Part 1) |N|At Privateer Bloads next to the bank.|
 A Akiris by the Bundle (Part 2)
-T "Pretty Boy" Duncan |N|At Catelyn the Blade inside the Inn.|
-A The Curse of the Tides
 A Up to Snuff |N|From Deeg on the 2nd floor of the Inn.|
 T The Bloodsail Buccaneers (Part 3) |N|At Fleet Master Seahorn on the 2nd Floor of the Inn.|
 A The Bloodsail Buccaneers (Part 4)
@@ -38,37 +34,17 @@ C The Bloodsail Buccaneers (Part 4) |N|From the Booty Bay exit head east to the 
 C Keep An Eye Out |N|Kill Bloodsails| |NODEBUG|
 C Up to Snuff |N| Kill Bloodsails|
 
-C Venture Company Mining |N|From the Booty Bay exit take the road north beyond Gurubashi Arena (39,41).  Kill Venture Co. Goblins|
-C Raptor Mastery (Part 3) |N|To the northwest across the road (34,37).|
-C Panther Mastery (Part 3) |N|Shadowmaw Panther are stealth, but you can find a lot to the northeast (36,35).|
-
-C Skullsplitter Tusks |N|To the east at Ziata'Jai Ruins (42,36).  Clear that camp, then hit Balia'mah Ruins (45,33) to the northeast and then Zul'Mamwe (47,38) to the southeast.  Repeat!|
-C Goblin Sponsorship (Part 4) |O| |N|Follow the road north to the Venture Co. Base at Lake Nezferiti, go to the oil rig (42,18). Go on top of it, kill Foreman Cozzle, loot his key. Jump down and go to the small house next to the lumber mill, open the chest and get the Fuel Regulator Blueprints.|
-C Kurzen's Mystery |N|First scroll is in a tablet named "Moon Over the Vale" in the Bal'lal Ruins (29,20).  Second is in a tablet named "Gri"lek the Wanderer", west of Bal'lal Ruins underwater (24,22).  Third and Fourth are in the Ruins of Zul'Kunda (21,10).  Third is in "Fall of Gurubashi" (23,12).  Fourth is in "The Emperor's Tomb" (25,9)|
-
-T Panther Mastery (Part 3) |N|Back at Nessingwary's Camp (35,10)|
-T Raptor Mastery (Part 3)
-T Kurzen's Mystery |N|North at the rebel camp|
-
-H Booty Bay |SZ|The Salty Sailor Tavern|
-T Venture Company Mining |N|At Crank Fizzlebub in the Inn.|
-T Up to Snuff |N|At Deeg on the 2nd Floor of the Inn.|
-T Skullsplitter Tusks |N|At Kebok, 2nd floor of the Inn.|
-T Goblin Sponsorship (Part 4) |N|At Baron Revilgaz, 2nd Floor of the Inn.|
-A Goblin Sponsorship (Part 5)
-T The Bloodsail Buccaneers |N|At Fleet Master Seahorn, 2nd Floor of the Inn.|
-T Keep An Eye Out |N|At Dizzy One-Eye to the right of the Blacksmith.| |NODEBUG|
-
 C Voodoo Dues |N|Run north to Ruins of Jubuwal (33,51), kill Jon-Jon the Crow and Maury "Club Foot" Wilkins.  Run south a tad (33,53) and then east to Ruins of Aboraz (40,57), kill Chucky "Ten Thumbs"|
 C Zanzil's Secret |N|Kill the trolls, but avoid Zanzil|
 
-R Booty Bay
-T Zanzil's Secret |N|At Crank Fizzlebub in the inn.|
+H Booty Bay |SZ|The Salty Sailor Tavern|
+T Venture Company Mining |N|At Crank Fizzlebub in the Inn.|
+T Zanzil's Secret
+T Up to Snuff |N|At Deeg on the 2nd Floor of the Inn.|
+T The Bloodsail Buccaneers |N|At Fleet Master Seahorn, 2nd Floor of the Inn.|
 T Voodoo Dues |N|At "Sea Wolf" MacKinley (First house next to the Inn).|
 A Cracking Maury's Foot
 A Stoley's Debt
-
+T Keep An Eye Out |N|At Dizzy One-Eye to the right of the Blacksmith.| |NODEBUG|
 ]]
-
 end)
-
