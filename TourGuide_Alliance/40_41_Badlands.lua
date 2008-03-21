03b253d5e632ec48f2a9fb627c28b7a20020f9e4
Tekkub Stoutwrithe <tekkub@gmail.com>
2008-03-21 02:16:37 -0600

    Finished ally Badlands

--- TourGuide_Alliance/40_41_Badlands.lua
+++ TourGuide_Alliance/40_41_Badlands.lua
@@ -12,48 +12,42 @@ N Upgrade your ammo! |C|Hunter|
 
 F Loch Modan
 N Stable your pet |C|Hunter|
-h Thelsamar
 A Badlands Reagent Run |N|In the house behind the Inn (37,49)| |Z|Loch Modan|
-T Ironband Wants You! |N|Ironband's Excavation Site (65,65)| |Z|Loch Modan|
+T Ironband Wants You! |N|Down at Ironband's Excavation Site (65,65)| |Z|Loch Modan|
 A Find Agmond
 
-R Badlands |N|(46,76)|
-A Fiery Blaze Enchantments |N|(54,43)|
+R Badlands |N|Southwest of Ironband's (46,76)| |Z|Loch Modan|
+A Fiery Blaze Enchantments |N|To the south, up a hill after the dark iron dig (54,43)|
 A Mirages
 A A Dwarf and His Tools
-A Indurium |N|(42,52)|
-A Barbecued Buzzard Wings
-A Pearl Diving
+A Barbecued Buzzard Wings |N|West, in the center of the zone (42,52)|
 
 N Kill any buzzards you see |N|For "Barbecued Buzzard Wings" and "Badlands Reagent Run"|
 
 P Dash (Rank 2) |C|Hunter| |N|Tame a Ridge Stalker Patriarch (20,53)|
 P Prowl (Rank 2)| |C|Hunter|
-H Thelsamar |C|Hunter|
+H Stoutlager Inn |SZ|Thelsamar| |C|Hunter|
 N Switch back to your cat |N|Don't dismiss the Badlands cat!| |C|Hunter|
 R Badlands |C|Hunter|
 
 C Mirages |N|Grind your way to the crate in the northwest at Camp Kosh (66,21)|
-A A Sign of Hope (Part 1) |N|Grind your way to the dig site (53,29).  Note is on the right side of the dig (53,33)|
-C A Dwarf and His Tools |N|Kill the dwarfs|
+A A Sign of Hope (Part 1) |N|At Hammertoe's Digsite.  Note is on the right side of the dig (53,33)|
+C A Dwarf and His Tools |N|Kill the dwarfs at Hammertoe's Digsite or Angor Fortress (43,31)|
 
-T Mirages |N|Grind your way back (53,43)|
+T Mirages |N|Back up the hill|
 A Scrounging
 T A Dwarf and His Tools
 T A Sign of Hope (Part 1)
 
-A Tremors of the Earth (Part 1) |N|From a blood elf to the southeast (61,54).  Grind along the way.|
-N Keep an eye out for the ogre pack |N|They spawn to the south of the questgiver (62,70).  They patrol down to the southwest side of the zone and back in a circle.  You should be able to pick Boss Tho'grun off the back of the pack easily|
+A Tremors of the Earth (Part 1) |N|From a blood elf to the southeast (61,54).|
+N Keep an eye out for the ogre pack |N|They spawn to the south of the questgiver (62,70).  They patrol down to the southwest side of the zone and back in a circle.  You should be able to pick Boss Tho'grun off the back of the pack easily.|
 
-T Find Agmond |N|To the south to the Ogre camp (62,70), clear it, then grind to the dead dorf (50,62)|
+T Find Agmond |N|Dead dorf to the southwest at Agmond's End (50,62)|
 A Murdaloc
 C Murdaloc |N|To the south in the trogg camp|
-C Indurium
 C Barbecued Buzzard Wings
 
 T Barbecued Buzzard Wings |N|Back at the neutral camp in the center of the zone (42,52)|
-T Indurium
-A News for Fizzle
 
 A Study of the Elements: Rock (Part 1) |N|From Lotwil Veriatus to the west (24,44)|
 A Coolant Heads Prevail |L|3829| |O|
@@ -70,30 +64,26 @@ C Study of the Elements: Rock (Part 2) |N|Rock Elementals in the same area as th
 T Study of the Elements: Rock (Part 2)
 A Study of the Elements: Rock (Part 3)
 
-C Badlands Reagent Run |N|Kill buzzards (15,60) and coyotes (33,62)|
-C Scrounging |N|Kill ogres in the southwest (11,77)|
+C Badlands Reagent Run |N|Kill buzzards and coyotes south of the gnomes.|
+C Scrounging |N|Kill ogres at Camp Cagg to the southwest (11,77)|
 C Study of the Elements: Rock (Part 3) |N|Greater Rock Elements (14,88)|
 
 T Study of the Elements: Rock (Part 3)
-A This Is Going to Be Hard (Part 1)
-T This Is Going to Be Hard (Part 1)
-A This Is Going to Be Hard (Part 2)
-T This Is Going to Be Hard (Part 2)
-A This Is Going to Be Hard (Part 3)
-C This Is Going to Be Hard (Part 3) |N|Kill the summoned elemental!|
-T This Is Going to Be Hard (Part 3)
+A This Is Going to Be Hard (Part 1) |O| |PRE|Liquid Stone|
+T This Is Going to Be Hard (Part 1) |O|
+A This Is Going to Be Hard (Part 2) |O| |PRE|Liquid Stone|
+T This Is Going to Be Hard (Part 2) |O|
+A This Is Going to Be Hard (Part 3) |O| |PRE|Liquid Stone|
+C This Is Going to Be Hard (Part 3) |O| |N|Kill the summoned elemental!|
+T This Is Going to Be Hard (Part 3) |O|
 
-T Scrounging |N|(53,43)|
-T Tremors of the Earth (Part 1) |N|(61,54) Skip part 2|
-C Fiery Blaze Enchantment |N|Kill whelps in the ravine to the east.  Droprate kind sucks, you may want to skip this.|
-T Fiery Blaze Enchantment
+T Scrounging |N|Back at the dwarfs on the hill (53,43)|
+T Tremors of the Earth (Part 1) |N|Back at the blood elf (61,54).  Skip the follow-up|
+C Fiery Blaze Enchantments |N|Kill whelps in the ravine to the east.  Droprate kind sucks, you may want to skip this.|
+T Fiery Blaze Enchantments
 
-H Thelsamar
-T Badlands Reagent Run
-A Badlands Reagent Run II
-T Murdaloc |N|Ironband's Excavation Site (65,65)| |Z|Loch Modan|
-A Agmond's Fate
-
+T Murdaloc |N|Back at Ironband's Excavation Site in Loch Modan (65,65).  Skip the follow-up.| |Z|Loch Modan|
+T Badlands Reagent Run |N|Back at Thelsamar, skip the follow-up.|
 ]]
 end)
 
