6a767b1afbf12949c58f74c25642d93557986c53
Tekkub Stoutwrithe <tekkub@gmail.com>
2008-04-03 02:14:45 -0600

    Ally Feralas tweaks

--- TourGuide_Alliance/43_45_Feralas.lua
+++ TourGuide_Alliance/43_45_Feralas.lua
@@ -4,14 +4,13 @@ return [[
 R Feathermoon Stronghold |N|Fly to Desolace and run southwest into Feralas (43,98).  Follow the road until the path down to the docks.  Take the boat if it's there, or swim due west.| |Z|Desolace|
 
 A The Mark of Quality
+h Feathermoon Stronghold
 A The Ruins of Solarsal
 A The Missing Courier (Part 1)
 A The High Wilderness
 A In Search of Knowledge
 T The Missing Courier (Part 1) |N|(31,45)|
 A The Missing Courier (Part 2)
-f Grab flight point
-h Feathermoon Stronghold
 
 T The Ruins of Solarsal |N|Southwest to the Ruins of Solarsal, at the "Solar Gazebo" (26,52)|
 A Return to Feathermoon Stronghold
@@ -30,7 +29,7 @@ A Delivering the Relic
 T Delivering the Relic
 
 C Screecher Spirits |N|Back on the main shore, avoid elementals and giants while killing Rogue Vale Screechers.  Use Yeh'Kinya's Bramble on their corpses and talk to the spirits.  Work your way south.| |U|10699|
-T The Missing Courier (Part 2) |N|Find the boat wreckage (45,64)|
+T The Missing Courier (Part 2) |N|Find the boat wreckage to the south (45.53, 64.98)|
 A Boat Wreckage
 
 T Boat Wreckage |N|Back at the stronghold|
@@ -44,17 +43,17 @@ A Find OOX-22/FE! |O| |U|8705|
 C The Mark of Quality |N|Back on mainland, follow the road to the yeti cave (56,56).|
 T Find OOX-22/FE! |O| |N|At the back of the yeti cave.  Don't accept the escort!|
 C The High Wilderness |N|Shamans are to the south at the temple (59,73).  Brutes at Ruins of Isildien (62,54).  If you have issues finding warlocks, you should be shot.|
-A Freedom for All Creatures |N|Clear around the cage in the camp, then head up the path behind the camp (65.63,46.76)|
-C Freedom for All Creatures |N|Escort!|
+A Freedom for All Creatures |N|Clear around the cage in the camp, then head up the path behind the camp (65.63, 46.76)|
+C Freedom for All Creatures |N|Lead Kindal Moonweaver down to the cage, open it, and protect the Sprite Darters as they escape.|
 T Freedom for All Creatures
 A Doling Justice (Part 1)
 C Doling Justice (Part 1)
 T Doling Justice (Part 1)
 A Doling Justice (Part 2)
 
-T The Woodpaw Gnolls |N|(73,56)|
+T The Woodpaw Gnolls |N|Look for a backpack hanging on a tree in one of the gnoll camps (73,56)|
 A The Writhing Deep
-T The Writhing Deep |N|Grind to the hive (73,63).  Go inside, take the southern tunnel and stay right.  Turnin at the Zukk'Ash Pod|
+T The Writhing Deep |N|In the hive to the south (73,63).  At the bottom, take the southern tunnel and stay right.  Turnin at the Zukk'Ash Pod|
 A Freed from the Hive
 
 H Feathermoon Stronghold
@@ -77,12 +76,9 @@ A The Borrower
 T Rise of the Silithid |N|In the temple of the moon, upstairs (39,86)| |Z|Darnassus| |NODEBUG|
 T Doling Justice (Part 2)
 
-H Feathermoon Stronghold
-F Gadgetzan
+F Gadgetzan |N|Hearth to Feathermoon Stronghold to cut down time, if it's up.|
 T The Borrower
 A The Super Snapper FX
-
 ]]
-
 end)
 
