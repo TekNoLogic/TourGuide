28e55958b8b9446c8aeb44a622b9fe3b3ef23f60
Tekkub Stoutwrithe <tekkub@gmail.com>
2008-04-04 13:40:27 -0600

    Ally Hinterlands tweaks

--- TourGuide_Alliance/46_47_Hinterlands.lua
+++ TourGuide_Alliance/46_47_Hinterlands.lua
@@ -9,40 +9,41 @@ A A Gesture of Goodwill
 A Preying on the Predators
 A Skulk Rock Clean-up |N|In the inn, to the right|
 A Troll Necklace Bounty
-h Aerie Peak
-T Featherbeard's Endorsement |N|Near the fireplace (13,55)|
+h Wildhammer Keep
+T Featherbeard's Endorsement |N|South at a small house, near the fireplace (13,55)|
 A In Pursuit of Featherbeard
 
+K Mangy Silverman wolves |Q|Preying on the Predators| |QO|Mangy Silvermane slain: 10/10|
+N Check First Cage |Q|Witherbark Cages| |QO|Check First Cage: 1/1| |N|At the troll camp (23,58)|
+N Check Second Cage |Q|Witherbark Cages| |QO|Check Second Cage: 1/1| |N|At the troll camp (23,58)|
 C A Gesture of Goodwill |N|To the south in a cave at Bogen's Ledge (25,66)|
-C Witherbark Cages |N|Down at the troll camp (23,58).  Last cage is to the east (31,57)|
+C Witherbark Cages |N|Last cage is to the east (31,57)|
 C Preying on the Predators
 C Troll Necklace Bounty |N|Kill any trolls you can find if you don't have all your necklaces|
-C Skulk Rock Clean-up |N|At Agor'watha (46,42)|
-C The Super Snapper FX |N|Jump off the waterfall at the east edge of the zone (81,46).  Run south along the shore|
+C Skulk Rock Clean-up |N|Northeast at Agor'watha (46,42)|
+C The Super Snapper FX |N|Jump off the waterfall at the east edge of the zone (81,46).  Run south along the shore.  Find tho aggro turtle and take it's picture.| |U|9328|
 C Whiskey Slim's Lost Grog
 
-H Aerie Peak
+H Aerie Peak |SZ|Wildhammer Keep|
 T Skulk Rock Clean-up
 T Troll Necklace Bounty
 T A Gesture of Goodwill
 T Preying on the Predators
 T Witherbark Cages |N|Up in the cave|
 A The Altar of Zul
-T In Pursuit of Featherbeard |N|To the southeast in a temple (37,72)|
+T In Pursuit of Featherbeard |N|To the southeast in Shadar'Alor, in the east temple (37,72)|
 A Reclaiming the Eggs
 
-C Reclaiming the Eggs |N|Two eggs at (34,72), 3 near Atal'ai Exile in a troff (33,75)|
-A Jammal'an the Prophet |N|(33,75)|
-C The Altar of Zul |N|Just run up the alter (48,68)|
+C Reclaiming the Eggs |N|Two eggs at the alter on the southe edge of the pond (34.16, 72.91), 3 near Atal'ai Exile in a troff (33,75)|
+C The Altar of Zul |N|East at the Altar of Zul (48,68).  Just run up the alter and back down, the mob at the top is level 50.|
 C Favored of Elune?
 
-A Find OOX-09/HL |U|8704| |O|
-T Find OOX-09/HL |O| |N|(49,37)|
-A Rescue OOX-09/HL |O| |PRE|Find OOX-09/HL|
-C Rescue OOX-09/HL |O|
+A Find OOX-09/HL! |U|8704| |O|
+T Find OOX-09/HL! |O| |N|On an island north of Agor'watha (49,37)|
+A Rescue OOX-09/HL! |O| |PRE|Find OOX-09/HL!|
+C Rescue OOX-09/HL! |O| |N|Just abandon if you fail.|
 
-H Aerie Peak
-N Turn in any more necklaces
+H Aerie Peak |SZ|Wildhammer Keep|
 T Reclaiming the Eggs
 T The Altar of Zul |N|Cave again|
 A Thadius Grimshade
@@ -52,6 +53,4 @@ T Favored of Elune?
 T The Super Snapper FX
 A Return to Troyas
 ]]
-
 end)
-
