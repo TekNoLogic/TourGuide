49dc0c66db5cf527f8a1027d2469c2d1f33326bd
Tekkub Stoutwrithe <tekkub@gmail.com>
2008-04-04 13:37:55 -0600

    Ally Feralas tweaks

--- TourGuide_Alliance/47_Feralas.lua
+++ TourGuide_Alliance/47_Feralas.lua
@@ -14,14 +14,13 @@ C Fuel for the Zapping
 T Zapped Giants
 T Fuel for the Zapping
 
-A An Orphan Looking For a Home |N|At the grimtotem camp (65.9,45.6), path at (65.63,46.76)| |NODEBUG|
-C Improved Quality
+C Improved Quality |N|North at Rage Scar Hold (51,31)|
 
-A The Giant Guardian |N|From Rockbiter (42,22)|
-C The Stave of Equinex |N|Essences at (38.52,15.79), (40.55,12.64), (39.93,9.46), (37.76,12.15). Use the staff at the monolith (38.8,13.1)| |U|9363|
+A The Giant Guardian |N|Find the giant "Rockbiter" north near the twin colossals (42,22)|
+C The Stave of Equinex |N|Northwest at the Ruins of Ravenwind.  Find the essences (38.52, 15.79), (40.55, 12.64), (39.93, 9.46), (37.76, 12.15). Use the staff at the monolith (38.8, 13.1)| |U|9363|
 T The Stave of Equinex
 A The Morrow Stone
-T The Giant Guardian |N|Back near the last flame (38.22,10.30)|
+T The Giant Guardian |N|Back near the last flame (38.22, 10.30)|
 A Wandering Shay
 C Wandering Shay |N|Grab the bell from the chest, use it if she wanders away| |U|9189|
 T Wandering Shay
@@ -32,6 +31,8 @@ A Pristine Yeti Hide |O| |U|18969|
 T Pristine Yeti Hide |O|
 T The Morrow Stone
 
+A An Orphan Looking For a Home |N|East at the grimtotem camp (65.9, 45.6)| |NODEBUG|
+R Thalanaar |N|Follow the road east to the border of Thousand Needles.  Watch out for the horde town!|
 ]]
 
 end)
