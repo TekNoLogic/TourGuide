d71c9356c77b22c0e6d1d183d561cfbfd3bde099
lowlee <lowlee@lowlee.info>
2008-05-01 11:19:05 +1000

    Horde Swamp of Sorrows 41-42 Changes Resort some completions to make more sense Added coords to some quests Added more notes

--- TourGuide_Horde/41_42_Swamp_of_Sorrows.lua
+++ TourGuide_Horde/41_42_Swamp_of_Sorrows.lua
@@ -1,33 +1,33 @@
 TourGuide:RegisterGuide("Swamp of Sorrows (41-42)","Stranglethorn Vale (42-43)", "Horde", function()
 return [[
 R Swamp of Sorrows
-C Dream Dust in the Swamp |N|Clear whelps once, will do more later (14,59)| |QID|1116|
-K Cudgel |L|6196|
+N Clear whelps around the small lake once. |N|Will do more later (14,59)|
+K Noboru the Cudgel |L|6196|
 A Noboru the Cudgel |U|6196| |QID|1392|
-T Noboru the Cudgel |N|At (25,31)| |QID|1392|
+T Noboru the Cudgel |N|At The Harborage (26.0,31.6)| |QID|1392|
 A Draenethyst Crystals |QID|1389|
 
 R Stonard |N|(45,54)|
 h Stonard
-A Lack of Surplus |QID|699|
-A Little Morsels |N|Skip Fresh Meat| |QID|9440|
-T Report to Helgrum |QID|1420|
-A Pool of Tears |QID|9610|
+A Lack of Surplus |T| |QID|699|
+A Little Morsels |N|In the other building| |T| |QID|9440|
+T Report to Helgrum |T| |QID|1420|
+A Pool of Tears |T| |QID|9610|
 
+C Lack of Surplus |N|Just north-east of Stonard| |QID|699|
 C Pool of Tears |N|Artifacts in the water around the big lake| |QID|9610|
-C Lack of Surplus |QID|699|
 
-T Lack of Surplus |N|At (81,80), skip the follow-up.| |QID|699|
+T Lack of Surplus |N|At Misty Reed Post on the southern part of the beach (81,80)| |QID|699|
 
-C Dream Dust in the Swamp |N|Kill Scalebane Dragons| |QID|1116|
-C Draenethyst Crystals |N| (62,22) along with little morsels| |QID|1389|
-C Little Morsels |N| (62,22)| |QID|9440|
+C Dream Dust in the Swamp |N|Kill Scalebane Dragons along the east and northern shores of the big lake| |QID|1116|
+C Little Morsels |N|Fallow Sanctuary (62,22). Pickup the Crystals at the same time.| |QID|9440|
+C Draenethyst Crystals |QID|1389|
 
-T Draenethyst Crystals |N|Turn in at (25,31)| |QID|1389|
+T Draenethyst Crystals |N|Back at The Harborage (26.0,31.6)| |QID|1389|
 
 H Stonard
-T Little Morsels |QID|9440|
-T Pool of Tears |QID|9610|
-A The Atal'ai Exile |QID|1429|
+T Little Morsels |T| |QID|9440|
+T Pool of Tears |T| |QID|9610|
+A The Atal'ai Exile |T| |QID|1429|
 ]]
 end)
