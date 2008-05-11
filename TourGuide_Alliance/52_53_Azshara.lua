a65aff9ed6fd9183021053410f321c9a8031a9e1
Tekkub Stoutwrithe <tekkub@gmail.com>
2008-05-11 16:09:56 -0600

    User-submitted re-done Ally Azshara guide  - This guide will NOT be integrated into the guide sequence, the user must manually load it

---
+++ TourGuide_Alliance/52_53_Azshara.lua
@@ -0,0 +1,70 @@
+
+TourGuide:RegisterGuide("Azshara (52-53)", "Felwood (53-54)", "Alliance", function()
+return [[
+A Cenarion Aid |QID|8254| |C|Priest|
+A A Simple Request |QID|8233| |C|Rogue|
+T A Simple Request |QID|8233| |C|Rogue|
+A Sealed Azure Bag |QID|8234| |C|Rogue|
+
+F Azshara |N|Fly to Forest Song or Astranaar and run east if you don't have the FP|
+
+A Spiritual Unrest |QID|5535| |N| from Loh'atu (11.4,78.1)|
+A A Land Filled with Hatred |QID|5536|
+C Spiritual Unrest |QID|5535| |N|Kill ghosts to the north at Shadowsong Shrine (14,72)|
+C A Land Filled with Hatred |QID|5536| |N|Further north at the Haldarr Encampment(19,61)|
+T Spiritual Unrest |QID|5535| |N| (11.4,78.1)|
+T A Land Filled with Hatred |QID|5536|
+
+T Meeting with the Master |QID|3503| |C|Mage| |N|from Sanath Lim-yo (28.1,50.1)|
+A Magic Dust |QID|8251| |C|Mage| |N|from Archmage Xylem (29.3,40.2)|
+
+N Get Beth'Amara Rubbing |N|In the Ruins of Eldarath (36.86, 53.20)| |L|10563|
+N Get Markri Rubbing |N|In the Temple of Zin-Malor (39.28, 55.51)| |L|10565|
+N Get Jin'yael Rubbing |N|In the Temple of Zin-Malor (39.55, 50.09)| |L|10564|
+
+T The Hunter's Charm |QID|8151| |C|Hunter| |N|(42.38,42.69)|
+A Courser Antlers |QID|8153| |C|Hunter|
+
+T Cenarion Aid |QID|8254| |C|Priest| |N|(42.38,42.69)|
+A Of Coursers We Know |QID|8255| |C|Priest|
+C Sealed Azure Bag |QID|8234| |C|Rogue| |N| Can be pickpocketed from Timbermaw Shaman around (45,25).  If you are Unfriendly or better with them, you will have to check 'At War' in their reputation panel|
+
+A Kim'jael Indeed! |QID|3601| |N|On top of a hill near the satyr camp to the north (53.4, 21.8)|
+C Kim'jael Indeed! |QID|3601| |N|To the south at the Thallasian Base Camp (56,29)(57,28)(60,31)(59,28).  Loot the crates till you find all his stuff.|
+C Magic Dust |QID|8251| |C|Mage| |N| Keep killing Blood Elves until you have all 10 dusts.|
+T Kim'jael Indeed! |QID|3601| |N|(53.4, 21.8)|
+A Kim'jael's "Missing" Equipment |QID|5534|
+
+T Meeting with the Master |QID|3503| |C|Mage| |N|from Sanath Lim-yo (28.1,50.1)|
+T Sealed Azure Bag |QID|8234| |C|Rogue| |N|(29.3,40.2)|
+A Encoded Fragments |QID|8235| |C|Rogue|
+
+T Magic Dust |C|Mage| |QID|8251| |N|(28.1,50.1)(29.3,40.2)|
+A The Siren's Coral |QID|8252| |C|Mage|
+
+C Courser Antlers |QID|8153| |C|Hunter|
+T Courser Antlers |QID|8153| |C|Hunter| |N|(42.38, 42.69)|
+A Wavethrashing |QID|8231| |C|Hunter|
+C Wavethrashing |QID|8231| |C|Hunter| |N|Down in the water, of course (87,26)| |C|Hunter|
+T Wavethrashing |QID|8231| |C|Hunter| |N|Skip the next part if you don't plan on doing Sunken Temple|
+
+C Of Coursers We Know |QID|8255| |C|Priest|
+T Of Coursers We Know |QID|8255| |C|Priest| |N|(42.38, 42.69)|
+A The Ichor of Undeath |QID|8256| |C|Priest|
+T The Ichor of Undeath |QID|8256| |C|Priest| |N|Skip the next part if you don't plan on doing Sunken Temple|
+
+C Kim'jael's "Missing" Equipment |QID|5534| |N|Kill naga in the ruins (37,52)|
+C The Siren's Coral |QID|8252| |C|Mage| |N| The Enchanted Coral only drops from the Sirens|
+N type '/target General' inside the Temple |N|There's a possible rare spawn named General Fangferror up the ramp in the Temple of Zin-Malor that often drops a blue sword.|
+T Kim'jael's "Missing" Equipment |QID|5534| |N|Back up north (53.4, 21.8)|
+T The Siren's Coral |QID|8252| |C|Mage| |N|(28.1,50.1)(29.3,40.2) Skip the next part if you don't plan on doing Sunken Temple|
+T Encoded Fragments |QID|8235| |C|Rogue| |N|(28.1,50.1)(29.3,40.2) Skip the next part if you don't plan on doing Sunken Temple|
+
+C Arcane Runes |QID|3449| |N|Get Sael'hai Rubbing (42.32, 64.22)|
+T Arcane Runes |QID|3449| |N|Use your flare gun at the helipad in the far southeastern edge of the zone (77.85, 91.38)|
+A Return to Tymor
+
+H Darnassus
+T Morrowgrain Research (Part 2) |N|Save 10 soil and 15 Morrowgrain for later|
+]]
+end)
