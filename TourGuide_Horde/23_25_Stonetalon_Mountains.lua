d61a385b4630aec0fdbb4c9db2a1c67744d9fb5e
Tekkub Stoutwrithe <tekkub@gmail.com>
2008-05-11 22:51:55 -0600

    More fixes to Horde Stonetalon

--- TourGuide_Horde/23_25_Stonetalon_Mountains.lua
+++ TourGuide_Horde/23_25_Stonetalon_Mountains.lua
@@ -6,10 +6,10 @@ T Boulderslide Ravine |N|Skip the follow-up| |T| |QID|6421|
 A Elemental War |T| |QID|6393|
 A Harpies Threaten |T| |QID|6282|
 h Sun Rock Retreat |T|
-A Cycle of Rebirth |N|Pick up seeds around (48,41)| |QID|6301|
-C Cycle of Rebirth |QID|6301|
-C Cenarius' Legacy |N|Done around (35,14)| |QID|1087|
-C Jin'Zil's Forest Magic |N|Mobs range from Stonetalon Peak to the north side of Mirkfallon Lake.| |QID|1058|
+A Cycle of Rebirth |QID|6301|
+C Cycle of Rebirth |N|Pick up seeds around Mirkfallon Lake (48,41)| |QID|6301|
+C Jin'Zil's Forest Magic |N|Mobs range from the north side of Mirkfallon Lake up to Stonetalon Peak.| |QID|1058|
+C Cenarius' Legacy |N|In the center of Stonetalon Peak.| |QID|1087|
 T Cycle of Rebirth |QID|6301|
 A New Life |QID|6381|
 T Cenarius' Legacy |QID|1087|
@@ -21,7 +21,7 @@ C Gerenzo Wrenchwhistle |N|At (64,41)| |QID|1096|
 C Shredding Machines |QID|1068|
 T Gerenzo Wrenchwhistle |QID|1096|
 T Jin'Zil's Forest Magic |QID|1058|
-T Shredding Machines |N|Turn in at near Barrens (35,27)| |QID|1068|
+T Shredding Machines |QID|1068|
 
 H Sun Rock Retreat
 C New Life |N|West in the Charred Vale (32,67)| |QID|6381|
