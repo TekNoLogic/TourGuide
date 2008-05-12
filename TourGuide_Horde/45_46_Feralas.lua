3c08f09220a27296f948a99357851e2a7349f293
lowlee <lowlee@lowlee.info>
2008-05-13 07:28:37 +1000

    Update Horde Feralas 45-46 QID's added, Notes updated, Quest order fixed

--- TourGuide_Horde/45_46_Feralas.lua
+++ TourGuide_Horde/45_46_Feralas.lua
@@ -1,59 +1,59 @@
 TourGuide:RegisterGuide("Feralas (45-46)", "Azshara (46-47)", "Horde", function()
 return [[
 F Freewind Post
-R Camp Mojache |N|South in Feralas|
-A A New Cloak's Sheen |QID|2973|
-A The Ogres of Feralas (Part 1)
-A Gordunni Cobalt |QID|2987|
-A War on the Woodpaw |QID|2862|
-A The Mark of Quality |QID|2822|
-A A Strange Request |QID|3121|
-h Camp Mojache
+R Camp Mojache |N|West in Feralas|
+A A New Cloak's Sheen |T| |QID|2973|
+A The Ogres of Feralas (Part 1) |T| |QID|2975| 
+A Gordunni Cobalt |T| |QID|2987|
+A War on the Woodpaw |T| |QID|2862|
+A The Mark of Quality |T| |QID|2822|
+A A Strange Request |T| |QID|3121|
+h Camp Mojache |T|
 
 C War on the Woodpaw |N|North of Camp Mojache (71,37)| |QID|2862|
-C Gordunni Cobalt |N|North of Camp Mojache, up the path by the waterfall (75,31)| |QID|2987|
-C The Ogres of Feralas (Part 1)
-A The Gordunni Scroll |N|From the scrolls lying around on the floor| |QID|2978|
+C Gordunni Cobalt |N|North of Camp Mojache, up the path by the waterfall (75,31)| |U|9466| |QID|2987|
+C The Ogres of Feralas (Part 1) |QID|2975|
+A The Gordunni Scroll |N|From the scrolls lying around on the floor| |U|9370| |QID|2978|
 C A New Cloak's Sheen |N|West of Camp Mojache, on the north side of the path (70,47)| |QID|2973|
 
-T War on the Woodpaw |N|Back at Camp Mojache| |QID|2862|
-T Gordunni Cobalt |QID|2987|
-T The Ogres of Feralas (Part 1)
-T The Gordunni Scroll |QID|2978|
-T A New Cloak's Sheen |QID|2973|
-
-A Alpha Strike |QID|2863|
-A A Grim Discovery (Part 1)
-A Dark Ceremony |QID|2979|
-A The Ogres of Feralas (Part 2)
+T War on the Woodpaw |N|Back at Camp Mojache| |T| |QID|2862|
+A Alpha Strike |T| |QID|2863|
+T Gordunni Cobalt |T| |QID|2987|
+T The Ogres of Feralas (Part 1) |T| |QID|2975|
+A The Ogres of Feralas (Part 2) |T| |QID|2980|
+T The Gordunni Scroll |T| |QID|2978|
+A Dark Ceremony |T| |QID|2979|
+T A New Cloak's Sheen |T| |QID|2973|
+A A Grim Discovery (Part 1) |T| |QID|2974|
 
 C Alpha Strike |N|Southwest of Camp Mojache (72,56)| |QID|2863|
-T Alpha Strike |QID|2863|
-A Woodpaw Investigation |QID|2902|
+T Alpha Strike |T| |QID|2863|
+A Woodpaw Investigation |T| |QID|2902|
 C Woodpaw Investigation |N|Southwest of Camp Mojache (71.6,55.9)| |QID|2902|
 T Woodpaw Investigation |QID|2902|
 A The Battle Plans |QID|2903|
 
-C A Grim Discovery (Part 1) |N|West of Camp Mojache (66,46)|
+C A Grim Discovery (Part 1) |N|West of Camp Mojache (66,46)| |QID|2974|
 
 H Camp Mojache
-T The Battle Plans |QID|2903|
-T A Grim Discovery (Part 1)
-A A Grim Discovery (Part 2)
+T The Battle Plans |T| |QID|2903|
+A Zukk'ash Infestation |T| |QID|7730|
+T A Grim Discovery (Part 1) |T| |QID|2974|
+A A Grim Discovery (Part 2) |T| |QID|2976|
 A Stinglasher |QID|7731|
-A Zukk'ash Infestation |QID|7730|
+
 
 C Stinglasher |N|Both in the hive area south of Camp Mojache (74,62)| |QID|7731|
 C Zukk'ash Infestation |QID|7730|
 C Screecher Spirits |N|South of Dire Maul (55,56)| |QID|3520|
-C The Ogres of Feralas (Part 2) |N|South of Dire Maul (59,68)|
+C The Ogres of Feralas (Part 2) |N|South of Dire Maul (59,68)| |QID|2980|
 N Pick up a Hippograph Egg |N|Around (55,76) in nests|
 C Dark Ceremony |N|In the ruins to the south (59,68)| |QID|2979|
 C The Mark of Quality |N|Southwest of Dire Maul (55.4,56.4)| |QID|2822|
 
 T Stinglasher |N|Back at Camp Mojache| |QID|7731|
 T Zukk'ash Infestation |QID|7730|
-T The Ogres of Feralas (Part 2)
+T The Ogres of Feralas (Part 2) |QID|2980|
 T Dark Ceremony |QID|2979|
 T The Mark of Quality |QID|2822|
 
