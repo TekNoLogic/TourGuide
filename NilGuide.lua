97388e1a00a3811f55d58c1de26a300fa116eb77
Tekkub <tekkub@gmail.com>
2008-05-29 13:52:47 -0600

    Fix LH error if no guide loaded

--- NilGuide.lua
+++ NilGuide.lua
@@ -1,2 +1,2 @@
-TourGuide:RegisterGuide("No Guide", nil, "Alliance", function() return "A No guide loaded... |N|Click to select a guide|" end)
-TourGuide:RegisterGuide("No Guide", nil, "Horde", function() return "A No guide loaded... |N|Click to select a guide|" end)
+TourGuide:RegisterGuide("No Guide", nil, "Alliance", function() return "K No guide loaded... |N|Click to select a guide|" end)
+TourGuide:RegisterGuide("No Guide", nil, "Horde", function() return "K No guide loaded... |N|Click to select a guide|" end)
