dace2cfa7e35403e741957c81e8793742c96e488
Steffen Dietz <steffo.dietz@googlemail.com>
2008-05-30 23:57:18 +0200

    Streamlined the guide a bit. Fixed minor Typo: Mankirk -> Mankrik

--- TourGuide_Horde/20_Barrens.lua
+++ TourGuide_Horde/20_Barrens.lua
@@ -7,17 +7,19 @@ A The Ashenvale Hunt |N|From the warcaller, who patrols all over town.  You must
 
 R The Crossroads
 h The Crossroads |T|
-A Consumed by Hatred |T| |N|From Mankirk, near the south entrance to Crossroads| |QID|899|
+A Consumed by Hatred |T| |N|From Mankrik, near the south entrance to Crossroads| |QID|899|
 A Lost in Battle |T| |QID|4921|
 
-R Thunder Bluff |N|Take the road south to Camp Taurajo.  Grab the flight point, take the road west into Mulgore, then north at Bloodhoof Village.|
-A The Sacred Flame (Part 1) |N|On the third level of the main rise (55.1, 53.1)| |Z|Thunder Bluff| |T| |QID|1195|
+C Lost in Battle |N|Take the road south, out of The Crossroads, and follow it for quite a while. Mankrik's wife is in a hut a bit off the road. (49.31, 50.34)| |QID|4921|
+R Camp Taurajo |N|Continue on the road south, to Camp Taurajo.|
+f Grab flight point |T|
+R Thunder Bluff |N|Take the road west into Mulgore, then north at Bloodhoof Village.|
+A The Sacred Flame (Part 1) |N|On the third level of the main rise. (55.1, 53.1)| |Z|Thunder Bluff| |T| |QID|1195|
 
 F Camp Taurajo
 A Melor Sends Word |T| |QID|1130|
 A Tribes at War |T| |QID|878|
-C Lost in Battle |N|In a hut off the road north of Camp Taurajo (49.31, 50.34)| |QID|4921|
-C Consumed by Hatred |N|Southeast of Mankirk's wife, northeast of Camp Taurajo| |QID|899|
+C Consumed by Hatred |N|Northeast of Camp Taurajo| |QID|899|
 C Tribes at War |QID|878|
 
 T Tribes at War |N|Back at Camp Taurajo| |T| |QID|878|
