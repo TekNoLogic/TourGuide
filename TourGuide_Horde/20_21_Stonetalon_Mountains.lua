9e0fdd484696471303b3f970e6b5cfbce1948ca1
Tekkub <tekkub@gmail.com>
2008-07-11 19:24:46 -0600

    Horde Stonetalon playtestes

--- TourGuide_Horde/20_21_Stonetalon_Mountains.lua
+++ TourGuide_Horde/20_21_Stonetalon_Mountains.lua
@@ -14,32 +14,31 @@ T Letter to Jin'Zil |O| |QID|1060|
 A Jin'Zil's Forest Magic |QID|1058|
 
 A Arachnophobia |N|From a sign outside Sishir Canyon (59.1, 75.8)| |QID|6284|
-C Arachnophobia |N|(52.20, 73.90)| |QID|6284|
+C Arachnophobia |N|In Sishir Canyon (52.20, 73.90)| |QID|6284|
 C Blood Feeders |QID|6461|
 C Deepmoss Spider Eggs |O| |QID|1069|
 
 T Ziz Fizziks |N|In a hut at Windshear Crag (60,63)| |QID|1483|
 A Super Reaper 6000 |QID|1093|
 
+C Super Reaper 6000 |QID|1093| |N|Kill operators on the broken down super reaper (62,53)|
 C Goblin Invaders |QID|1062|
-C Super Reaper 6000 |QID|1093|
 
 T Super Reaper 6000 |QID|1093|
 A Further Instructions |QID|1095|
 
-R Sun Rock Retreat |N|(46,59)|
-T Arachnophobia |T| |QID|6284|
+T Arachnophobia |T| |QID|6284| |N|Back at Sun Rock Retreat|
 A Boulderslide Ravine |T| |QID|6421|
-N Stable your pet |C|Hunter| |T|
 
-P Bite (Rank 3) |N|Tame a Deepmoss Creeper, along the road to the south.|
-C Boulderslide Ravine |N|(61,92)| |QID|6421|
+N Stable your pet |C|Hunter| |T|
+P Bite (Rank 3) |N|Tame a Deepmoss Creeper, along the road to the south.| |C|Hunter|
+C Boulderslide Ravine |N|Far south down the road, hidden little cave up in the mountains (61,92)| |QID|6421|
 
 T Blood Feeders |QID|6461|
 T Kill Grundig Darkcloud |QID|6629|
 T Goblin Invaders |QID|1062|
-A Shredding Machines |QID|1068|
 A The Elder Crone |QID|1063|
+A Shredding Machines |QID|1068|
 ]]
 end)
 
