2755f8e52fa0f5d1c364d2abc198e1611b45c19f
Tekkub <tekkub@gmail.com>
2008-08-17 23:40:40 -0600

    Clean up Ally Howling Fjord guide

--- TourGuide_Alliance/70_71_Howling_Fjord.lua
+++ TourGuide_Alliance/70_71_Howling_Fjord.lua
@@ -1,107 +1,110 @@
 
 TourGuide:RegisterGuide("Howling Fjord (70-71)", nil, "Alliance", function()
 return [[
-A Hell Has Frozen Over... |QID|11228| |N|Howling Fjord, Valgarde (61.13, 62.59)|
-A If Valgarde Falls... |QID|11243| |N|,  (79.76, 84.60)|
-- |QID|11243.1| |QO|Dragonflayer Invader slain: 12/12| |N|Howling Fjord, Valgarde (58.82, 59.35)|
-T If Valgarde Falls... |QID|11243| |N|Howling Fjord, Valgarde (60.53, 61.20)|
-A Rescuing the Rescuers |QID|11244| |N|Howling Fjord, Valgarde (60.53, 61.20)|
-- |QID|11244.1| |QO|Valgarde Scout Rescued: 8/8| |N|Howling Fjord, Wyrmskull Village (57.04, 55.93)|
-T Rescuing the Rescuers |QID|11244| |N|Howling Fjord, Valgarde (60.48, 61.12)|
-A Prisoners of Wyrmskull |QID|11255| |N|Howling Fjord, Valgarde (60.48, 61.12)|
-A The Human League |QID|11273| |N|Howling Fjord, Valgarde (60.23, 61.00)|
-A The Path to Payback |QID|11420| |N|Howling Fjord, Valgarde (60.17, 62.44)|
-A Into the World of Spirits |QID|11333| |N|Howling Fjord, Valgarde (59.74, 61.48)|
+A Hell Has Frozen Over... |QID|11228| |N|ON the dock as you get off the ship (61.13, 62.59)| |T|
+T Hell Has Frozen Over... |QID|11228| |N|Just north of the dock (79.76, 84.60)| |T|
+A If Valgarde Falls... |QID|11243| |T|
+
+C If Valgarde Falls... |QID|11243| |N|Kill Dragonflayer Invaders on the north edge of town|
+
+T If Valgarde Falls... |QID|11243| |T|
+A Rescuing the Rescuers |QID|11244| |T|
+
+C Rescuing the Rescuers |QID|11244| |N|North across the stream, impaled all over the place, eew.|
+
+T Rescuing the Rescuers |QID|11244| |N|Back in Valgarde (60.48, 61.12)| |T|
+A Prisoners of Wyrmskull |QID|11255| |T|
+A The Human League |QID|11273| |T|
+A The Path to Payback |QID|11420| |T|
+A Into the World of Spirits |QID|11333| |T|
+
 A Daggercap Divin' |QID|11443| |N|Howling Fjord, Daggercap Bay (62.84, 59.11)|
-- |QID|11333.1| |QO|Reagent Pouch: 1/1| |N|Howling Fjord, Daggercap Bay (62.05, 57.69)|
-U |cffffffff|Hitem:24421:0:0:0:0:0:0:2119156836:70|h[Nagrand Cherry]|h|r |N|Howling Fjord, Daggercap Bay (61.34, 56.45)|
-U |cffffffff|Hitem:34082:0:0:0:0:0:0:1203993116:70|h[Diving Helm]|h|r |N|Howling Fjord, Daggercap Bay (62.51, 59.62)|
-- |QID|11443.1| |QO|Valgarde Supply Crate: 10/10| |N|Howling Fjord, Daggercap Bay (62.46, 59.25)|
+U Put on the Diving Helm |U|34082|
+C Into the World of Spirits |QID|11333| |N|On the deck of the northern boat (62.05, 57.69)|
+C Daggercap Divin' |QID|11443|
 T Daggercap Divin' |QID|11443| |N|Howling Fjord, Daggercap Bay (62.17, 57.31)|
-U |cff0070dd|Hitem:27410:0:0:0:0:0:0:2051038093:70|h[Collar of Command]|h|r |N|Howling Fjord, Daggercap Bay (61.63, 57.43)|
-T The Human League |QID|11273| |N|Howling Fjord, Wyrmskull Village (59.13, 54.54)|
-A Zedd's Probably Dead |QID|11274| |N|Howling Fjord, Wyrmskull Village (59.13, 54.54)|
-A Into Utgarde! |QID|11252| |N|Howling Fjord, Wyrmskull Village (59.52, 48.88)|
-T Zedd's Probably Dead |QID|11274| |N|Howling Fjord, Wyrmskull Village (56.62, 52.44)|
-A And Then There Were Two... |QID|11276| |N|Howling Fjord, Wyrmskull Village (56.62, 52.44)|
-N catacomb entrance |N|Utgarde Keep, Utgarde Catacombs (79.04, 79.80)|
-T And Then There Were Two... |QID|11276| |N|Utgarde Keep, Utgarde Catacombs (79.18, 81.49)|
-A The Depths of Depravity |QID|11277| |N|Utgarde Keep, Utgarde Catacombs (79.18, 81.49)|
-A The Shining Light |QID|11288| |N|Utgarde Keep, Utgarde Catacombs (79.99, 82.06)|
-- |QID|11420.1| |QO|Harpoon Operation Manual: 1/1| |N|Utgarde Keep, Utgarde Catacombs (79.97, 82.07)|
-- |QID|11288.1| |QO|Sacred Artifact: 1/1| |N|Utgarde Keep, Utgarde Catacombs (79.10, 81.41)|
-T The Shining Light |QID|11288| |N|Utgarde Keep, Utgarde Catacombs (80.00, 82.07)|
-A Guided by Honor |QID|11289| |N|Utgarde Keep, Utgarde Catacombs (80.00, 82.07)|
-- |QID|11277.1| |QO|Wyrmskull Tablet: 10/10| |N|Utgarde Keep, Utgarde Catacombs (79.76, 82.10)|
-T The Depths of Depravity |QID|11277| |N|Utgarde Keep, Utgarde Catacombs (79.18, 81.49)|
-A The Ring of Judgement |QID|11299| |N|Utgarde Keep, Utgarde Catacombs (79.18, 81.49)|
-T The Ring of Judgement |QID|11299| |N|Utgarde Keep, Ring of Judgement (78.76, 82.68)|
-A Stunning Defeat at the Ring |QID|11300| |N|Utgarde Keep, Ring of Judgement (78.76, 82.68)|
-- |QID|11300.1| |QO|Ancient Cipher: 1/1| |N|Utgarde Keep, Ring of Judgement (78.53, 82.73)|
-T Stunning Defeat at the Ring |QID|11300| |N|Utgarde Keep, Utgarde Catacombs (79.17, 81.50)|
-A Return to Valgarde |QID|11278| |N|Utgarde Keep, Utgarde Catacombs (79.17, 81.50)|
+
+T The Human League |QID|11273| |N|In a building in Wyrmskull Village (59.13, 54.54)|
+A Zedd's Probably Dead |QID|11274|
+T Zedd's Probably Dead |QID|11274| |N|Hanging from a hook in the worg pit (56.62, 52.44).  Eeeeeeeeewwwww...|
+A And Then There Were Two... |QID|11276|
+
+T And Then There Were Two... |QID|11276| |N|Take the bridge over the worg pit and head down into the Utgarde Catacombs (79.04, 79.80)|
+A The Depths of Depravity |QID|11277|
+A The Shining Light |QID|11288| |N|Down the hall, hang a left (79.99, 82.06)|
+A The Shining Light |QID|11288| |N|Back down the hall, down the stairs, wade thru a bunch of undead (79.10, 81.41)|
+T The Shining Light |QID|11288| |N|Back upstairs|
+A Guided by Honor |QID|11289|
+C The Path to Payback |QID|11420| |N|On the floor right next to you!|
+C The Depths of Depravity |QID|11277| |N|Tablets scattered all over the place|
+T The Depths of Depravity |QID|11277| |N|Back near the entrance to the catacombs (79.18, 81.49)|
+A The Ring of Judgement |QID|11299|
+T The Ring of Judgement |QID|11299| |N|Back down the hall, hang a right, down the stairs in the Ring of Judgement (78.76, 82.68)|
+A Stunning Defeat at the Ring |QID|11300|
+C Stunning Defeat at the Ring |QID|11300| |N|Kill a few named mobs, the item drops for everyone so you don't have to group up to do this if others are working on it.|
+T Stunning Defeat at the Ring |QID|11300| |N|Back near the entrance to the catacombs (79.17, 81.50)|
+A Return to Valgarde |QID|11278|
 - |QID|11255.1| |QO|Captured Valgarde Prisoner Rescued: 3/3| |N|Howling Fjord, Wyrmskull Village (57.68, 53.35)|
-T Return to Valgarde |QID|11278| |N|Howling Fjord, Valgarde (60.17, 60.99)|
-A The Explorers' League Outpost |QID|11448| |N|Howling Fjord, Valgarde (60.17, 60.99)|
-T Prisoners of Wyrmskull |QID|11255| |N|Howling Fjord, Valgarde (60.35, 61.03)|
-A Dragonflayer Battle Plans |QID|11290| |N|Howling Fjord, Valgarde (60.35, 61.03)|
-T The Path to Payback |QID|11420| |N|Howling Fjord, Valgarde (60.13, 62.38)|
-A Locating the Mechanism |QID|11426| |N|Howling Fjord, Valgarde (60.13, 62.38)|
-T Guided by Honor |QID|11289| |N|Howling Fjord, Valgarde (59.79, 62.40)|
-T Into the World of Spirits |QID|11333| |N|Howling Fjord, Valgarde (59.76, 61.55)|
-A The Echo of Ymiron |QID|11343| |N|Howling Fjord, Valgarde (59.76, 61.55)|
-U |cffffffff|Hitem:33637:0:0:0:0:0:0:959160948:70|h[Incense Burner]|h|r |N|Howling Fjord, Wyrmskull Village (59.23, 56.58)|
-- |QID|11343.1| |QO|Secrets of Wyrmskull Uncovered| |N|Howling Fjord, Wyrmskull Village (60.15, 50.73)|
-- |QID|11426.1| |QO|Harpoon Control Mechanism: 1/1| |N|Howling Fjord, Wyrmskull Village (60.76, 51.19)|
-- |QID|11290.1| |QO|Dragonflayer Battle Plans: 1/1| |N|Howling Fjord,  (55.68, 52.65)|
-T Dragonflayer Battle Plans |QID|11290| |N|Howling Fjord, Valgarde (60.49, 61.15)|
-A To Westguard Keep! |QID|11291| |N|Howling Fjord, Valgarde (60.49, 61.15)|
-T The Echo of Ymiron |QID|11343| |N|Howling Fjord, Valgarde (59.85, 61.55)|
-A Anguish of Nifflevar |QID|11344| |N|Howling Fjord, Valgarde (59.80, 61.52)|
-T Locating the Mechanism |QID|11426| |N|Howling Fjord, Valgarde (60.15, 62.36)|
-A Meet Lieutenant Icehammer... |QID|11427| |N|Howling Fjord, Valgarde (60.16, 62.30)|
-T Meet Lieutenant Icehammer... |QID|11427| |N|Howling Fjord,  (64.47, 46.94)|
-A Drop It then Rock It! |QID|11429| |N|Howling Fjord,  (64.47, 46.94)|
-U |cffffffff|Hitem:34051:0:0:0:0:0:0:910890846:70|h[Alliance Banner]|h|r |N|Howling Fjord, Nifflevar (67.94, 53.04)|
-- |QID|11429.2| |QO|Alliance Banner Placed: 1/1| |N|Howling Fjord, Baleheim (64.88, 39.82)|
-- |QID|11429.1| |QO|Alliance Banner Defended| |N|Howling Fjord, Baleheim (64.89, 39.87)|
-T Drop It then Rock It! |QID|11429| |N|Howling Fjord,  (64.48, 46.95)|
-A Harpoon Master Yavus |QID|11430| |N|Howling Fjord,  (64.48, 46.95)|
-- |QID|11430.1| |QO|Harpoon Master Yavus slain: 1/1| |N|Howling Fjord, Nifflevar (65.32, 56.50)|
-U |cffffffff|Hitem:33774:0:0:0:0:0:0:1110671932:70|h[Incense Burner]|h|r |N|Howling Fjord, Nifflevar (66.56, 55.49)|
-- |QID|11344.1| |QO|Secrets of Nifflevar Uncovered| |N|Howling Fjord, Nifflevar (69.13, 54.75)|
-T Harpoon Master Yavus |QID|11430| |N|Howling Fjord,  (64.47, 46.99)|
-A It Goes to 11... |QID|11421| |N|Howling Fjord,  (64.53, 46.84)|
-U |cffffffff|Hitem:34032:0:0:0:0:0:0:1072246135:70|h[Harpoon Control Mechanism]|h|r |N|Howling Fjord, Nifflevar (64.59, 54.33)|
-- |QID|11421.4| |QO|Dragonflayer Storage Facility Destroyed: 1/1| |N|Howling Fjord, Nifflevar (64.77, 52.65)|
-- |QID|11421.2| |QO|Dragonflayer Longhouse Destroyed: 1/1| |N|Howling Fjord, Nifflevar (64.76, 52.68)|
-- |QID|11421.3| |QO|Dragonflayer Dockhouse Destroyed: 1/1| |N|Howling Fjord, Nifflevar (64.77, 52.64)|
-- |QID|11421.1| |QO|Dragonflayer Defender slain: 8/8| |N|Howling Fjord, Nifflevar (64.77, 52.65)|
-T It Goes to 11... |QID|11421| |N|Howling Fjord,  (64.44, 46.89)|
-A Let's Go Surfing Now |QID|11436| |N|Howling Fjord,  (64.55, 46.76)|
-- |QID|11436.1| |QO|Go Harpoon Surfing| |N|Howling Fjord, Valgarde (60.03, 61.96)|
-T Let's Go Surfing Now |QID|11436| |N|Howling Fjord, Valgarde (60.12, 62.37)|
-T Anguish of Nifflevar |QID|11344| |N|Howling Fjord, Valgarde (59.86, 61.56)|
-N plane ride |N|Howling Fjord, Valgarde (60.75, 61.45)|
-T The Explorers' League Outpost |QID|11448| |N|Howling Fjord, Explorers' League Outpost (74.92, 65.44)|
-A Problems on the High Bluff |QID|11474| |N|Howling Fjord, Explorers' League Outpost (74.92, 65.44)|
-T Problems on the High Bluff |QID|11474| |N|Howling Fjord, Explorers' League Outpost (74.99, 65.46)|
-A Tools to Get the Job Done |QID|11475| |N|Howling Fjord, Explorers' League Outpost (74.99, 65.46)|
-A Trust is Earned |QID|11460| |N|Howling Fjord, Explorers' League Outpost (75.30, 64.99)|
-A Out of My Element? |QID|11477| |N|Howling Fjord, Ivald's Ruin (78.81, 48.96)|
-- |QID|11477.3| |QO|Iron Rune Sage slain: 2/2| |N|Howling Fjord, Ivald's Ruin (79.00, 48.37)|
-- |QID|11475.1| |QO|Building Tools: 1/1| |N|Howling Fjord, Ivald's Ruin (79.04, 47.59)|
-- |QID|11477.2| |QO|Iron Rune Laborer slain: 5/5| |N|Howling Fjord, Ivald's Ruin (79.09, 46.77)|
-N Destroyers around the outside top |N|Howling Fjord, Ivald's Ruin (79.87, 45.89)|
-- |QID|11477.1| |QO|Iron Rune Destroyer slain: 10/10| |N|Howling Fjord, Ivald's Ruin (80.19, 47.56)|
-T Out of My Element? |QID|11477| |N|Howling Fjord, Ivald's Ruin (78.80, 48.93)|
-T Tools to Get the Job Done |QID|11475| |N|Howling Fjord, Explorers' League Outpost (75.06, 65.44)|
-A We Can Rebuild It |QID|11483| |N|Howling Fjord, Explorers' League Outpost (75.06, 65.44)|
-A We Have the Technology |QID|11484| |N|Howling Fjord, Explorers' League Outpost (75.06, 65.44)|
-- |QID|11460.1| |QO|Fjord Rock Falcon Fed| |N|Howling Fjord, Explorers' League Outpost (75.27, 64.99)|
-T Trust is Earned |QID|11460| |N|Howling Fjord, Explorers' League Outpost (75.27, 64.99)|
-A The Ransacked Caravan |QID|11465| |N|Howling Fjord, Explorers' League Outpost (75.27, 64.99)|
+C Prisoners of Wyrmskull |QID|11255| |N|You should have plenty of keys to do this.  Kill more vrykul if you don't.|
 
+T Return to Valgarde |QID|11278| |N|Back at Valgarde (60.17, 60.99)| |T|
+A The Explorers' League Outpost |QID|11448| |T|
+T Prisoners of Wyrmskull |QID|11255| |T|
+A Dragonflayer Battle Plans |QID|11290| |T|
+T The Path to Payback |QID|11420| |T|
+A Locating the Mechanism |QID|11426| |T|
+T Guided by Honor |QID|11289| |T|
+T Into the World of Spirits |QID|11333| |T|
+A The Echo of Ymiron |QID|11343| |T|
 
+C The Echo of Ymiron |QID|11343| |U|33637| |N|Head back to Wyrmskull Village and use the Incense.  Then go in the building near the harpooners (60.15, 50.73)|
+C Locating the Mechanism |QID|11426| |N|Kill Harpooners, of course|
+C Dragonflayer Battle Plans |QID|11290| |N|In a cave on the edge of the worg pit (55.68, 52.65)|
+
+T Dragonflayer Battle Plans |QID|11290| |N|Back in Valgarde (60.49, 61.15)| |T|
+A To Westguard Keep! |QID|11291| |T|
+T The Echo of Ymiron |QID|11343| |T|
+A Anguish of Nifflevar |QID|11344| |T|
+T Locating the Mechanism |QID|11426| |T|
+A Meet Lieutenant Icehammer... |QID|11427| |T|
+
+T Meet Lieutenant Icehammer... |QID|11427|
+A Drop It then Rock It! |QID|11429|
+C Drop It then Rock It! |QID|11429| |N|Place the Alliance Banner at the center of town in Baleheim (64.88, 39.82), kill a few vrykul.| |U|34051|
+T Drop It then Rock It! |QID|11429| |N|Back at Lieutenant Icehammer (64.48, 46.95)|
+A Harpoon Master Yavus |QID|11430|
+
+C Harpoon Master Yavus |QID|11430| |N|Kill Harpoon Master Yavus in the southern harpoon building at Nifflevar (65.32, 56.50)|
+C Anguish of Nifflevar |QID|11344| |N|Use the Incense and watch the flashback.  It seems you need to be near King Ymiron (69.13, 54.75) to get credit.| |U|33774|
+
+T Harpoon Master Yavus |QID|11430| |N|Back at Lieutenant Icehammer (64.48, 46.95)|
+A It Goes to 11... |QID|11421|
+C It Goes to 11... |QID|11421| |U|34032| |N|Use the Harpoon Control Mechanism at one of the two haroons in Nifflevar (64.77, 52.65) (64.59, 54.33).  Note that you must target the defenders to shoot at them.|
+T It Goes to 11... |QID|11421| |N|Back at Lieutenant Icehammer (64.48, 46.95)|
+A Let's Go Surfing Now |QID|11436|
+C Let's Go Surfing Now |QID|11436| |N|Pull the lever at the harpoon near the southern harpoon building (TODO)|
+
+T Let's Go Surfing Now |QID|11436| |T|
+T Anguish of Nifflevar |QID|11344| |T|
+
+F Explorers' League Outpost |N|Hop a plane ride from McGoyver (60.75, 61.45)| |T|
+T The Explorers' League Outpost |QID|11448|
+A Problems on the High Bluff |QID|11474|
+T Problems on the High Bluff |QID|11474|
+A Tools to Get the Job Done |QID|11475|
+A Trust is Earned |QID|11460|
+
+A Out of My Element? |QID|11477| |N|Northeast at Ivald's Ruin (78.81, 48.96)|
+C Tools to Get the Job Done |QID|11475| |N|In a bucket on a platform in the dig (79.04, 47.59)|
+C Out of My Element? |QID|11477| |N|Destroyers are around the outside above the dig|
+T Out of My Element? |QID|11477|
+
+N Find a Fjord Grub |N|Under loose rocks all over the place| |L|34102|
+C Trust is Earned |QID|11460| |N|Back at the Explorers' League Outpost (75.27, 64.99).  Feed the grub to the folcon in the cage|
+T Trust is Earned |QID|11460|
+T Tools to Get the Job Done |QID|11475|
+A We Can Rebuild It |QID|11483|
+A We Have the Technology |QID|11484|
+A The Ransacked Caravan |QID|11465|
 ]]
 end)
