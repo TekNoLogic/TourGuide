9e80542ff2382d0765131d20fd244fb3c93c0c52
Tekkub <tekkub@gmail.com>
2008-08-20 16:33:27 -0600

    Raw data for ally Borean Tundra guide

---
+++ TourGuide_Alliance/70_71_Borean_Tundra.lua
@@ -0,0 +1,101 @@
+
+TourGuide:RegisterGuide("Borean Tundra (70-71)", nil, "Alliance", function()
+return [[
+A Enlistment Day |QID|11672| |N|Borean Tundra, Valiance Keep (57.84, 67.67)|
+T Enlistment Day |QID|11672| |N|Borean Tundra, Valiance Keep (56.69, 72.57)|
+A A Time for Heroes |QID|11727| |N|Borean Tundra, Valiance Keep (56.69, 72.57)|
+T A Time for Heroes |QID|11727| |N|Borean Tundra, Valiance Keep (56.40, 69.58)|
+A The Siege |QID|11797| |N|Borean Tundra, Valiance Keep (56.40, 69.58)|
+A A Soldier in Need |QID|11789| |N|Borean Tundra, Sands of Nasam (55.00, 68.83)|
+- |QID|11797.1| |QO|Crypt Crawler slain: 6/6| |N|Borean Tundra, Sands of Nasam (53.04, 72.03)|
+T The Siege |QID|11797| |N|Borean Tundra, Valiance Keep (56.38, 69.49)|
+A Death From Above |QID|11889| |N|Borean Tundra, Valiance Keep (56.38, 69.49)|
+- |QID|11789.1| |QO|Hawthorn's Anti-Venom: 1/1| |N|Borean Tundra, The Stormbreaker (57.47, 69.24)|
+A Cultists Among Us |QID|11920| |N|Borean Tundra, The Stormbreaker (57.51, 69.18)|
+T Cultists Among Us |QID|11920| |N|Borean Tundra, The Stormbreaker (57.80, 69.13)|
+A Notify Arlos |QID|11791| |N|Borean Tundra, The Stormbreaker (57.80, 69.13)|
+T Notify Arlos |QID|11791| |N|Borean Tundra, Valiance Keep (56.69, 72.57)|
+A A Diplomatic Mission |QID|12141| |N|Borean Tundra, Valiance Keep (56.69, 72.57)|
+A Enemies of the Light |QID|11792| |N|Borean Tundra, Valiance Keep (56.63, 72.51)|
+T A Soldier in Need |QID|11789| |N|Borean Tundra, Valiance Keep (55.02, 68.84)|
+U |cffffffff|Hitem:35278:0:0:0:0:0:0:481373504:70|h[Reinforced Net]|h|r |N|Borean Tundra, Valiance Keep (55.08, 69.92)|
+- |QID|11889.1| |QO|Scourged Flamespitter slain: 6/6| |N|Borean Tundra, Sands of Nasam (54.97, 70.37)|
+T Death From Above |QID|11889| |N|Borean Tundra, Valiance Keep (56.33, 69.46)|
+A Plug the Sinkholes |QID|11897| |N|Borean Tundra, Valiance Keep (56.33, 69.46)|
+U |cffffffff|Hitem:35704:0:0:0:0:0:0:1832012417:70|h[Incendiary Explosives]|h|r |N|Borean Tundra,  (50.60, 71.43)|
+- |QID|11897.1| |QO|Explosives set at southern sinkhole: 1/1| |N|Borean Tundra,  (50.60, 71.43)|
+- |QID|11897.2| |QO|Explosives set at northern sinkhole: 1/1| |N|Borean Tundra, Farshire (54.26, 63.89)|
+T Plug the Sinkholes |QID|11897| |N|Borean Tundra, Valiance Keep (56.42, 69.49)|
+A Farshire |QID|11928| |N|Borean Tundra, Valiance Keep (56.42, 69.49)|
+A Word on the Street |QID|11927| |N|Borean Tundra, Valiance Keep (56.70, 69.52)|
+A Nick of Time |QID|11575| |N|Borean Tundra, Valiance Keep (58.41, 67.85)|
+T Word on the Street |QID|11927| |N|Borean Tundra, Valiance Keep (58.72, 68.32)|
+A Thassarian, My Brother |QID|11599| |N|Borean Tundra, Valiance Keep (58.72, 68.32)|
+A Distress Call |QID|11707| |N|Borean Tundra, Valiance Keep (58.81, 68.65)|
+T Farshire |QID|11928| |N|Borean Tundra, Farshire (58.15, 62.75)|
+A Military?  What Military? |QID|11901| |N|Borean Tundra, Farshire (58.15, 62.75)|
+T Military?  What Military? |QID|11901| |N|Borean Tundra, Farshire Mine (56.82, 55.67)|
+A Pernicious Evidence |QID|11902| |N|Borean Tundra, Farshire Mine (56.82, 55.67)|
+T Thassarian, My Brother |QID|11599| |N|Borean Tundra, Farshire Mine (56.05, 55.46)|
+A The Late William Allerton |QID|11600| |N|Borean Tundra, Farshire Mine (56.05, 55.46)|
+T Pernicious Evidence |QID|11902| |N|Borean Tundra, Farshire (58.19, 62.76)|
+A It's Time for Action |QID|11903| |N|Borean Tundra, Farshire (58.19, 62.76)|
+A Take No Chances |QID|11913| |N|Borean Tundra, Farshire (58.19, 62.76)|
+A Reference Material |QID|11908| |N|Borean Tundra, Farshire (58.20, 62.89)|
+U |cffffffff|Hitem:35491:0:0:0:0:0:0:1488370430:70|h[Wendy's Torch]|h|r |N|Borean Tundra, Farshire (55.54, 60.23)|
+- |QID|11908.1| |QO|Fields, Factories and Workshops: 1/1| |N|Borean Tundra, Farshire (55.77, 58.37)|
+- |QID|11913.1| |QO|Farshire Grain burnt: 8/8| |N|Borean Tundra, Farshire (55.40, 59.10)|
+- |QID|11903.1| |QO|Plagued Scavenger slain: 14/14| |N|Borean Tundra, Farshire (55.89, 60.18)|
+T Take No Chances |QID|11913| |N|Borean Tundra, Farshire (58.24, 62.74)|
+T It's Time for Action |QID|11903| |N|Borean Tundra, Farshire (58.24, 62.74)|
+A Fruits of Our Labor |QID|11904| |N|Borean Tundra, Farshire (58.24, 62.74)|
+T Reference Material |QID|11908| |N|Borean Tundra, Farshire (58.22, 62.90)|
+A Repurposed Technology |QID|12035| |N|Borean Tundra, Farshire (58.22, 62.90)|
+U |cffffffff|Hitem:35943:0:0:0:0:0:0:437445380:70|h[Jeremiah's Tools]|h|r |N|Borean Tundra, Farshire (58.45, 61.16)|
+- |QID|12035.1| |QO|Harvest Collector Rewired: 5/5| |N|Borean Tundra, Farshire (56.66, 61.80)|
+N mine entrance |N|Borean Tundra, Farshire Mine (56.68, 57.39)|
+- |QID|11904.1| |QO|Ore Cart Released: 1/1| |N|Borean Tundra, Farshire Mine (57.40, 54.42)|
+N cart here |N|Borean Tundra, Farshire Mine (57.09, 54.69)|
+N summon here |N|Borean Tundra, Farshire (55.24, 63.35)|
+N sinkhole here |N|Borean Tundra, Farshire (54.88, 62.73)|
+- |QID|11792.1| |QO|Cultist Communique: 1/1| |N|Borean Tundra, Farshire (54.45, 62.28)|
+N summon here too |N|Borean Tundra, Farshire (54.16, 62.35)|
+T Fruits of Our Labor |QID|11904| |N|Borean Tundra, Farshire (58.17, 62.77)|
+A One Last Delivery |QID|11962| |N|Borean Tundra, Farshire (58.17, 62.77)|
+T Repurposed Technology |QID|12035| |N|Borean Tundra, Farshire (58.15, 62.94)|
+T One Last Delivery |QID|11962| |N|Borean Tundra, Valiance Keep (57.31, 66.59)|
+A Weapons for Farshire |QID|11963| |N|Borean Tundra, Valiance Keep (57.31, 66.59)|
+T Weapons for Farshire |QID|11963| |N|Borean Tundra, Farshire (58.16, 62.75)|
+A Call to Arms! |QID|11965| |N|Borean Tundra, Farshire (58.16, 62.75)|
+- |QID|11965.1| |QO|Farshire Bell Rung: 1/1| |N|Borean Tundra, Farshire (57.39, 59.31)|
+T Call to Arms! |QID|11965| |N|Borean Tundra, Farshire (58.20, 62.70)|
+U |cff0070dd|Hitem:38228:0:0:0:0:0:0:1428652331:70|h[Pendant of Revolutionary Thought]|h|r |N|Borean Tundra, Farshire (58.20, 62.70)|
+T The Late William Allerton |QID|11600| |N|Borean Tundra, Valiance Keep (58.73, 68.34)|
+U |cff1eff00|Hitem:37207:0:0:0:0:0:0:139710829:70|h[Seafarer Boots]|h|r |N|Borean Tundra, Valiance Keep (58.73, 68.34)|
+A Lost and Found |QID|11601| |N|Borean Tundra, Valiance Keep (58.73, 68.34)|
+T Lost and Found |QID|11601| |N|Borean Tundra, Valiance Keep (58.35, 68.03)|
+A In Wine, Truth |QID|11603| |N|Borean Tundra, Valiance Keep (58.35, 68.03)|
+- |QID|11603.1| |QO|Kul Tiras Wine: 1/1| |N|Borean Tundra,  (61.88, 65.75)|
+T In Wine, Truth |QID|11603| |N|Borean Tundra, Valiance Keep (58.52, 68.12)|
+A A Deserter |QID|11604| |N|Borean Tundra, Valiance Keep (58.52, 68.12)|
+T A Deserter |QID|11604| |N|Borean Tundra, Valiance Keep (56.70, 71.46)|
+A Cowards and Fools |QID|11932| |N|Borean Tundra, Valiance Keep (56.70, 71.46)|
+T Enemies of the Light |QID|11792| |N|Borean Tundra, Valiance Keep (56.65, 72.46)|
+A Further Investigation |QID|11793| |N|Borean Tundra, Valiance Keep (56.65, 72.46)|
+T Further Investigation |QID|11793| |N|Borean Tundra, Valiance Keep (58.53, 67.37)|
+A The Hunt is On |QID|11794| |N|Borean Tundra, Valiance Keep (58.53, 67.37)|
+U |cffffffff|Hitem:35125:0:0:0:0:0:0:1765811318:70|h[Oculus of the Exorcist]|h|r |N|Borean Tundra, Valiance Keep (58.63, 67.09)|
+- |QID|11794.3| |QO|Cultist in kitchen defeated: 1/1| |N|Borean Tundra, Valiance Keep (58.62, 67.10)|
+- |QID|11794.1| |QO|Cultist in docks defeated: 1/1| |N|Borean Tundra, Valiance Keep (59.18, 68.56)|
+- |QID|11794.2| |QO|Cultist in jail defeated: 1/1| |N|Borean Tundra, Valiance Keep (56.67, 71.76)|
+T The Hunt is On |QID|11794| |N|Borean Tundra, Valiance Keep (58.54, 67.32)|
+T A Diplomatic Mission |QID|12141| |N|Borean Tundra,  (47.15, 75.40)|
+A Karuk's Oath |QID|11613| |N|Borean Tundra,  (47.15, 75.40)|
+- |QID|11613.1| |QO|Skadir Raider slain: 6/6| |N|Borean Tundra, Riplash Strand (46.13, 80.48)|
+- |QID|11613.2| |QO|Skadir Longboatsman slain: 5/5| |N|Borean Tundra, Riplash Strand (47.48, 79.62)|
+T Karuk's Oath |QID|11613| |N|Borean Tundra,  (47.14, 75.42)|
+A Gamel the Cruel |QID|11619| |N|Borean Tundra,  (47.14, 75.42)|
+- |QID|11619.1| |QO|Gamel the Cruel slain: 1/1| |N|Borean Tundra, Riplash Strand (46.26, 78.83)|
+T Gamel the Cruel |QID|11619| |N|Borean Tundra,  (47.15, 75.40)|
+]]
+end)
