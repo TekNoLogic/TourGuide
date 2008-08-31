debaa4115512087901ac0437b66b08c96e6bb486
Tekkub <tekkub@gmail.com>
2008-08-30 19:23:28 -0600

    More raw data for Ally Fjord

--- TourGuide_Alliance/70_71_Howling_Fjord.lua
+++ TourGuide_Alliance/70_71_Howling_Fjord.lua
@@ -105,5 +105,207 @@ T Tools to Get the Job Done |QID|11475|
 A We Can Rebuild It |QID|11483|
 A We Have the Technology |QID|11484|
 A The Ransacked Caravan |QID|11465|
+
+U |cff1eff00|Hitem:35839:0:0:0:0:0:0:159298047:70|h[Runed Clamshell Choker]|h|r |N|Howling Fjord, Explorers' League Outpost (74.97, 65.19)|
+U |cff0070dd|Hitem:29334:0:0:0:0:0:0:228671857:70|h[Sethekk Oracle's Focus]|h|r |N|Howling Fjord, Explorers' League Outpost (74.97, 65.19)|
+U |cff1eff00|Hitem:35818:0:0:0:0:0:0:576816486:70|h[Stretch-Hide Spaulders]|h|r |N|Howling Fjord, Explorers' League Outpost (74.97, 65.19)|
+U |cffa335ee|Hitem:21874:2979:0:0:0:0:0:90807144:70|h[Primal Mooncloth Shoulders]|h|r |N|Howling Fjord, Explorers' League Outpost (74.97, 65.19)|
+U |cff1eff00|Hitem:35832:0:0:0:0:0:0:1685293859:70|h[Blauvelt's Special Occasion Gloves]|h|r |N|Howling Fjord, Explorers' League Outpost (74.97, 65.19)|
+U |cffa335ee|Hitem:29055:2934:0:0:0:0:0:0:70|h[Handwraps of the Incarnate]|h|r |N|Howling Fjord, Explorers' League Outpost (74.97, 65.19)|
+U |cff1eff00|Hitem:35814:0:0:0:0:0:0:568883719:70|h[Benevolent Hood]|h|r |N|Howling Fjord, Explorers' League Outpost (74.97, 65.19)|
+U |cff0070dd|Hitem:27410:0:0:0:0:0:0:2051038093:70|h[Collar of Command]|h|r |N|Howling Fjord, Explorers' League Outpost (74.97, 65.19)|
+U |cff1eff00|Hitem:35827:0:0:0:0:0:0:1743972606:70|h[Regal Sceptre]|h|r |N|Howling Fjord, Explorers' League Outpost (74.97, 65.19)|
+U |cff0070dd|Hitem:25806:0:0:0:0:0:0:1588431717:70|h[Nethekurse's Rod of Torment]|h|r |N|Howling Fjord, Explorers' League Outpost (74.97, 65.19)|
+- |QID|11483.1| |QO|Large Barrel: 1/1| |N|Howling Fjord, Nifflevar (67.67, 54.92)|
+U |cffffffff|Hitem:39505:0:0:0:0:0:0:0:70|h[Scribe Tools]|h|r |N|Dalaran,  (45.94, 38.51)|
+U |cffffffff|Hitem:39354:0:0:0:0:0:0:0:70|h[Light Parchment]|h|r |N|Dalaran,  (45.72, 38.56)|
+U |cffffffff|Hitem:39354:0:0:0:0:0:0:0:70|h[Light Parchment]|h|r |N|Dalaran,  (45.80, 38.53)|
+U |cffffffff|Hitem:39354:0:0:0:0:0:0:0:70|h[Light Parchment]|h|r |N|Dalaran,  (45.71, 38.79)|
+A The Ransacked Caravan |QID|11465| |N|,  (79.79, 84.70)|
+U |cffffffff|Hitem:8485:0:0:0:0:0:0:0:70|h[Cat Carrier (Bombay)]|h|r |N|Dalaran,  (42.47, 79.06)|
+- |QID|11483.3| |QO|Dark Iron Ingots: 1/1| |N|Howling Fjord, Valgarde (60.81, 61.59)|
+- |QID|11484.1| |QO|Pristine Shoveltusk Hide: 1/1| |N|Howling Fjord,  (75.55, 63.84)|
+- |QID|11483.1| |QO|Large Barrel: 1/1| |N|Howling Fjord, Nifflevar (67.70, 54.96)|
+- |QID|11484.2| |QO|Steel Ribbing: 1/1| |N|Howling Fjord, Baleheim (65.15, 40.91)|
+- |QID|11483.2| |QO|Industrial Strength Rope: 1/1| |N|Howling Fjord, Baleheim (64.77, 40.89)|
+U |cffffffff|Hitem:34111:0:0:0:0:0:0:1722023040:70|h[Trained Rock Falcon]|h|r |N|Howling Fjord,  (69.13, 63.04)|
+- |QID|11465.1| |QO|Fjord Turkey: 5/5| |N|Howling Fjord,  (69.30, 65.36)|
+T We Can Rebuild It |QID|11483| |N|Howling Fjord, Explorers' League Outpost (75.05, 65.43)|
+T We Have the Technology |QID|11484| |N|Howling Fjord, Explorers' League Outpost (75.05, 65.43)|
+T The Ransacked Caravan |QID|11465| |N|Howling Fjord, Explorers' League Outpost (75.31, 65.02)|
+A Falcon Versus Hawk |QID|11468| |N|Howling Fjord, Explorers' League Outpost (75.31, 65.02)|
+A Iron Rune Constructs and You: Rocket Jumping |QID|11485| |N|Howling Fjord, Explorers' League Outpost (75.05, 65.43)|
+- |QID|11485.1| |QO|Rocket Jump Mastered| |N|Howling Fjord, Explorers' League Outpost (75.08, 64.55)|
+T Iron Rune Constructs and You: Rocket Jumping |QID|11485| |N|Howling Fjord, Explorers' League Outpost (75.09, 65.43)|
+A Iron Rune Constructs and You: Collecting Data |QID|11489| |N|Howling Fjord, Explorers' League Outpost (75.09, 65.43)|
+- |QID|11489.1| |QO|Test Data Collected: 1/1| |N|Howling Fjord, Explorers' League Outpost (74.82, 65.63)|
+T Iron Rune Constructs and You: Collecting Data |QID|11489| |N|Howling Fjord, Explorers' League Outpost (75.02, 65.51)|
+A Iron Rune Constructs and You: The Bluff |QID|11491| |N|Howling Fjord, Explorers' League Outpost (75.02, 65.51)|
+- |QID|11491.1| |QO|Lebronski Bluffed| |N|Howling Fjord, Explorers' League Outpost (74.79, 65.28)|
+T Iron Rune Constructs and You: The Bluff |QID|11491| |N|Howling Fjord, Explorers' League Outpost (74.99, 65.50)|
+A Lightning Infused Relics |QID|11494| |N|Howling Fjord, Explorers' League Outpost (74.99, 65.50)|
+A The Delicate Sound of Thunder |QID|11495| |N|Howling Fjord, Explorers' League Outpost (74.99, 65.50)|
+N right rune lands here |N|Howling Fjord, Baelgun's Excavation Site (73.31, 73.49)|
+N runes here |N|Howling Fjord, Baelgun's Excavation Site (72.12, 70.39)|
+N lands here |N|Howling Fjord, Baelgun's Excavation Site (74.33, 70.82)|
+- |QID|11495.1| |QO|Thundering Cave Investigated| |N|Howling Fjord, Baelgun's Excavation Site (71.52, 69.85)|
+- |QID|11494.1| |QO|Iron Rune Data Collected: 15/15| |N|Howling Fjord, Baelgun's Excavation Site (71.82, 71.62)|
+T Lightning Infused Relics |QID|11494| |N|Howling Fjord, Explorers' League Outpost (75.08, 65.47)|
+T The Delicate Sound of Thunder |QID|11495| |N|Howling Fjord, Explorers' League Outpost (75.08, 65.47)|
+A Orfus of Kamagua |QID|11573| |N|Howling Fjord, Explorers' League Outpost (74.83, 65.17)|
+A News From the East |QID|11501| |N|Howling Fjord, Explorers' League Outpost (75.05, 65.48)|
+U |cffffffff|Hitem:34121:0:0:0:0:0:0:1399690818:70|h[Trained Rock Falcon]|h|r |N|Howling Fjord,  (73.81, 65.88)|
+- |QID|11468.1| |QO|Fjord Hawk: 10/10| |N|Howling Fjord,  (75.71, 66.56)|
+T Falcon Versus Hawk |QID|11468| |N|Howling Fjord, Explorers' League Outpost (75.28, 65.01)|
+A There Exists No Honor Among Birds |QID|11470| |N|Howling Fjord, Explorers' League Outpost (75.28, 65.01)|
+U |cff1eff00|Hitem:35844:0:0:0:0:0:0:1135218774:70|h[Shock-Resistant Hood]|h|r |N|Howling Fjord, Explorers' League Outpost (75.28, 65.01)|
+U |cffffffff|Hitem:34124:0:0:0:0:0:0:418103168:70|h[Trained Rock Falcon]|h|r |N|Howling Fjord,  (76.86, 67.40)|
+- |QID|11470.1| |QO|Fjord Hawk Egg: 8/8| |N|Howling Fjord,  (76.86, 67.40)|
+N use cage here |N|Howling Fjord,  (76.84, 67.42)|
+T There Exists No Honor Among Birds |QID|11470| |N|Howling Fjord, Explorers' League Outpost (75.28, 65.02)|
+A Break the Blockade |QID|11153| |N|Howling Fjord, Westguard Keep (28.98, 41.93)|
+A The Clutches of Evil |QID|11157| |N|Howling Fjord, Westguard Keep (28.91, 44.23)|
+T To Westguard Keep! |QID|11291| |N|Howling Fjord, Westguard Keep (28.91, 44.23)|
+T News From the East |QID|11501| |N|Howling Fjord, Westguard Keep (28.91, 44.23)|
+A Shoveltusk Soup Again? |QID|11155| |N|Howling Fjord, Westguard Inn (31.16, 40.90)|
+A Break the Blockade |QID|11153| |N|,  (70.32, 77.39)|
+A One Size Does Not Fit All |QID|11190| |N|Howling Fjord, Westguard Keep (33.95, 43.75)|
+A Root Causes |QID|11182| |N|Howling Fjord, Ember Clutch (36.62, 48.61)|
+- |QID|11157.1| |QO|Proto-Drake Egg destroyed: 15/15| |N|Howling Fjord, Ember Clutch (38.99, 49.24)|
+- |QID|11182.2| |QO|Skeld Drakeson slain: 1/1| |N|Howling Fjord, Ember Clutch (41.42, 52.58)|
+- |QID|11182.1| |QO|Dragonflayer Handler slain: 5/5| |N|Howling Fjord, Ember Clutch (41.01, 51.55)|
+- |QID|11157.2| |QO|Proto-Whelp slain: 15/15| |N|Howling Fjord, Ember Clutch (39.76, 47.26)|
+T Root Causes |QID|11182| |N|Howling Fjord, Ember Clutch (41.12, 49.42)|
+N this guy wanders the burning area |N|Howling Fjord, Ember Clutch (41.12, 49.42)|
+- |QID|11155.1| |QO|Shoveltusk Meat: 6/6| |N|Howling Fjord,  (42.18, 44.22)|
+- |QID|11190.1| |QO|Westguard Cannonball: 10/10| |N|Howling Fjord,  (37.30, 44.61)|
+T One Size Does Not Fit All |QID|11190| |N|Howling Fjord, Westguard Keep (33.95, 43.77)|
+T Shoveltusk Soup Again? |QID|11155| |N|Howling Fjord, Westguard Inn (31.16, 40.88)|
+T The Clutches of Evil |QID|11157| |N|Howling Fjord, Westguard Keep (28.91, 44.14)|
+A Mage-Lieutenant Malister |QID|11187| |N|Howling Fjord, Westguard Keep (28.91, 44.14)|
+T Mage-Lieutenant Malister |QID|11187| |N|Howling Fjord, Westguard Keep (28.91, 44.14)|
+A Two Wrongs... |QID|11188| |N|Howling Fjord, Westguard Keep (28.91, 44.14)|
+U |cffffffff|Hitem:33119:0:0:0:0:0:0:1936621438:70|h[Malister's Frost Wand]|h|r |N|Howling Fjord, Ember Clutch (37.24, 46.82)|
+- |QID|11188.1| |QO|Proto-Drake slain: 3/3| |N|Howling Fjord, Ember Clutch (37.70, 49.04)|
+T Two Wrongs... |QID|11188| |N|Howling Fjord, Westguard Keep (28.92, 44.13)|
+A Report to Scout Knowles |QID|11199| |N|Howling Fjord, Westguard Keep (28.92, 44.13)|
+A Danger! Explosives! |QID|11218| |N|Howling Fjord, Westguard Keep (29.11, 41.82)|
+A Send Them Packing |QID|11224| |N|Howling Fjord, Westguard Keep (31.57, 41.53)|
+N enter here |N|Howling Fjord,  (33.83, 33.70)|
+U |cffffffff|Hitem:33190:0:0:0:0:0:0:2001619580:70|h[Steelring's Foolproof Dynamite]|h|r |N|Howling Fjord, Whisper Gulch (33.50, 36.03)|
+- |QID|11218.2| |QO|Whisper Gulch Gem: 18/18| |N|Howling Fjord, Whisper Gulch (33.72, 40.23)|
+- |QID|11218.1| |QO|Whisper Gulch Ore Fragment: 6/6| |N|Howling Fjord, Whisper Gulch (33.88, 39.66)|
+- |QID|11224.1| |QO|Abandoned Pack Mule Sent Packing: 10/10| |N|Howling Fjord, Whisper Gulch (33.38, 36.82)|
+T Send Them Packing |QID|11224| |N|Howling Fjord, Westguard Keep (31.55, 41.49)|
+T Danger! Explosives! |QID|11218| |N|Howling Fjord, Westguard Keep (29.08, 41.85)|
+A Leader of the Deranged |QID|11240| |N|Howling Fjord, Westguard Keep (29.08, 41.85)|
+N cave here |N|Howling Fjord, Whisper Gulch (31.38, 35.39)|
+- |QID|11240.1| |QO|Squeeg Idolhunter slain: 1/1| |N|Howling Fjord, Whisper Gulch (31.66, 34.83)|
+T Leader of the Deranged |QID|11240| |N|Howling Fjord, Westguard Keep (29.09, 41.85)|
+A My Daughter |QID|11175| |N|Howling Fjord, Westguard Keep (31.62, 41.98)|
+T Report to Scout Knowles |QID|11199| |N|Howling Fjord, Ember Spear Tower (44.44, 57.56)|
+A Mission: Eternal Flame |QID|11202| |N|Howling Fjord, Ember Spear Tower (44.44, 57.56)|
+U |cffffffff|Hitem:33164:0:0:0:0:0:0:112328580:70|h[Ever-burning Torches]|h|r |N|Howling Fjord, Halgrind (48.36, 55.89)|
+- |QID|11202.1| |QO|Southwest Plague Tank Destroyed: 1/1| |N|Howling Fjord, Halgrind (48.36, 55.89)|
+- |QID|11202.4| |QO|Southeast Plague Tank Destroyed: 1/1| |N|Howling Fjord, Halgrind (51.35, 57.81)|
+- |QID|11202.3| |QO|Northeast Plague Tank Destroyed: 1/1| |N|Howling Fjord, Halgrind (51.42, 49.93)|
+- |QID|11202.2| |QO|Northwest Plague Tank Destroyed: 1/1| |N|Howling Fjord, Halgrind (48.12, 52.97)|
+T Mission: Eternal Flame |QID|11202| |N|Howling Fjord, Ember Spear Tower (44.40, 57.51)|
+A Mission: Package Retrieval |QID|11327| |N|Howling Fjord, Ember Spear Tower (44.40, 57.51)|
+- |QID|11327.1| |QO|Apothecary's Package: 1/1| |N|Howling Fjord, Halgrind (50.79, 53.90)|
+T Mission: Package Retrieval |QID|11327| |N|Howling Fjord, Ember Spear Tower (44.46, 57.71)|
+A Mission: Forsaken Intel |QID|11328| |N|Howling Fjord, Ember Spear Tower (44.46, 57.71)|
+T Mission: Forsaken Intel |QID|11328| |N|Howling Fjord, Westguard Inn (30.80, 41.65)|
+A Absholutely... Thish Will Work! |QID|11330| |N|Howling Fjord, Westguard Inn (30.80, 41.65)|
+U |cffffffff|Hitem:33627:0:0:0:0:0:0:573822918:70|h[Peppy's Special Mix]|h|r |N|Howling Fjord, Westguard Keep (29.41, 43.44)|
+- |QID|11330.1| |QO|Peppy's Mix Administered To The Vrykul Prisoner: 1/1| |N|Howling Fjord, Westguard Keep (29.41, 43.44)|
+T Absholutely... Thish Will Work! |QID|11330| |N|Howling Fjord, Westguard Inn (30.81, 41.62)|
+A You Tell Him ...Hic! |QID|11331| |N|Howling Fjord, Westguard Inn (30.81, 41.62)|
+T You Tell Him ...Hic! |QID|11331| |N|Howling Fjord, Westguard Keep (28.86, 44.10)|
+A Mission: Plague This! |QID|11332| |N|Howling Fjord, Westguard Keep (28.86, 44.10)|
+- |QID|11332.1| |QO|Plague Tank Hits: 5/5| |N|Howling Fjord, New Agamand (50.80, 69.30)|
+N bomb id 33634 |N|Howling Fjord, Halgrind (47.74, 58.11)|
+T Mission: Plague This! |QID|11332| |N|Howling Fjord, Westguard Keep (28.85, 44.07)|
+A Operation: Skornful Wrath |QID|11248| |N|Howling Fjord, Westguard Keep (28.85, 44.07)|
+A Everything Must Be Ready |QID|11406| |N|Howling Fjord, Westguard Keep (30.64, 42.70)|
+A I'll Try Anything! |QID|11329| |N|Howling Fjord, Caldemere Lake (60.20, 18.68)|
+U |cff9d9d9d|Hitem:38520:0:0:0:0:0:0:-1962202983:70|h[Diving Log]|h|r |N|Howling Fjord, Caldemere Lake (62.26, 19.73)|
+- |QID|11329.1| |QO|Northern Barbfish: 7/7| |N|Howling Fjord, Caldemere Lake (64.03, 20.44)|
+T I'll Try Anything! |QID|11329| |N|Howling Fjord, Caldemere Lake (60.32, 18.68)|
+A The One That Got Away |QID|11410| |N|Howling Fjord, Caldemere Lake (60.32, 18.68)|
+U |cffffffff|Hitem:34013:0:0:0:0:0:0:1212778950:70|h[Fresh Barbfish Bait]|h|r |N|Howling Fjord, Caldemere Lake (63.79, 19.42)|
+- |QID|11410.1| |QO|Frostfin slain: 1/1| |N|Howling Fjord, Caldemere Lake (63.79, 19.42)|
+T The One That Got Away |QID|11410| |N|Howling Fjord, Caldemere Lake (60.33, 18.67)|
+A The Enigmatic Frost Nymphs |QID|11302| |N|Howling Fjord, Fort Wildervar (61.79, 17.23)|
+A The Book of Runes |QID|11346| |N|Howling Fjord, Fort Wildervar (62.26, 17.12)|
+T Everything Must Be Ready |QID|11406| |N|Howling Fjord, Fort Wildervar (62.56, 16.82)|
+A Down to the Wire |QID|11269| |N|Howling Fjord, Fort Wildervar (62.56, 16.82)|
+A Preying Upon the Weak |QID|11292| |N|Howling Fjord, Fort Wildervar (62.61, 16.81)|
+A March of the Giants |QID|11355| |N|Howling Fjord, Fort Wildervar (62.58, 16.58)|
+N that's a 2-man quest |N|Howling Fjord, Fort Wildervar (62.58, 16.58)|
+A The Yeti Next Door |QID|11284| |N|Howling Fjord, Fort Wildervar (60.17, 15.74)|
+U |cffffffff|Hitem:33352:0:0:0:0:0:0:692884346:70|h[Tough Ram Meat]|h|r |N|Howling Fjord, Wildervar Mine (60.38, 12.00)|
+U |cffffffff|Hitem:33477:0:0:0:0:0:0:527883880:70|h[Giant Yeti Meal]|h|r |N|Howling Fjord, Wildervar Mine (60.38, 12.00)|
+- |QID|11284.1| |QO|Shatterhorn slain: 1/1| |N|Howling Fjord, Wildervar Mine (60.38, 12.00)|
+T The Yeti Next Door |QID|11284| |N|Howling Fjord, Fort Wildervar (60.16, 15.71)|
+U |cffffffff|Hitem:33806:0:0:0:0:0:0:652475042:70|h[Runeseeking Pick]|h|r |N|Howling Fjord,  (63.98, 29.52)|
+U |cffffffff|Hitem:33778:0:0:0:0:0:0:570683104:70|h[Book of Runes - Chapter 1]|h|r |N|Howling Fjord, Giants' Run (69.47, 27.86)|
+- |QID|11346.1| |QO|The Book of Runes: 1/1| |N|Howling Fjord, Giants' Run (69.47, 27.86)|
+U |cffffffff|Hitem:33458:0:0:0:0:0:0:2061430746:70|h[Scroll of Intellect VI]|h|r |N|Howling Fjord, Giants' Run (69.15, 29.56)|
+U |cffffffff|Hitem:22829:0:0:0:0:0:0:1993252278:70|h[Super Healing Potion]|h|r |N|Howling Fjord, Giants' Run (68.72, 28.96)|
+- |QID|11355.1| |QO|Runed Stone Giant Corpse Analyzed: 4/4| |N|Howling Fjord, Giants' Run (69.79, 28.45)|
+N 58 18 seems a good spot |N|Howling Fjord, The Twisted Glade (55.61, 18.55)|
+- |QID|11269.1| |QO|Spotted Hippogryph Down: 10/10| |N|Howling Fjord,  (52.90, 13.84)|
+- |QID|11292.1| |QO|Trapped Prey: 8/8| |N|Howling Fjord,  (50.05, 17.49)|
+T The Book of Runes |QID|11346| |N|Howling Fjord, Fort Wildervar (62.25, 17.16)|
+A Mastering the Runes |QID|11349| |N|Howling Fjord, Fort Wildervar (62.25, 17.16)|
+T March of the Giants |QID|11355| |N|Howling Fjord, Fort Wildervar (62.52, 16.59)|
+A The Lodestone |QID|11358| |N|Howling Fjord, Fort Wildervar (62.52, 16.59)|
+T Down to the Wire |QID|11269| |N|Howling Fjord, Fort Wildervar (62.57, 16.79)|
+A We Call Him Steelfeather |QID|11418| |N|Howling Fjord, Fort Wildervar (62.57, 16.79)|
+T Preying Upon the Weak |QID|11292| |N|Howling Fjord, Fort Wildervar (62.60, 16.78)|
+U |cffffffff|Hitem:34026:0:0:0:0:0:0:858669380:70|h[Feathered Charm]|h|r |N|Howling Fjord, Fort Wildervar (62.47, 16.37)|
+- |QID|11418.1| |QO|Learn Steelfeather's Secret: 1/1| |N|Howling Fjord, Fort Wildervar (62.47, 16.37)|
+T We Call Him Steelfeather |QID|11418| |N|Howling Fjord, Fort Wildervar (62.57, 16.79)|
+T The Enigmatic Frost Nymphs |QID|11302| |N|Howling Fjord, The Frozen Glade (61.48, 22.79)|
+A Spirits of the Ice |QID|11313| |N|Howling Fjord, The Frozen Glade (61.48, 22.79)|
+- |QID|11313.1| |QO|Icy Core: 15/15| |N|Howling Fjord, The Frozen Glade (62.52, 24.62)|
+T Spirits of the Ice |QID|11313| |N|Howling Fjord, The Frozen Glade (61.49, 22.79)|
+A The Fallen Sisters |QID|11314| |N|Howling Fjord, The Frozen Glade (61.49, 22.79)|
+A Wild Vines |QID|11315| |N|Howling Fjord, The Frozen Glade (61.49, 22.79)|
+U |cffffffff|Hitem:33606:0:0:0:0:0:0:1447531609:71|h[Lurielle's Pendant]|h|r |N|Howling Fjord, The Vibrant Glade (53.10, 27.91)|
+- |QID|11315.1| |QO|Scarlet Ivy slain: 8/8| |N|Howling Fjord, The Vibrant Glade (52.39, 26.98)|
+- |QID|11314.1| |QO|Chill Nymphs Freed: 7/7| |N|Howling Fjord, The Vibrant Glade (52.31, 26.28)|
+T The Fallen Sisters |QID|11314| |N|Howling Fjord, The Frozen Glade (61.48, 22.77)|
+T Wild Vines |QID|11315| |N|Howling Fjord, The Frozen Glade (61.48, 22.77)|
+A Spawn of the Twisted Glade |QID|11316| |N|Howling Fjord, The Frozen Glade (61.48, 22.77)|
+A Seeds of the Blacksouled Keepers |QID|11319| |N|Howling Fjord, The Frozen Glade (61.48, 22.77)|
+- |QID|11316.1| |QO|Thornvine Creeper slain: 10/10| |N|Howling Fjord, The Twisted Glade (54.44, 15.84)|
+- |QID|11319.1| |QO|Spores Shattered: 8/8| |N|Howling Fjord, The Twisted Glade (53.79, 17.13)|
+T Spawn of the Twisted Glade |QID|11316| |N|Howling Fjord, The Frozen Glade (61.52, 22.87)|
+A Keeper Witherleaf |QID|11428| |N|Howling Fjord, The Frozen Glade (61.52, 22.87)|
+T Seeds of the Blacksouled Keepers |QID|11319| |N|Howling Fjord, The Frozen Glade (61.52, 22.87)|
+- |QID|11428.1| |QO|Keeper Witherleaf slain: 1/1| |N|Howling Fjord, The Twisted Glade (53.71, 17.64)|
+T Keeper Witherleaf |QID|11428| |N|Howling Fjord, The Frozen Glade (61.46, 22.80)|
+- |QID|11349.1| |QO|Iron Rune Carving Tools: 1/1| |N|Howling Fjord, Giants' Run (67.56, 23.39)|
+U |cffffffff|Hitem:33819:0:0:0:0:0:0:1184857378:71|h[Rune Sample]|h|r |N|Howling Fjord, Giants' Run (71.66, 17.57)|
+- |QID|11358.1| |QO|Compare Runes with the Broken Tablet: 1/1| |N|Howling Fjord, Giants' Run (71.63, 17.44)|
+T The Lodestone |QID|11358| |N|Howling Fjord, Fort Wildervar (62.63, 16.51)|
+A Demolishing Megalith |QID|11359| |N|Howling Fjord, Fort Wildervar (62.63, 16.51)|
+T Mastering the Runes |QID|11349| |N|Howling Fjord, Fort Wildervar (62.23, 17.16)|
+A The Rune of Command |QID|11348| |N|Howling Fjord, Fort Wildervar (62.23, 17.16)|
+U |cffffffff|Hitem:33796:0:0:0:0:0:0:185369699:71|h[Rune of Command]|h|r |N|Howling Fjord, Giants' Run (70.37, 23.49)|
+- |QID|11348.1| |QO|Test Rune of Command: 1/1| |N|Howling Fjord, Giants' Run (70.37, 23.49)|
+- |QID|11359.1| |QO|Megalith slain: 1/1| |N|Howling Fjord, Frostblade Peak (67.31, 10.35)|
+- |QID|11348.2| |QO|Binder Murdis slain: 1/1| |N|Howling Fjord, Giants' Run (71.79, 24.31)|
+T Demolishing Megalith |QID|11359| |N|Howling Fjord, Fort Wildervar (62.65, 16.50)|
+T The Rune of Command |QID|11348| |N|Howling Fjord, Fort Wildervar (62.25, 17.14)|
+U |cff0070dd|Hitem:27410:0:0:0:0:0:0:2051038093:71|h[Collar of Command]|h|r |N|City of Ironforge, Ironforge (32.70, 57.27)|
+U |cff1eff00|Hitem:35844:0:0:0:0:0:0:1135218774:71|h[Shock-Resistant Hood]|h|r |N|City of Ironforge, Ironforge (32.70, 57.27)|
+U |cffffffff|Hitem:34054:0:0:0:0:0:0:322711164:71|h[Infinite Dust]|h|r |N|City of Ironforge, Vault of Ironforge (36.23, 62.17)|
+U |cff0070dd|Hitem:22449:0:0:0:0:0:0:280181417:71|h[Large Prismatic Shard]|h|r |N|City of Ironforge, Vault of Ironforge (36.23, 62.17)|
+U |cffffffff|Hitem:33470:0:0:0:0:0:0:1988522263:71|h[Frostweave Cloth]|h|r |N|City of Ironforge, Vault of Ironforge (36.23, 62.17)|
+U |cffffffff|Hitem:33470:0:0:0:0:0:0:1759879520:71|h[Frostweave Cloth]|h|r |N|City of Ironforge, Vault of Ironforge (36.23, 62.17)|
+U |cffffffff|Hitem:38682:0:0:0:0:0:0:6819038:71|h[Bleached Vellum]|h|r |N|City of Ironforge, Vault of Ironforge (36.35, 62.15)|
 ]]
 end)
