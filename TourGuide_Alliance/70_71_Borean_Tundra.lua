ba9143bc3ca0cf2473226c8b4c1fb40aeda2799d
Tekkub <tekkub@gmail.com>
2008-09-26 20:03:45 -0600

    My own raw data, ally borean

--- TourGuide_Alliance/70_71_Borean_Tundra.lua
+++ TourGuide_Alliance/70_71_Borean_Tundra.lua
@@ -251,6 +251,197 @@ A Surrender... Not! |QID|11566| |N|Borean Tundra, Winterfin Retreat (43.49, 13.8
 U |cffffffff|Hitem:34620:0:0:0:0:0:0:1784573872:70|h[King Mrgl-Mrgl's Spare Suit]|h|r |N|Borean Tundra, Winterfin Retreat (43.49, 13.89)|
 - |QID|11566.1| |QO|Claw of Claximus: 1/1| |N|Borean Tundra, Winterfin Caverns (37.72, 27.07)|
 T Surrender... Not! |QID|11566| |N|Borean Tundra, Winterfin Retreat (43.50, 13.92)|
+T The Assassination of Harold Lane |QID|11892| |N|Borean Tundra, D.E.H.T.A. Encampment (57.04, 44.25)|
+T Monitoring the Rift: Winterfin Cavern |QID|12728| |N|Borean Tundra, Amber Ledge (45.03, 33.39)|
+A Have They No Shame? |QID|13094| |N|Borean Tundra, Transitus Shield (33.45, 34.30)|
+U |cffffffff|Hitem:35648:0:0:0:0:0:0:694560900:70|h[Scintillating Fragment]|h|r |N|Borean Tundra, Coldarra (34.37, 30.28)|
+A Puzzling... |QID|11941| |N|Borean Tundra, Coldarra (34.37, 30.28)|
+- |QID|11900.3| |QO|Northern Coldarra Reading: 1/1| |N|Borean Tundra, Coldarra (31.31, 20.29)|
+- |QID|11900.4| |QO|Western Coldarra Reading: 1/1| |N|Borean Tundra, Coldarra (22.36, 24.17)|
+- |QID|11910.1| |QO|Glacial Splinter: 3/3| |N|Borean Tundra, Coldarra (21.99, 32.82)|
+- |QID|11918.1| |QO|Coldarra Spellweaver slain: 10/10| |N|Borean Tundra, Coldarra (22.63, 33.13)|
+- |QID|11912.1| |QO|Frostberry: 10/10| |N|Borean Tundra, Coldarra (25.09, 35.16)|
+- |QID|11900.2| |QO|Southern Coldarra Reading: 1/1| |N|Borean Tundra, Coldarra (28.33, 34.98)|
+- |QID|11900.1| |QO|Nexus Geological Reading: 1/1| |N|Borean Tundra, The Nexus (28.26, 28.47)|
+- |QID|11910.2| |QO|Magic-Bound Splinter: 3/3| |N|Borean Tundra, Coldarra (29.84, 30.73)|
+T Reading the Meters |QID|11900| |N|Borean Tundra, Transitus Shield (33.00, 34.33)|
+A Postponing the Inevitable |QID|11905| |N|Borean Tundra, Transitus Shield (33.00, 34.33)|
+T Secrets of the Ancients |QID|11910| |N|Borean Tundra, Transitus Shield (33.00, 34.33)|
+A Quickening |QID|11911| |N|Borean Tundra, Transitus Shield (33.00, 34.33)|
+T Nuts for Berries |QID|11912| |N|Borean Tundra, Transitus Shield (33.43, 34.40)|
+A Keep the Secret Safe |QID|11914| |N|Borean Tundra, Transitus Shield (33.43, 34.40)|
+T Basic Training |QID|11918| |N|Borean Tundra, Transitus Shield (33.37, 34.44)|
+A Hatching a Plan |QID|11936| |N|Borean Tundra, Transitus Shield (33.37, 34.44)|
+T Puzzling... |QID|11941| |N|Borean Tundra, Transitus Shield (33.37, 34.44)|
+A The Cell |QID|11943| |N|Borean Tundra, Transitus Shield (33.37, 34.44)|
+N No shame, postponing and quickening are dungeon quests |N|Borean Tundra, Transitus Shield (33.56, 34.18)|
+- |QID|11943.1| |QO|Energy Core: 1/1| |N|Borean Tundra, Coldarra (24.24, 28.92)|
+- |QID|11943.2| |QO|Prison Casing: 1/1| |N|Borean Tundra, Coldarra (27.54, 20.71)|
+- |QID|11936.1| |QO|Dragon Eggs Destroyed: 5/5| |N|Borean Tundra, The Nexus (27.31, 24.15)|
+- |QID|11914.1| |QO|Nexus Mana Essence: 5/5| |N|Borean Tundra, The Nexus (27.56, 23.73)|
+T Hatching a Plan |QID|11936| |N|Borean Tundra, Transitus Shield (33.33, 34.46)|
+A Drake Hunt |QID|11919| |N|Borean Tundra, Transitus Shield (33.33, 34.46)|
+T The Cell |QID|11943| |N|Borean Tundra, Transitus Shield (33.33, 34.46)|
+U |cffffffff|Hitem:35671:0:0:0:0:0:0:1548877126:70|h[Augmented Arcane Prison]|h|r |N|Borean Tundra, Transitus Shield (33.33, 34.29)|
+A Keristrasza |QID|11946| |N|Borean Tundra, Transitus Shield (33.33, 34.29)|
+T Keristrasza |QID|11946| |N|Borean Tundra, Transitus Shield (33.33, 34.29)|
+A Bait and Switch |QID|11951| |N|Borean Tundra, Transitus Shield (33.33, 34.29)|
+T Keep the Secret Safe |QID|11914| |N|Borean Tundra, Transitus Shield (33.43, 34.38)|
+U |cffffffff|Hitem:39691:0:0:0:0:0:0:2117283020:70|h[Succulent Orca Stew]|h|r |N|Borean Tundra, Transitus Shield (33.43, 34.38)|
+- |QID|11951.1| |QO|Crystallized Mana Shard: 10/10| |N|Borean Tundra, The Nexus (25.31, 27.64)|
+U |cffffffff|Hitem:35506:0:0:0:0:0:0:1727718481:70|h[Raelorasz's Spear]|h|r |N|Borean Tundra, Coldarra (22.40, 24.25)|
+T Bait and Switch |QID|11951| |N|Borean Tundra, Coldarra (23.77, 25.66)|
+- |QID|11919.1| |QO|Captured Nexus Drake: 1/1| |N|Borean Tundra, Transitus Shield (33.07, 34.16)|
+T Drake Hunt |QID|11919| |N|Borean Tundra, Transitus Shield (33.31, 34.46)|
+A Cracking the Code |QID|11931| |N|Borean Tundra, Transitus Shield (33.31, 34.46)|
+A Drake Hunt |QID|11940| |N|Borean Tundra, Transitus Shield (33.31, 34.46)|
+U |cffffffff|Hitem:6948:0:0:0:0:0:0:0:71|h[Hearthstone]|h|r |N|City of Ironforge, Vault of Ironforge (36.29, 62.15)|
+- |QID|11708.1| |QO|Fizzcrank's tale listened to.| |N|,  (22.61, 70.25)|
+A The Mechagnomes |QID|11708| |N|,  (22.61, 70.25)|
+- |QID|11931.2| |QO|Azure Codex: 1/1| |N|Borean Tundra, Coldarra (32.77, 27.84)|
+- |QID|11931.1| |QO|Shimmering Rune: 3/3| |N|Borean Tundra, Coldarra (31.36, 26.13)|
+U |cffffffff|Hitem:35671:0:0:0:0:0:0:1548877126:71|h[Augmented Arcane Prison]|h|r |N|Borean Tundra, Coldarra (34.00, 30.24)|
+A Saragosa's End |QID|11957| |N|Borean Tundra, Coldarra (34.00, 30.24)|
+U |cffffffff|Hitem:28455:0:0:0:0:0:0:362290436:71|h[Archmage Vargoth's Staff]|h|r |N|Borean Tundra, Saragosa's Landing (21.19, 22.47)|
+U |cffffffff|Hitem:5462:0:0:0:0:0:0:1278524770:71|h[Dartol's Rod of Transformation]|h|r |N|Borean Tundra, Saragosa's Landing (21.19, 22.47)|
+U |cffffffff|Hitem:35690:0:0:0:0:0:0:2116985946:71|h[Arcane Power Focus]|h|r |N|Borean Tundra, Saragosa's Landing (21.31, 22.57)|
+- |QID|11957.1| |QO|Saragosa's Corpse: 1/1| |N|Borean Tundra, Saragosa's Landing (22.01, 22.63)|
+T Saragosa's End |QID|11957| |N|Borean Tundra, Saragosa's Landing (22.01, 22.63)|
+- |QID|11967.1| |QO|Augmented Arcane Prison: 1/1| |N|Borean Tundra, Saragosa's Landing (22.01, 22.63)|
+A Mustering the Reds |QID|11967| |N|Borean Tundra, Saragosa's Landing (22.01, 22.63)|
+T Cracking the Code |QID|11931| |N|Borean Tundra, Transitus Shield (33.28, 34.44)|
+T Mustering the Reds |QID|11967| |N|Borean Tundra, Transitus Shield (33.28, 34.44)|
+A Springing the Trap |QID|11969| |N|Borean Tundra, Transitus Shield (33.28, 34.44)|
+U |cffffffff|Hitem:35718:0:0:0:0:0:0:1541306506:71|h[Raelorasz' Flare]|h|r |N|Borean Tundra, The Nexus (26.84, 25.11)|
+- |QID|11969.1| |QO|Assault on Malygos Complete: 1/1| |N|Borean Tundra, Coldarra (34.35, 25.20)|
+T Springing the Trap |QID|11969| |N|Borean Tundra, Transitus Shield (33.27, 34.46)|
+U |cff0070dd|Hitem:43517:0:0:0:0:0:0:1260354408:71|h[Penguin Egg]|h|r |N|Borean Tundra, Fizzcrank Airstrip (57.00, 19.09)|
+T The Mechagnomes |QID|11708| |N|Borean Tundra, Fizzcrank Airstrip (57.43, 18.79)|
+A Re-Cursive |QID|11712| |N|Borean Tundra, Fizzcrank Airstrip (57.43, 18.79)|
+A What's the Matter with the Transmatter? |QID|11710| |N|Borean Tundra, Fizzcrank Airstrip (57.51, 18.72)|
+A Dirty, Stinkin' Snobolds! |QID|11645| |N|Borean Tundra, Fizzcrank Airstrip (58.16, 18.33)|
+U |cffffffff|Hitem:35946:0:0:0:0:0:0:397509974:71|h[Fizzcrank Practice Parachute]|h|r |N|Borean Tundra, Fizzcrank Airstrip (57.38, 17.21)|
+U |cffffffff|Hitem:35946:0:0:0:0:0:0:2012266696:71|h[Fizzcrank Practice Parachute]|h|r |N|Borean Tundra, Fizzcrank Airstrip (57.06, 17.37)|
+U |cff1eff00|Hitem:37078:0:0:0:0:0:0:2128043874:71|h[Rod of the Crimson Keeper]|h|r |N|Borean Tundra, Fizzcrank Airstrip (57.01, 19.73)|
+U |cffffffff|Hitem:34973:0:0:0:0:0:0:331216544:71|h[Re-Cursive Transmatter Injection]|h|r |N|Borean Tundra, Scalding Pools (59.48, 19.34)|
+U |cffffffff|Hitem:34984:0:0:0:0:0:0:945450700:71|h[The Ultrasonic Screwdriver]|h|r |N|Borean Tundra, Scalding Pools (60.59, 18.44)|
+A The Ultrasonic Screwdriver |QID|11729| |N|Borean Tundra, Scalding Pools (60.59, 18.44)|
+T The Ultrasonic Screwdriver |QID|11729| |N|Borean Tundra, Fizzcrank Airstrip (57.56, 18.73)|
+A Master and Servant |QID|11730| |N|Borean Tundra, Fizzcrank Airstrip (57.56, 18.73)|
+U |cffffffff|Hitem:35116:0:0:0:0:0:0:4660206:71|h[The Ultrasonic Screwdriver]|h|r |N|Borean Tundra, Scalding Pools (58.36, 19.14)|
+U |cffffffff|Hitem:34972:0:0:0:0:0:0:736650688:71|h[Fizzcrank Spare Parts]|h|r |N|Borean Tundra, Scalding Pools (58.64, 17.26)|
+- |QID|11712.1| |QO|Fizzcrank Gnome cursed & ported: 6/6| |N|Borean Tundra, Scalding Pools (58.85, 16.65)|
+- |QID|11730.1| |QO|Robots reprogrammed: 15/15| |N|Borean Tundra, Scalding Pools (61.82, 17.09)|
+U |cffffffff|Hitem:35116:0:0:0:0:0:0:4660206:71|h[The Ultrasonic Screwdriver]|h|r |N|Borean Tundra, Scalding Pools (62.34, 17.75)|
+- |QID|11710.1| |QO|Fizzcrank Spare Parts: 15/15| |N|Borean Tundra, Scalding Pools (61.30, 19.00)|
+T What's the Matter with the Transmatter? |QID|11710| |N|Borean Tundra, Fizzcrank Airstrip (57.47, 18.69)|
+A Check in With Bixie |QID|11692| |N|Borean Tundra, Fizzcrank Airstrip (57.47, 18.69)|
+T Re-Cursive |QID|11712| |N|Borean Tundra, Fizzcrank Airstrip (57.47, 18.69)|
+A Lefty Loosey, Righty Tighty |QID|11788| |N|Borean Tundra, Fizzcrank Airstrip (57.47, 18.69)|
+T Master and Servant |QID|11730| |N|Borean Tundra, Fizzcrank Airstrip (58.16, 18.39)|
+A Finding Pilot Tailspin |QID|11725| |N|Borean Tundra, Fizzcrank Airstrip (57.01, 18.73)|
+T Check in With Bixie |QID|11692| |N|Borean Tundra,  (73.44, 18.82)|
+A Oh Great... Plagued Magnataur! |QID|11693| |N|Borean Tundra,  (73.44, 18.82)|
+- |QID|11693.1| |QO|Plagued Magnataur slain: 10/10| |N|Borean Tundra, The Dens of Dying (79.49, 22.52)|
+T Oh Great... Plagued Magnataur! |QID|11693| |N|Borean Tundra,  (73.40, 18.75)|
+A There's Something Going On In Those Caves |QID|11694| |N|Borean Tundra,  (73.40, 18.75)|
+- |QID|11694.1| |QO|Plague Cauldron Neutralized: 1/1| |N|Den of Dying,  (74.79, 14.24)|
+T There's Something Going On In Those Caves |QID|11694| |N|Borean Tundra,  (73.43, 18.70)|
+A Rats, Tinky Went into the Necropolis! |QID|11697| |N|Borean Tundra,  (73.43, 18.70)|
+A Might As Well Wipe Out the Scourge |QID|11698| |N|Borean Tundra,  (73.43, 18.70)|
+T Rats, Tinky Went into the Necropolis! |QID|11697| |N|Borean Tundra, Talramas (69.89, 14.76)|
+A I'm Stuck in this Damned Cage... But Not For Long! |QID|11699| |N|Borean Tundra, Talramas (69.89, 14.76)|
+A Might As Well Wipe Out the Scourge |QID|11698| |N|,  (22.59, 70.24)|
+U |cffffffff|Hitem:5462:0:0:0:0:0:0:1278524770:71|h[Dartol's Rod of Transformation]|h|r |N|Howling Fjord, Valgarde (58.29, 62.39)|
+U |cffffffff|Hitem:39691:0:0:0:0:0:0:2117283020:71|h[Succulent Orca Stew]|h|r |N|Borean Tundra, Magmoth (54.46, 11.84)|
+- |QID|11645.1| |QO|Crafty's Stuff: 10/10| |N|Borean Tundra, Magmoth (56.61, 10.86)|
+T Dirty, Stinkin' Snobolds! |QID|11645| |N|Borean Tundra, Fizzcrank Airstrip (57.60, 18.94)|
+A Just a Few More Things... |QID|11650| |N|Borean Tundra, Fizzcrank Airstrip (57.60, 18.94)|
+U |cffffffff|Hitem:34801:0:0:0:0:0:0:642813802:71|h[Crafty's Shopping List]|h|r |N|Borean Tundra, Fizzcrank Airstrip (57.60, 18.94)|
+U |cff1eff00|Hitem:36186:0:0:0:0:0:-6:1168900130:71|h[Garmaul Bracers of the Eagle]|h|r |N|Borean Tundra, Fizzcrank Airstrip (57.02, 19.13)|
+A Might As Well Wipe Out the Scourge |QID|11698| |N|Borean Tundra, Fizzcrank Airstrip (57.07, 18.98)|
+U |cffffffff|Hitem:34801:0:0:0:0:0:0:642813802:71|h[Crafty's Shopping List]|h|r |N|Borean Tundra, Fizzcrank Airstrip (57.11, 19.65)|
+A Get Me Outa Here! |QID|11673| |N|Borean Tundra, Magmoth (55.57, 12.59)|
+N ent |N|Borean Tundra, Magmoth (54.28, 12.85)|
+- |QID|11673.1| |QO|Bonker Togglevolt escorted to safety.| |N|Borean Tundra, Magmoth (53.96, 13.57)|
+T Get Me Outa Here! |QID|11673| |N|Borean Tundra, Fizzcrank Airstrip (57.00, 18.70)|
+U |cff1eff00|Hitem:37047:0:0:0:0:0:0:1718134195:71|h[Branch of Everlasting Flame]|h|r |N|Borean Tundra, Fizzcrank Airstrip (57.00, 18.70)|
+- |QID|11698.1| |QO|Talramas Scourge Destroyed: 20/20| |N|Borean Tundra, Talramas (69.65, 12.93)|
+- |QID|11699.1| |QO|Engine-Core Crystal: 1/1| |N|Borean Tundra, Talramas (69.65, 12.93)|
+- |QID|11699.3| |QO|Piloting Scourgestone: 1/1| |N|Borean Tundra, Talramas (69.84, 13.84)|
+N up top |N|Borean Tundra, Talramas (69.71, 13.41)|
+- |QID|11699.2| |QO|Magical Gyroscope: 1/1| |N|Borean Tundra, Talramas (69.67, 13.20)|
+N stairs up here |N|Borean Tundra, Talramas (69.55, 15.73)|
+T I'm Stuck in this Damned Cage... But Not For Long! |QID|11699| |N|Borean Tundra, Talramas (69.89, 14.75)|
+A Let Bixie Know |QID|11700| |N|Borean Tundra, Talramas (69.89, 14.75)|
+T Might As Well Wipe Out the Scourge |QID|11698| |N|Borean Tundra,  (73.47, 18.77)|
+T Let Bixie Know |QID|11700| |N|Borean Tundra,  (73.47, 18.77)|
+A Back to the Airstrip |QID|11701| |N|Borean Tundra,  (73.47, 18.77)|
+T Back to the Airstrip |QID|11701| |N|Borean Tundra, Fizzcrank Airstrip (57.00, 18.70)|
+A Drake Hunt |QID|11940| |N|,  (22.19, 54.21)|
+U |cffffffff|Hitem:34801:0:0:0:0:0:0:642813802:71|h[Crafty's Shopping List]|h|r |N|Borean Tundra, Fizzcrank Airstrip (57.11, 18.73)|
+- |QID|11788.1| |QO|Twonky slain: 1/1| |N|Borean Tundra, West Point Station (60.20, 20.42)|
+- |QID|11788.4| |QO|The Grinder slain: 1/1| |N|Borean Tundra, South Point Station (65.19, 28.79)|
+- |QID|11788.2| |QO|ED-210 slain: 1/1| |N|Borean Tundra, North Point Station (65.46, 17.51)|
+- |QID|11788.3| |QO|Max Blasto slain: 1/1| |N|Borean Tundra, Fizzcrank Pumping Station (63.74, 22.40)|
+T Finding Pilot Tailspin |QID|11725| |N|Borean Tundra, Scalding Pools (61.66, 35.73)|
+A A Little Bit of Spice |QID|11726| |N|Borean Tundra, Scalding Pools (61.66, 35.73)|
+- |QID|11726.1| |QO|Gorloc Spice Pouch: 4/4| |N|Borean Tundra, The Flood Plains (71.04, 44.13)|
+T A Little Bit of Spice |QID|11726| |N|Borean Tundra, Scalding Pools (61.71, 35.77)|
+A Lupus Pupus |QID|11728| |N|Borean Tundra, Scalding Pools (61.79, 35.95)|
+U |cffffffff|Hitem:35121:0:0:0:0:0:0:286123646:71|h[Wolf Bait]|h|r |N|Borean Tundra, Scalding Pools (61.34, 37.32)|
+- |QID|11728.1| |QO|Microfilm: 8/8| |N|Borean Tundra, Scalding Pools (60.55, 27.83)|
+T Lupus Pupus |QID|11728| |N|Borean Tundra, Scalding Pools (61.68, 35.75)|
+A Emergency Protocol: Section 8.2, Paragraph C |QID|11795| |N|Borean Tundra, Scalding Pools (61.68, 35.75)|
+N one here |N|Borean Tundra, Scalding Pools (61.16, 30.33)|
+N one here |N|Borean Tundra, Scalding Pools (60.69, 35.96)|
+N and here |N|Borean Tundra, Scalding Pools (59.80, 35.74)|
+N and here |N|Borean Tundra, Scalding Pools (58.77, 38.32)|
+- |QID|11795.1| |QO|Fizzcrank Pilot's Insignia: 6/6| |N|Borean Tundra, Scalding Pools (60.55, 38.41)|
+T Emergency Protocol: Section 8.2, Paragraph C |QID|11795| |N|Borean Tundra, Scalding Pools (61.63, 35.80)|
+A Emergency Protocol: Section 8.2, Paragraph D |QID|11796| |N|Borean Tundra, Scalding Pools (61.63, 35.80)|
+U |cffffffff|Hitem:35224:0:0:0:0:0:0:1757098318:71|h[Emergency Torch]|h|r |N|Borean Tundra, Scalding Pools (61.63, 35.80)|
+- |QID|11796.2| |QO|Scuttle a Southern Wreck: 1/1| |N|Borean Tundra, Scalding Pools (61.08, 39.96)|
+- |QID|11796.3| |QO|Scuttle a Northwestern Wreck: 1/1| |N|Borean Tundra, Scalding Pools (60.89, 33.74)|
+- |QID|11796.1| |QO|Scuttle the Eastern Wreck: 1/1| |N|Borean Tundra, Scalding Pools (63.38, 36.91)|
+T Emergency Protocol: Section 8.2, Paragraph D |QID|11796| |N|Borean Tundra, Scalding Pools (61.64, 35.80)|
+A Give Fizzcrank the News |QID|11873| |N|Borean Tundra, Scalding Pools (61.64, 35.80)|
+A Scouting the Sinkholes |QID|11713| |N|Borean Tundra, Fizzcrank Airstrip (57.12, 19.88)|
+T Lefty Loosey, Righty Tighty |QID|11788| |N|Borean Tundra, Fizzcrank Airstrip (57.37, 18.82)|
+A The Gearmaster |QID|11798| |N|Borean Tundra, Fizzcrank Airstrip (57.37, 18.82)|
+T Give Fizzcrank the News |QID|11873| |N|Borean Tundra, Fizzcrank Airstrip (57.01, 18.72)|
+- |QID|11650.2| |QO|Overcharged Capacitor: 1/1| |N|Borean Tundra, Fizzcrank Airstrip (57.04, 19.08)|
+U |cffffffff|Hitem:36781:0:0:0:0:0:0:1536414936:71|h[Darkwater Clam]|h|r |N|Borean Tundra, Fizzcrank Airstrip (57.04, 19.08)|
+U |cffffffff|Hitem:34801:0:0:0:0:0:0:642813802:71|h[Crafty's Shopping List]|h|r |N|Borean Tundra, Fizzcrank Airstrip (57.04, 19.08)|
+- |QID|11798.1| |QO|The Gearmaster's Manual researched: 1/1| |N|Borean Tundra, Fizzcrank Pumping Station (64.50, 23.39)|
+- |QID|11798.2| |QO|Mechazod's Head: 1/1| |N|Borean Tundra, Fizzcrank Pumping Station (64.51, 23.33)|
+- |QID|11650.1| |QO|Crafty's Tools: 1/1| |N|Borean Tundra, Fizzcrank Pumping Station (64.37, 23.06)|
+U |cffffffff|Hitem:34920:0:0:0:0:0:0:1529599360:71|h[Map of the Geyser Fields]|h|r |N|Borean Tundra,  (71.39, 17.68)|
+- |QID|11713.3| |QO|Mark Location of Northwest Sinkhole: 1/1| |N|Borean Tundra, The Geyser Fields (66.23, 32.15)|
+- |QID|11713.1| |QO|Mark Location of South Sinkhole: 1/1| |N|Borean Tundra, The Geyser Fields (70.23, 36.34)|
+- |QID|11713.2| |QO|Mark Location of Northeast Sinkhole: 1/1| |N|Borean Tundra, The Geyser Fields (69.70, 32.08)|
+- |QID|11650.3| |QO|A Handful of Rocknar's Grit: 1/1| |N|Borean Tundra, Frozen Reach (81.17, 42.71)|
+T Scouting the Sinkholes |QID|11713| |N|Borean Tundra, Fizzcrank Airstrip (57.10, 20.05)|
+A Fueling the Project |QID|11715| |N|Borean Tundra, Fizzcrank Airstrip (57.10, 20.05)|
+T Just a Few More Things... |QID|11650| |N|Borean Tundra, Fizzcrank Airstrip (57.54, 18.77)|
+A Hah... You're Not So Big Now! |QID|11653| |N|Borean Tundra, Fizzcrank Airstrip (57.54, 18.77)|
+T The Gearmaster |QID|11798| |N|Borean Tundra, Fizzcrank Airstrip (57.01, 18.73)|
+U |cffffffff|Hitem:34975:0:0:0:0:0:0:1787712166:71|h[Portable Oil Collector]|h|r |N|Borean Tundra, Magmoth (59.13, 12.54)|
+U |cffffffff|Hitem:34812:0:0:0:0:0:0:1949723203:71|h[Crafty's Ultra-Advanced Proto-Typical Shortening Blaster]|h|r |N|Borean Tundra, Magmoth (59.13, 12.54)|
+- |QID|11653.1| |QO|Crafty's Blaster Tested: 5/5| |N|Borean Tundra, Magmoth (61.68, 10.03)|
+T Hah... You're Not So Big Now! |QID|11653| |N|Borean Tundra, Fizzcrank Airstrip (57.63, 18.62)|
+A Plan B |QID|11658| |N|Borean Tundra, Fizzcrank Airstrip (57.63, 18.62)|
+- |QID|11715.1| |QO|Barrels of Oil Collected: 8/8| |N|Borean Tundra, Scalding Pools (58.25, 27.64)|
+T Fueling the Project |QID|11715| |N|Borean Tundra, Fizzcrank Airstrip (57.08, 20.25)|
+A A Bot in Mammoth's Clothing |QID|11718| |N|Borean Tundra, Fizzcrank Airstrip (57.08, 20.25)|
+- |QID|11658.1| |QO|Warsong Outfit: 10/10| |N|Borean Tundra, Steeljaw's Caravan (50.44, 24.68)|
+- |QID|11658.2| |QO|Warsong Banner: 1/1| |N|Borean Tundra, Steeljaw's Caravan (49.57, 26.55)|
+- |QID|11718.1| |QO|Thick Mammoth Hide: 6/6| |N|Borean Tundra,  (55.26, 30.61)|
+T A Bot in Mammoth's Clothing |QID|11718| |N|Borean Tundra, Fizzcrank Airstrip (57.11, 20.18)|
+A Deploy the Shake-n-Quake! |QID|11723| |N|Borean Tundra, Fizzcrank Airstrip (57.08, 19.93)|
+T Plan B |QID|11658| |N|Borean Tundra, Fizzcrank Airstrip (58.00, 18.55)|
+A It Was The Orcs, Honest! |QID|11670| |N|Borean Tundra, Fizzcrank Airstrip (58.00, 18.55)|
+U |cff1eff00|Hitem:37274:0:0:0:0:0:0:338603175:71|h[Mender's Cover]|h|r |N|Borean Tundra, Fizzcrank Airstrip (57.07, 18.74)|
 ]]
 end)
 
