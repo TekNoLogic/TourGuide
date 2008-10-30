f873e9aec565dc3a0a8faddd34cadc2e11a3806c
Tekkub <tekkub@gmail.com>
2008-10-29 23:27:13 -0600

    Remove bits of Ony attunement questline that are gone in 3.0

--- TourGuide_Alliance/55_56_Burning_Steppes.lua
+++ TourGuide_Alliance/55_56_Burning_Steppes.lua
@@ -14,7 +14,6 @@ R Burning Steppes |N|Follow the road north (46,6)|
 f Grab flight point |N|Up in Morgan's Vigil (83,63)|
 A Extinguish the Firegut |QID|3823|
 A FIFTY! YEP! |QID|4283|
-A Dragonkin Menace |QID|4182|
 
 C Extinguish the Firegut |N|Kill ogres down at the mountain (82,54).  Most of the mages can be found in the cave on the northern face (83,39)| |QID|3823|
 T Extinguish the Firegut |QID|3823|
@@ -30,7 +29,6 @@ C Gor'tesh the Brute Lord |N|At Pillar of Ash (40,55)| |QID|3824|
 C FIFTY! YEP! |N|Kill orcs at Blackrock Stronghold (43,36) or Pillar of Ash (49,55)| |QID|4283|
 
 C Broodling Essence |N|Dragons to the northeast.  Use the device on them, then kill them. Get help if you can.| |U|12284| |QID|4726|
-C Dragonkin Menace |N|Get help if you can.| |QID|4182|
 
 A A Taste of Flame |N|In the cave at Slither Rock (94,31)| |QID|4024|
 T A Taste of Flame |N|Skip the follow-up| |QID|4024|
@@ -38,40 +36,14 @@ T A Taste of Flame |N|Skip the follow-up| |QID|4024|
 T FIFTY! YEP! |N| Back at Morgan's Vigil (84,68)| |QID|4283|
 T Gor'tesh the Brute Lord |QID|3824|
 A Ogre Head On A Stick = Party |NODEBUG| |QID|3825|
-T Dragonkin Menace |QID|4182|
-A The True Masters (Part 1) |QID|4183|
 
 C Ogre Head On A Stick = Party |NODEBUG| |N|Up on the ogre mountain (81.0, 46.8)| |QID|3825|
 T Ogre Head On A Stick = Party |NODEBUG| |QID|3825|
 
-F Lakeshire
-T The True Masters (Part 1) |N|In the town hall (30.0, 44.45)| |Z|Redridge Mountains| |QID|4183|
-A The True Masters (Part 2) |QID|4184|
-
-F Stormwind City
-T The True Masters (Part 2) |N|In Stormwind Keep (78,18)| |Z|Stormwind City| |QID|4184|
-A The True Masters (Part 3) |QID|4185|
-C The True Masters (Part 3) |N|Speak to Lady Katrana Prestor| |QID|4185|
-T The True Masters (Part 3) |QID|4185|
-A The True Masters (Part 4) |QID|4186|
-
-F Lakeshire
-T The True Masters (Part 4) |N|Town hall again| |QID|4186|
-A The True Masters (Part 5) |QID|4223|
-
-F Burning Steppes
-T The True Masters (Part 5) |QID|4223|
-A The True Masters (Part 6) |QID|4224|
-
 T Tablet of the Seven |N|Back at Flame Crest to the north (65,23)| |QID|4296|
 T Broodling Essence |QID|4726|
-C The True Masters (Part 6) |N|Talk to Ragged John| |QID|4224|
 
-T The True Masters (Part 6) |N|Skip the follow-up| |QID|4224|
 H City of Ironforge
 T The Smoldering Ruins of Thaurissan (Part 2) |N|In the throne room| |QID|3701|
-
 ]]
-
 end)
-
