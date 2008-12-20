c292becc9d5cfb131cf663c8c475d0683f0aa084
lowlee <lowlee@lowlee.info>
2008-12-21 09:53:39 +1100

    First part of Horde Borean Tundra

---
+++ TourGuide_Horde/70_71_Borean_Tundra.lua
@@ -0,0 +1,82 @@
+
+TourGuide:RegisterGuide("Borean Tundra (70-71)", nil, "Horde", function()
+return [[
+A Hellscream's Vigil |QID|11585| |T|
+T Hellscream's Vigil |QID|11585| |T|
+A The Defense of Warsong Hold |QID|11596| |T|
+T The Defense of Warsong Hold |QID|11596| |T|
+A Taking Back Mightstone Quarry |QID|11598| |T|
+A Patience is a Virtue that We Don't Need |QID|11606| |T|
+A Taken by the Scourge |QID|11611| |T|
+
+C Taken by the Scourge |QID|11611|
+C Patience is a Virtue that We Don't Need |QID|11606|
+C Taking Back Mightstone Quarry |QID|11598|
+
+T Taken by the Scourge |QID|11611| |T|
+T Patience is a Virtue that We Don't Need |QID|11606| |T|
+A Bury Those Cockroaches! |QID|11608| |T|
+T Taking Back Mightstone Quarry |QID|11598| |T|
+A Cutting Off the Source |QID|11602| |T|
+A Untold Truths |QID|11614| |T|
+
+N North Sinkhole |Q|Bury Those Cockroaches!| |QO|North Nerub'ar Sinkhole Destroyed: 1/1| |N|Northern Mightstone Quarry (41.78, 50.73)| |U|34710|
+T Untold Truths |QID|11614|
+A Nerub'ar Secrets |QID|11615| |N|Be sure to attack the egg sacs while doing the 3 remaining sinkholes.|
+N West Sinkhole |Q|Bury Those Cockroaches!| |QO|West Nerub'ar Sinkhole Destroyed: 1/1| |N|Western Mightstone Quarry (39.46, 52.84)| |U|34710|
+N South Sinkhole |Q|Bury Those Cockroaches!| |QO|South Nerub'ar Sinkhole Destroyed: 1/1| |N|Southern Mightstone Quarry (41.82, 58.83)| |U|34710|
+N East Sinkhole |Q|Bury Those Cockroaches!| |QO|East Nerub'ar Sinkhole Destroyed: 1/1| |N|Eastern Mightstone Quarry (44.08, 57.28)| |U|34710|
+C Bury Those Cockroaches! |QID|11608|
+C Cutting Off the Source |QID|11602|
+
+T Bury Those Cockroaches! |QID|11608| |T|
+T Nerub'ar Secrets |QID|11615| |T|
+A Message to Hellscream |QID|11616| |T|
+T Cutting Off the Source |QID|11602| |T|
+A Wind Master To'bor |QID|11634| |T|
+A Too Close For Comfort |QID|11574| |T|
+A Ride to Taunka'le Village |QID|11888| |T|
+T Message to Hellscream |QID|11616| |T|
+A Reinforcements Incoming... |QID|11618| |T|
+A To Bor'gorok Outpost, Quickly! |QID|12486| |T|
+T Wind Master To'bor |QID|11634| |N|Go up the elevator| |T|
+A Magic Carpet Ride |QID|11636| |T|
+
+T Magic Carpet Ride |QID|11636|
+A Tank Ain't Gonna Fix Itself |QID|11642|
+T Tank Ain't Gonna Fix Itself |QID|11642|
+A Mobu's Pneumatic Tank Transjigamarig |QID|11643|
+A Super Strong Metal Plates! |QID|11644|
+A Into the Mist |QID|11655|
+A Horn of the Ancient Mariner |QID|11660|
+
+C Mobu's Pneumatic Tank Transjigamarig |QID|11643|
+A Escaping the Mist |QID|11664| |N|In the building near the other quest givers (31.86, 52.44)|
+C Super Strong Metal Plates! |QID|11644|
+C Horn of the Ancient Mariner |QID|11660| |N|Keep killing Kvaldir Mistweaver's until it drops.|
+C Into the Mist |QID|11655| |N|Keep killing Kvaldir until it drops.|
+
+T Escaping the Mist |QID|11664|
+T Mobu's Pneumatic Tank Transjigamarig |QID|11643|
+T Super Strong Metal Plates! |QID|11644|
+A Tanks a lot... |QID|11651|
+T Into the Mist |QID|11655|
+A Burn in Effigy |QID|11656|
+T Horn of the Ancient Mariner |QID|11660|
+A Orabus the Helmsman |QID|11661|
+T Tanks a lot... |QID|11651|
+A The Plains of Nasam |QID|11652|
+
+N Bor's Hammer |Q|Burn in Effigy| |QO|Bor's Hammer destroyed: 1/1| |N|On the coast (29.74, 52.85)| |U|34830|
+N The Kur Drakkar |Q|Burn in Effigy| |QO|The Kur Drakkar destroyed: 1/1| |N|On the coast (30.90, 48.97)| |U|34830|
+N The Serpent's Maw |Q|Burn in Effigy| |QO|The Serpent's Maw destroyed: 1/1| |N|On the coast (31.48, 48.32)| |U|34830|
+N Bor's Anvil |Q|Burn in Effigy| |QO|Bor's Anvil destroyed: 1/1| |N|On the coast (30.06, 61.60)| |U|34830|
+C Orabus the Helmsman |QID|11661| |N|At the end off Warsong Jetty (26.58, 54.60)| |U|34844|
+
+T Burn in Effigy |QID|11656| |N|Borean Tundra, Coast of Echoes (32.29, 54.24)|
+T Orabus the Helmsman |QID|11661| |N|Borean Tundra, Coast of Echoes (32.29, 54.24)|
+A Seek Out Karuk! |QID|11662| |N|Borean Tundra, Coast of Echoes (32.29, 54.24)|
+
+C The Plains of Nasam |QID|11652| |N|Hop in a siege tank and head south-east|
+]]
+end)
