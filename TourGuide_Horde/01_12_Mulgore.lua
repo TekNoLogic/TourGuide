b29167e051c359012a49d8b2fa3be084fd6a5c8a
Tekkub <tekkub@gmail.com>
2009-06-16 17:06:24 -0600

    Complete that note if possilbe

--- TourGuide_Horde/01_12_Mulgore.lua
+++ TourGuide_Horde/01_12_Mulgore.lua
@@ -65,7 +65,7 @@ C Poison Water |QID|748|
 T Sharing the Land |QID|745|
 T Poison Water |QID|748|
 A Winterhoof Cleansing |QID|754|
-N Cook your ass off! |N|If you plan on leveling cooking (why wouldn't you?) buy the skill, 40x Simple Flour and 40x Mild Spices (about 3.5s total).  Cook up bread before you cook any meat that dropped.|
+N Cook your ass off! |L|30816 40| |N|If you plan on leveling cooking (why wouldn't you?) buy the skill, 40x Simple Flour and 40x Mild Spices (about 3.5s total).  Cook up bread before you cook any meat that dropped.|
 C Kyle's Gone Missing! |N|Find the dog running around the village and feed him| |U|33009| |QID|11129|
 C Winterhoof Cleansing |N|South of town at the well (53.55, 66.42)| |U|5411| |QID|754|
 C Rite of Vision (Part 2) |N|Grab the well stones| |QID|771|
