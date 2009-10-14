8a8d6090229f292d70f150a03c94d8c7000444fa
tekkub <tekkub@gmail.com>
2009-10-13 23:33:17 -0600

    Don't save turnin name if we have a QID

--- TourGuide.lua
+++ TourGuide.lua
@@ -182,6 +182,7 @@ function TourGuide:GetObjectiveStatus(i)
 end
 
 
+local qidactions = {ACCEPT = true, COMPLETE = true, TURNIN = true}
 function TourGuide:SetTurnedIn(i, value, noupdate)
 	if not i then
 		i = self.current
@@ -191,8 +192,8 @@ function TourGuide:SetTurnedIn(i, value, noupdate)
 	if value then value = true else value = nil end -- Cleanup to minimize savedvar data
 
 	local qid = self:GetObjectiveTag("QID", i)
-	if qid then self.turnedinquests[qid] = value end
-	self.turnedin[self.quests[i]] = value
+	if qidactions[self:GetObjectiveInfo(i)] and qid then self.turnedinquests[qid] = value
+	else self.turnedin[self.quests[i]] = value end
 	self:DebugF(1, "Set turned in %q = %s", self.quests[i], tostring(value))
 	if not noupdate then self:UpdateStatusFrame()
 	else self.updatedelay = i end
