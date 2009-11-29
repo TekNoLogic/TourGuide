c974d309ad79de4fb8389ddd1d0a8b5df2807244
tekkub <tekkub@gmail.com>
2009-11-28 19:37:00 -0700

    Yet more hording

--- TourGuide_Horde/32_34_Desolace.lua
+++ TourGuide_Horde/32_34_Desolace.lua
@@ -1,26 +1,25 @@
 TourGuide:RegisterGuide("Desolace (32-34)", "Stranglethorn Vale (34-36)", "Horde", function()
 return [[
 H Orgrimmar
-T The Swarm Grows (Part 1) |N|Valley of Honor (75,34)| |Z|Orgrimmar| |QID|1145|
+T The Swarm Grows (Part 1) |QID|1145| |N|Valley of Honor (75.05, 34.13)| |Z|Orgrimmar|
 A The Swarm Grows (Part 2) |QID|1146|
-A Alliance Relations (Part 1) |N|From Craven Drok in the Cleft of Shadow (50,47)| |Z|Orgrimmar| |QID|1431|
-T Alliance Relations (Part 1) |N|To Keldran near west gate of Orgimmar past Valley of Spirits (22,54)| |Z|Orgrimmar| |QID|1431|
+A Alliance Relations |QID|1431| |N|From Craven Drok in the Cleft of Shadow (50.37, 47.23)| |Z|Orgrimmar|
+T Alliance Relations (Part 1) |QID|1431| |N|From Keldran, in a hut near the west gate of Orgimmar (22.32, 53.69)| |Z|Orgrimmar|
 A Alliance Relations (Part 2) |QID|1432|
 
-R Desolace |N|Fly to Stonetalon Mountains and run south|
+R Desolace |N|Fly to Stonetalon Mountains and run southwest|
 
-A Bone Collector |QID|5501| |N|From Bibbly F'utzbuckle at Kormek's Hut (62,38).|
+A Bone Collector |QID|5501| |N|From Bibbly F'utzbuckle at Kormek's Hut (62.33, 38.89).|
 
-R Ghost Walker Post |N|Southwest, you probably want to stick to the road (56,59).|
-T The Kolkar of Desolace |QID|1362|
+T The Kolkar of Desolace |QID|1362| |N|Southwest at Ghost Walker Post (56.25, 59.55), you probably want to stick to the road.|
 A Khan Dez'hepah |QID|1365|
-A Gelkis Alliance |QID|1368| |N|Skip Magram Alliance|
+A Gelkis Alliance |QID|1368| |N|Skip "Magram Alliance"|
 T Alliance Relations (Part 2) |QID|1432|
 A Alliance Relations (Part 3) |QID|1433|
 T Alliance Relations (Part 3) |QID|1433|
 A The Burning of Spirits |QID|1435|
 
-A Kodo Roundup |QID|5561| |N|From Smeed Scrabblescrew (60.8, 61.9)|
+A Kodo Roundup |QID|5561| |N|From Smeed Scrabblescrew (60.83, 61.86)|
 C Kodo Roundup |QID|5561| |N|In the Kodo Graveyard|
 C Bone Collector |QID|5501|
 
@@ -42,13 +41,13 @@ A The Corrupter (Part 1) |QID|1480| |U|20310| |N|From the [Flayed Demon Skin] th
 
 T Bone Collector |QID|5501| |N|Back at Bibbly F'utzbuckle (62,38).|
 
-T The Corrupter (Part 1) |QID|1480| |N|Back at Ghost Walker Post|
+T The Corrupter (Part 1) |QID|1480| |N|Back at Ghost Walker Post (52.26, 53.57)|
 A The Corrupter (Part 2) |QID|1481|
 A Befouled by Satyr |QID|1434|
 
-C Befouled by Satyr |N|(75,22)| |QID|1434|
-C The Corrupter (Part 2) |QID|1481| |N|Shadowstalker Scalp is obtained from a Hatefury Shadowstalker|
-C Khan Dez'hepah |QID|1365| |N|In the Kolkar village (73,48)|
+C Befouled by Satyr |QID|1434| |N|In the northeast corner of the zone at Sargeron (79,20).|
+C The Corrupter (Part 2) |QID|1481| |N|Kill Hatefury Shadowstalkers for the scalp.|
+C Khan Dez'hepah |QID|1365| |N|In a big hut in the Kolkar Village (73.47, 48.60).|
 
 T Befouled by Satyr |QID|1434| |N|Back at Ghost Walker Post|
 T The Corrupter (Part 2) |QID|1481|
@@ -75,11 +74,10 @@ T The Corrupter (Part 3) |QID|1482|
 A The Corrupter (Part 4) |QID|1484|
 T The Corrupter (Part 4) |N|Skip last follow-up| |QID|1484|
 
-C Gelkis Alliance |QID|1368| |N|Kill Magram centaurs until you gain Gelkis (Friendly)|
-
-T Gelkis Alliance |N|To Uthek the Wise at (36.3, 79.2)| |QID|1368|
-
+C Gelkis Alliance |QID|1368| |N|Kill Magram centaurs (69.10, 71.10) until you are friendly with the Gelkis centaurs.|
+T Gelkis Alliance |QID|1368| |N|Find Uthek the Wise outside the Gelkis Village (36.29, 79.15).|
 A Stealing Supplies |QID|1370|
+
 T Kodo Roundup |QID|5561|
 C Stealing Supplies |N|In the Magram village with Centaur Bounty (70,74)| |QID|1370|
 C Centaur Bounty |QID|1366|
