d4ea5c68a8e72645e57e41b28a7bb17c2146c6b8
tekkub <tekkub@gmail.com>
2009-12-01 02:29:50 -0700

    More horde RAFink

--- TourGuide_Horde/22_23_Southern_Barrens.lua
+++ TourGuide_Horde/22_23_Southern_Barrens.lua
@@ -7,20 +7,20 @@ A Weapons of Choice |QID|893| |T| |NORAF|
 T Jorn Skyseer |QID|3261| |T| |O| |NORAF|
 A Ishamuhale |QID|882| |T| |O| |PRE|Jorn Skyseer| |NORAF|
 
-C Egg Hunt |QID|868| |N|Southwest of Camp Taurajo at the Field of Giants (43.5, 71.0).|
+C Egg Hunt |QID|868| |N|Southwest of Camp Taurajo at the Field of Giants (43.5, 71.0).| |NORAF|
 C Betrayal from Within (Part 1) |QID|879| |N|South at Blackthorn Ridge.  Kuz wanders around (44.06, 79.97), Lok is in a hut on the hill to the west (40.13, 80.55), and Nak wanders around to the south (43.74, 83.12).|
 C Weapons of Choice |QID|893| |N|Wand is dropped by the seers, shield by warfrenzies, and backstabber by stalkers (stealth) and pathfinders.| |NORAF|
 A Gann's Reclamation |QID|843| |N|Patrolling along Southern Gold Road (46.14, 79.57)|
-C Gann's Reclamation |QID|843| |N|Southeast at Bael Modan.  Kill Prospector Khazgorm (47.20, 85.18) for his journal, and a dozen or so dorfs for good measure.|
+C Gann's Reclamation |QID|843| |N|Southeast at Bael Modan.  Kill Prospector Khazgorm (47.20, 85.18) for his journal, and a dozen or so dorfs for good measure.  If you can't find Khazgorm he may be stuck in a pillar.  Use '/target pros' to find him.|
 T Gann's Reclamation |QID|843|
 A Revenge of Gann (Part 1) |QID|846|
 
-T Weapons of Choice |QID|893| |N|Back at Camp Taurajo (45.09, 57.76)| |T|
-T Betrayal from Within (Part 1) |QID|879| |T|
+T Betrayal from Within (Part 1) |QID|879| |T| |N|Back at Camp Taurajo (44.56, 59.22)|
 A Betrayal from Within (Part 2) |QID|906| |T|
+T Weapons of Choice |QID|893| |T| |NORAF|
 
-T Egg Hunt |QID|868| |N|Back at the Crossroads (51.10, 29.65)| |T|
-T Betrayal from Within (Part 2) |QID|906| |T|
+T Betrayal from Within (Part 2) |QID|906| |N|Back at the Crossroads (51.53, 30.84)| |T|
+T Egg Hunt |QID|868| |T| |NORAF|
 
 C Ishamuhale |QID|882| |N|Kill a Zhevra to get a carcas, then use it at the tree northwest of Ratchet (59.73, 29.86) to summon the raptor.| |O| |PRE|Jorn Skyseer| |U|10338| |NORAF|
 
