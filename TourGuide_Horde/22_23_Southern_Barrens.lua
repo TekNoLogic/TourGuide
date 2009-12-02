bd309a136240648bde92e25e90a8207cf95f78a8
tekkub <tekkub@gmail.com>
2009-12-02 02:07:55 -0700

    Add orc sucky bus quests

--- TourGuide_Horde/22_23_Southern_Barrens.lua
+++ TourGuide_Horde/22_23_Southern_Barrens.lua
@@ -2,10 +2,15 @@
 TourGuide:RegisterGuide("The Barrens (22-23)", "Stonetalon Mountains (23-25)", "Horde", function()
 return [[
 F Camp Taurajo
+T Ken'zigla's Draught |QID|1511| |T| |C|Warlock|
+A Dogran's Captivity |QID|1515| |T| |C|Warlock|
 h Camp Taurajo |T|
 A Weapons of Choice |QID|893| |T| |NORAF|
 T Jorn Skyseer |QID|3261| |T| |O| |NORAF|
 A Ishamuhale |QID|882| |T| |O| |PRE|Jorn Skyseer| |NORAF|
+
+T Dogran's Captivity |QID|1515| |N|North at one of the quilboar camps (43.34, 47.93)| |C|Warlock|
+A Love's Gift |QID|1512| |C|Warlock|
 
 C Egg Hunt |QID|868| |N|Southwest of Camp Taurajo at the Field of Giants (43.5, 71.0).| |NORAF|
 C Betrayal from Within (Part 1) |QID|879| |N|South at Blackthorn Ridge.  Kuz wanders around (44.06, 79.97), Lok is in a hut on the hill to the west (40.13, 80.55), and Nak wanders around to the south (43.74, 83.12).|
@@ -27,4 +32,10 @@ C Ishamuhale |QID|882| |N|Kill a Zhevra to get a carcas, then use it at the tree
 T Further Instructions (Part 1) |N|Down in Ratchet| |T| |QID|1094|
 A Further Instructions (Part 2) |T| |QID|1095|
 T Deepmoss Spider Eggs |O| |T| |QID|1069| |NORAF|
+
+F Orgrimmar |C|Warlock|
+T Love's Gift |QID|1512| |T| |C|Warlock| |N|Back in the Cleft of Shadow (48.23, 45.74)| |Z|Orgrimmar|
+A The Binding |QID|1513| |T| |C|Warlock|
+C The Binding |QID|1513| |T| |C|Warlock| |U|6626| |N|Use the pendant and kill the sucky bus at Neeru's hut (49.17, 49.96)| |Z|Orgrimmar|
+T The Binding |QID|1513| |T| |C|Warlock| |N|Back at the warlock trainer (48.22, 45.60)| |Z|Orgrimmar|
 ]] end)
