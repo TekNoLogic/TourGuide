6b1fd16eba91841bb357b875eee474202f830ebd
tekkub <tekkub@gmail.com>
2009-12-10 03:36:29 -0700

    Horde RAF Dustwallow

--- TourGuide_Horde/37_38_Dustwallow_Marsh.lua
+++ TourGuide_Horde/37_38_Dustwallow_Marsh.lua
@@ -19,6 +19,7 @@ A The Black Shield (Part 2) |QID|1321| |T|
 T Suspicious Hoofprints |QID|1268| |T|
 T The Black Shield (Part 2) |QID|1321| |T|
 A The Black Shield (Part 3) |QID|1322| |T|
+h Brackenwall Village |T|
 A Theramore Spies |T| |QID|1201|
 A Check Up on Tabetha |T| |QID|11213|
 A Twilight of the Dawn Runner |T| |QID|9437|
@@ -40,78 +41,100 @@ T What's Haunting Witch Hill? |QID|11180|
 A The Witch's Bane |QID|11181|
 
 C The Witch's Bane |QID|11181| |N|Around the water, herbalists can track these.|
-C Hungry! |QID|1177| |NORAF| |N|North of the house, along the coast.|
 
 T The Witch's Bane |QID|11181|
 A Cleansing Witch Hill |QID|11183|
 C Cleansing Witch Hill |QID|11183| |N|Plant the torch at the end of the dock and kill the gargoyle.| |U|33113|
 T Cleansing Witch Hill |QID|11183|
 
-A Stinky's Escape |N|North of the tower (46.78, 17.00).  You may want to clear the area first.| |QID|1270|
+A Stinky's Escape |QID|1270| |N|North of the tower (46.87, 17.62).  You may want to clear the area first.|
 C Stinky's Escape |QID|1270|
 
 C Jarl Needs Eyes |N|Kill spiders to the southwest at Darkmist Cavern (35,21).| |QID|1206|
 C The Black Shield (Part 3) |QID|1322|
 C Theramore Spies |N|Stealthed mobs all around Brackenwall Village| |QID|1201|
 
-T Theramore Spies |N|Back in Brackenwall Village| |T| |QID|1201|
-A The Theramore Docks |T| |QID|1202|
-T The Lost Report |T| |QID|1238|
-A Help Mudsprocket |T| |QID|11215|
-T The Black Shield (Part 3) |T| |QID|1322|
-A The Black Shield (Part 4) |T| |QID|1323|
-T The Black Shield (Part 4) |T| |QID|1323|
-A Inspecting the Ruins |N|Easy XP and its on the way to Tabetha.| |T| |QID|11124|
-T Twilight of the Dawn Runner |T| |QID|9437|
+T Theramore Spies |N|Back in Brackenwall Village| |QID|1201| |T|
+A The Theramore Docks |QID|1202| |T|
+T The Lost Report |QID|1238| |T|
+A Help Mudsprocket |QID|11215| |T|
+T The Black Shield (Part 3) |QID|1322| |T|
+A The Black Shield (Part 4) |QID|1323| |T|
+T The Black Shield (Part 4) |QID|1323| |T|
+A The Black Shield (Part 5) |QID|1276| |T|
+A Inspecting the Ruins |QID|11124| |T|
+T Twilight of the Dawn Runner |QID|9437| |T|
+
+F Thunder Bluff |QID|11204|
+T The Black Shield (Part 5) |QID|1276| |T| |N|Down on Hunter Rise (54.61, 80.07)| |Z|Thunder Bluff|
+A Return to Krog |QID|11204| |T|
+
+F Brackenwall Village |QID|11204|
+T Return to Krog |QID|11204| |T|
+A More than Coincidence |QID|11200| |T|
+A The Grimtotem Plot |QID|11201| |T|
+
+C More than Coincidence |QID|11200| |N|North at Blackhoof Village.|
+C The Grimtotem Plot |QID|11201| |N|Kill elders for the notes, combine them once you have 4.| |U|33050|
+
+T Jarl Needs Eyes |QID|1206|
+A Jarl Needs a Blade |QID|1203|
+C Jarl Needs a Blade |QID|1203| |N|Head north to the beach (58.09, 17.78)|
+C Hungry! |QID|1177| |NORAF| |N|North of the house, along the coast.|
+T Jarl Needs a Blade |QID|1203|
+A The Severed Head |QID|1239| |N|Dig in the dirt near the cabin again.|
+
+C The Theramore Docks |QID|1202| |N|Swim around the north side of Theramore, find the box under the northeast pier (71.50, 51.10).|
+
+H Brackenwall Village |QID|11203|
+T More than Coincidence |QID|11200| |T|
+T The Grimtotem Plot |QID|11201| |T|
+A Seek Out Tabetha |QID|11203| |T|
+T The Theramore Docks |T| |QID|1202|
 
 T Hungry! |N|Southwest at Mudcrush Durtfeet (35,38)| |QID|1177| |NORAF|
-T Inspecting the Ruins |N|Follow the path south-west from Mudcrush (30,48)| |QID|11124|
+T Inspecting the Ruins |QID|11124| |N|Back at the burned down inn.|
 
-T Check Up on Tabetha |N|Follow the road south, then east to Tabetha's Farm (46.04, 57.17).| |QID|11213|
+T Seek Out Tabetha |QID|11203| |N|Follow the road south, then east to Tabetha's Farm (46.04, 57.17).|
+A Raze Direhorn Post! |QID|11205|
+T Check Up on Tabetha |QID|11213|
 A The Grimtotem Weapon |QID|11169|
 A The Reagent Thief |QID|11173| |NORAF|
 A Direhorn Raiders |N|Just outside of hut| |QID|11156|
 A The Zeppelin Crash |QID|11172|
 
-C The Grimtotem Weapon |N|Place the totem near crocs to the north and kill them.  If you have a pet you may not want to use it, you won't get credit if the croc is not targeting you when it dies.| |U|33101| |QID|11169|
-C The Reagent Thief |N|Kill spiders southeast of the farm (48,62)| |QID|11173| |NORAF|
-
-C Direhorn Raiders |N|Kill Grimtotems north of the farm| |QID|11156|
-K Apothecary Cylla |L|33115|
+C Raze Direhorn Post! |QID|11205| |U|33072| |N|Due north, burn down each hut.|
+C Direhorn Raiders |QID|11156|
+K Apothecary Cylla |L|33115| |QID|11186| |N|In one of the huts.|
 A Signs of Treachery? |U|33115| |QID|11186|
 
-T Direhorn Raiders |N|Back at the farm| |QID|11156|
+C The Grimtotem Weapon |N|Place the totem near crocs to the north and kill them.| |U|33101| |QID|11169|
+C The Reagent Thief |N|Kill spiders southeast of the farm (48,62)| |QID|11173| |NORAF|
+
+T Raze Direhorn Post! |QID|11205| |N|Back at Tabetha's Farm (45.97, 57.12)|
+A Justice Dispensed |QID|11206|
 T The Grimtotem Weapon |QID|11169|
+T Direhorn Raiders |QID|11156|
 T The Reagent Thief |QID|11173| |NORAF|
 
-C Deadmire |N|Roams around (47,57)| |QID|1205| |NORAF|
+C Deadmire |QID|1205| |NORAF|
 
-T The Zeppelin Crash |N|To the east at Beezil's Wreck (54,56).| |QID|11172|
+T The Zeppelin Crash |QID|11172| |N|To the east at Beezil's Wreck (53.61, 56.86).|
 A Corrosion Prevention |QID|11174|
-A Secure the Cargo! |QID|11207|
+A Secure the Cargo! |QID|11207| |NORAF|
 
 C Corrosion Prevention |U|33108| |N|Make sure you have the "Energized" buff and use the [Ooze Buster] on the slimes.  Note that there is a bug with this quest which may allow you to complete it without leaving the questgiver: target a nearby ooze, use the buster... while it is channelling target a different ooze (at any range).| |QID|11174|
-C Secure the Cargo! |QID|11207|
-C Razzeric's Tweaking |N|Box at (54,56) right behind the zeppelin| |QID|1187|
+C Secure the Cargo! |QID|11207| |NORAF|
+C Razzeric's Tweaking |N|Box at (54,56) right behind the zeppelin| |QID|1187| |NORAF|
 
 T Corrosion Prevention |QID|11174|
-T Secure the Cargo! |QID|11207|
-A Delivery for Drazzit |QID|11208|
+T Secure the Cargo! |QID|11207| |NORAF|
+A Delivery for Drazzit |QID|11208| |NORAF|
 
-T Delivery for Drazzit |N|At Mudsprocket (42.31, 72.94)| |T| |QID|11208|
-T Help Mudsprocket |T| |QID|11215|
-
-F Brackenwall Village
-A The Severed Head |N|At Jarl's cabin (55,25)| |QID|1239|
-T Jarl Needs Eyes |QID|1206|
-A Jarl Needs a Blade |QID|1203|
-C Jarl Needs a Blade |N|Head north to the beach (58.09, 17.78)| |QID|1203|
-T Jarl Needs a Blade |N|At Jarl's cabin (55,25)| |QID|1203|
-
-C The Theramore Docks |N|Under the eastern pier (71,51).  Watch out for the shark!| |QID|1202|
+T Help Mudsprocket |QID|11215| |N|Follow the road south to Mudsprocket (42.28, 72.89).| |T|
+T Delivery for Drazzit |QID|11208| |T| |NORAF|
 
 H Brackenwall Village
-T The Theramore Docks |T| |QID|1202|
 T Signs of Treachery? |T| |QID|11186|
 T The Severed Head |T| |QID|1239|
 A The Troll Witchdoctor |T| |QID|1240|
