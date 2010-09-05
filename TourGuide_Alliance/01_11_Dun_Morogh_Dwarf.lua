7edeb0ccfdc06de24b74421caa4323558a811d30
tekkub <tekkub@gmail.com>
2010-09-05 02:43:28 -0600

    New dorf starting EXTRAVAGANZA!

---
+++ TourGuide_Alliance/01_11_Dun_Morogh_Dwarf.lua
@@ -0,0 +1,60 @@
+TourGuide:RegisterGuide("Dun Morogh (1-11 Dwarf)", nil, "Alliance", function()
+return [[
+A Hold the Line! |QID|24469|
+C Hold the Line! |QID|24469|
+T Hold the Line! |QID|24469|
+A Give 'em What-For |QID|24470|
+A Aid for the Wounded |QID|24471|
+C Aid for the Wounded |QID|24471|
+C Give 'em What-For |QID|24470|
+T Aid for the Wounded |QID|24471|
+T Give 'em What-For |QID|24470|
+A Lockdown in Anvilmar |QID|24473|
+
+T Lockdown in Anvilmar |QID|24473|
+A First Things First: We're Gonna Need Some Beer |QID|24474|
+A Dwarven Artifacts |QID|24477|
+C First Things First: We're Gonna Need Some Beer |QID|24474|
+C Dwarven Artifacts |QID|24477|
+T Dwarven Artifacts |QID|24477|
+A Make Hay While the Sun Shines |QID|24486|
+T First Things First: We're Gonna Need Some Beer |QID|24474|
+A All the Other Stuff |QID|24475|
+C Make Hay While the Sun Shines |QID|24486| |N|Hey, they throw the items to you, how handy!|
+C All the Other Stuff |QID|24475|
+T Make Hay While the Sun Shines |QID|24486|
+T All the Other Stuff |QID|24475|
+A Whitebeard Needs Ye |QID|24487|
+
+A Tainted Rune |QID|3115| |C|Warlock|
+T Tainted Rune |QID|3115| |C|Warlock|
+A Harnessing the Flames |QID|26904| |C|Warlock|
+C Harnessing the Flames |QID|26904| |C|Warlock|
+T Harnessing the Flames |QID|26904| |C|Warlock|
+
+T Whitebeard Needs Ye |QID|24487|
+A The Troll Menace |QID|182|
+A A Refugee's Quandary |QID|3361|
+A Trolling for Information |QID|24489|
+C Trolling for Information |QID|24489| |N|Stand near each of the "friendly" trolls at the camps.|
+C A Refugee's Quandary |QID|3361|
+C The Troll Menace |QID|182|
+T A Refugee's Quandary |QID|3361|
+T The Troll Menace |QID|182|
+T Trolling for Information |QID|24489|
+A Ice and Fire |QID|218|
+C Ice and Fire |QID|218| |N|Careful, these guys like to link.|
+T Ice and Fire |QID|218|
+A A Trip to Ironforge |QID|24490|
+
+T A Trip to Ironforge |QID|24490|
+A Follow that Gyro-Copter! |QID|24491|
+
+T Follow that Gyro-Copter! |QID|24491|
+A Pack Your Bags |QID|24492|
+A Don't Forget About Us |QID|24493|
+T Pack Your Bags |QID|24492|
+
+T Don't Forget About Us |QID|24493|
+A Frostmane Aggression |QID|25724|
+]] end)
